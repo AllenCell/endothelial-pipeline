c7d03bdd0c37d9e059250373b3698f71b5a4bc0a
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-07-03 12:54:55 -0700

    add resolution

--- cellsmap/util/io.py
+++ cellsmap/util/io.py
@@ -2,6 +2,7 @@ import yaml
 from pathlib import Path
 import numpy as np
 from bioio import BioImage
+import dask.array
 
 def extract_key_from_config(key: str) -> str:
     parent_folder = Path(__file__).resolve().parent
@@ -15,9 +16,10 @@ def extract_key_from_config(key: str) -> str:
 def get_frame(filename):
     return int(str(filename).split('.')[0][-4:])
 
-def load_dataset(movie_name: str, time_start:int = 0, time_end: int=576) -> np.ndarray:
+def load_dataset(movie_name: str, time_start:int = 0, time_end: int=576, resolution:int=0) -> dask.array.Array:
     movie_path = extract_key_from_config(movie_name)
-
-    img = BioImage(movie_path).get_image_dask_data("TYX",T=range(time_start, time_end+1) )
-
+    img = BioImage(movie_path)
+    assert resolution in img.resolution_levels, f'Invalid resolution level {resolution}. Available levels are {img.resolution_levels}'
+    img.set_resolution_level(resolution)
+    img = img.get_image_dask_data("TYX",T=range(time_start, time_end+1) )
     return img
--- tests/test_load_movie.py
+++ tests/test_load_movie.py
@@ -1,10 +1,15 @@
 from cellsmap.util import load_dataset
 
 def test_load_dataset():
+    # check end point specification
     movie = load_dataset('cdh5_path', time_end = 2)
     assert movie.shape[0] == 3
-
+    # check start point specification
     movie = load_dataset('cdh5_path', time_start=1, time_end = 2)
     assert movie.shape[0] == 2
+    # check resolution specification
+    movie = load_dataset('cdh5_path', time_start=1, time_end = 2, resolution=1)
+    print(movie.shape)
+    assert movie.shape[1:] == (856, 4796)
 
     
