661aeea7edc9d25fb2d8bf3ff0213920374ce65c
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-07-16 11:52:08 -0700

    simplify csv saver

--- cellsmap/model_features/utils/csv_saver.py
+++ cellsmap/model_features/utils/csv_saver.py
@@ -8,14 +8,22 @@ class CSVSaver(Callback):
         self.save_dir.mkdir(parents=True, exist_ok=True)
         self.meta_keys = meta_keys
 
+    def to_dataframe(self, x):
+        return pd.DataFrame(x)
+
     def on_predict_epoch_end(self, trainer, pl_module):
         # Access the list of predictions from all predict_steps
         predictions = trainer.predict_loop.predictions
         feats = []
         for pred, meta in predictions:
             # exclude cls token
-            batch_feats = pd.DataFrame(pred[1:].mean(axis=0))
-            batch_feats['location'] = range(batch_feats.shape[0])
+            batch_feats = self.to_dataframe(pred)
             batch_feats['time'] = meta['T']
+            batch_feats['crop_path'] = meta['crop_path'] 
             feats.append(batch_feats)
         pd.concat(feats).to_csv(self.save_dir / 'predictions.csv', index=False)
+
+class MAESaver(CSVSaver):
+    def to_dataframe(self, x):
+        return pd.DataFrame(x[1:].mean(axis=0))
+    
