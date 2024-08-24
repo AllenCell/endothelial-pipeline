ed49b1b30f578a0e022656d5b59a295a2d108628
Matheus Viana <vianamp@gmail.com>
2024-08-24 14:27:18 -0700

    Comparing feats

--- cellsmap/analyses/playground/main.py
+++ cellsmap/analyses/playground/main.py
@@ -2,21 +2,28 @@ import numpy as np
 from cyto_dl.api import CytoDLModel
 from cyto_dl.utils import extract_array_predictions
 from cellsmap.util import io
-
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
     df = io.load_precomputed_features(dataset_name="20240305_T01_001", model_name="mae_cdh5")
-    feature_crop_0_T_0 = df.query('crop_index == 0 and T == 0').iloc[0]
+    feature_crop_0_T_0 = df.query('crop_index == 0 and T == 0').drop(columns=["crop_index","T"]).iloc[0]
 
     model = CytoDLModel()
     model.load_config_from_file("//allen/aics/assay-dev/users/Benji/cellsmap/cellsmap/model_features/configs/mae_cdh5/array_eval_config.yaml")
-    img = io.load_dataset("20240305_T01_001", channels=["CDH5_Tubulin", "Nuc_Seg"])
+    reader = io.load_dataset("20240305_T01_001", channels=["CDH5_Tubulin", "Nuc_Seg"], time_start=0, time_end=0)
+    img = reader.compute()
 
     data = [img[0, :1, :512, :512]]
 
     _, _, output = model.predict(data=data)
 
-    # Need to compare "output" with "feature_crop_0_T_0" to make sure they match
+    feature_crop_0_T_0_new = output[0][0].mean(axis=(0,1))
 
-    print(output)
+    fig, ax = plt.subplots(1,1)
+    ax.scatter(feature_crop_0_T_0, feature_crop_0_T_0_new)
+    ax.set_xlabel("Pre computed features for patch 0 at time T=0")
+    ax.set_ylabel("Array predicted features for patch 0 at time T=0")
+    plt.savefig("test.png")
+
+    # Features don't quite match
