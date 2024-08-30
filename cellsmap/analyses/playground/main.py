ebc7062f5f200948a01d71ee3deb87b2856177df
Matheus Viana <vianamp@gmail.com>
2024-08-30 13:40:06 -0700

    New test for norm features

--- cellsmap/analyses/playground/main.py
+++ cellsmap/analyses/playground/main.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
 
     model = CytoDLModel()
     model.load_config_from_file("//allen/aics/assay-dev/users/Benji/cellsmap/cellsmap/model_features/configs/mae_cdh5/array_eval_config.yaml")
-    reader = io.load_dataset("20240305_T01_001", channels=["CDH5_Tubulin", "Nuc_Seg"], time_start=0, time_end=0)
+    reader = io.load_dataset("20240305_T01_001", channels=["CDH5_Tubulin", "Nuc_Seg"], time_start=0, time_end=1)
     img = reader.compute()
 
-    data = [img[0, :1, :512, :512]]
+    data = [img[0, slice(1,2), -512:, -512:]]
 
     _, _, output = model.predict(data=data)
 
@@ -22,8 +22,8 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots(1,1)
     ax.scatter(feature_crop_0_T_0, feature_crop_0_T_0_new)
-    ax.set_xlabel("Pre computed features for patch 0 at time T=0")
-    ax.set_ylabel("Array predicted features for patch 0 at time T=0")
+    ax.set_xlabel("Pre computed features for patch -1 at time T=0")
+    ax.set_ylabel("Array predicted features for patch -1 at time T=0")
     plt.savefig("test.png")
 
     # Features don't quite match
