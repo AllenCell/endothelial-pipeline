Fix for output mean calculation

cellsmap/analyses/playground/main.py
@@ -14,16 +14,14 @@ if __name__ == '__main__':
     reader = io.load_dataset("20240305_T01_001", channels=["CDH5_Tubulin", "Nuc_Seg"], time_start=0, time_end=1)
     img = reader.compute()
 
-    data = [img[0, slice(1,2), -512:, -512:]]
+    data = [img[0, slice(0,1), :512, :512]]
 
     _, _, output = model.predict(data=data)
-
-    feature_crop_0_T_0_new = output[0][0].mean(axis=(0,1))
+    feature_crop_0_T_0_new = output[0][0][1:].mean(axis=(0,1))
 
     fig, ax = plt.subplots(1,1)
     ax.scatter(feature_crop_0_T_0, feature_crop_0_T_0_new)
-    ax.set_xlabel("Pre computed features for patch -1 at time T=0")
-    ax.set_ylabel("Array predicted features for patch -1 at time T=0")
+    ax.set_xlabel("Pre computed features for patch 0 at time T=0")
+    ax.set_ylabel("Array predicted features for patch 0 at time T=0")
     plt.savefig("test.png")
 
-    # Features don't quite match