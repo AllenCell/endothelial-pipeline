3073c57e55fbdc08b8ae1119860984a441746a52
Serge Parent <serge.edouard.parent@gmail.com>
2024-11-19 09:28:13 -0800

    added cdh5_seg_density_map_correlations.py

---
+++ cellsmap/features/cdh5_seg_density_map_correlations.py
@@ -0,0 +1,97 @@
+import numpy as np
+import pandas as pd
+from cellsmap.util import io, cdh5_preprocessing as preproc
+import matplotlib.pyplot as plt
+import seaborn as sns
+from cellsmap.features import cdh5_seg_density_map as cellsden
+from pathlib import Path
+from skimage.measure import label
+from skimage.measure import regionprops
+
+
+def evaluate_density_against_number_of_nuclei(dataset_name, T, bbox_radius=256, nsamples=1000):
+    np.random.seed(666)
+    # The density maps from thresholds and segmentations are comparable form my test on dataset 20240305_T01_001
+    # dmap = cellsden.get_density_map_from_segmentations(dataset_name, T, density_map_sigma=10)
+    dmap = cellsden.get_density_map_from_thresholds(dataset_name, T, density_map_sigma=40)
+    nuc_seg = io.load_dataset(dataset_name, channels=["Nuc_Seg"], time_start=T, time_end=T, level=0).squeeze()
+    region_seg = np.array(*preproc.get_cdh5_classic_segmentation(dataset_name, T, channels=['segmentations_merged'])).squeeze()
+    nuc_seg = nuc_seg.compute().astype(int)
+    df = []
+    r = bbox_radius
+    rs = int(r*(0.5**2))
+    for sample in range(nsamples):
+        x = np.random.randint(r, nuc_seg.shape[-1]-r)
+        y = np.random.randint(r, nuc_seg.shape[-2]-r)
+        crop_region = slice(y-r, y+r), slice(x-r, x+r)
+
+        xs, ys = int(x*(0.5**2)), int(y*(0.5**2))
+        crop_region_small = slice(ys-rs, ys+rs), slice(xs-rs, xs+rs)
+
+        nuc_crop = nuc_seg[crop_region]
+        # loading the nuclear segmentation at a less-than-native resolution results in
+        # blurring of the segmentation labels, therefore we must re-label them if loaded
+        # like that (not an issue at native resolution though)
+        n = len(regionprops(label(nuc_crop.astype(bool))))
+        n_cdh5_regions = len(regionprops(region_seg[crop_region]))
+        dens = dmap[crop_region_small].mean()
+        df.append({"density": dens, "n_nuclei": n, "n_CDH5_regions": n_cdh5_regions})
+    df = pd.DataFrame(df)
+
+    pearson = np.corrcoef(df.density, df.n_nuclei)[0, 1]
+    pearson2 = np.corrcoef(df.density, df.n_CDH5_regions)[0, 1]
+    df['dataset'] = dataset_name
+    df['T'] = T
+    df['pearson_nuclei'] = pearson
+    df['pearson_cdh5_regions'] = pearson2
+
+    fig, axs = plt.subplots(figsize=(10,5), ncols=2, nrows=1)
+    axs[0].scatter(df.n_nuclei, df.density, s=5)
+    axs[0].set_xlabel("number of nuclei")
+    axs[0].set_ylabel("density from thresholds")
+    axs[0].set_title(f"Dataset: {dataset_name} \ntimepoint={T}, Pearson={pearson:.2f}")
+
+    axs[1].scatter(df.n_nuclei, df.density, s=5)
+    axs[1].set_xlabel("number of CDH5 regions")
+    axs[1].set_ylabel("density from thresholds")
+    axs[1].set_title(f"Dataset: {dataset_name} \ntimepoint={T}, Pearson={pearson:.2f}")
+    plt.tight_layout()
+    # plt.show()
+    return df, pearson, fig
+
+def save_results(out_dir, t, df, pearson, fig):
+    Path.mkdir(out_dir / 'plots', exist_ok=True, parents=True)
+    Path.mkdir(out_dir / 'tables', exist_ok=True, parents=True)
+    fig.savefig(out_dir / f'plots/T{t}_pearson={pearson:.2f}.png')
+    df.to_csv(out_dir / f'tables/T{t}_results.csv')
+
+
+# dataset_name_list = ['20240305_T01_001', '20240917_20X_48hr', '20241016_20X']
+dataset_name_list = ['20240305_T01_001',]
+
+for dataset_name in dataset_name_list:
+    print(dataset_name)
+    T_range = range(0, io.get_dataset_duration_in_frames(dataset_name), 72)
+
+    prj_dir = Path('../').resolve()
+    out_dir = prj_dir / 'results/cdh5_seg_density_map_validation' / dataset_name
+
+    for t in T_range:
+        print(f'T={t}')
+        df, pearson, fig = evaluate_density_against_number_of_nuclei(dataset_name, t, bbox_radius=256, nsamples=1000)
+        save_results(out_dir, t, df, pearson, fig)
+
+    table_paths = [fp for fp in out_dir.glob('tables/*.csv')]
+    master_table = pd.concat([pd.read_csv(fp) for fp in table_paths])
+
+    fig, ax = plt.subplots(figsize=(5,5), ncols=1, nrows=1)
+    sns.lineplot(x='T', y='pearson_nuclei', data=master_table, ls='-', marker='o', ax=ax)
+    ax.twinx()
+    sns.lineplot(x='T', y='pearson_cdh5_regions', data=master_table, ls='-', marker='o', ax=ax)
+    ax.set_ylim(-1, 1)
+    ax.axhline(0, color='black', linestyle='--')
+    [ax.axhline(y, color='grey', linestyle=':') for y in (-0.5, 0.5)]
+    ax.set_ylabel('Pearson correlation coefficient between threshold \n density map and number of nuclei')
+    ax.set_ylabel('Pearson correlation coefficient between threshold \n density map and number of CDH5 regions')
+    plt.tight_layout()
+    fig.savefig(out_dir / 'plots/T_vs_pearson.png')
