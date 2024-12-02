42ac2abbe678c2f40a0ab47b17fcbc4f07ba09be
Serge Parent <serge.edouard.parent@gmail.com>
2024-12-02 13:50:25 -0800

    1. updated `flow_calculator.py` with additional functions for saving flow field information as an image and loading random crops of images 2. added `flow_features.py` as an example workflow on a few of the timelapses

---
+++ cellsmap/analyses/flow/flow_calculator.py
@@ -0,0 +1,331 @@
+import pickle
+import concurrent
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import statsmodels.api as statm
+import matplotlib.pyplot as plt
+from skimage import registration as skreg
+from matplotlib.backends.backend_agg import FigureCanvas
+from pathlib import Path
+from bioio import BioImage
+from bioio.writers import OmeTiffWriter
+from bioio_base.types import PhysicalPixelSizes
+
+from cellsmap.util import io
+
+def expand_crop_region(crop_region, padding):
+    crop_region = tuple(crop if isinstance(crop, slice) else slice(*crop) for crop in crop_region)
+    crop_region = tuple((max(0, crop.start - padding if crop.start else 0) or None, crop.stop + padding if crop.stop else None) for crop in crop_region)
+    return tuple((slice(*crop)) for crop in crop_region)
+
+class FlowCalculator():
+    """
+    Class to implement functionalities related to flow field. We want
+    to use the flow field as a proxy for the collective motion of
+    neighbor nuclei. Velocities in here are computed in units of
+    micron/min.
+
+    Example (it takes about 1 hour to finish a single colony):
+    --------
+    from nuc_morph_analysis.flow import tools as ftools
+    flowc = ftools.FlowCalculator(dataset="papa_bear")
+    flowc.initialize()
+    flowc.run_flow_field_analysis()
+    flowc.save_flow_field(path="./")
+    flowc.display_average_flow_velocities()
+    """
+    debug = False # Run in debug mode or not
+    ncores = 1 # Number of cores to be used in the calculation
+    radius = 30 # Radius of the neighborhood after downscaling
+
+    def __init__(self, dataset, debug=False):
+        self.dataset = dataset
+        if debug:
+            self.set_debug_mode_on()
+
+    def initialize(self, channel: list[str], delta_t: int=1, load_from_file=None, ncores: int=20):
+        """
+        channel: list[str]
+            A list of the names of the channels to be loaded from the dataset.
+        delta_t: int
+            The difference between the start and end timeframes when calculating the flow field vectors.
+        """
+        self.channel = channel
+        self.channel_index = io.get_channel_index(self.dataset, channel)
+        assert len(self.channel) == 1 and len(self.channel) == 1, f"Only one channel is implemented for flow calculation. Channels provided were {self.channel} corresponding to channel indices {self.channel_index}."
+        self.delta_t = delta_t
+        self.load_dataset()
+        # self.dims = dict(zip('TCYX', self.data.shape))
+        # self.dim_order = dict(zip('TCYX', range(len(self.data.shape))))
+        if load_from_file:
+            self.load_flow_from_file(load_from_file)
+        self.set_number_of_cores(ncores)
+
+    def load_dataset(self):
+        self.data = io.load_dataset(self.dataset, channels=self.channel, level=2)
+        # self.df = load_data.load_dataset(self.dataset)
+        # self.reader = load_data.get_dataset_original_file_reader(self.dataset)
+        # self.reader = AICSImage("/allen/aics/microscopy/Data/RnD_Sandbox/3500006247_20240227_Deliverable_ZSD1/3500006247_20240227_20X_Timelapse-03.czi")
+        # self.reader.set_scene("P22-C7")
+
+    def load_flow_from_file(self, fname):
+        with open(fname, "rb") as fpk:
+            result = pickle.load(fpk)
+        assert result["dataset"] == self.dataset
+        self.vx = np.array(result["vx"]).reshape(result["shape"])
+        self.vy = np.array(result["vy"]).reshape(result["shape"])
+
+    def set_debug_mode_on(self):
+        self.debug = True
+
+    def set_flow_radius(self, radius):
+        self.radius = radius
+
+    def set_number_of_cores(self, ncores):
+        self.ncores = ncores
+
+    def compute_flow_field(self, executor=None, save=None):
+        step = 1
+        duration = io.get_dataset_duration_in_frames(self.dataset)
+        if self.debug:
+            step = int(duration/10)
+            print(f"Running debug mode. Using step of {step} frames.")
+
+        tps = np.arange(self.data.shape[0]-self.delta_t)[::step]
+
+        # NOTE trying to instantiate executor in this function results in the script hanging
+        # after completing the first dataset in the for-loop in flow_features.py;
+        # instantiating executor in the main function and passing it as an argument fixes this issue
+        if not executor:
+            print('starting single processing')
+            flow = list(tqdm(map(self.compute_flow_at_timepoint, tps), total=len(tps)))
+            print('done single processing')
+        else:
+            print('starting multiprocessing')
+            flow = list(tqdm(executor.map(self.compute_flow_at_timepoint, tps), total=len(tps)))
+            print('done multiprocessing')
+
+        self.im = np.array([im for (im, _, _) in flow])
+        self.vx = np.array([fx for (_, fx, _) in flow])
+        self.vy = np.array([fy for (_, _ ,fy) in flow])
+        self.tps = tps
+
+    def compute_flow_at_timepoint(self, time):
+        raw0 = self.data[time, 0]
+        raw1 = self.data[time+self.delta_t, 0]
+        (vx, vy) = self.compute_flow(raw0, raw1, radius=self.radius)
+        return (raw1, vx, vy)
+
+    def save_vector_field_as_img(self, out_dir):
+        Path.mkdir(out_dir, exist_ok=True, parents=True)
+        out_path = out_dir / f"{self.dataset}_vector_field.ome.tiff"
+        vector_data = np.asarray([np.asarray(FlowCalculator.compute_flow_features_as_img(im, vx, vy, keepdims=True)) for im, vx, vy in zip(self.im, self.vx, self.vy)])
+        # keep only the vx and vy components and the norms when saving as an
+        # image since the t, x, and y are trivial to determine from the image
+        vector_data = vector_data[:, 2:6, ...]
+
+        image_name = self.dataset
+        ch_colors = [(0,255,255), (255,0,255), (255,255,0), (255,255,255)]
+        ch_names = ['vx', 'vy', 'norm', 'theta']
+        img_dim_order = 'YX'
+        px_res = PhysicalPixelSizes(*[1, *[io.get_xy_pixel_size_in_um(self.dataset)] * len(img_dim_order)])
+        dim_order_out = 'TCYX'
+
+        OmeTiffWriter.save(vector_data,
+                           out_path,
+                           physical_pixel_sizes=px_res,
+                           dim_order=dim_order_out,
+                           image_name=image_name,
+                           channel_names=ch_names,
+                           channel_colors=ch_colors)
+        return out_path
+
+    @staticmethod
+    def compute_flow_features(vx, vy):
+        magnitudes = FlowCalculator.compute_angles(vx, vy)
+        angles = FlowCalculator.compute_angles(vy, vx)
+        features = [magnitudes, angles]
+        return features
+
+    @staticmethod
+    def compute_angles(vx, vy):
+        """
+        Angles are in the correct quadrants (see docs for numpy.arctan2).
+        """
+        return np.arctan2(vy, vx)
+
+    @staticmethod
+    def compute_magnitudes(vx, vy):
+        return np.linalg.norm([vx, vy], axis=0)
+
+    @staticmethod
+    def summarize_flow_features(vx, vy, axis=None):
+        magnitudes = FlowCalculator.compute_angles(vx, vy)
+        angles = FlowCalculator.compute_angles(vy, vx)
+        features = [magnitudes, angles]
+        feature_means = [np.mean(ft, axis=axis) for ft in features]
+        feature_stds = [np.std(magnitudes, axis=axis) for ft in features]
+        return feature_means, feature_stds
+
+    @staticmethod
+    def compute_flow(image0, image1, radius=radius, display=False, return_map=False):
+
+        vy, vx = skreg.optical_flow_ilk(image0, image1, radius=radius)
+
+        if return_map or display:
+
+            plot = FlowCalculator.make_vector_field_map(image1, vx, vy, display=display)
+
+            return (vx, vy), plot
+
+        return (vx, vy)
+
+    @staticmethod
+    def make_vector_field_map(image, vx, vy, resolution=20, display=True, return_map=False):
+
+        norm = np.sqrt(vx ** 2 + vy ** 2)
+
+        nl, nc = image.shape
+        step = max(nl//resolution, nc//resolution)
+
+        y, x = np.mgrid[:nl:step, :nc:step]
+        vx_ = vx[::step, ::step]
+        vy_ = vy[::step, ::step]
+        n_ = norm[::step, ::step]
+
+        fig, ax = plt.subplots(1, 1, dpi=150)
+        canvas = FigureCanvas(fig)
+        ax.imshow(image, cmap='gray')
+        ax.set_axis_off()
+        ax.quiver(x, y, vx_, vy_, n_, units='dots', angles='xy', scale_units='xy', width=4, cmap="inferno")
+        plt.tight_layout()
+
+        canvas.draw()
+        plot = np.frombuffer(canvas.tostring_rgb(), dtype="uint8")
+        plot = plot.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+        if display:
+            plt.show()
+
+        plt.close("all")
+
+        if return_map:
+            return plot
+
+    def save_vector_field_as_pickle(self, out_dir):
+        out_dir = out_dir / Path(__file__).stem
+        Path.mkdir(out_dir, exist_ok=True, parents=True)
+        out_path = out_dir / f"{self.dataset}_vector_field.flow"
+        vector_data = dict(zip(('timeframe', 'x', 'y', 'vx', 'vy', 'norm', 'shape'), zip(*[(tp, *FlowCalculator.get_vector_field_as_img(im, vx, vy), vx.shape) for tp, im, vx, vy in zip(self.tps, self.im, self.vx, self.vy)])))
+        # pd.DataFrame(vector_data).to_csv(out_path, sep='\t')
+        with open(f"{out_path}", "wb") as fpk:
+            pickle.dump(vector_data, fpk)
+
+    @staticmethod
+    def compute_flow_features_as_img(image, vx, vy, keepdims=True):
+        norm = FlowCalculator.compute_magnitudes(vx, vy)
+        theta = FlowCalculator.compute_angles(vx, vy)
+        nl, nc = image.shape
+        y, x = np.mgrid[:nl, :nc]
+        return [x, y, vx, vy, norm, theta] if keepdims else [np.ravel(arr) for arr in (x, y, vx, vy, norm)]
+
+    def save_flow_field(self, path):
+        result = {
+            "dataset": self.dataset,
+            "vx": self.vx.flatten().tolist(),
+            "vy": self.vy.flatten().tolist(),
+            "shape": np.array(self.vx.shape).tolist()
+        }
+        with open(f"{path}/{self.dataset}.flow", "wb") as fpk:
+            pickle.dump(result, fpk)
+
+    def run_flow_field_analysis(self):
+        self.compute_flow_field()
+        self.calculate_flow_velocity()
+        self.calculate_instantaneous_velocity()
+
+def compute_and_save_flow_field(out_dir, dataset_name, delta_t=1, executor=None, ncores=1, debug=False):
+    print(f'Analyzing dataset: {dataset_name}')
+
+    # Initialize the flow field calculator
+    flowc = FlowCalculator(dataset=dataset_name, debug=debug)
+    channel_name = [chan for chan in io.get_available_channels(dataset_name) if chan in ('CDH5_Tubulin', 'CDH5')]
+    print('initializing...')
+    flowc.initialize(channel=channel_name, delta_t=delta_t, ncores=ncores)
+
+    # Compute the flow field
+    print('computing flow field...')
+    flowc.compute_flow_field(executor=executor)
+
+    # Save the flow field results
+    print('saving results...')
+    out_path = flowc.save_vector_field_as_img(out_dir)
+    print(f'Vector field image saved to {out_path}')
+
+    return out_path
+
+def get_vector_field_image_paths(out_dir):
+    out_dir = Path(out_dir)
+    df = pd.read_csv(out_dir / 'vector_field_image_paths.csv')
+    return dict(zip(df['dataset_name'], df['vector_field_image_paths']))
+
+def load_vector_field_img(img_dir, dataset_name):
+    # Get the paths to the vector field images
+    image_paths = get_vector_field_image_paths(img_dir)
+    im_path = image_paths[dataset_name]
+
+    # Lazy-load the vector field images
+    img = BioImage(im_path)
+    vector_field = img.get_image_dask_data("TCYX")
+
+    return vector_field
+
+def get_random_roi(image_shape: np.ndarray.shape, roi_shape: tuple[int, ...], num_rois: int=1, random_seed: int=None) -> tuple[slice, ...]:
+    """
+    Returns a random region of interest (roi) within an image.
+
+    Parameters
+    ----------
+    image_shape : np.ndarray.shape
+        Shape of the image from which to extract the roi.
+    roi_shape : tuple[int, ...]
+        Shape of the roi to extract from the image.
+    num_rois : int, optional
+        Number of rois to extract from the image. Default is 1.
+    random_seed : int, optional
+        Seed for the random number generator. Default is None.
+
+    Returns
+    -------
+    roi: list[tuple[slice, ...]]
+        A list of tuples of slices that can be used in the numpy array
+        that image_shape is based on to get a slice of that array.
+
+    Example usage:
+    Given some ndarray 'image' with shape (12, 2048, 2048) representing
+    the size of the dimensions 'TYX' in that order:
+
+    >>> image_shape = image.shape
+    >>> roi_shape = (3, 64, 64)
+    >>> rand_roi = get_random_roi(image_shape, roi_shape, num_rois=1, random_seed=42)
+    >>> rand_crop = [image[roi] for roi in rand_roi]
+    >>> print(rand_crop.shape)
+    (3, 64, 64)
+
+    """
+    assert isinstance(num_rois, int) and num_rois > 0, f"num_rois must be a positive integer, not {num_rois}."
+    assert len(image_shape) == len(roi_shape), f"image_shape and roi_shape must have the same number of dimensions, but image_shape has {len(image_shape)} dimensions and roi_shape has {len(roi_shape)} dimensions."
+    image_shape = np.asarray(image_shape)
+    roi_shape = np.array(roi_shape)
+    rand_gen = np.random.default_rng(seed=random_seed)
+    rand_coord = np.asarray([np.array(rand_gen.integers(0, random_space_limit + 1, size=num_rois), ndmin=1) for random_space_limit in image_shape - roi_shape])
+    roi = [tuple([slice(start, stop) for start, stop in zip(*coord_pair)]) for coord_pair in zip(rand_coord.T, rand_coord.T + np.array(roi_shape, ndmin=rand_coord.ndim))]
+    return roi
+
+# Check that the last image is the same when loaded as it was when being saved:
+# import numpy as np
+# test_path = Path(r'C:\Users\serge.parent\OneDrive - Allen Institute\Desktop\projects\holistic_state\cellsmap\cellsmap\results\flow_calculator\20241016_20X_vector_field.ome.tiff')
+# test_img = BioImage(test_path)
+# img = test_img.get_image_data("TCYX")
+# np.all(flowc.vx == img[:,0,...])
---
+++ cellsmap/analyses/flow/flow_features.py
@@ -0,0 +1,133 @@
+# %% Import libraries
+from pathlib import Path
+import importlib
+import flow_calculator
+import concurrent
+from pandas import DataFrame
+importlib.reload(flow_calculator)
+
+# %% Make list of datasets to analzye
+dataset_list = ['20240305_T01_001', '20240917_20X_48hr', '20241016_20X']
+debug = True
+ncores = 1#30
+delta_t = 1
+out_dir = Path('../../').resolve() / 'results' / Path(__file__).stem
+out_path_list = []
+
+# %%
+if __name__ == '__main__':
+    # run using multiple cores
+    if ncores > 1:
+        with concurrent.futures.ProcessPoolExecutor(ncores) as executor:
+            for dataset_name in dataset_list:
+                out_path = flow_calculator.compute_and_save_flow_field(out_dir, dataset_name, delta_t, executor, ncores, debug)
+                out_path_list.append(out_path)
+
+    else:
+        for dataset_name in dataset_list:
+            executor = None
+            out_path = flow_calculator.compute_and_save_flow_field(out_dir, dataset_name, delta_t, executor, ncores, debug)
+            out_path_list.append(out_path)
+
+# %% Save the locations of the outputs
+DataFrame({'dataset_name':dataset_list, 'vector_field_image_paths': out_path_list}).to_csv(out_dir / 'vector_field_image_paths.csv', index=False)
+
+
+# %% Example of usage:
+from bioio import BioImage
+from cellsmap.util.io import load_dataset
+import numpy as np
+from skimage.exposure import rescale_intensity
+import matplotlib.pyplot as plt
+
+# Get the paths to the vector field images
+dataset_list = ['20241016_20X']
+
+for dataset_name in dataset_list:
+    # Load vector field image for a dataset
+    img = flow_calculator.load_vector_field_img(out_dir, dataset_name)
+
+    # Get the channel names and their indices from the outputted image
+    # (the vector field images have metadata saved with them)
+    image_paths = flow_calculator.get_vector_field_image_paths(out_dir)
+    im_path = image_paths[dataset_name]
+    chan_map = {img.channel_names: i for i, img.channel_names in enumerate(BioImage(im_path).channel_names)}
+    print(f'The channel names and their indices are: {chan_map}')
+
+    # Get some random crops of the vector fields (this is randomly sampling in time too)
+    # and load the roi crops into memory
+    roi_shape = (1, 4, 64, 64)
+    rois = flow_calculator.get_random_roi(img.shape, roi_shape, num_rois=10, random_seed=42)
+    crops = [img[r] for r in rois]
+    crops_in_memory = [c.compute() for c in crops]
+
+    # Get a couple of basic descriptive statistics of the norm and angle channels
+    norm_chan_idx = chan_map['norm']
+    mag_means = [c[0, norm_chan_idx, ...].mean() for c in crops_in_memory]
+    mag_stds = [c[0, norm_chan_idx, ...].std() for c in crops_in_memory]
+
+    angle_chan_idx = chan_map['theta']
+    ang_means = [c[0, angle_chan_idx, ...].mean() for c in crops_in_memory]
+    ang_stds = [c[0, angle_chan_idx, ...].std() for c in crops_in_memory]
+
+    # Show the vector magnitude channel from the random crops
+    nrows, ncols, figsize = 2, 5, (15, 7)
+
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+    [ax.imshow(crops_in_memory[i][0, norm_chan_idx, ...], cmap='gray') for i,ax in enumerate(axs.flatten())]
+    [ax.axis('off') for i,ax in enumerate(axs.flatten())]
+    [ax.set_title(f'mean: {mag_means[i]:.2f}, std: {mag_stds[i]:.2f}') for i,ax in enumerate(axs.flatten())]
+    fig.suptitle('Vector norms from random crops', size=20)
+    plt.tight_layout()
+    plt.show()
+
+    # Show the vector angles channel from the random crops
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+    [ax.imshow(crops_in_memory[i][0, angle_chan_idx, ...], cmap='gray') for i,ax in enumerate(axs.flatten())]
+    [ax.axis('off') for i,ax in enumerate(axs.flatten())]
+    [ax.set_title(f'mean: {mag_means[i]:.2f}, std: {mag_stds[i]:.2f}') for i,ax in enumerate(axs.flatten())]
+    fig.suptitle('Vector angles from random crops', size=20)
+    plt.tight_layout()
+    plt.show()
+
+    # Show the image crops from the random roi regions vs the same
+    # image at the next timepoint too
+    img = load_dataset(dataset_name, channels=['CDH5'], level=2)
+    rois_future = [(slice(r[0].start+delta_t, r[0].stop+delta_t), *r[1:]) for r in rois]
+    crops = [np.stack([img[rois[i]], img[rois_future[i]], np.zeros(img[rois[i]].shape)], axis=-1) for i in range(len(rois))]
+    crops_in_memory = [c.compute() for c in crops]
+    crops_in_memory = [rescale_intensity(c, out_range=np.uint8) for c in crops_in_memory]
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+    [ax.imshow(crops_in_memory[i][0, 0, ...], cmap='gray') for i,ax in enumerate(axs.flatten())]
+    [ax.axis('off') for i,ax in enumerate(axs.flatten())]
+    fig.suptitle('CDH5 from random crops (red = t, green = t + delta_t)', size=20)
+    plt.tight_layout()
+    plt.show()
+
+    # Use the loaded vector information to create the flow field
+    vfield = flow_calculator.load_vector_field_img(out_dir, dataset_name)
+    vfield_crops = [vfield[r] for r in rois]
+    vfield_crops = [c.compute() for c in vfield_crops]
+    vx_crops, vy_crops = zip(*[(c[0, chan_map['vx'], ...].squeeze(), c[0, chan_map['vy'], ...].squeeze()) for c in vfield_crops])
+
+    img = load_dataset(dataset_name, channels=['CDH5'], level=2)
+    img_crops = [img[r] for r in rois]
+    img_crops = [c.compute().squeeze() for c in img_crops]
+
+    flow_graphs = [flow_calculator.FlowCalculator.make_vector_field_map(img, vx, vy, display=False, return_map=True) for img, vx, vy in zip(img_crops, vx_crops, vy_crops)]
+    # trim white-space from the flow graphs
+    keep_me_indices = [np.where(~np.all(g==255, axis=-1)) for g in flow_graphs]
+    keep_me_slices = [(slice(np.min(i), np.max(i)), slice(np.min(j), np.max(j)), slice(None)) for i,j in keep_me_indices]
+    flow_graphs = [flow_graphs[i][arr_slice] for i,arr_slice in enumerate(keep_me_slices)]
+
+    rois_as_titles = [list(zip(*[(slc.start, slc.stop) for slc in r])) for r in rois]
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=[d*1.5 for d in figsize])
+    [ax.imshow(flow_graphs[i], cmap='gray') for i, ax in enumerate(axs.flatten())]
+    [ax.axis('off') for i, ax in enumerate(axs.flatten())]
+    [ax.set_title(f'roi start: {rois_as_titles[i][0]}\nroi stop:{rois_as_titles[i][1]}') for i,ax in enumerate(axs.flatten())]
+    fig.suptitle('Flow fields', size=20)
+    plt.tight_layout()
+    plt.show()
+
+# %%
+
