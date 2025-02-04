8fc5c9e96c4d39bf2bcd6921840eb8fbf01ccd68
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-02-04 11:09:02 -0800

    set channel order in config

--- cellsmap/util/io.py
+++ cellsmap/util/io.py
@@ -68,6 +68,11 @@ def get_channel_index(dataset_name:str, channel_names:list) -> int:
     available_channels = get_available_channels(dataset_name)
     return [available_channels.index(channel) for channel in channel_names]
 
+def get_channel_order(dataset_name:str):
+    gfp_index = get_dataset_info(dataset_name)['egfp_channel_index']
+    bf_index = get_dataset_info(dataset_name)['brightfield_channel_index']
+    return gfp_index, bf_index
+
 def load_dataset(dataset_name:str, channels:list, time_start:int=0, time_end:int=-1, level:int=0) -> dask.array.Array:
     path = get_zarr_path(dataset_name)
     reader = BioImage(path)
@@ -105,6 +110,9 @@ def get_dim_map(dim_order: str) -> dict:
     return dim_map
 
 def get_original_path(dataset_name: str) -> str:
+    """
+    Example path format: /{date}/{dataset_name}.dir/{dataset_name_number}.imgdir
+    """
     dataset_info = get_dataset_info(dataset_name)
     return Path(dataset_info['original_path'])
 
