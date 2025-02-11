173400d4dfb62baa3a3909bc81f7cf9fa4bf9be4
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-02-10 17:57:51 -0800

    update doc

--- cellsmap/image_conversion/process_images/process_sldy.py
+++ cellsmap/image_conversion/process_images/process_sldy.py
@@ -9,12 +9,34 @@ import numpy as np
 
 
 def get_slidebook_image_path(dataset_name: str, channel: int, timepoint: int):
+    """
+    Constructs the file path for a SlideBook image.
+
+    Parameters:
+    dataset_name (str): The name of the dataset.
+    channel (int): The channel index.
+    timepoint (int): The timepoint index.
+
+    Returns:
+    pathlib.Path: The constructed file path for the SlideBook image.
+    """
     sld_path = get_original_path(dataset_name)
     sld_path = sld_path / f"ImageData_Ch{channel}_TP{timepoint:07d}.npy"
     return sld_path
 
 
 def get_image_format(dataset_name: str, channel: int, timepoint: int):
+    """
+    Retrieves the shape and data type of a SlideBook image.
+
+    Parameters:
+    dataset_name (str): The name of the dataset.
+    channel (int): The channel index.
+    timepoint (int): The timepoint index.
+
+    Returns:
+    tuple: A tuple containing the shape and data type of the image.
+    """
     sld_path = get_slidebook_image_path(dataset_name, channel, timepoint)
     array = np.load(sld_path)
     return array.shape, array.dtype
@@ -22,18 +44,50 @@ def get_image_format(dataset_name: str, channel: int, timepoint: int):
 
 @dask.delayed
 def delayed_np_load(filename):
+    """
+    Loads a NumPy array from a file in a delayed manner using Dask.
+
+    Parameters:
+    filename (str): The file path to the NumPy array. (i.e. SlideBook image)
+
+    Returns:
+    numpy.ndarray: The loaded NumPy array.
+    """
     return np.load(filename)
 
 
 def load_original_slidebook_image(
     dataset_name: str, channel: int, timepoint: int
 ) -> da.Array:
+    """
+    Loads a SlideBook image as a Dask array.
+
+    Parameters:
+    dataset_name (str): The name of the dataset.
+    channel (int): The channel index.
+    timepoint (int): The timepoint index.
+
+    Returns:
+    dask.array.Array: The loaded SlideBook image as a Dask array.
+    """
     sld_path = get_slidebook_image_path(dataset_name, channel, timepoint)
     delayed_array = delayed_np_load(sld_path)
     return delayed_array
 
 
-def process_timepoint(tp: int, dataset: str, shape, dtype):
+def process_timepoint(tp: int, dataset: str, shape: tuple, dtype: np.dtype) -> da.Array:
+    """
+    Processes a single timepoint for a given dataset.
+
+    Parameters:
+    tp (int): The timepoint index to process.
+    dataset (str): The name of the dataset.
+    shape (tuple): The shape of the images to be processed. This is required to correctly initialize the Dask arrays.
+    dtype (str): The data type of the images to be processed. This is required to correctly initialize the Dask arrays.
+
+    Returns:
+    dask.array.Array: A Dask array containing the stacked GFP and BF images for the given timepoint.
+    """
     gfp_index, bf_index = get_channel_order(dataset)
     gfp = load_original_slidebook_image(dataset, channel=gfp_index, timepoint=tp)
     bf = load_original_slidebook_image(dataset, channel=bf_index, timepoint=tp)
@@ -44,12 +98,34 @@ def process_timepoint(tp: int, dataset: str, shape, dtype):
 
 
 def get_timepoints(pos: int, dataset: str, number_positions: int = 6):
+    """
+    Generates a list of timepoints for a given position in the dataset.
+
+    Parameters:
+    pos (int): The position index.
+    dataset (str): The name of the dataset.
+    number_positions (int): The total number of positions in the dataset. Default is 6.
+
+    Returns:
+    range: A range object containing the timepoints for the given position.
+    """
     t_final = get_dataset_duration_in_frames(dataset)  # if testing set t_final = 10
     timepoints = range(pos, t_final * number_positions, number_positions)
     return timepoints
 
 
 def process_position(pos: int, dataset: str, number_positions: int = 6) -> da.Array:
+    """
+    Processes all timepoints for a given position in the dataset.
+
+    Parameters:
+    pos (int): The position index to process.
+    dataset (str): The name of the dataset.
+    number_positions (int): The total number of positions in the dataset. Default is 6.
+
+    Returns:
+    dask.array.Array: A Dask array containing the processed images for all timepoints at the given position.
+    """
     timepoints = get_timepoints(pos, dataset, number_positions)
     shape, dtype = get_image_format(dataset, 0, 0)
     results = [process_timepoint(tp, dataset, shape, dtype) for tp in timepoints]
--- cellsmap/image_conversion/process_images/write_zarr.py
+++ cellsmap/image_conversion/process_images/write_zarr.py
@@ -6,7 +6,16 @@ from bioio_base.types import PhysicalPixelSizes
 from bioio.writers import ome_zarr_writer_2 as ome_zarr_writer
 
 
-def get_sldy_metadata(dataset):
+def get_sldy_metadata(dataset: str) -> PhysicalPixelSizes:
+    """
+    Retrieves sldy metadata for the given dataset in the format of PhysicalPixelSizes.
+
+    Parameters:
+    dataset (str): The name of the dataset.
+
+    Returns:
+    PhysicalPixelSizes: The physical pixel sizes for the dataset.
+    """
     original_path = str(io.get_original_path(dataset))
     dataset_path = original_path.rsplit("/", 1)[0]
     im = BioImage(dataset_path)
@@ -22,41 +31,54 @@ def get_sldy_metadata(dataset):
     return physical_pixel_sizes
 
 
-def _get_level_shapes(
-    _shape,
-):
+def get_level_shapes(shape: tuple) -> list[tuple]:
     """
     Uses XY and Z scaling parameters to determine the
     image data shape at different resolutions.
-    """
-    _xy_scaling = [0.5]
-    _z_scaling = [1.0]
 
-    if len(_xy_scaling) != len(_z_scaling):
+    Parameters:
+    shape (tuple): The shape of the original image data.
+
+    Returns:
+    list[tuple]: A list of shapes for each resolution level.
+    """
+    xy_scaling = [0.5]
+    z_scaling = [1.0]
+
+    if len(xy_scaling) != len(z_scaling):
         raise ValueError(
-            f"Found XY and Z scaling with different length: XY={_xy_scaling}, Z={_z_scaling}."
+            f"Found XY and Z scaling with different length: XY={xy_scaling}, Z={z_scaling}."
         )
-    source_shape = _shape
+    source_shape = shape
     level_shapes = [source_shape]
     nchannels = source_shape[1]
 
-    for sid, _ in enumerate(_xy_scaling):
-        z_scaling = np.prod(_z_scaling[: sid + 1])
-        xy_scaling = np.prod(_xy_scaling[: sid + 1])
+    for sid, _ in enumerate(xy_scaling):
+        z_scaling_factor = np.prod(z_scaling[: sid + 1])
+        xy_scaling_factor = np.prod(xy_scaling[: sid + 1])
         level_shape = (
             source_shape[0],
             nchannels,
-            int(source_shape[2] * z_scaling),
-            int(source_shape[3] * xy_scaling),
-            int(source_shape[4] * xy_scaling),
+            int(source_shape[2] * z_scaling_factor),
+            int(source_shape[3] * xy_scaling_factor),
+            int(source_shape[4] * xy_scaling_factor),
         )
         level_shapes.append(level_shape)
     return level_shapes
 
 
-def _get_zarr_chunk_dims(im_shape):
+def get_zarr_chunk_dims(im_shape: tuple) -> list[tuple]:
+    """
+    Determines the chunk dimensions for Zarr storage.
+
+    Parameters:
+    im_shape (tuple): The shape of the image data.
+
+    Returns:
+    list[tuple]: A list of chunk dimensions for each resolution level.
+    """
     chunk_dims = []
-    level_shapes = _get_level_shapes(im_shape)
+    level_shapes = get_level_shapes(im_shape)
     for i, dim in enumerate(level_shapes):
         z = np.min([dim[-3], 4**i])
         chunk_dims.append((1, 1, z, dim[-2], dim[-1]))
@@ -71,23 +93,37 @@ def write_scene(
     full_zarr_path: Path,
     dataset: str,
     position: int,
-    physical_pixel_sizes,
-):
+    physical_pixel_sizes: PhysicalPixelSizes,
+) -> None:
+    """
+    Writes a scene to a Zarr store.
 
-    zarr_chunk_dims_tuples = _get_zarr_chunk_dims(im.shape)
+    Parameters:
+    im (np.array): The image data array.
+    channels (list[str]): The list of channel names.
+    full_zarr_path (Path): The full path to the Zarr store.
+    dataset (str): The name of the dataset.
+    position (int): The position index.
+    physical_pixel_sizes (PhysicalPixelSizes): The physical pixel sizes for the dataset.
+
+    Returns:
+    None
+    """
+
+    zarr_chunk_dims_tuples = get_zarr_chunk_dims(im.shape)
 
     writer = ome_zarr_writer.OmeZarrWriter()
     writer.init_store(
         output_path=full_zarr_path,
-        shapes=_get_level_shapes(im.shape),
+        shapes=get_level_shapes(im.shape),
         chunk_sizes=zarr_chunk_dims_tuples,
         dtype=im.dtype,
     )
 
     # Use all channels, if channels are not specific by user
-    _channels = [c for c in range(im.shape[1])]
+    channels_to_use = [c for c in range(im.shape[1])]
 
-    writer.write_t_batches_array(im, channels=_channels, tbatch=4)
+    writer.write_t_batches_array(im, channels=channels_to_use, tbatch=4)
 
     physical_scale = {
         "c": 1.0,  # default value for channel
