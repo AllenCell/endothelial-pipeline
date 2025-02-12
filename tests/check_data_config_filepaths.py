d0e47c1601473b8b40173473f5ea77c9b9b4dce4
Serge Parent <serge.edouard.parent@gmail.com>
2025-02-11 16:23:17 -0800

    Addressed most PR comments. Will bring up which file we want to use for the original filepath in the next working group meeting.

---
+++ tests/check_data_config_filepaths.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from cellsmap.util import io
+
+print('Available datasets:')
+dataset_name_list = io.get_available_datasets()
+print('\n')
+filepaths_found = {}
+for dataset_name in dataset_name_list:
+    config_data = io.get_dataset_info(dataset_name)
+    original_path = Path(config_data['original_path'])
+    zarr_path = Path(config_data['original_path'])
+    print(f"{dataset_name}: original_path {original_path.exists()}, zarr_path {zarr_path.exists()}")
+    filepaths_found[dataset_name] = {'original': original_path.exists(), 'zarr': zarr_path.exists()}
+print('\n')
+[print(f'{dataset_name} has one or more invalid paths...') for dataset_name in dataset_name_list if not all(filepaths_found[dataset_name].values())]
+assert all([all(fp_exists.values()) for dataset_name, fp_exists in filepaths_found.items()]), "Invalid filepaths found."
