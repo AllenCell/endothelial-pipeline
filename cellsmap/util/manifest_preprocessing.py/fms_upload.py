c3c78576eb23365a5e2044df138e3a556885d617
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-03-12 13:41:51 -0700

    add ml flow id

--- cellsmap/util/manifest_preprocessing.py/fms_upload.py
+++ cellsmap/util/manifest_preprocessing.py/fms_upload.py
@@ -1,18 +1,21 @@
 from aicsfiles import FileManagementSystem
 
 
-def save_file_to_fms(file_path, dataset, model_version, commit_hash, misc_notes, file_type, env="prod"):
+def save_file_to_fms(file_path, dataset, commit_hash, misc_notes, file_type, model_version="", mlflow_run_id=None, env="prod"):
     """
     Save a file to FMS with Endo project specific metadata annotations. 
     Manifests should represent one dataset. 
+    
+    If a model was used to produce the output, add the model version and mlflow run id.
 
     Parameters:
     file_path (str): The path to the file to be uploaded.
     dataset (str): The name of the dataset matching the dataconfig.yaml file.
-    model_version (str): The version of the model used to generate the file. If using the date use the format YYYYMMDD.
     commit_hash (str): The commit hash of the code used to generate the file. 
     misc_notes (str): Additional relavent notes.
     file_type (str): The type of the file. (e.g., "parquet", "csv", etc.)
+    model_version (str): The version of the model used to generate the file. If using the date use the format YYYYMMDD. Optional.
+    mlflow_run_id (str): The mlflow run id of the model run that generated the file. Optional.
     env (str): The environment to upload the file to. Default is "prod", use "stg" for staging.
 
     Returns:
@@ -29,6 +32,8 @@ def save_file_to_fms(file_path, dataset, model_version, commit_hash, misc_notes,
     metadata_builder.add_annotation("Produced By", "python code")
     if env == "prod":
         metadata_builder.add_annotation("Program", "Endothelial")
+        if mlflow_run_id:
+            metadata_builder.add_annotation("mlflow run id", mlflow_run_id)
     annotations = metadata_builder.build()
 
     fms_file = fms.upload_file(file_path, file_type, annotations)
