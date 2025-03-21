503e62fe27231d895a1f49cc83269a963f59a869
Erin Angelini <erin.angelini@n80.corp.alleninst.org>
2025-03-21 15:21:49 -0700

    plotting gen potential wrapped in model_analysis func

--- cellsmap/analyses/workflows/dynamics_summarize.py
+++ cellsmap/analyses/workflows/dynamics_summarize.py
@@ -9,7 +9,7 @@ from cellsmap.analyses.utils.viz import dynamics_viz, viz_base as vb
 
 from cellsmap.analyses.configs.manifest_postproc_config import savedir, ds_to_skip, PCs
 from cellsmap.analyses.configs.dynamics_viz_config import Nbins_plot, bin_limits, \
-    pplane_args, shear_range, fpt_args, Nbins_gp, shear_range_gp, p_tol, gp_args
+    pplane_args, shear_range, fpt_args, Nbins_gp, shear_range_gp, gp_args
 
 # %%
 # load fitted drift-diffusion model (list of fit SINDy objects)
@@ -37,14 +37,21 @@ model_analysis.run_epr_analysis(myModel,bins,centers,shear_range,savedir)
 # %%
 ################### Generalized potential energy landscape ###################
 
-# this will all go in wrapper function in model_analysis.py
-
+# get bins and centers for plotting generalized potential energy landscape (fixed across all values of shear stress)
 bins_gp, centers_gp = rh.get_bins(Nbins_gp,bin_limits=bin_limits)
+
+# plot generalized potential energy landscape for each shear stress specified in shear_range_gp
+model_analysis.run_gen_potential_analysis(myModel,bins_gp,centers_gp,shear_range_gp,gp_args,savedir)
+# %%
 X1,X2 = np.meshgrid(*centers_gp)
 
+f = model_eval.vector_field_function(driftModel)
+D = model_eval.vector_field_function(diffModel)
+
+f_mesh = model_eval.mesh_grid_function(f)
+D_mesh = model_eval.mesh_grid_function(D)
+
 for ii, u in enumerate(shear_range_gp):
-    f = model_eval.vector_field_function(driftModel)
-    D = model_eval.vector_field_function(diffModel)
 
     p_fit = model_eval.get_stationary_probability(f,D,bins_gp,centers_gp,u,tol=p_tol)
     U= -np.log(p_fit)
@@ -56,28 +63,20 @@ for ii, u in enumerate(shear_range_gp):
     fig.suptitle('Shear stress: '+str(u)+' dyn/cm$^2$', y = 1.05, fontsize=16)
     vb.save_plot(fig,savedir+'figs/gp_shear_'+str(ii))
 
-    normed = False # if True, normalize vectors by their magnitudes
-
-    f_mesh = model_eval.mesh_grid_function(f)
-    D_mesh = model_eval.mesh_grid_function(D)
-
     f_vals = f_mesh([X1,X2],u).T
     D_vals = D_mesh([X1,X2],u).T
 
-    _, grad_term, _, flux_term = gp.grad_flux_decomposition(f_vals,D_vals,centers_gp,tol=p_tol)
-    grad_ = grad_term.copy()
-    flux_ = np.array(flux_term)
-    if normed:
-        grad_ = grad_/(np.sqrt(grad_[0]**2+grad_[1]**2))
-        flux_ = flux_/(np.sqrt(flux_[0]**2+flux_[1]**2))
+    _, grad_term, _, flux_term = gp.grad_flux_decomposition(f_vals,D_vals,centers_gp,tol=gp_args['p_tol'])
+    flux_term = np.array(flux_term)
 
     fig,ax = dynamics_viz.plot_grad_flux_decomposition(U,centers_gp[0],centers_gp[1],
-                                                       grad_,flux_,cmap='jet',normed=normed,
+                                                       grad_term,flux_term,
+                                                       cmap='jet',normed=gp_args['normed'],
                                                        downsample=gp_args['downsample'])
     ax.set_xlabel(gp_args['plt_xlabel'])
     ax.set_ylabel(gp_args['plt_ylabel'])
-    ax.set_title('Shear stress: '+str(u)+' dyn/cm$^2$')
-    fig.suptitle(gp_args['plt_title'], y = 1.05, fontsize=16)
+    ax.set_title('Shear stress: '+str(np.round(u,2))+' dyn/cm$^2$')
+    fig.suptitle(gp_args['plt_title'], y = 0.8, fontsize=16)
     vb.save_plot(fig,savedir+'figs/gp_decomp_shear_'+str(ii))
 
 # %%
