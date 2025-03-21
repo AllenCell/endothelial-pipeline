228bd83e2c22cb1b70fb10153f6771947ba6d6c6
Erin Angelini <erin.angelini@n80.corp.alleninst.org>
2025-03-21 12:07:53 -0700

    Model fitting

--- cellsmap/analyses/workflows/dynamics_fit.py
+++ cellsmap/analyses/workflows/dynamics_fit.py
@@ -4,7 +4,7 @@ import pysindy as ps
 from cellsmap.analyses.utils.io import dynamics_io
 from cellsmap.analyses.utils import model_fitting
 # import config parameters
-from cellsmap.analyses.configs.manifest_postproc_config import savedir, PCs
+from cellsmap.analyses.configs.manifest_postproc_config import savedir, PCs, dt
 from cellsmap.analyses.configs.dynamics_fit_config import drift_deg, diff_deg, param_deg_drift, param_deg_diff, sigmoid_funcs, func_names, include_sigmoid
 
 # %%
@@ -18,8 +18,7 @@ drift_lib = model_fitting.build_drift_lib(ndim=len(PCs),drift_deg=drift_deg,para
 diff_lib = model_fitting.build_diff_lib(ndim=len(PCs),diff_deg=diff_deg,param_deg=param_deg_diff)
 # %%
 # fit model for drift term - SINDy based regression
-driftModel = ps.SINDy(feature_library = drift_lib, optimizer = ps.SSR())
-driftModel.fit(train_test_dict['X_train'],t=5,x_dot=train_test_dict['Y_train'],u=train_test_dict['u_train'])
+driftModel = model_fitting.fit_sindy_model(drift_lib,train_test_dict['X_train'],train_test_dict['Y_train'],train_test_dict['u_train'],dt)
 
 # score on test set
 drift_R2 = driftModel.score(train_test_dict['X_test'],x_dot=train_test_dict['Y_test'],u=train_test_dict['u_test'])
@@ -28,8 +27,7 @@ driftModel.print()
 print('Score (R^2) for model of drift term: %f' %drift_R2)
 # %%
 # fit model for diffusion term - SINDy based regression
-diffModel = ps.SINDy(feature_library = diff_lib, optimizer = ps.SSR())
-diffModel.fit(train_test_dict['X_train'],t=5,x_dot=train_test_dict['V_train'],u=train_test_dict['u_train'])
+diffModel = model_fitting.fit_sindy_model(diff_lib,train_test_dict['X_train'],train_test_dict['V_train'],train_test_dict['u_train'],dt)
 
 # score on test set
 diff_R2 = diffModel.score(train_test_dict['X_test'],x_dot=train_test_dict['V_test'],u=train_test_dict['u_test'])
