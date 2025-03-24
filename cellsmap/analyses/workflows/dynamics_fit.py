a03e34a4893b4f0425010e4594c319c55c4e12f5
Erin Angelini <erin.angelini@n80.corp.alleninst.org>
2025-03-24 11:45:01 -0700

    Issue narrowed down to fitting wrapper function, not really needed imo

--- cellsmap/analyses/workflows/dynamics_fit.py
+++ cellsmap/analyses/workflows/dynamics_fit.py
@@ -15,9 +15,32 @@ drift_lib = model_fitting.build_drift_lib(ndim=len(PCs),drift_deg=drift_deg,para
 
 diff_lib = model_fitting.build_diff_lib(ndim=len(PCs),diff_deg=diff_deg,param_deg=param_deg_diff)
 # %%
+
+import pysindy as ps
+# fit model for drift term - SINDy based regression
+driftModel = ps.SINDy(feature_library = drift_lib, optimizer = ps.SSR())
+driftModel.fit(train_test_dict['X_train'], t= dt,x_dot=train_test_dict['Y_train'],u=train_test_dict['u_train'])
+
+# score on test set
+drift_R2 = driftModel.score(train_test_dict['X_test'],x_dot=train_test_dict['Y_test'],u=train_test_dict['u_test'])
+driftModel.print()
+
+print('Coefficient of determination (R^2) for model of drift term: %f' %drift_R2)
+
+# fit model for diffusion term - SINDy based regression
+diffModel = ps.SINDy(feature_library = diff_lib, optimizer = ps.SSR())
+diffModel.fit(train_test_dict['X_train'], t= dt,x_dot=train_test_dict['V_train'],u=train_test_dict['u_train'])
+
+# score on test set
+diff_R2 = diffModel.score(train_test_dict['X_test'],x_dot=train_test_dict['V_test'],u=train_test_dict['u_test'])
+diffModel.print()
+
+print('Coefficient of determination (R^2) for model of diffusion term: %f' %diff_R2)
+
+# %%
 # fit model for drift term - SINDy based regression
 driftModel = model_fitting.fit_sindy_model(drift_lib,train_test_dict['X_train'],train_test_dict['Y_train'],
-                                           train_test_dict['u_train'],t=dt,optimizer=ps.SSR())
+                                           train_test_dict['u_train'],t=dt)
 
 # score on test set
 drift_R2 = driftModel.score(train_test_dict['X_test'],x_dot=train_test_dict['Y_test'],u=train_test_dict['u_test'])
@@ -27,7 +50,7 @@ print('Score (R^2) for model of drift term: %f' %drift_R2)
 # %%
 # fit model for diffusion term - SINDy based regression
 diffModel = model_fitting.fit_sindy_model(diff_lib,train_test_dict['X_train'],train_test_dict['V_train'],
-                                          train_test_dict['u_train'],t=dt,optimizer=ps.SSR())
+                                          train_test_dict['u_train'],t=dt)
 
 # score on test set
 diff_R2 = diffModel.score(train_test_dict['X_test'],x_dot=train_test_dict['V_test'],u=train_test_dict['u_test'])
@@ -39,54 +62,4 @@ print('Score (R^2) for model of diffusion term: %f' %diff_R2)
 model_dict = {'driftModel':driftModel,'diffModel':diffModel}
 dynamics_io.save_model(model_dict, savedir+'outputs/')
 # %%
-
-
-
-
-# %%
-import numpy as np
-import pysindy as ps
-
-# full library for drift term (functions of state variables)
-drift_feature_lib = ps.PolynomialLibrary(degree=3, include_bias=True)
-
-
-ndim = len(PCs)
-
-# library for model dependence on control parameters (shear stress)
-drift_parameter_lib=ps.PolynomialLibrary(degree=3, include_bias=True) # library for model dependence on control parameters (shear stress)
-
-# build full library for drift term: pySINDy parameterized library
-drift_lib_=ps.ParameterizedLibrary(feature_library=drift_feature_lib,
-    parameter_library=drift_parameter_lib,num_features=ndim,num_parameters=1) 
-
-# build library for diffusion term (polynomial library only)
-diff_feature_lib=ps.PolynomialLibrary(degree=0, include_bias=True)
-diff_parameter_lib=ps.PolynomialLibrary(degree=3, include_bias=False)
-diff_lib_=ps.ParameterizedLibrary(feature_library=diff_feature_lib,
-    parameter_library=diff_parameter_lib,num_features=ndim,num_parameters=1)
-
-
-# fit model for drift term - SINDy based regression
-driftModel_ = ps.SINDy(feature_library = drift_lib_, optimizer = ps.SSR())
-driftModel_.fit(train_test_dict['X_train'], t= dt,x_dot=train_test_dict['Y_train'],u=train_test_dict['u_train'])
-
-# score on test set
-drift_R2 = driftModel_.score(train_test_dict['X_test'],x_dot=train_test_dict['Y_test'],u=train_test_dict['u_test'])
-driftModel_.print()
-
-print('Coefficient of determination (R^2) for model of drift term: %f' %drift_R2)
-
-# fit model for diffusion term - SINDy based regression
-diffModel_ = ps.SINDy(feature_library = diff_lib_, optimizer = ps.SSR())
-diffModel_.fit(train_test_dict['X_train'], t= dt,x_dot=train_test_dict['V_train'],u=train_test_dict['u_train'])
-
-# score on test set
-diff_R2 = diffModel_.score(train_test_dict['X_test'],x_dot=train_test_dict['V_test'],u=train_test_dict['u_test'])
-diffModel_.print()
-
-print('Coefficient of determination (R^2) for model of diffusion term: %f' %diff_R2)
-# %%
-model_dict = {'driftModel':driftModel_,'diffModel':diffModel_}
-dynamics_io.save_model(model_dict, savedir)
 # %%
