89922fbc4bddbc6f3cd258a5a4bfaf247c7b774f
Erin Angelini <erin.angelini@n80.corp.alleninst.org>
2025-03-25 11:57:06 -0700

    Command line input of config via Fire

--- cellsmap/analyses/README.md
+++ cellsmap/analyses/README.md
@@ -4,7 +4,7 @@
 
 To start, ensure that your time series data are saved in a `.csv` or `.parquet` file where the columns represent the extracted features (i.e., observed variables), the rows represent inividual data points. There may be additional metedata columns as needed.
 
-For example, suppose we have data that are 8 features for each crop of the image at each frame in the image series. In the manifest file that we load into `manifest_postproc.py`, the rows are each instance of the data (i.e., one single image crop at a given frame), the first 8 columns represent each of the extracted features, and the remaining columns are metadata for, e.g., what dataset the original image is from, the FOV the crop was taken from, the time point in the movie, etc.
+For example, suppose we have data that are 8 features for each crop of the image at each frame in the image series. In the manifest file that we load into `dynamics_preproc.py`, the rows are each instance of the data (i.e., one single image crop at a given frame), the first 8 columns represent each of the extracted features, and the remaining columns are metadata for, e.g., what dataset the original image is from, the FOV the crop was taken from, the time point in the movie, etc.
 
 ## Environment variables and management
 
@@ -14,8 +14,7 @@ Install `pdm` and configure project at the root of the `cellsmap` repository.
 
 Working directory: head of `cellsmap` repository
 
-
-`pdm run cellsmap/analyses/workflows/manifest_postproc.py`
+`pdm run cellsmap/analyses/workflows/dynamics_preproc.py [config_name]`
 * Load manifest (Diffusion AE output: crop-based features for mutliple datasets), remove outliers, fit PCA to get shared low dimensional state space. 
 * Get one time step displacements of crops over time, train/test split for fitting drift
  $$\mathbf{f}(\mathbf{x})$$ 
@@ -23,21 +22,26 @@ Working directory: head of `cellsmap` repository
  $$D(\mathbf{x})$$ 
  coefficients from these displacements.
 
-`pdm run cellsmap/analyses/workflows/dynamics_fit.py`
+`pdm run cellsmap/analyses/workflows/dynamics_fit.py [config_name]`
 * Load train/test sets from `manifest_postproc.py`, regression (SINDy - regression against set of basis functions) to fit callable drift and diffusion functions.
 
-`pdm run cellsmap/analyses/workflows/dynamics_summarize.py`
+`pdm run cellsmap/analyses/workflows/dynamics_summarize.py [config_name]`
 * Using fit SINDy objects (callable functions learned via regression), generate summary plots of various analyses of the SDE model 
 $$\frac{d\mathbf{x}}{dt} = \mathbf{f}(\mathbf{x}) + \sqrt{2 D(\mathbf{x})} \xi(t)$$
+
+* Analyses include:
     * Phase portrait for shear stress values (control parameter of fit model) present in the data
     * Comparison of "stationary" histogram of the data and predicted stationary distribution for the SDE model
     * Fixed points and stability as a function of shear stress (quasi bifurcation diagram)
     * Entropy production rate as a function of shear stress
     * Generalized potential energy ("landscape") $U = -ln P$ for various values of shear stress
 
+For each workflow, `[config_name]` is an optional command line input to specify the config in `dynamics_config.yaml` to use when running the workflow. If this is not specified via command line, the workflows are run using the `default` config in `dynamics_config.yaml`.
 
 ### Acessing and interpreting outputs
-Set output directory via `cellsmap/analyses/configs/manifest_postproc.py`
-* `outputs` subdirectory: fit PCA model (`sklearn.pipeline.Pipeline` object), train/test sets for displacement vectors, and fit drift and diffusion functions (`pysindy.SINDy` object)
-* `figs` subdirectory: summary figures for data, PCA, and SDE model
+Set output directory via `dynamics_config.yaml`, subdirectory of `cellsmap/analyses/results`
+* Contains fit PCA model (`sklearn.pipeline.Pipeline` object), train/test sets for displacement vectors, and fit drift and diffusion functions (`pysindy.SINDy` object)
+
+Figures are saved to the `figs` directory at the head of the `cellsmap` repo
+* summary figures for data, PCA, and SDE model
 
--- cellsmap/analyses/workflows/dynamics_fit.py
+++ cellsmap/analyses/workflows/dynamics_fit.py
@@ -1,64 +1,61 @@
-# %%
+import fire
 import pysindy as ps # import pysindy package for SINDy based regression
 from pathlib import Path
 
 from cellsmap.analyses.utils.io import dynamics_io
 from cellsmap.analyses.utils import model_fitting
-# %%
-################### Load configs from dynamics_config ###################
-config = dynamics_io.load_dynamics_config()
-assert "output_subdir" in config, "output_subdir not found in dynamics_config.yaml"
 
-# get head of analyses folder in cellsmap repo
-analyses_folder = Path(__file__).resolve().parent.parent
-savedir = str(analyses_folder / 'results' / config["output_subdir"])+'/' # directory to save results
+def main(config_name:str='default') -> None:
+    ################### Load configs from dynamics_config ###################
+    config = dynamics_io.load_dynamics_config(config_name)
+    assert "output_subdir" in config, "output_subdir not found in dynamics_config.yaml"
 
-# figures saved into folder at head of repo (should already exist from previous workflow)
-parent_folder = analyses_folder.parent
-fig_savedir = str(parent_folder / 'figs')+'/'
+    # get head of analyses folder in cellsmap repo
+    analyses_folder = Path(__file__).resolve().parent.parent
+    savedir = str(analyses_folder / 'results' / config["output_subdir"])+'/' # directory to save results
 
-# get inputs for regression from config
-PCs = config['PCs_to_analyze']
-dt = config['dt']
-drift_deg = config['polynomial_lib']['drift_feats']
-diff_deg = config['polynomial_lib']['diffusion_feats']
-param_deg_drift = config['polynomial_lib']['drift_param']
-param_deg_diff = config['polynomial_lib']['diffusion_param']
+    # get inputs for regression from config
+    PCs = config['PCs_to_analyze']
+    dt = config['dt']
+    drift_deg = config['polynomial_lib']['drift_feats']
+    diff_deg = config['polynomial_lib']['diffusion_feats']
+    param_deg_drift = config['polynomial_lib']['drift_param']
+    param_deg_diff = config['polynomial_lib']['diffusion_param']
 
-# %%
-################### Load train test data from file ###################
-train_test_dict = dynamics_io.load_train_test(savedir+'train_test_data.npz')
-# %%
-################### Build SINDy libraries ###################
-# for fitting model of drift and diffusion terms
-drift_lib = model_fitting.build_drift_lib(ndim=len(PCs),drift_deg=drift_deg,param_deg=param_deg_drift)
 
-diff_lib = model_fitting.build_diff_lib(ndim=len(PCs),diff_deg=diff_deg,param_deg=param_deg_diff)
-# %%
-################### Fit SINDy models ###################
+    ################### Load train test data from file ###################
+    train_test_dict = dynamics_io.load_train_test(savedir+'train_test_data.npz')
 
-# fit model for drift term - SINDy based regression
-driftModel = ps.SINDy(feature_library = drift_lib, optimizer = ps.SSR())
-driftModel.fit(train_test_dict['X_train'],t=dt,x_dot=train_test_dict['Y_train'],u=train_test_dict['u_train'])
+    ################### Build SINDy libraries ###################
+    # for fitting model of drift and diffusion terms
+    drift_lib = model_fitting.build_drift_lib(ndim=len(PCs),drift_deg=drift_deg,param_deg=param_deg_drift)
 
-# score on test set
-drift_R2 = driftModel.score(train_test_dict['X_test'],x_dot=train_test_dict['Y_test'],u=train_test_dict['u_test'])
-driftModel.print()
+    diff_lib = model_fitting.build_diff_lib(ndim=len(PCs),diff_deg=diff_deg,param_deg=param_deg_diff)
+    ################### Fit SINDy models ###################
 
-print('Coefficient of determination (R^2) for model of drift term: %f' %drift_R2)
+    # fit model for drift term - SINDy based regression
+    driftModel = ps.SINDy(feature_library = drift_lib, optimizer = ps.SSR())
+    driftModel.fit(train_test_dict['X_train'],t=dt,x_dot=train_test_dict['Y_train'],u=train_test_dict['u_train'])
 
-# fit model for diffusion term - SINDy based regression
-diffModel = ps.SINDy(feature_library = diff_lib, optimizer = ps.SSR())
-diffModel.fit(train_test_dict['X_train'],t=dt,x_dot=train_test_dict['V_train'],u=train_test_dict['u_train'])
+    # score on test set
+    drift_R2 = driftModel.score(train_test_dict['X_test'],x_dot=train_test_dict['Y_test'],u=train_test_dict['u_test'])
+    driftModel.print()
 
-# score on test set
-diff_R2 = diffModel.score(train_test_dict['X_test'],x_dot=train_test_dict['V_test'],u=train_test_dict['u_test'])
-diffModel.print()
+    print('Coefficient of determination (R^2) for model of drift term: %f' %drift_R2)
 
-print('Coefficient of determination (R^2) for model of diffusion term: %f' %diff_R2)
-# %%
-################### Save trained models ###################
-model_dict = {'driftModel':driftModel,'diffModel':diffModel}
-dynamics_io.save_model(model_dict, savedir)
+    # fit model for diffusion term - SINDy based regression
+    diffModel = ps.SINDy(feature_library = diff_lib, optimizer = ps.SSR())
+    diffModel.fit(train_test_dict['X_train'],t=dt,x_dot=train_test_dict['V_train'],u=train_test_dict['u_train'])
 
-# %%
+    # score on test set
+    diff_R2 = diffModel.score(train_test_dict['X_test'],x_dot=train_test_dict['V_test'],u=train_test_dict['u_test'])
+    diffModel.print()
+
+    print('Coefficient of determination (R^2) for model of diffusion term: %f' %diff_R2)
+
+    ################### Save trained models ###################
+    model_dict = {'driftModel':driftModel,'diffModel':diffModel}
+    dynamics_io.save_model(model_dict, savedir)
+
+if __name__ == "__main__":
+    fire.Fire(main)
