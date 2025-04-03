c75f5569ed4d7b80b1fd40d26e5261b2df52a98d
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-04-03 13:21:09 -0700

    remove density work

--- cellsmap/analyses/workflows/density/support/calculate_density.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def get_nuclear_centroids(df, dataset, position, frame):
-    df_fov = df[(df['position'] == position) & (df['frame'] == frame)]
-    x = df_fov.x.values
-    y = df_fov.y.values
-    centroids = np.column_stack((x, y))
-    return centroids
