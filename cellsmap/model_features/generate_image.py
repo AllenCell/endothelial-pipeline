f5e248ae254707e54c2fc6ca6007cb88befef966
Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
2025-04-04 14:50:31 -0700

    convert list of lists to numpy array

--- cellsmap/model_features/generate_image.py
+++ cellsmap/model_features/generate_image.py
@@ -4,11 +4,12 @@ from typing import List
 from pathlib import Path
 from cellsmap.model_features.utils.mlflow_utils import load_mlflow_model
 from cellsmap.util.set_ouput import get_output_path
+import numpy as np
 
-
-def generate_from_coords(model_name, coords:List[List[float]], n_noise_samples=1, average=False):
+def generate_from_coords(model_name: str, coords:List[List[float]], n_noise_samples:int=1, average:bool=False):
+    coords = np.array(coords)
     mlflow_id = get_model_info(model_name)['mlflow_run_id']
-    model_path = get_output_path('models')
+    model_path = Path(get_output_path(f'models/{model_name}'))
     model = load_mlflow_model(mlflow_id, save_path=model_path)
 
     coords  = torch.from_numpy(coords).float()
