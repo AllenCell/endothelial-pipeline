31ae9fc6bd042a2a9dd3c1c1a3a941987cf4683e
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-04-04 15:07:37 -0700

    mypy checks

--- cellsmap/analyses/workflows/nuclear_based_features/plot/plot_nuclei.py
+++ cellsmap/analyses/workflows/nuclear_based_features/plot/plot_nuclei.py
@@ -5,15 +5,19 @@ from matplotlib.lines import Line2D
 from cellsmap.util import dataset_io
 import pandas as pd
 from typing import List
+import numpy as np
 
 def plot_flow_over_time_per_dataset(dataset_name_list: List[str], fig_savedir: str):
-    fig = plt.figure(figsize=(15, 10))
+    fig = plt.figure(figsize=(12, 10))
     colors = list(mcolors.TABLEAU_COLORS.values())
+    linestyles = ['-', '--', '-.', ':']
+    
     for i, dataset in enumerate(dataset_name_list):
         duration = dataset_io.get_dataset_duration_in_frames(dataset)
         frames = range(0, duration, 1)
         flows = [dataset_io.get_flow_for_frame(dataset, frame) for frame in frames]
-        plt.plot(frames, flows, label=dataset, color=colors[i], alpha=0.75, linewidth=2.5)
+        plt.plot(frames, flows, label=dataset, color=colors[i % len(colors)], 
+                 alpha=0.75, linewidth=2.5, linestyle=linestyles[i % len(linestyles)])
         
     plt.xlabel("Time (frames)")
     plt.ylabel("Flow (dyn/cm²)")
@@ -28,14 +32,15 @@ def plot_number_of_nuclei_per_fov(df: pd.DataFrame, dataset: str, fig_savedir: s
     added_labels = set()
 
     for frame, dft in df.groupby("frame"):
-        for position, dfp in dft.groupby("position"):
-            x = frame
+        assert isinstance(frame, np.int64)
+        x = frame
+        for idx, (position, dfp) in enumerate(dft.groupby("position")):
             y = dfp["nuclear_label"].nunique()
             if position not in added_labels and len(added_labels) < 6:
-                plt.scatter(x, y, alpha=0.5, color=colors[position], label=position)
+                plt.scatter(x, y, alpha=0.5, color=colors[idx % len(colors)], label=position)
                 added_labels.add(position)
             else:
-                plt.scatter(x, y, alpha=0.75, color=colors[position])
+                plt.scatter(x, y, alpha=0.75, color=colors[idx % len(colors)])
 
     plt.ylim(0, 350)
     plt.xlabel("Time (frames)")
@@ -62,8 +67,9 @@ def plot_number_of_nuclei_per_dataset(df_list: List[pd.DataFrame], fig_savedir: 
         print(f"Processing dataset {i+1}/{len(df_list)}: {dataset}")
 
         for frame, dft in df.groupby("frame"):
+            assert isinstance(frame, np.int64)
+            x = frame
             for position, dfp in dft.groupby("position"):
-                x = frame
                 y = dfp["nuclear_label"].nunique()
                 plt.scatter(x, y, alpha=0.4, color=colors[i])
 
