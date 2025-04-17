28290dcf71b0aec87a47a7d9b7870ca2101b1653
Serge Parent <serge.edouard.parent@gmail.com>
2025-04-16 18:06:29 -0700

    - finished modifying `track_validation.py`, including enabling multiprocessing

--- cellsmap/analyses/track_validation.py
+++ cellsmap/analyses/track_validation.py
@@ -2,28 +2,29 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# import seaborn as sns
-# from scipy.ndimage import gaussian_filter1d
-# from cellsmap.analyses.cdh5_nodes_and_edges_analysis import stringified_floatlist_to_floatlist
-# from matplotlib.colors import TwoSlopeNorm
-from cellsmap.util.dataset_io import extract_T, get_tracking_data_filtered, load_config
+from cellsmap.util.dataset_io import get_tracking_data_filtered, load_config, get_cdh5_classic_segmentation_path, get_dataset_info, ipython_cli_flexecute
 from cellsmap.util.set_output import get_output_path
+from cellsmap.util.general_image_preprocessing import get_dim_map, build_analysis_queue
 from bioio import BioImage
 from skimage import measure
 from skimage.color import label2rgb
 from skimage.exposure import rescale_intensity
+from skimage.segmentation import find_boundaries
+from multiprocessing import Pool
 from tqdm import tqdm
 
-def save_validation_images(roi, img_arr, C_labels, C_outlines, C_raws, out_dir, dataset_name, T, padding=50):
+def save_validation_images(roi, img_arr, seg_arr, out_dir, dataset_name, T, padding=50):
     track_id = roi.label
-    validation_subfolder = out_dir / dataset_name / str(track_id)
+    validation_subfolder = out_dir / str(track_id)
     Path.mkdir(validation_subfolder, exist_ok=True, parents=True)
 
     expanded_bbox = tuple([slice(max(0, sl.start - padding), sl.stop + padding) for sl in roi.slice])
 
-    crop = img_arr[(..., *expanded_bbox)].squeeze().compute()
-    track_of_interest = (crop[C_labels] == track_id * 1) + (crop[C_outlines] > 0) * 2
-    raw_img_crop = rescale_intensity(np.clip(crop[C_raws], 0, np.percentile(crop[C_raws], 98)), out_range=(0, 1))
+    crop_img = img_arr[(..., *expanded_bbox)].squeeze().compute()
+    crop_seg = seg_arr[(..., *expanded_bbox)].squeeze().compute()
+    crop_seg_outline = find_boundaries(crop_seg)
+    track_of_interest = (crop_seg == track_id * 1) + (crop_seg_outline > 0) * 2
+    raw_img_crop = rescale_intensity(np.clip(crop_img, 0, np.percentile(crop_img, 98)), out_range=(0, 1))
     overlay = label2rgb(label=track_of_interest, image=raw_img_crop, bg_label=0, colors=['magenta', 'cyan'])
 
     fig, ax = plt.subplots()
@@ -36,69 +37,51 @@ def save_validation_images(roi, img_arr, C_labels, C_outlines, C_raws, out_dir, 
     return
 
 
-def generate_and_save_validation_images(dframe, dataset_name, T, out_dir):
+def generate_and_save_validation_images(dframe):
+
+    # unpack needed variables
+    dataset_name = dframe['dataset_name'].unique()[0]
+    scene_index = int(dframe['scene_index'].unique()[0])
+    position = dframe['position'].unique()[0]
+    T = dframe['T'].unique()[0]
+    out_dir = dframe['out_dir'].unique()[0] / f'{dataset_name}/P{position}'
+
     print(f'Working on dataset {dataset_name}, T = {T}...')
-    # T = 0
-    # C = 0
-    C_labels, C_outlines, C_raws = 0, 1 ,2
 
-    img = BioImage(tracking_img_paths[dataset_name][T])
-    img_arr = img.get_image_dask_data('TCZYX')
+    raw_path = Path(get_dataset_info(dataset_name)['original_path'])
+    seg_path = Path(get_cdh5_classic_segmentation_path(dataset_name, position))
+    seg_path_list = list(seg_path.glob(f'*_T{T}.ome.tiff'))
+    if len(seg_path_list) == 0:
+        print(f'No segmentation file found for {dataset_name} at T{T}.')
+        return
+    elif len(seg_path_list) > 1:
+        print(f'Multiple segmentation files found for {dataset_name} at T{T}. Files are: {seg_path}. Skipping.')
+        return
+    else:
+        seg_path = Path(seg_path_list[0])
 
-    props = measure.regionprops(img_arr[:, C_labels, :, :, :].squeeze())
-    rois = [reg for reg in props if reg.label in dframe.query('T==@T')['track_id'].unique()]
-    # roi[0].slice
-    # ymin, xmin, ymax, xmax = 0, 0, img.dims.Y+1, img.dims.X+1
-    # test = list(zip((ymin, xmin, ymax, xmax), roi[0].bbox))
-    # [max(x) for x in test]
-    # ypad, xpad = 50, 50
-    # padding = np.array([-ypad, -xpad, ypad, xpad])
-    padding = 50
+        dim_order = 'TCZYX'
+        dim_map = get_dim_map(dim_order)
 
-    # np.array(roi[0].bbox)
-    # np.array(roi[0].bbox) + padding
+        seg = BioImage(seg_path)
+        seg_arr = seg.get_image_dask_data(dim_order).squeeze()
 
-    for roi in tqdm(rois):
-        save_validation_images(roi, img_arr, C_labels, C_outlines, C_raws, out_dir, dataset_name, T, padding=padding)
-    # list(tqdm(rois, total=len(rois), desc='Saving validation images...').map(lambda roi: save_validation_images(roi, img_arr, out_dir, dataset_name, T, padding=padding))
+        img = BioImage(raw_path)
+        img.set_scene(scene_index)
+        cdh5_channel = get_dataset_info(dataset_name)['egfp_channel_index']
+        img_arr = img.get_image_dask_data(dim_order).max(axis=dim_map['Z'], keepdims=True)
+        img_arr = img_arr[T, cdh5_channel, :, :, :].squeeze()
 
-    # for roi in rois:
-    #     track_id = roi.label
-    #     validation_subfolder = out_dir / dataset_name / str(track_id)
-    #     Path.mkdir(validation_subfolder, exist_ok=True, parents=True)
+        props = measure.regionprops(seg_arr)
+        rois = [reg for reg in props if reg.label in dframe[dframe['T']==T]['track_id'].unique()]
+        padding = 50
 
-    #     # for sl in roi.slice:
-    #         # print(sl.start)
-    #         # break
-    #         # slice(max(0, sl.start - padding), sl.stop + padding)
-
-    #     expanded_bbox = tuple([slice(max(0, sl.start - padding), sl.stop + padding) for sl in roi.slice])
-
-    #     crop = img_arr[(..., *expanded_bbox)].squeeze().compute()
-    #     track_of_interest = (crop[C_labels] == track_id * 1) + (crop[C_outlines] > 0) * 2
-    #     raw_img_crop = rescale_intensity(np.clip(crop[C_raws], 0, np.percentile(crop[C_raws], 98)), out_range=(0, 1))
-    #     overlay = label2rgb(label=track_of_interest, image=raw_img_crop, bg_label=0, colors=['magenta', 'cyan'])
-
-    #     fig, ax = plt.subplots()
-    #     ax.imshow(overlay)
-    #     ax.axis('off')
-    #     plt.tight_layout()
-    #     fig.savefig(validation_subfolder / f'{dataset_name}_track{track_id}_T{T}.png', bbox_inches='tight', pad_inches=0, dpi=180)
-    #     plt.close(fig)
-
-    # if T > 0: break
-    return
+        for roi in rois:
+            save_validation_images(roi, img_arr, seg_arr, out_dir, dataset_name, T, padding=padding)
+        return
 
 
-# filtered_track_data_dir = Path('//allen/aics/endothelial/morphological_features/analysis/track_filtering')
-# assert filtered_track_data_dir.exists(), f'Data directory {filtered_track_data_dir} not found.'
-
-tracking_img_dir = Path('//allen/aics/assay-dev/users/Serge/cellsmap_out/cdh5_classic_seg_tracking')
-assert tracking_img_dir.exists(), f'Data directory {tracking_img_dir} not found.'
-
-# tracking_img_paths = {dataset_path.name: {extract_T(fp): fp for fp in sorted(dataset_path.glob('tracked_images/*.tif*'), key=extract_T)} for dataset_path in tracking_img_dir.glob('*')}
-# tracking_df = pd.read_csv(filtered_track_data_dir / 'filtered_tracking_results.tsv', sep='\t')
-def main(dataset_name=None):
+def main(n_proc=1, dataset_name=None):
 
     out_dir = Path(get_output_path(Path(__file__).stem, verbose=False))
 
@@ -112,25 +95,41 @@ def main(dataset_name=None):
     else:
         dataset_name_list = [dataset_name]
 
+    analysis_queue = build_analysis_queue(dataset_name_list,
+                                          use_original_data=True,
+                                          out_dir=out_dir,
+                                          verbose=True)
+    analysis_queue_df = pd.DataFrame(analysis_queue)
     for dataset_name in dataset_name_list:
         tracking_df = get_tracking_data_filtered([dataset_name], as_dask=False)
-        break
 
-tracking_df = tracking_df.query('track_duration >= 168')
+        analysis_queue_sub = analysis_queue_df[analysis_queue_df['dataset_name'] == dataset_name]
+        position_scene_map = dict(zip(analysis_queue_sub['position'], analysis_queue_sub['scene_index']))
+        tracking_df['scene_index'] = tracking_df['position'].transform(lambda x: position_scene_map[x])
+        
+        tracking_df['out_dir'] = out_dir
 
-# track_id 65 from 20241016_20X is pretty long and might be good to check
-# dataset_name = '20241016_20X'
-# track_id = 65
-df_sub = tracking_df.query('dataset_name==@dataset_name')
+        min_track_duration = 120
+        tracking_df = tracking_df[tracking_df['track_duration'] >= min_track_duration]
 
-# for T in df_sub['T'].unique():
-for (dataset_name, T), df_subset in df_sub.groupby(['dataset_name', 'T']):
+        grp_nm_list, df_subset_list = zip(*tracking_df.groupby(['dataset_name', 'position', 'T']))
+        if n_proc > 1:
+            if __name__ == '__main__':
+                print('Using multiprocessing...')
+                with Pool(processes=n_proc) as pool:
+                    list(tqdm(pool.imap(generate_and_save_validation_images, df_subset_list), total=len(df_subset_list)))
+                    pool.close()
+                    pool.join()
+                print('Finished multiprocessing.')
+        else:
+            print('Using single processing...')
+            for df_subset in tqdm(df_subset_list, total=len(df_subset_list)):
+                generate_and_save_validation_images(df_subset)
+            print('Finished single processing.')
+    
+    print(f'\N{microscope} Done.')
 
-    print(dataset_name, T)
-    if T >= 0:
-        generate_and_save_validation_images(df_subset, dataset_name, T, out_dir)
 
-
-# NOTE a plotting test for distance travelled by the centroid
-# plt.plot(df_sub['T'], np.cumsum(df_sub['centroid_displacement']))
+if __name__ == '__main__':
+    ipython_cli_flexecute(main)
 
