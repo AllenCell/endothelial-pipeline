66078ca963b0973661a3981093891bb184438a37
Erin Angelini <erin.angelini@alleninstitute.org>
2025-04-24 16:21:55 -0700

    checkout

--- cellsmap/analyses/workflows/stochastic_dynamics/full_latent_dynamics.py
+++ cellsmap/analyses/workflows/stochastic_dynamics/full_latent_dynamics.py
@@ -1,9 +1,13 @@
 # %%
+import numpy as np
+import pysindy as ps
+import numdifftools as nd
+
 from cellsmap.util import manifest_io
 from cellsmap.util.set_output import get_output_path
-from cellsmap.analyses.utils import regression_helper as rh
+from cellsmap.analyses.utils import regression_helper as rh, model_eval
 from cellsmap.analyses.utils.io import dynamics_io
-from cellsmap.analyses.utils.viz import manifest_viz, viz_base as vb
+from cellsmap.analyses.utils.viz import manifest_viz, viz_base as vb,pplane
 
 # %%
 # get output subdirectory for intermediate workflow outputs (set in config file dynamics_config.yaml)
@@ -42,14 +46,83 @@ for ds_name in list_of_datasets:
 ds_to_analyze = "20250319_20X"
 df_ = df[df['dataset_name'] == ds_to_analyze].copy()
 df_ = manifest_io.add_crop_index(df_)
-latent_dims = ['3','5']
+df_.sort_values(by=['crop_index','T'],inplace=True)
+latent_idxs = [0,2,3,6]
+latent_dims = [str(i) for i in latent_idxs]
 
 X_list, dX_list, dT_list = rh.get_X_dX_and_dT(df_, latent_dims)
+
 # %%
-Nbins = [100,100]
+Nbins = [40 for _ in range(len(latent_dims))]
 bins, centers = rh.get_bins(Nbins,data=X_list)
+kernel_params = {'bandwidth': 0.1,'kernel': 'gaussian'}
 
 # get drift and diffusion estimates (Kramers-Moyal coefficients)
-f_KM_, D_KM_ = rh.get_kramers_moyal(X_list,dX_list,dT_list,bins,dt=5)
+f_KM_, D_KM_ = rh.get_kramers_moyal(X_list,dX_list,dT_list,bins,dt=5,kernel_params=kernel_params)
+
+f_KM_noNAN, X_pts_, = rh.masked_vector_field(f_KM_, np.array(np.meshgrid(*centers)).T)
+D_KM_noNAN, _ = rh.masked_vector_field(D_KM_, np.array(np.meshgrid(*centers)).T)
+
+del f_KM_, D_KM_
+
+X_train, X_test, Y_train, Y_test, V_train, V_test = rh.train_test_all([X_pts_],[f_KM_noNAN],[D_KM_noNAN])
+
+del f_KM_noNAN, D_KM_noNAN, X_pts_
+# %%
+# for fitting model of drift and diffusion terms
+drift_lib = ps.PolynomialLibrary(degree=4, include_bias=True)
+
+diff_lib = ps.PolynomialLibrary(degree=4, include_bias=True)
+################### Fit SINDy models ###################
+
+# fit model for drift term - SINDy based regression
+driftModel = ps.SINDy(feature_library = drift_lib, optimizer = ps.SSR())
+driftModel.fit(X_train,t=5,x_dot=Y_train)
+
+# score on test set
+drift_R2 = driftModel.score(X_test,x_dot=Y_test)
+driftModel.print()
+
+print('Coefficient of determination (R^2) for model of drift term: %f' %drift_R2)
+
+# fit model for diffusion term - SINDy based regression
+diffModel = ps.SINDy(feature_library = diff_lib, optimizer = ps.SSR())
+diffModel.fit(X_train,t=5,x_dot=V_train)
+
+# score on test set
+diff_R2 = diffModel.score(X_test,x_dot=V_test)
+diffModel.print()
+
+print('Coefficient of determination (R^2) for model of diffusion term: %f' %diff_R2)
+# %%
+del X_train, X_test, Y_train, Y_test, V_train, V_test
+# %%
+f = model_eval.vector_field_function(driftModel)
+D = model_eval.vector_field_function(diffModel)
+
+myModel = [f,D]
+
+# save model to output directory
+model_dict = {'driftModel':driftModel,'diffModel':diffModel}
+dynamics_io.save_model(model_dict, savedir)
+# %%
+# get initial conditions by sub sampling points from centers
+centers_coarse = [centers[i][::10] for i in range(len(centers))]
+init_coarse = np.array(np.meshgrid(*centers_coarse)).T.reshape(-1,len(latent_dims))
 
 # %%
+fpts = pplane.get_fps(f,init_coarse) # get fixed points
+# %%
+fpts_new = []
+fpt_types = []
+flowJacobian = nd.Jacobian(f)
+
+# define Jacobian as a function of x - for getting stability:
+for fpt in fpts:
+    J = flowJacobian(fpt)
+    eigvals = np.linalg.eigvals(J)
+    # check for stability of fixed points
+    if np.all(np.real(eigvals) < 0):
+        fptStability = 'Stable'
+        print(f'  • {fptStability} at x = {[fpt_i for fpt_i in fpt]}')
+        fpts_new.append(fpt)
