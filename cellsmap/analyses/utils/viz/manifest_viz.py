c8a5d1d683e7bc51708b2bd548316d683e92a9b1
Erin Angelini <erin.angelini@alleninstitute.org>
2025-04-24 12:12:25 -0700

    initial loading and viz

--- cellsmap/analyses/utils/viz/manifest_viz.py
+++ cellsmap/analyses/utils/viz/manifest_viz.py
@@ -1,66 +1,40 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.pipeline import Pipeline
 from typing import Tuple
 
 import cellsmap.analyses.utils.viz.viz_base as vb
-import cellsmap.util.manifest_io as mio
+from cellsmap.analyses.utils.viz.pca_viz import * # pca_viz gets loaded as a submodule of manifest_viz
 
-def plot_explained_variance(explained_variance_ratio:np.ndarray) -> Tuple:
+def plot_latent_component_mean(feats:np.ndarray) -> Tuple:
     '''
-    Plot explained variance ratio of PCA components.
-    
+    Plot mean values of latent components for a gven dataset.     
+    At each frame in the dataset, takes the mean and standard 
+    deviation of the feature data over all crops. Then plots 
+    the mean and standard deviation of the feature data over 
+    all frames in the dataset.
+
     Input:
-    - explained_variance_ratio: np.ndarray, explained variance ratio of PCA components
+    - feats: np.ndarray, feature data for a single dataset
+        - shape (num_crops, num_frames, num_features)
 
     Output:
     - fig: plt.Figure
     - ax: plt.Axes
     '''
-    fig, ax = vb.init_plot() # initialize figure and axes
+    # right now, this function is only used for 8D latent space
+    assert feats.shape[-1] == 8, 'Number of latent components must be 8'
 
-    # plot explained variance ratio
-    n_components = len(explained_variance_ratio)
-    ax.plot(np.arange(1,n_components+1),np.cumsum(explained_variance_ratio),'k-o')
-    ax.plot(np.arange(1,n_components+1),0.95*np.ones(n_components),'r--', alpha=0.8) # 95% explained variance line
-    ax.set_xlabel('Number of components')
-    ax.set_ylabel('Cumulative explained variance')
-    ax.set_title('Explained variance ratio of PCA components')
-
-    return fig, ax
-
-def plot_top_3_PCs(feats_proj:np.ndarray,fig_ax:Tuple|None=None) -> Tuple:
-    '''
-    Plot Diffusion AE feature data from a dataset along the top 3 principal components.
-    At each frame in the dataset, takes the mean and standard deviation of the feature data 
-    projected onto the top 3 PCs over all crops. Then plots the mean and standard deviation
-    of the feature data projected onto each PC over all frames in the dataset.
-
-    Input:
-    - feats_proj: np.ndarray, feature data projected onto the top 3 PCs for a single dataset
-    - fig_ax: tuple (default=None), tuple of plt.Figure and plt.Axes objects to plot on
-        - if None, initializes a new figure and axes
-    
-    Output:
-    - fig: plt.Figure
-    - ax: plt.Axes
-    '''
-    # initialize figure and axes, if not provided
-    if fig_ax is None:
-        fig, ax = vb.init_subplots(1,3,figsize=(15,5))
-    else:
-        fig, ax = fig_ax
-    assert len(ax) == 3, 'Number of subplots must be 3'
+    fig, ax = vb.init_subplots(4,2,figsize=(15,20))
 
     # get mean and standard deviation of feature data projected onto top 3 PCs
     # mean and standard deviation taken over all crops at each timepoint
-    num_T = feats_proj.shape[1]
-    st_dev = np.std(feats_proj,axis=0)
-    mean_feats = np.mean(feats_proj,axis=0)
+    num_T = feats.shape[1]
+    # take standard deviation and mean over all crops at each timepoint
+    st_dev = np.std(feats,axis=0)
+    mean_feats = np.mean(feats,axis=0)
 
     # loop over PCs, plot mean and standard deviation of feature data projected onto each PC
-    for col, ax_ in enumerate(ax): # len(ax) = 3
+    for col, ax_ in enumerate(ax.flatten()):
         # plot mean values
         ax_.plot(np.arange(num_T),mean_feats[:,col],'k-')
 
@@ -70,106 +44,32 @@ def plot_top_3_PCs(feats_proj:np.ndarray,fig_ax:Tuple|None=None) -> Tuple:
                         color='k',alpha=0.5)
         
         # set axis labels and title
-        ax_.set_title(f'PC{col+1}')
+        ax_.set_title(f'Latent dimension {col+1}')
         ax_.set_xlabel('Frame number')
-
+    
+    fig.subplots_adjust(hspace=0.5)
     return fig, ax
 
-def plot_top_3_PCs_alldata(df:pd.DataFrame,pca:Pipeline) -> Tuple:
+def plot_latent_component_histogram(feats:np.ndarray) -> Tuple:
     '''
-    Plot projection of feature data from all datasets along the top 3 principal components.
-
-    For each dataset, projects the feature data onto the top 3 PCs, gets the mean and standard deviation
-    over all crops at each frame, and plots this mean and standard deviation vs. frame number for each PC.
-    Calls plot_top_3_PCs() to plot the data for each dataset.
-
-    TO DO: set y-axis limits to be the same for all subplots (tbd based on inputs or data)
+    Plot histogram of latent components for a given dataset.
+    At each frame in the dataset, computes the histogram of the
+    crops for each latent component. Then plots the histogram
+    for each latent component as a function of time.
 
     Input:
-    - df: pd.DataFrame, the manifest dataframe containing feature data for multiple datasets
-    - pca: Pipeline, the PCA model used to project the feature data onto the top 3 PCs
-        - can include any preprocessing steps before PCA, such as scaling
-    
+    - feats: np.ndarray, feature data for a single dataset
+        - shape (num_crops, num_frames, num_features)
+
     Output:
     - fig: plt.Figure
     - ax: plt.Axes
     '''
-    # plot top 3 PCs for each dataset in one figure (each row is a dataset)
-    list_of_datasets = mio.get_list_of_datasets(df)
-    title_dict = mio.get_descriptive_metadata(df) # get description of dataset by flow conditions, for title of subfig
+    # right now, this function is only used for 8D latent space
+    assert feats.shape[-1] == 8, 'Number of latent components must be 8'
 
-    # initialize figure with subfigures for each dataset
-    n_ = len(list_of_datasets)
-    fig = plt.figure(figsize=(15,5*n_),constrained_layout=True)
-    subfigs = fig.subfigures(nrows=n_, ncols=1) # create n_ subfigures, one for each dataset (will add columns in the loop)
+    fig, ax = vb.init_subplots(4,2,figsize=(15,20))
 
-    # for setting plot limits, initialize to 0 (will be updated in the loop)
-    y_lims = [[0,0],[0,0],[0,0]] # y-limits for each PC
-    # loop over datasets, project feature data onto top 3 PCs, and plot
-    for row, subfig in enumerate(subfigs):
-        ds_name = list_of_datasets[row] # get the dataset name
-        df_proj = mio.project_PCA_one_dataset(df,pca,ds_name) # project the dataset onto the PCA space
-        PCs = [str(i) for i in range(3)] # top 3 PCs
-        feats_proj = mio.df_to_array(df_proj,PCs) # get the feature data projected onto the top 3 PCs
-
-        for j in range(3):
-            if ds_name == '20241203_20X': # dataset with bubbles, outliers will skew the y-limits
-                continue
-            # get y-limits for each PC
-            y_lims[j][0] = min(y_lims[j][0],np.min(feats_proj[...,j]))
-            y_lims[j][1] = max(y_lims[j][1],np.max(feats_proj[...,j]))
-
-        subfig.suptitle(title_dict[ds_name],fontsize=26) # title of subfig: description of dataset by flow conditions
-
-        # create 1x3 subplots per subfig
-        axs = subfig.subplots(nrows=1, ncols=3)
-        
-        # plot top 3 PCs for the dataset
-        fig, axs = plot_top_3_PCs(feats_proj,fig_ax=(fig,axs))
-    
-    # set y-limits for each PC across all datasets
-    for j in range(3):
-        for row, subfig in enumerate(subfigs):
-            axs = subfig.axes[j]
-            axs.set_ylim(y_lims[j][0],y_lims[j][1]) # set y-limits for each PC
-
-    return fig, axs
-
-def plot_PCA_projection_2D(feats_proj:np.ndarray,fig_title:str|None=None,fig_ax:Tuple|None=None) -> Tuple:
-    '''
-    Plot mean values of projected feature data onto the top 2 PCs for each frame in the dataset.
-
-    Input:
-    - feats_proj: np.ndarray, feature data projected onto the top 2 PCs for a single dataset
-    - fig_title: str (default=None), title of the figure
-    - fig_ax: tuple (default=None), tuple of plt.Figure and plt.Axes objects to plot on
-        - if None, initializes a new figure and axes
-    
-    Output:
-    - fig: plt.Figure
-    - ax: plt.Axes
-    '''
-    # initialize figure and axes, if not provided
-    if fig_ax is not None:
-        fig, ax = fig_ax
-    else:
-        fig, ax = vb.init_plot()
-
-    # get mean values of feature data projected onto top 2 PCs
-    # mean taken over all crops at each timepoint
-    num_T = feats_proj.shape[1]
-    mean_feats = np.mean(feats_proj,axis=0)
-
-    # plot mean values, color coded by frame number (timepoint)
-    ax.scatter(mean_feats[:,0],mean_feats[:,1],c = range(num_T),cmap='jet')
-
-    # set axis labels and title
-    ax.set_xlabel('PC1')
-    ax.set_ylabel('PC2')
-    if fig_title is not None:
-        ax.set_title(fig_title)
-
-    return fig, ax
 
 def plot_km(centers:list[np.ndarray],kmc:np.ndarray,PCs:list[int],shear_stress:float) -> Tuple:
     '''
---
+++ cellsmap/analyses/workflows/stochastic_dynamics/full_latent_dynamics.py
@@ -0,0 +1,36 @@
+# %%
+from cellsmap.util import manifest_io
+from cellsmap.util.set_output import get_output_path
+from cellsmap.analyses.utils import regression_main
+from cellsmap.analyses.utils.io import dynamics_io
+from cellsmap.analyses.utils.viz import manifest_viz, viz_base as vb
+
+# %%
+# get output subdirectory for intermediate workflow outputs (set in config file dynamics_config.yaml)
+# if directory does not exist, get_output_path function will create it
+workflow_name = "full_latent_dynamics"
+workflow_output_folder = f"stochastic_dynamics/{workflow_name}/outputs"
+savedir = get_output_path(workflow_output_folder)
+
+# get output subdirectory for figures that workflow outputs (set in config file dynamics_config.yaml)
+# if directory does not exist, get_output_path function will create it
+workflow_fig_folder = f"stochastic_dynamics/{workflow_name}/figs"
+fig_savedir = get_output_path(workflow_fig_folder)
+
+# load manifest to DataFrame with metadata
+df = manifest_io.load_manifest_to_df()
+
+# %%
+feat_cols = [str(i) for i in range(8)]
+
+list_of_datasets = manifest_io.get_list_of_datasets(df,verbose=False)
+
+for ds_name in list_of_datasets:
+    print(f"Processing dataset: {ds_name}")
+    df_ds = df[df['dataset_name'] == ds_name].copy()
+    df_ds = manifest_io.add_crop_index(df_ds)
+    feats = manifest_io.df_to_array(df_ds, feat_cols)
+    fig, ax = manifest_viz.plot_latent_component_mean(feats)
+    fig.suptitle(f"Dataset: {ds_name}")
+    vb.save_plot(fig,f"{fig_savedir}/{ds_name}_latent_mean")
+# %%
