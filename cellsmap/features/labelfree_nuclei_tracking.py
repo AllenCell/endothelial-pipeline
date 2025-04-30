4ed0fc539f07d90c301535b7c08e208353451b0d
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-04-30 14:37:09 -0700

    mypy error

--- cellsmap/features/labelfree_nuclei_tracking.py
+++ cellsmap/features/labelfree_nuclei_tracking.py
@@ -8,12 +8,10 @@ out_dir = get_output_path('tracking_output', verbose=False)
 dataset_name = '20241120_20X'
 
 nuclei_dir = Path(get_nuclear_prediction_path(dataset_name, 0))
-nuclei_paths = sorted(nuclei_dir.glob('*.ome.tif*'), key=lambda fp: extract_T(fp.name))
-nuclei_paths = nuclei_paths
-
+in_dir = sorted(nuclei_dir.glob('*.ome.tif*'), key=lambda fp: extract_T(fp.name))
 
 run_tracking(
-    in_dir=nuclei_paths,
+    in_dir=in_dir,
     out_dir=Path(out_dir),
     out_filename_prefix=f'{dataset_name}_P0',
     tracking_metrics=['centroid'],
