755135a5e8a66b937da64d46cf20d15506620e78
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-05-02 16:47:10 -0700

    image functions

---
+++ cellsmap/vis/get_images.py
@@ -0,0 +1,49 @@
+from cellsmap.util import dataset_io
+from pathlib import Path
+from bioio import BioImage
+from typing import Literal
+import dask.array as da
+
+
+def get_zarr_img_for_dataset(
+    dataset: str, position: int, resolution_level: Literal[0, 1] = 1
+) -> BioImage:
+    """
+    Retrieve the BioImage object for a given dataset and position.
+    """
+    zarr_name = dataset_io.get_zarr_name(dataset, position)
+    zarr_path = dataset_io.get_zarr_dir(dataset)
+    filepath = Path(zarr_path) / zarr_name
+    img = BioImage(filepath)
+    img.set_resolution_level(resolution_level)
+    return img
+
+
+def get_timepoint(
+    img: BioImage,
+    timepoint: int,
+) -> da.Array:
+    """
+    Get the image data for a specific timepoint.
+    """
+    img_timepoint = img.get_image_dask_data("TCZYX", T=timepoint)
+    return img_timepoint
+
+
+def get_crop(
+    img: BioImage,
+    channel: int | list | None,
+    timepoint: int | None,
+    start_x: int,
+    start_y: int,
+    crop_size_x: int,
+    crop_size_y: int,
+) -> da.Array:
+    img_crop = img.get_image_dask_data(
+        "TCZYX",
+        T=timepoint,
+        channel=channel,
+        Y=slice(start_y, start_y + crop_size_y),
+        X=slice(start_x, start_x + crop_size_x),
+    )
+    return img_crop
