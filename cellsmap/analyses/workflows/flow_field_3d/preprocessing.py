d7326e353a30374b62daaabff623edb3bb20ae93
Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
2025-05-05 14:13:46 -0700

    isort preprocessing

--- cellsmap/analyses/workflows/flow_field_3d/preprocessing.py
+++ cellsmap/analyses/workflows/flow_field_3d/preprocessing.py
@@ -10,7 +10,9 @@ from cellsmap.util import manifest_io
 from cellsmap.util.manifest_preprocessing import (
     diffae_feature_preprocessing as diffae_preproc,
 )
-from cellsmap.util.manifest_preprocessing import manifest_pca
+from cellsmap.util.manifest_preprocessing import (
+    manifest_pca,
+)
 from cellsmap.util.set_output import get_output_path
 
 # %%
