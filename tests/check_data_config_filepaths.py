1ae9bcd2755eef487bcc60f3ad49c6c7f85da887
Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
2025-05-06 13:11:48 -0700

    update tests

--- tests/check_data_config_filepaths.py
+++ tests/check_data_config_filepaths.py
@@ -1,102 +1,35 @@
-# %%
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 from bioio import BioImage
-from tqdm import tqdm
 
 from cellsmap.util import dataset_io
 
-# %%
-print("Available datasets:")
-dataset_name_list = dataset_io.get_available_datasets()
-print("\n")
 
-# %% Test opening original data file
-for dataset_name in tqdm(
-    dataset_name_list,
-    total=len(dataset_name_list),
-    desc="Opening original files",
-    unit="dataset",
-):
+@pytest.mark.parameterize(
+    "dataset_name", dataset_io.get_available_datasets(verbose=False)
+)
+def check_open_original_data(dataset_name: str) -> bool:
     config_data = dataset_io.get_dataset_info(dataset_name)
     original_path = Path(config_data["original_path"])
     try:
-        img = BioImage(original_path)
+        BioImage(original_path)
     except Exception as e:
         print(f"Failed to open original for dataset {dataset_name}: {e}")
+        return False
+    return True
 
 
-# %% Test opening zarr file
-def get_position_zarr_path(zarr_path, position):
-    name_fmsid = Path(zarr_path).name
-    position_path = f"{zarr_path}/{name_fmsid}_P{position}.ome.zarr"
-    return position_path
+@pytest.mark.parameterize(
+    "dataset_name", dataset_io.get_available_datasets(verbose=False)
+)
+def check_open_zarr_data(dataset_name: str) -> bool:
+    zarr_paths = dataset_io.get_zarr_path(dataset_name)
+    assert len(zarr_paths) > 0, f"No zarr paths found for dataset {dataset_name}"
 
-
-for dataset_name in tqdm(
-    dataset_name_list,
-    total=len(dataset_name_list),
-    desc="Opening zarr files",
-    unit="dataset",
-):
-    config_data = dataset_io.get_dataset_info(dataset_name)
-    zarr_path = Path(config_data["zarr_path"])
-
-    if "scene_list" in config_data and config_data["scene_list"]:
-        number_of_positions = len(config_data["scene_list"])
-    else:
-        number_of_positions = dataset_io.get_total_number_of_positions(dataset_name)
-
-    try:
-        # Check if zarr_path is 'none' and skip processing if true
-        if str(zarr_path).lower() == "none":
-            print(f"Zarr path does not exist for {dataset_name}")
-            continue  # Skip to the next dataset
-
-        # Process positions if zarr_path is valid
-        for position in range(number_of_positions):
-            position_path = Path(get_position_zarr_path(zarr_path, position))
-            img = BioImage(position_path)
-    except Exception as e:
-        print(f"Failed to open zarr for dataset {dataset_name}: {e}")
-
-# %% Quickly visualize crop in first position, first timepoint of each zarr to confirm channel order is correct
-for dataset_name in dataset_name_list:
-    print(f"Dataset: {dataset_name}")
-    config_data = dataset_io.get_dataset_info(dataset_name)
-    zarr_path = Path(config_data["zarr_path"])
-
-    position_path = get_position_zarr_path(zarr_path, 0)
-    img = BioImage(position_path)
-    print(f"Image shape: {img.shape}")
-    n_channels = img.shape[1]
-
-    def get_channel_crop(img, T, C, crop_size=(128, 128)):
-        """Helper function to get cropped data for a specific channel."""
-        return img.get_image_dask_data("ZYX", T=T, C=C)[
-            :,  # Keep all Z-slices
-            0 : crop_size[0],  # Crop along Y-axis
-            0 : crop_size[1],  # Crop along X-axis
-        ]
-
-    # Compute projections for all channels
-    channel_projections = []
-    for c in range(n_channels):
-        channel = get_channel_crop(img, T=0, C=c)
-        if c == 1:  # Special case for Channel 1 (BF): use center slice
-            projection = channel[channel.shape[0] // 2, :, :]
-        else:  # Default: use max projection
-            projection = channel.max(axis=0)
-        channel_projections.append(projection)
-
-    # Plot all channels
-    fig, axes = plt.subplots(1, n_channels, figsize=(6 * n_channels, 6))
-    if n_channels == 1:
-        axes = [axes]  # Ensure axes is iterable for a single channel
-    for c, ax in enumerate(axes):
-        ax.imshow(channel_projections[c], cmap="gray")
-        ax.set_title(f"{dataset_name} - Channel {c}")
-    plt.show()
-
-# %%
+    for name, zarr_path in zarr_paths.items():
+        try:
+            BioImage(zarr_path)
+        except Exception as e:
+            print(f"Failed to open zarr for dataset {dataset_name} {name}: {e}")
+            return False
+    return True
