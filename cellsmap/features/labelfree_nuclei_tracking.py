a6369b5381703c1186fc0781621a993836d62d83
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-05-06 17:37:01 -0700

    revert

--- cellsmap/features/labelfree_nuclei_tracking.py
+++ cellsmap/features/labelfree_nuclei_tracking.py
@@ -8,12 +8,11 @@ out_dir = get_output_path("tracking_output", verbose=False)
 dataset_name = "20241120_20X"
 
 nuclei_dir = Path(get_nuclear_prediction_path(dataset_name, 0))
-nuclei_paths = sorted(nuclei_dir.glob("*.ome.tif*"), key=lambda fp: extract_T(fp.name))
+nuclei_paths = sorted(nuclei_dir.glob('*.ome.tif*'), key=lambda fp: extract_T(fp.name))
 nuclei_paths = nuclei_paths
 
-
 run_tracking(
-    in_dir=nuclei_dir,
+    in_dir=nuclei_paths,
     out_dir=Path(out_dir),
     out_filename_prefix=f"{dataset_name}_P0",
     tracking_metrics=["centroid"],
