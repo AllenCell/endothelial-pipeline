79d7c9a310d3ae9a2da8f99a45eacb25c233f5fc
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-05-15 17:51:42 -0700

    update workflow with new dataset

--- cellsmap/analyses/immunofluorescence/crop_based_if_workflow.py
+++ cellsmap/analyses/immunofluorescence/crop_based_if_workflow.py
@@ -14,20 +14,14 @@ from cellsmap.analyses.immunofluorescence.if_support.plots import (
 )
 from cellsmap.util import manifest_io, set_output
 
-# from cellsmap.util.manifest_preprocessing.fms_upload import
-
 # %%
-DATASET = "20250122_SMAD1"
+DATASET = "20250509_20X_IF1"
 RES_LEVEL = 0
 output_dir = set_output.get_output_path("immunoflourescence_analysis")
-df_manifest = manifest_io.get_diffae_manifest(DATASET)
+df = manifest_io.get_diffae_manifest(DATASET)
 
-# %% Filter FOVs that crop nuclei in Z
-df_filtered = df_manifest[df_manifest["position"].isin(["P5", "P6", "P7", "P8", "P9"])]
-df = df_filtered.reset_index(drop=True)  # Reset the index and drop the old one
 
-# %%
-for channel in ["DAPI", "SMAD1"]:  # "NR2F2", "SOX17"
+for channel in ["NucViolet", "SMAD1", "SOX17"]:  # "NR2F2", "DAPI"
     df = add_if_cols_to_df(
         df,
         channel_name=channel,
@@ -35,58 +29,57 @@ for channel in ["DAPI", "SMAD1"]:  # "NR2F2", "SOX17"
     )
 # %%
 # Filter crop outlier with bright puncta outlier
-MARKER = "SMAD1"
-df = df[df[f"crop_cyto_mean_intensity_{MARKER}"] < 3000]
-# # Save the updated DataFrame to a new CSV file
-# df.to_csv(output_dir + f"{DATASET}_IF_results.csv", index=False)
+for marker in ["SMAD1", "SOX17"]:
 
+    for feature, xlim in [
+        # (f"crop_nuc_mean_intensity_{marker}", None),
+        # (f"crop_cyto_mean_intensity_{marker}", None),
+        # (f"crop_nuc_to_cyto_mean_ratio_{marker}", None),
+        (f"crop_nuc_median_intensity_{marker}", None),
+        (f"crop_cyto_median_intensity_{marker}", None),
+        (f"crop_nuc_to_cyto_median_ratio_{marker}", None),
+    ]:
+        plot_intensity_distribution(
+            df,
+            xlabel=feature,
+            dataset=DATASET,
+            output_dir=output_dir,
+            xlim=xlim,
+        )
 
-# %% Visualize the intensity distributions
-for feature, xlim in [
-    (f"crop_nuc_mean_intensity_{MARKER}", 2600),
-    (f"crop_cyto_mean_intensity_{MARKER}", 2600),
-    (f"crop_nuc_median_intensity_{MARKER}", 2600),
-    (f"crop_cyto_median_intensity_{MARKER}", 2600),
-    (f"crop_nuc_to_cyto_mean_ratio_{MARKER}", 4),
-    (f"crop_nuc_to_cyto_median_ratio_{MARKER}", 4),
-]:
-    plot_intensity_distribution(
-        df,
-        xlabel=feature,
-        dataset=DATASET,
-        output_dir=output_dir,
-        xlim=xlim,
+    index = 2
+    row = df.iloc[index]
+
+    seg_mask = get_segmentation_mask_crop(row, resolution_level=RES_LEVEL, channel=0)
+
+    dapi_crop = get_raw_intensity_crop(
+        row, resolution_level=RES_LEVEL, channel_name="NucViolet"
     )
-# %% Visualize resulting images and intensity
-index = 2
-row = df.iloc[index]
+    background_subtracted_dapi_crop = background_subtract(dapi_crop, camera_offset=100)
+    sum_proj_dapi_img = sum_projection(background_subtracted_dapi_crop)
 
-seg_mask = get_segmentation_mask_crop(row, resolution_level=RES_LEVEL, channel=0)
+    raw_crop = get_raw_intensity_crop(
+        row, resolution_level=RES_LEVEL, channel_name=marker
+    )
+    background_subtracted_crop = background_subtract(raw_crop, camera_offset=100)
+    sum_proj_img = sum_projection(background_subtracted_crop)
 
-dapi_crop = get_raw_intensity_crop(row, resolution_level=RES_LEVEL, channel_name="DAPI")
-background_subtracted_dapi_crop = background_subtract(dapi_crop, camera_offset=100)
-sum_proj_dapi_img = sum_projection(background_subtracted_dapi_crop)
-
-raw_crop = get_raw_intensity_crop(row, resolution_level=RES_LEVEL, channel_name=MARKER)
-background_subtracted_crop = background_subtract(raw_crop, camera_offset=100)
-sum_proj_img = sum_projection(background_subtracted_crop)
-
-projection_image(
-    sum_proj_img,
-    seg_mask,
-    row.dataset,
-    row.position,
-    row.start_x,
-    row.start_y,
-    str(output_dir),
-)
-projection_image(
-    sum_proj_dapi_img,
-    seg_mask,
-    row.dataset,
-    row.position,
-    row.start_x,
-    row.start_y,
-    str(output_dir),
-)
-# %%
+    projection_image(
+        sum_proj_img,
+        seg_mask,
+        row.dataset,
+        row.position,
+        row.start_x,
+        row.start_y,
+        str(output_dir),
+    )
+    projection_image(
+        sum_proj_dapi_img,
+        seg_mask,
+        row.dataset,
+        row.position,
+        row.start_x,
+        row.start_y,
+        str(output_dir),
+    )
+    # %%
