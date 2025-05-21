07af17b0b5b0e047a14e260a78c2ecf0fc49d7da
Benjamin Morris <benjamin.morris@prd-aics-dcp-038.corp.alleninstitute.org>
2025-05-21 12:53:26 -0700

    move include scenes function

--- cellsmap/image_conversion/raw_img_to_zarr.py
+++ cellsmap/image_conversion/raw_img_to_zarr.py
@@ -5,13 +5,13 @@ from bioio import BioImage
 
 from cellsmap.image_conversion.process_images.process_image import (
     get_delayed_array_for_position,
+    get_included_scenes,
 )
 from cellsmap.image_conversion.process_images.write_zarr import (
     get_sldy_pixel_sizes,
     write_scene,
 )
 from cellsmap.util.dataset_io import (
-    get_dataset_info,
     get_fmsid,
     get_microscope,
     get_original_path,
@@ -49,17 +49,6 @@ Example (using API):
 This will process the dataset '20240305_T01_001' and save the output to the specified directory.
 The resulting zarr contains images from one scene.
 """
-
-
-def get_included_scenes(dataset_name: str) -> list:
-    dataset_info = get_dataset_info(dataset_name)
-    include_scenes = dataset_info.get("include_scenes")
-
-    # by default, include all scenes
-    if include_scenes is None:
-        include_scenes = range(len(BioImage(dataset_info["original_path"]).scenes))
-
-    return include_scenes
 
 
 def convert_dataset(
