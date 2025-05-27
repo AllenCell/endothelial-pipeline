17a3aa245a1d1cd4870fed6c211f6f6edad69e4e
Serge Parent <serge.edouard.parent@gmail.com>
2025-05-26 18:23:07 -0700

    - added some code to generate plots for SAC

--- cellsmap/analyses/immunofluorescence/integration_workflow.py
+++ cellsmap/analyses/immunofluorescence/integration_workflow.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 from cellsmap.analyses.immunofluorescence.if_support.add_if_cols import (
     add_if_cols_to_df,
@@ -11,6 +13,7 @@ from cellsmap.analyses.immunofluorescence.if_support.add_if_cols import (
 from cellsmap.analyses.integration.feats_diffae_classic_comparison import (
     get_traj_and_flowfield,
     plot_measured_feat_overlay_on_flowfield,
+    plot_quiver_slices_from_diffae_table,
 )
 from cellsmap.analyses.utils.numerics import data_driven_flow_field as ddff
 from cellsmap.util import manifest_io
@@ -146,4 +149,114 @@ for condition in conditions:
                 alpha=1.0,
                 hue_min_max=None,
             )
-    # %%
+
+# %%
+# generate plots for the SAC-2025 slides
+for condition in conditions:
+    if "20250509_20X_IF9" in condition["corresponding_if_datasets"]:
+        break
+
+dataset_name = str(condition["ref_dataset_name"])
+flow_regime = condition["flow_regime"]
+corresponding_if_datasets = condition["corresponding_if_datasets"]
+
+print(f"Processing dataset: {dataset_name}, Flow regime: {flow_regime}")
+
+output_dir = get_output_path(f"immunoflourescence_analysis_integration/SAC_2025")
+diffae_grid_crops = get_manifest_for_dynamics_workflows(dataset_name, pca)
+print("getting trajectory and flow field for grid-based crops...")
+traj_grids, flow_field_dict_grids = get_traj_and_flowfield(
+    diffae_grid_crops, bounds, col_names="feat"
+)
+
+if_dataset_name = "20250509_20X_IF9"  # SAC-2025 slide dataset
+
+df_condition = df_if[df_if["dataset"].isin(corresponding_if_datasets)]
+
+df_if_dataset = df_if[df_if["dataset"] == if_dataset_name]
+
+df_if_dataset = project_manifest_to_pcs(
+    df_if_dataset, pca, overwrite_feature_columns=False
+)
+
+# this is where we would apply a PC correction to PC1, 2 and 3
+
+channels = get_channels_for_if_processing(if_dataset_name)
+print(f"Plotting {if_dataset_name}")
+
+channel = "SMAD1"
+
+feature = f"crop_nuc_mean_intensity_{channel}"
+hue_min = df_condition[feature].min()
+hue_max = df_condition[feature].max()
+
+# plot and save just the flow field first
+Path(output_dir).mkdir(exist_ok=True, parents=True)
+
+fig, axs = plot_quiver_slices_from_diffae_table(
+    diffae_grid_crops, traj_grids, flow_field_dict_grids
+)
+fig.savefig(
+    Path(output_dir) / f"{dataset_name}_flow_field_quiver.png",
+    dpi=300,
+)
+plt.close(fig)
+
+# next plot and save the flow field with the measured feature overlay
+plot_measured_feat_overlay_on_flowfield(
+    Path(output_dir),
+    if_dataset_name,
+    diffae_grid_crops,
+    traj_grids,
+    flow_field_dict_grids,
+    diffae_measured_feat_df=df_if_dataset,
+    meas_feat_col_name_for_color_coding=feature,
+    track_id_to_plot=None,
+    show_plot=True,
+    alpha=1.0,
+    hue_min_max=None,
+)
+plt.close(fig)
+
+# lastly save the quiver slices with the measured feature overlay
+# in grey with a single crop highlighted
+index = 5
+chosen_datapoint = df_if_dataset.iloc[index]
+
+fig, axs = plot_quiver_slices_from_diffae_table(
+    diffae_grid_crops, traj_grids, flow_field_dict_grids
+)
+
+pc_x, pc_y = ("pc1", "pc1"), ("pc2", "pc3")
+
+for i, ax in enumerate(axs):
+    sns.scatterplot(
+        data=df_if_dataset,
+        x=pc_x[i],
+        y=pc_y[i],
+        color="grey",
+        linewidth=0,
+        marker=".",
+        s=50,
+        alpha=1.0,
+        ax=ax,
+    )
+    sns.scatterplot(
+        data=chosen_datapoint.to_frame().T,
+        x=pc_x[i],
+        y=pc_y[i],
+        color="red",
+        linewidth=0,
+        marker="o",
+        s=50,
+        ax=ax,
+    )
+plt.tight_layout()
+fig.savefig(
+    Path(output_dir) / f"{dataset_name}_flow_field_{channel}_iloc{index}.png",
+    dpi=300,
+)
+plt.close(fig)
+
+
+# %%
