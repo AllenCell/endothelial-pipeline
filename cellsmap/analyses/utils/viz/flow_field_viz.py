ea12fa10581f0930beb7421adf039d9918ed191e
Erin Angelini <erin.angelini@alleninstitute.org>
2025-05-30 15:41:27 -0700

    caveat

--- cellsmap/analyses/utils/viz/flow_field_viz.py
+++ cellsmap/analyses/utils/viz/flow_field_viz.py
@@ -348,6 +348,10 @@ def plot_stable_fixed_points_together(fig_savedir: str, output_savedir: str) -> 
 
     traj_dict = np.load(output_savedir + "traj_dict.npy", allow_pickle=True).item()
 
+    # hard coded list of datasets
+    # this is for the purposes of generating
+    # the plot for the SAC slides
+    # Can work to generalize this later
     list_of_datasets = [
         "20250409_20X",
         "20241120_20X",
