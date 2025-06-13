d025d098d9fd0f3235e11a2d3cb2b78626a216c8
Erin Angelini <erin.angelini@alleninstitute.org>
2025-06-13 13:35:29 -0700

    review comments

--- cellsmap/model_features/generate_csv_for_training.py
+++ cellsmap/model_features/generate_csv_for_training.py
@@ -1,5 +1,5 @@
 # %%
-import os
+from pathlib import Path
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -16,24 +16,10 @@ output_savedir = get_output_path(output_folder, verbose=False)
 data_config = load_config("data")
 
 # %%
-# initialize dataframe for training
-train_df = pd.DataFrame(
-    columns=[
-        "path",
-        "channel",
-        "resolution",
-    ]
-)
-# initialize dataframe for validation
-val_df = pd.DataFrame(
-    columns=[
-        "path",
-        "channel",
-        "resolution",
-    ]
-)
-
+zarr_file_paths = []
 for dataset_name in data_config:
+    # this line will change when the PRs
+    # changing the data config structure are merged
     config_dict = data_config[dataset_name]
     zarr_path = config_dict["zarr_path"]
     # only train on datasets that have been converted to zarr
@@ -53,47 +39,17 @@ for dataset_name in data_config:
 
     print(f"Processing dataset {dataset_name} (FMS ID {config_dict['fmsid']})")
     # get all zarr files in zarr path
-    list_of_zarr_paths = []
-    # just want to grab files in the root directory of the zarr path
-    for root, dirs, files in os.walk(zarr_path):
-        for file in dirs:  # zarrs are directories
-            if file.endswith(".zarr"):
-                full_path = os.path.join(root, file)
-                list_of_zarr_paths.append(full_path)
-        # break after the first iteration to avoid going into subdirectories
-        break
+    # append to list of zarr file paths
+    glob_list = list(Path(zarr_path).glob("*zarr"))
+    zarr_file_paths.extend(glob_list)
 
-    # split into train and validation sets
-    # update the dataframes with the paths
-    # and set channel and resolution (same for all)
-    train_paths, val_paths = train_test_split(
-        list_of_zarr_paths, test_size=0.2, random_state=42
-    )
-    for path_ in train_paths:
-        row_list = [
-            path_,
-            "0,1",  # cdh5, brightfield
-            1,  # zarr resolution level 1
-        ]
-        train_df = pd.concat(
-            [train_df, pd.DataFrame([row_list], columns=train_df.columns)],
-            ignore_index=True,
-        )
-    for path_ in val_paths:
-        row_list = [
-            path_,
-            "0,1",  # cdh5, brightfield
-            1,  # zarr resolution level 1
-        ]
-        val_df = pd.concat(
-            [val_df, pd.DataFrame([row_list], columns=val_df.columns)],
-            ignore_index=True,
-        )
+zarr_path_df = pd.DataFrame({"path": zarr_file_paths})
+zarr_path_df["channel"] = "0,1"  # cdh5, brightfield
+zarr_path_df["resolution"] = 1  # downsample by half
+
+train, val = train_test_split(zarr_path_df, test_size=0.2, random_state=42)
 # %%
-train_df = train_df.reset_index(drop=True)
-val_df = val_df.reset_index(drop=True)
-
 # save the dataframes to csv files
-train_df.to_csv(os.path.join(output_savedir, "train.csv"), index=False)
-val_df.to_csv(os.path.join(output_savedir, "val.csv"), index=False)
+train.to_csv(Path(output_savedir) / "train.csv", index=False)
+val.to_csv(Path(output_savedir) / "val.csv", index=False)
 # %%
