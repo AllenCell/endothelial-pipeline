43136fff8bafa43b7546dd2f6e44f923879c7f92
Erin Angelini <erin.angelini@alleninstitute.org>
2025-06-13 15:29:06 -0700

    CLI

--- cellsmap/model_features/generate_csv_for_training.py
+++ cellsmap/model_features/generate_csv_for_training.py
@@ -1,6 +1,6 @@
-# %%
 from pathlib import Path
 
+import fire
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -30,38 +30,42 @@ def check_dataset_for_model_training(dataset_name: str, data_config: dict) -> bo
     return True, config_dict["zarr_path"]
 
 
-# %%
-# set output directory
-output_folder = "manifests"
-output_savedir = get_output_path(output_folder, verbose=False)
+def main(model_name: str | None = None) -> None:
+    # set output directory
+    output_folder = "manifests"
+    if model_name is not None:
+        output_folder = f"{output_folder}/{model_name}"
+    output_savedir = get_output_path(output_folder, verbose=False)
 
-# load data config
-data_config = load_config("data")
+    # load data config
+    data_config = load_config("data")
 
-# %%
-zarr_file_paths = []
-for dataset_name in data_config:
-    # check if the dataset is suitable for training
-    # see check_dataset_for_training function for
-    # the criteria used to filter datasets
-    is_for_training, zarr_path = check_dataset_for_model_training(
-        dataset_name, data_config
-    )
-    if not is_for_training:
-        continue
-    print(f"Processing dataset {dataset_name}")
-    # get all zarr files in zarr path
-    # append to list of zarr file paths
-    glob_list = list(Path(zarr_path).glob("*zarr"))
-    zarr_file_paths.extend(glob_list)
+    zarr_file_paths = []
+    for dataset_name in data_config:
+        # check if the dataset is suitable for training
+        # see check_dataset_for_training function for
+        # the criteria used to filter datasets
+        is_for_training, zarr_path = check_dataset_for_model_training(
+            dataset_name, data_config
+        )
+        if not is_for_training:
+            continue
+        print(f"Processing dataset {dataset_name}")
+        # get all zarr files in zarr path
+        # append to list of zarr file paths
+        glob_list = list(Path(zarr_path).glob("*zarr"))
+        zarr_file_paths.extend(glob_list)
 
-zarr_path_df = pd.DataFrame({"path": zarr_file_paths})
-zarr_path_df["channel"] = "0,1"  # cdh5, brightfield
-zarr_path_df["resolution"] = 1  # downsample by half
+    zarr_path_df = pd.DataFrame({"path": zarr_file_paths})
+    zarr_path_df["channel"] = "0,1"  # cdh5, brightfield
+    zarr_path_df["resolution"] = 1  # downsample by half
 
-train, val = train_test_split(zarr_path_df, test_size=0.2, random_state=42)
-# %%
-# save the dataframes to csv files
-train.to_csv(Path(output_savedir) / "train.csv", index=False)
-val.to_csv(Path(output_savedir) / "val.csv", index=False)
-# %%
+    train, val = train_test_split(zarr_path_df, test_size=0.2, random_state=42)
+
+    # save the dataframes to csv files
+    train.to_csv(Path(output_savedir) / "train.csv", index=False)
+    val.to_csv(Path(output_savedir) / "val.csv", index=False)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
