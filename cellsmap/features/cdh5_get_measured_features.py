ab884edb3bd3b7107f4c2d30cc81711b27a03b5b
Serge Parent <serge.edouard.parent@gmail.com>
2025-06-18 10:23:31 -0700

    - made verbose=False produce no printed output

--- cellsmap/features/cdh5_get_measured_features.py
+++ cellsmap/features/cdh5_get_measured_features.py
@@ -194,7 +194,7 @@ def build_measured_features_tables(
     # # the timestamp that this script was run:
     # timestamp = pd.Timestamp.now().strftime("%Y-%m-%d %X")
 
-    print(f"Working on {dataset_name} -- T={T}...")
+    print(f"Working on {dataset_name} -- T={T}...") if verbose else False
 
     dim_order = "TCZYX"
 
