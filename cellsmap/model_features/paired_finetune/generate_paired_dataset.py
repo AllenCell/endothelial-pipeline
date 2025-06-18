be7fb10d3df40379d00e329e2881c07faf01b315
Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
2025-06-18 15:10:17 -0700

    simplify dataset generation

--- cellsmap/model_features/paired_finetune/generate_paired_dataset.py
+++ cellsmap/model_features/paired_finetune/generate_paired_dataset.py
@@ -9,25 +9,11 @@ from bioio import BioImage
 from bioio.writers import OmeTiffWriter
 from sklearn.model_selection import train_test_split
 
+from cellsmap.model_features.paired_data_validation import align_all_positions
 from cellsmap.util.set_output import get_output_path
 
 
-def find_csvs(base_path: Path, directories: list[str] | None = None):
-    if directories is None:
-        directories = [p for p in base_path.glob("*_vs_*") if p.is_dir()]
-        for d in directories:
-            files = list(d.glob("aligned*.csv"))
-            if len(files) != 1:
-                directories.remove(d)
-
-    csv_paths = [list((base_path / d).glob("aligned_*.csv"))[0] for d in directories]
-
-    df = pd.concat([pd.read_csv(p) for p in csv_paths], ignore_index=True)
-    df = df.dropna(subset=["fixed", "moving"])
-    return df
-
-
-def _get_concat_path(row, savedir):
+def _get_concat_path(row: pd.Series, savedir: Path) -> Path:
     """
     Generate a path for the concatenated image based on the fixed image path.
     The moving image path is not used in the final file name.
@@ -35,7 +21,47 @@ def _get_concat_path(row, savedir):
     return savedir / f"{str(Path(row.fixed).stem).replace('_fixed', '')}.ome.tiff"
 
 
-def concat(row, savedir):
+def _get_aligned_paths(
+    dataset_type: Literal["live_fixed", "20x_40x"],
+    save_path: Path,
+    fixed_datasets: list[str] | None = None,
+    moving_datasets: list[str] | None = None,
+) -> pd.DataFrame:
+    datasets = {
+        "live_fixed": {
+            "fixed": ["20250214_pairedPreFixation"],
+            "moving": ["20250214_pairedPostFixation"],
+        },
+        "20x_40x": {
+            "fixed": ["20250110_paired20X", "20250227_paired20X", "20250228_paired20X"],
+            "moving": [
+                "20250110_paired40X",
+                "20250227_paired40X",
+                "20250228_paired40X",
+            ],
+        },
+    }[dataset_type]
+
+    fixed_datasets = fixed_datasets or datasets["fixed"]
+    moving_datasets = moving_datasets or datasets["moving"]
+
+    df = []
+    for fixed, moving in zip(fixed_datasets, moving_datasets):
+        df.append(
+            align_all_positions(
+                fixed,
+                moving,
+                save_path,
+                alignment_method="sift" if dataset_type == "live_fixed" else "template",
+            )
+        )
+    df = pd.concat(df, ignore_index=True)
+    df = df.dropna(subset=["fixed", "moving"])
+    print(f"Found {len(df)} pairs of images to save")
+    return df
+
+
+def concat(row: pd.Series, savedir: Path) -> Path:
     save_path = _get_concat_path(row, savedir)
     if save_path.exists():
         print(f"Skipping {save_path} as it already exists.")
@@ -51,9 +77,9 @@ def concat(row, savedir):
 
 
 def generate_paired_dataset(
-    model_name: str,
     dataset_type: Literal["live_fixed", "20x_40x"],
-    directories: list[str] | None = None,
+    fixed_datasets: list[str] | None = None,
+    moving_datasets: list[str] | None = None,
     split: bool = False,
 ):
     """
@@ -61,20 +87,18 @@ def generate_paired_dataset(
 
     Parameters
     ----------
-    model_name: str
-        This is used to find the aligned image pairs in `models/{model_name}` and should match either `fixed_finetuned_model_name` or `model_name` from `paired_data_validation`.
     dataset_type: Literal['live_fixed', '20x_40x']
         Whether paired dataset is aligned live/fixed or 20x/40x. This will determine the directory structure to search for aligned image pairs. If `model_name` matches `fixed_finetuned_model_name`, then `dataset_type` should be `live_fixed`. If `model_name` matches `model_name` from `paired_data_validation`, then `dataset_type` should be `20x_40x`.
-    directories: list[str] | None
-        An optional list of directories to search for aligned image pairs. If None, all directories in `outputs/models/{model_name}` that match the pattern `*_vs_*` will be used.
+    fixed_datasets: list[str] | None
+        A list of fixed datasets to use for generating the dataset. If None, the function will use the default datasets for the specified `dataset_type`.
+    moving_datasets: list[str] | None
+        A list of moving datasets to use for generating the dataset. The order should be paired with fixed_datasets. If None, the function will use the default datasets for the specified `dataset_type`.
     split: bool
         If True, the dataset will be split into training and validation sets. The split will be saved as `train.csv` and `val.csv`. If False, the entire dataset will be saved as `dataset.csv`.
     """
     save_path = Path(get_output_path(f"finetune_paired_dataset/{dataset_type}"))
-    base_path = Path(get_output_path(f"models/{model_name}"))
 
-    df = find_csvs(base_path, directories)
-    print(f"Found {len(df)} pairs of images to save")
+    df = _get_aligned_paths(dataset_type, save_path, fixed_datasets, moving_datasets)
 
     out_paths = [concat(row, save_path) for row in tqdm.tqdm(df.itertuples())]
 
