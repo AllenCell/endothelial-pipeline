762054551ecdcc2b33841bd820abd1cb4598be69
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-06-20 17:09:06 -0700

    remove unused function

--- src/endo_pipeline/library/process/image_processing.py
+++ src/endo_pipeline/library/process/image_processing.py
@@ -15,12 +15,6 @@ def bf_slice(img: BioImage, frame: int) -> np.ndarray:
     return bf_slice.compute()
 
 
-def bf_max_proj(img: BioImage, frame: int) -> np.ndarray:
-    bf_img = img.get_image_dask_data("ZYX", C=1, T=frame)
-    bf_max_proj = bf_img.max(axis=0)
-    return bf_max_proj.compute()
-
-
 def bf_std_dev(img: BioImage, frame: int) -> np.ndarray:
     """Calculate the standard deviation of the brightfield image for a given frame."""
     bf_img = img.get_image_dask_data("ZYX", C=1, T=frame)
