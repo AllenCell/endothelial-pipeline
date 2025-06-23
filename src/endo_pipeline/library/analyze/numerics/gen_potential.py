e9661eb1d67b46e14c38f04dad6cddb810f92515
Erin Angelini <erin.angelini@alleninstitute.org>
2025-06-23 14:38:13 -0700

    caught import error

--- src/endo_pipeline/library/analyze/numerics/gen_potential.py
+++ src/endo_pipeline/library/analyze/numerics/gen_potential.py
@@ -4,7 +4,7 @@ from scipy.integrate import simpson
 from src.endo_pipeline.library.analyze.diffae_feature_dynamics.model_eval import (
     get_normalization_constant,
 )
-from src.endo_pipeline.library.analyze.diffae_feature_dynamics.numerics import fp_solvers as fps
+from src.endo_pipeline.library.analyze.numerics import fp_solvers as fps
 
 
 def gradient_flow_term(potential: np.ndarray, diffusion: np.ndarray, x: list) -> np.ndarray:
