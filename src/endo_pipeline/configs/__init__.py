91c4710be8a71a928823940b031e6ea02c733a65
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2025-06-26 11:57:50 -0400

    Split dataset config classes and methods into separate modules

---
+++ src/endo_pipeline/configs/__init__.py
@@ -0,0 +1,11 @@
+from .dataset_config import DatasetConfig, ValidTimepoints
+from .dataset_config_io import (
+    get_available_dataset_names,
+    get_dataset_config_dir,
+    load_all_dataset_configs,
+    load_reference_dataset_configs,
+    load_single_dataset_config,
+    save_dataset_config,
+    validate_all_dataset_configs,
+    validate_single_dataset_config,
+)
--- src/endo_pipeline/configs/dataset_config.py
+++ src/endo_pipeline/configs/dataset_config.py
@@ -1,13 +1,9 @@
-import logging
-from dataclasses import dataclass
-from pathlib import Path
+"""Data structures for dataset configs."""
+
 from typing import Literal
 
-import yaml
-from mashumaro.codecs.yaml import YAMLDecoder, YAMLEncoder
 from mashumaro.config import BaseConfig
-
-logger = logging.getLogger(__name__)
+from pydantic.dataclasses import dataclass
 
 
 @dataclass
@@ -129,102 +125,3 @@ class DatasetConfig:
     class Config(BaseConfig):
         forbid_extra_keys = True
         omit_none = False
-
-
-def get_config_dir() -> Path:
-    """Get path to config directory."""
-
-    return Path(__file__).resolve().parents[1] / "configs"
-
-
-def get_available_dataset_names() -> list[str]:
-    """Get list of available dataset names."""
-
-    dataset_names = [path.stem for path in (get_config_dir() / "datasets").iterdir()]
-    logger.info("Available datasets [ %s ]", " | ".join(dataset_names))
-
-    return dataset_names
-
-
-def validate_all_dataset_configs() -> None:
-    """Validate all dataset configs against defined schema."""
-
-    dataset_names = get_available_dataset_names()
-
-    for dataset_name in dataset_names:
-        validate_single_dataset_config(dataset_name)
-
-
-def validate_single_dataset_config(dataset_name: str) -> None:
-    """Validate given dataset config against defined schema."""
-
-    config_dir = get_config_dir()
-    config_file = config_dir / "datasets" / f"{dataset_name}.yaml"
-
-    logger.info("Validating config file [ %s ]", dataset_name)
-    config = YAMLDecoder(DatasetConfig).decode(config_file.read_text())
-
-    if config.name != config_file.stem:
-        logger.error(
-            "Config file name [ %s ] does not match name field [ %s ]",
-            config_file,
-            config.name,
-        )
-
-
-def load_all_dataset_configs() -> list[DatasetConfig]:
-    """Load all dataset configs."""
-
-    dataset_names = get_available_dataset_names()
-
-    datasets = [load_single_dataset_config(name) for name in dataset_names]
-    logger.info("Loaded all available datasets [ %s ]", " | ".join(dataset_names))
-
-    return datasets
-
-
-def load_reference_dataset_configs() -> list[DatasetConfig]:
-    """Load all reference dataset configs."""
-
-    all_datasets = load_all_dataset_configs()
-    reference_datasets = [dataset for dataset in all_datasets if dataset.is_reference]
-
-    reference_dataset_names = [dataset.name for dataset in reference_datasets]
-    logger.info("Loaded all reference datasets [ %s ]", " | ".join(reference_dataset_names))
-
-    return reference_datasets
-
-
-def load_single_dataset_config(dataset_name: str) -> DatasetConfig:
-    """Load single dataset config by name."""
-
-    config_dir = get_config_dir()
-    config_file = config_dir / "datasets" / f"{dataset_name}.yaml"
-
-    if not config_file.exists():
-        logger.error("Dataset [ %s ] could not be loaded", dataset_name)
-        raise FileNotFoundError(f"No such file '{config_file}'")
-    else:
-        logger.debug("Loaded dataset [ %s ]", dataset_name)
-        return YAMLDecoder(DatasetConfig).decode(config_file.read_text())
-
-
-def save_dataset_config(dataset: DatasetConfig) -> None:
-    """Save dataset config to config directory."""
-
-    config_dir = get_config_dir()
-    config_file = config_dir / "datasets" / f"{dataset.name}.yaml"
-
-    def yaml_encoder(data):
-        list_representer = lambda dumper, data: dumper.represent_sequence(
-            "tag:yaml.org,2002:seq", data, flow_style=True
-        )
-        yaml.SafeDumper.add_representer(list, list_representer)
-        return yaml.safe_dump(data, default_flow_style=False, sort_keys=False, width=80, indent=2)
-
-    content = YAMLEncoder(DatasetConfig, post_encoder_func=yaml_encoder).encode(dataset)
-    config_file.write_text(content)
-
-
-if __name__ == "__main__":
-    validate_all_dataset_configs()
