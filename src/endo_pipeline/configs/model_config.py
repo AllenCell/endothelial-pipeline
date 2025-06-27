a781f4195edd7f9f17051cddb5116e0be3a4d91b
Erin Angelini <erin.angelini@alleninstitute.org>
2025-06-27 10:27:22 -0700

    docstrings

--- src/endo_pipeline/configs/model_config.py
+++ src/endo_pipeline/configs/model_config.py
@@ -4,8 +4,10 @@ from dataclasses import dataclass
 @dataclass
 class ModelManifest:
     dataset_name: str
+    """Name of the dataset for which the model was applied."""
 
     fmsid: str
+    """FMS ID of the resulting feature manifest."""
 
 
 @dataclass
@@ -19,9 +21,17 @@ class ModelConfig:
     """MLFlow run id for model."""
 
     manifest_fmsids: list[ModelManifest] | None = None
+    """
+    List of manifest FMS IDs for datasets with features obtained from the model.
+
+    (i.e., as obtained from the `apply_diffae_model` workflow)
+    """
 
     training_datasets: list[str] | None = None
+    """List of datasets used for training the model."""
 
     train_manifest_fmsid: str | None = None
+    """FMS ID of the training manifest dataset."""
 
-    val_manifest_fmsid: str | None = None
+    test_manifest_fmsid: str | None = None
+    """FMS ID of the test manifest dataset."""
