c2466930df4dcd73460b1f225ba48ada3b882906
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2025-07-01 17:47:32 -0400

    Add deprecation to old get_output_path method

--- cellsmap/util/set_output.py
+++ cellsmap/util/set_output.py
@@ -1,6 +1,35 @@
 import os
 
+from deprecated import deprecated
 
+
+@deprecated(
+    """
+This method deprecated and will be removed.
+
+1. To get a path for outputs, use the following pattern:
+
+    from src.endo_pipeline.io import get_output_path
+
+    output_path = get_output_path(__file__)
+
+For a workflow named "example_workflow", this method will return:
+
+    Path("/path/to/results/YYYY-MM-DD/example_workflow")
+
+2. If you wish to turn off the timestamp (not recommended), use:
+
+    output_path = get_output_path(__file__, include_timestamp=False)
+
+3. You may provide additional subfolder to the method:
+
+    output_path = get_output_path(__file__, subfolder1, subfolder2)
+
+These subfolders will be added to the returned output Path:
+
+    Path("/path/to/results/YYYY-MM-DD/example_workflow/subfolder1/subfolder2")
+"""
+)
 def get_output_path(workflow_name: str, verbose: bool = True) -> str:
     """
     Save results to a universal results directory in a folder titled after the workflow.
