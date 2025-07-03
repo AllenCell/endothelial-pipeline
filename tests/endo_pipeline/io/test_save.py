4fe251104a94b1a43b12b6659fc52974a25f7095
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2025-07-03 19:07:37 -0400

    Add new `get_output_path` method and deprecate old version (#406)
    * Add new get_output_path method
    
    * Add deprecation to old get_output_path method
    
    * Use UTC alias
    
    * Add tests for get_output_path method

---
+++ tests/endo_pipeline/io/test_save.py
@@ -0,0 +1,40 @@
+import datetime
+
+import pytest
+
+from src.endo_pipeline.io.save import get_output_path
+
+
+@pytest.fixture
+def mock_output_dir(tmp_path, mocker):
+    output_dir_mock = mocker.patch("src.endo_pipeline.io.save.get_output_dir")
+    output_dir_mock.return_value = tmp_path
+    yield tmp_path
+
+
+@pytest.fixture
+def mock_timestamp(mocker):
+    timestamp = "2025-07-05"
+    datetime_mock = mocker.patch("src.endo_pipeline.io.save.datetime")
+    datetime_mock.datetime.now.return_value = datetime.datetime.strptime(timestamp, "%Y-%m-%d")
+    yield timestamp
+
+
+def test_get_output_path_file_name(mock_output_dir, mock_timestamp):
+    path = get_output_path(__file__)
+    assert path == mock_output_dir / mock_timestamp / "test_save"
+
+
+def test_get_output_path_file_name_with_subdirs(mock_output_dir, mock_timestamp):
+    path = get_output_path(__file__, "subdir1", "subdir2")
+    assert path == mock_output_dir / mock_timestamp / "test_save" / "subdir1" / "subdir2"
+
+
+def test_get_output_path_file_name_no_timestamp(mock_output_dir):
+    path = get_output_path(__file__, include_timestamp=False)
+    assert path == mock_output_dir / "test_save"
+
+
+def test_get_output_path_file_name_with_subdirs_no_timestamp(mock_output_dir):
+    path = get_output_path(__file__, "subdir1", "subdir2", include_timestamp=False)
+    assert path == mock_output_dir / "test_save" / "subdir1" / "subdir2"
