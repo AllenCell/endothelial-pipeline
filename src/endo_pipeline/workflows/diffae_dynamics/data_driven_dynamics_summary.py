e54dafdc001b2bfc617bd5cbc8923a1440fd4eed
Erin Angelini <erin.angelini@alleninstitute.org>
2025-07-07 15:57:04 -0700

    data driven dynamics

--- src/endo_pipeline/workflows/diffae_dynamics/data_driven_dynamics_summary.py
+++ src/endo_pipeline/workflows/diffae_dynamics/data_driven_dynamics_summary.py
@@ -1,15 +1,14 @@
 import fire
 
-from cellsmap.util import manifest_io
-from src.endo_pipeline.configs import dynamics_io
+from src.endo_pipeline.configs import dynamics_io, get_timelapse_model_manifests, load_model_config
 from src.endo_pipeline.io import get_output_path
 from src.endo_pipeline.library.analyze.diffae_features import ddd_main
-from src.endo_pipeline.library.analyze.diffae_manifest import manifest_pca
+from src.endo_pipeline.library.analyze.diffae_manifest.manifest_pca import fit_pca
 from src.endo_pipeline.library.visualize import viz_base
 from src.endo_pipeline.library.visualize.diffae_features import manifest_viz
 
 
-def main(config_name: str = "default") -> None:
+def main(dynamics_config_name: str = "default", model_name: str = "diffae_04_10") -> None:
     """
     Get and visualize data-driven flow fields for all datasets in the manifest.
 
@@ -19,58 +18,47 @@ def main(config_name: str = "default") -> None:
     #### Load manifest data and fit PCA ####
     # make save directory for workflow outputs
     # (set in config file dynamics_config.yaml)
-    print("\n", "*** Running workflow using config: ", config_name, "\n")
-    config = dynamics_io.load_dynamics_config(config_name)
+    print("\n", "*** Running workflow using config: ", dynamics_config_name, "\n")
+    config = dynamics_io.load_dynamics_config(dynamics_config_name)
 
     # get output subdirectory for intermediate workflow outputs
     # (set in config file dynamics_config.yaml)
-    # if directory does not exist, get_output_path
-    # function will create it
-    workflow_output_folder = "stochastic_dynamics/" + config["name"] + "/outputs"
-    savedir = get_output_path(workflow_output_folder)
+    # if directory does not exist, get_output_path function will create it
+    savedir = get_output_path(
+        "stochastic_dynamics", dynamics_config_name, model_name, "outputs", include_timestamp=False
+    )
 
     # get output subdirectory for figures that workflow outputs
     # (set in config file dynamics_config.yaml)
-    # if directory does not exist, get_output_path
-    # function will create it
-    workflow_fig_folder = "stochastic_dynamics/" + config["name"] + "/figs"
-    fig_savedir = get_output_path(workflow_fig_folder)
+    # if directory does not exist, get_output_path function will create it
+    fig_savedir = get_output_path(
+        "stochastic_dynamics", dynamics_config_name, model_name, "figs", include_timestamp=False
+    )
 
     # fit PCA to reference timepoints of
     # reference datasets
-    pca = manifest_pca.fit_pca()
-
-    # save out PCA object (need later for analysis
-    # and summary of fit dynamical systems model)
-    manifest_io.save_pca_model(pca, savedir)
-
-    #### Visualize PCA results ####
-    # plot explained variance ratio of PCA components
-    fig, _ = manifest_viz.plot_explained_variance(pca["pca"].explained_variance_ratio_)
-    viz_base.save_plot(
-        fig, filename=fig_savedir + "explained_variance_ratio", format=".png", dpi=500
-    )
+    pca = fit_pca()
 
     #### Get data driven flow fields (kernel method) ####
     # load inputs from dynamics_config.yaml
-    ds_to_skip = config["datasets_to_skip"]
     kramers_moyal_config = config["kramers_moyal"]
     kernel_params = None
     if "kernel_params" in kramers_moyal_config:
         kernel_params = kramers_moyal_config["kernel_params"]
 
-    # loop through datasets, get flow field
-    # estimates, and save out figures
-    list_of_datasets = manifest_io.list_datasets_with_manifest()
+    #################### Load model manifest data ###################
+    # get model config from model name
+    model_config = load_model_config(model_name)
 
-    for name in list_of_datasets:
-        if name in ds_to_skip:
-            print(f"**** Skipping dataset {name} **** \n")
-            continue
-        print(f"Computing drift and diffusion fields for dataset {name}")
+    # filter out datasets that are not timelapse
+    # and load model manifests
+    model_manifests = get_timelapse_model_manifests(model_config)
+
+    for dataset in model_manifests:
+        print(f"Computing drift and diffusion fields for dataset {dataset.dataset_name}")
 
         ddd_main.get_and_analyze_ddd(
-            name,
+            dataset,
             pca,
             kernel_params,
             fig_savedir,
