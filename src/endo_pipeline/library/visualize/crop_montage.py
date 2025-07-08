9eb57e1750e0f5a8b98d57b06ee8c09240aabad8
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-07-08 14:40:16 -0700

    update plot montage remove axis, number crops

--- src/endo_pipeline/library/visualize/crop_montage.py
+++ src/endo_pipeline/library/visualize/crop_montage.py
@@ -1,23 +1,18 @@
-import random
-
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def plot_crop_montage(
     list_of_crops: list[np.ndarray],
+    df_sample_sorted: pd.DataFrame,
+    pc_axis: int,
     channel_index: int | None = 1,  # brightfield standard dev
-    max_num_crops: int = 100,
 ) -> tuple[plt.Figure, np.ndarray]:
     """Plot a montage of 12 crops from a larger image."""
 
-    # only plot up to max_num_crops crops
-    num_crops = min(len(list_of_crops), max_num_crops)
-    # take a random sample of these crops
-    list_of_crops_ = random.sample(list_of_crops, num_crops)
-
     fig, ax = plt.subplots(10, 10, figsize=(32, 32))
-    for i, crop in enumerate(list_of_crops_):
+    for i, crop in enumerate(list_of_crops):
         if channel_index is None:
             # if not a multichannel image
             crop_ = crop.copy()
@@ -25,4 +20,10 @@ def plot_crop_montage(
             # multichannel, grab one
             crop_ = crop[channel_index, 0]
         ax[i // 10, i % 10].imshow(crop_, cmap="gray")
+        ax[i // 10, i % 10].set_title(
+            f"{i+1}"
+            # f"\n{df_sample_sorted['dataset'].iloc[i]}, "
+            # f"{df_sample_sorted['frame_number'].iloc[i]}"
+        )
+        ax[i // 10, i % 10].axis("off")  # Turn off axis ticks after setting the title
     return fig, ax
--- src/endo_pipeline/workflows/explore_feature_heatmap.py
+++ src/endo_pipeline/workflows/explore_feature_heatmap.py
@@ -151,7 +151,7 @@ df_sample = df_filtered.sample(
 # the rows in the filtered dataframe
 crop_list, df_sample_sorted = get_crops_in_dataframe(df_sample)
 # %%
-fig, _ = plot_crop_montage(crop_list)
+fig, _ = plot_crop_montage(crop_list, df_sample_sorted, pc_axis)
 fig.suptitle(f"PC{pc_axis+1} value: {pc_val}", y=1.0, fontsize=45)
 plt.tight_layout()
 plt.show()
@@ -162,9 +162,9 @@ viz_base.save_plot(
     + f"PC{pc_axis+1}_val"
     + "p".join(str(pc_val).split(".")),
 )
-
+# %%
 # cdh5 contact sheet
-fig, _ = plot_crop_montage(crop_list, channel_index=2)
+fig, _ = plot_crop_montage(crop_list, df_sample_sorted, pc_axis, channel_index=2)
 fig.suptitle(f"PC{pc_axis+1} value: {pc_val}", y=1.0, fontsize=45)
 plt.tight_layout()
 plt.show()
@@ -175,7 +175,7 @@ viz_base.save_plot(
     + f"PC{pc_axis+1}_val"
     + "p".join(str(pc_val).split(".")),
 )
-
+# %%
 # get reconstructed ve-cad crops
 # corresponding to the rows in the filtered dataframe
 # this requires a GPU to run, so
@@ -188,7 +188,9 @@ if gpu_available:
 
     reconstructed_crop_list = get_reconstructed_crops_in_dataframe(df_filtered)
 
-    fig, _ = plot_crop_montage(reconstructed_crop_list, channel_index=None)
+    fig, _ = plot_crop_montage(
+        reconstructed_crop_list, df_sample_sorted, pc_axis, channel_index=None
+    )
     fig.suptitle(f"PC{pc_axis+1} value: {pc_val}", y=1.0, fontsize=45)
     plt.tight_layout()
     plt.show()
