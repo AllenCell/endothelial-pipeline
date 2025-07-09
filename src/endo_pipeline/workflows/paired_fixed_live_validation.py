adde571aecd9d20be233ac95706ad9000eb1f9a3
Julie Dixon <juliec@alleninstitute.org>
2025-07-09 14:16:18 -0700

    formatting

--- src/endo_pipeline/workflows/paired_fixed_live_validation.py
+++ src/endo_pipeline/workflows/paired_fixed_live_validation.py
@@ -1,6 +1,20 @@
-from src.endo_pipeline.library.analyze.immunofluorescence import validate
+from src.endo_pipeline.library.analyze.immunofluorescence import validate_pcs_for_intergration
 
 if __name__ == "__main__":
+    """
+    This workflow validates the integration of paired fixed and live data
+    intended for integration of immunofluorescence data from fixed images
+    into the PC space derived from live data.
+
+    To do this, it does the following:
+        1. Applies a fine-tuned diffAE model to extract features
+        2. Projects the features into the reference PC space
+        3. Constructs confidence ellipses to determine fixed/live PC mapping
+           and uncertainty
+        4. Plots the raw data for paired fixed and live PC values, confidence
+              ellipses, linear model mapping between fixed and live data, and
+              uncertainty.
+    """
 
     live_dataset_name: str = "20250214_pairedPreFixation"
     fixed_dataset_name: str = "20250214_pairedPostFixation"
@@ -8,26 +22,32 @@ if __name__ == "__main__":
     n_pcs = 3
 
     # Align paired fixed and live data and apply a diffAE model to extract features.
-    save_path, fixed_features_path, live_features_path = validate.apply_model_paired_fixed_live(
-        fixed_dataset_name, live_dataset_name, model_name
+    save_path, fixed_features_path, live_features_path = (
+        validate_pcs_for_intergration.apply_model_paired_fixed_live(
+            fixed_dataset_name, live_dataset_name, model_name
+        )
     )
 
     # Project features from applying fine tuned diffAE model to fixed and live data into
     # reference PC space.
-    fixed_features, live_features = validate.project_paired_fixed_live_data_into_ref_PC_space(
-        fixed_features_path, live_features_path
+    fixed_features, live_features = (
+        validate_pcs_for_intergration.project_paired_fixed_live_data_into_ref_PC_space(
+            fixed_features_path, live_features_path
+        )
     )
 
     for pc in range(1, n_pcs + 1):
 
         # Construct confidence ellipse to determine fixed/live PC mapping and uncertainty
-        raw_data, validation_data = validate.get_paired_fixed_live_validation_features(
-            pc, fixed_features, live_features
+        raw_data, validation_data = (
+            validate_pcs_for_intergration.get_paired_fixed_live_validation_features(
+                pc, fixed_features, live_features
+            )
         )
 
         # Plot raw data for paired fixed and live PC values as well as confidence ellipse,
         # linear model mapping between fixed and live data and uncertainty.
-        validate.plot_paired_fixed_live_validation_features(
+        validate_pcs_for_intergration.plot_paired_fixed_live_validation_features(
             save_path,
             pc,
             raw_data,
