982959e10d99608af22a22b70fa2a4eacfbedba0
Erin Angelini <erin.angelini@alleninstitute.org>
2025-07-14 20:05:50 -0700

    test workflow

--- src/endo_pipeline/workflows/train_baseline_diffae_model.py
+++ src/endo_pipeline/workflows/train_baseline_diffae_model.py
@@ -3,11 +3,15 @@ from pathlib import Path
 
 import fire
 from cyto_dl.api import CytoDLModel
+from omegaconf import OmegaConf
 
-from src.endo_pipeline.configs import ModelConfig, load_config, load_model_config, save_model_config
+from src.endo_pipeline.configs import (
+    ModelConfig,
+    get_config_dir,
+    load_model_config,
+    save_model_config,
+)
 from src.endo_pipeline.io import get_output_path
-
-CONFIG_PATH = "../../configs/cyto_dl"
 
 
 def _generate_training_overrides(model_name: str, crop_size: int, save_path: Path) -> dict:
@@ -49,23 +53,24 @@ def _generate_training_overrides(model_name: str, crop_size: int, save_path: Pat
 
 
 def _initialize_diffae_model(
-    training_config: dict,
+    training_config: OmegaConf,
+    crop_size: int,
     model_name: str,
-    save_dir: Path,
+    save_path: Path,
 ) -> CytoDLModel:
     """
     Initialize a DiffAE model for training.
 
     Parameters
     ----------
-    training_config: DictConfig
+    training_config: OmegaConf
         The training configuration to use.
     model_name: str
         The name of the model to train.
     save_dir: Path
     """
     # user overrides for training
-    overrides = _generate_training_overrides(model_name=model_name, save_path=save_dir)
+    overrides = _generate_training_overrides(model_name, crop_size, save_path)
 
     # init model
     model = CytoDLModel()
@@ -100,11 +105,9 @@ def _update_model_config_with_run_id(model_name: str, run_id: str) -> None:
     save_model_config(model_name, model_config)
 
 
-def main(
-    crop_size: int = 128,
-) -> None:
+def main(crop_size: int = 128) -> None:
     """
-    Finetune a DiffAE model with paired live/fixed data.
+    Train a DiffAE model using the provided configuration.
 
     Parameters
     ----------
@@ -116,18 +119,20 @@ def main(
         batch size, etc. This is loaded and compiled
         by hydra from the global config path CONFIG_PATH.
     """
-    training_config = load_config("train_diffae")
+    # load training config
+    training_config = OmegaConf.load(get_config_dir() / "train_diffae.yaml")
+
     # set model name via timestamp and crop size
     timestamp = datetime.datetime.now(tz=datetime.UTC).strftime("%Y-%m-%d")
     model_name = f"diffae_{crop_size}x{crop_size}_{timestamp}"
     # set save directory
-    save_dir = get_output_path("models", model_name, include_timestamp=False)
+    save_path = get_output_path("models", model_name, include_timestamp=False)
 
     model = _initialize_diffae_model(
         training_config,
         crop_size,
         model_name,
-        save_dir,
+        save_path,
     )
     model.print_config()
     _, object_dict = model.train()
@@ -140,4 +145,4 @@ def main(
 
 
 if __name__ == "__main__":
-    fire.Fire(main())
+    fire.Fire(main)
