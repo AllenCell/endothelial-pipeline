3ec89ffa0c38575ad5f0c954a23a19c78bcd1dab
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-07-16 11:50:48 -0700

    simplify get_images code

--- src/endo_pipeline/library/process/get_images.py
+++ src/endo_pipeline/library/process/get_images.py
@@ -73,7 +73,7 @@ def get_crops_in_dataframe(df: pd.DataFrame) -> tuple[
         # Loop through each position in the dataframe
         for position, df_pos in df.groupby("position"):
             p = dataset_io.extract_P(position)
-            img = get_zarr_img_for_dataset(dataset, p)
+            img = get_zarr_img_for_dataset(dataset, p, resolution_level=1)
 
             for _, row in df_pos.iterrows():
                 timepoint = row["frame_number"]
@@ -87,7 +87,8 @@ def get_crops_in_dataframe(df: pd.DataFrame) -> tuple[
                     crop_size_y=row["crop_size_y"],
                 )
 
-                # Extract channels once
+                # Extract channels once, these channel indecies are hardcoded
+                # because we defined the order of channels in the zarr
                 bf_channel = crop[:, 1, :, :, :].squeeze()
                 gfp_channel = crop[:, 0, :, :, :].squeeze()
 
@@ -109,20 +110,6 @@ def get_crops_in_dataframe(df: pd.DataFrame) -> tuple[
         crops_gfp_max_projection,
         df_sorted,
     )
-
-
-def get_channel_from_list(crop_list: list, channel_index: int) -> list[np.ndarray]:
-    """
-    Extract a specific channel from a list of crops and reduce the shape to (Y, X).
-
-    Args:
-        crop_list (list): List of crops, each crop is a numpy array with shape (C, Z, Y, X).
-        channel_index (int): Index of the channel to extract.
-
-    Returns:
-        list[np.ndarray]: List of extracted channels with shape (Y, X).
-    """
-    return [crop[channel_index] for crop in crop_list]
 
 
 def global_contrast_crop_list(
@@ -168,65 +155,3 @@ def individual_contrast_crop_list(
         contrast_crop = contrast_stretching(crop, method=contrast_method)
         contrasted_channel.append(contrast_crop)
     return contrasted_channel
-
-
-def add_position(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Extract position from zarr path and add it as
-    its own column to the dataframe.
-
-    This is needed for the current test manifest downloaded from FMS.
-    """
-    df["position"] = df.zarr_path.apply(lambda s: s.split("/")[-1].split("_")[-1].split(".")[0])
-    return df
-
-
-def load_egfp_crop_image(row: pd.Series) -> BioImage:
-    """
-    Load the VE-Cad EGFP maximum projection image
-    for a given crop from the zarr path.
-    The input row should be a single row of the dataframe
-    containing the zarr path, crop coordinates, and time point
-    (frame in the movie).
-    """
-    # load image from zarr path
-    img = BioImage(row.zarr_path)
-    img.set_resolution_level(1)
-    img = img.get_image_dask_data("ZYX", C=0, T=row["frame_number"]).max(0)
-    # crops are 128x128, hardcoded for now
-    img = img[row.start_y : row.start_y + 128, row.start_x : row.start_x + 128]
-    return img
-
-
-def get_images_for_crop(
-    df: pd.DataFrame, crop_index: str, frame_range: Sequence | None = None
-) -> list[BioImage]:
-    """
-    For a given crop index, load the corresponding VE-Cadherin max
-    projection images for the specified time points (frames).
-    The input DataFrame should be a single dataset that has columns
-    with the zarr path, crop index, and timepoint frame_number.
-    The crop index is a unique identifier for each crop in the dataset
-    based on the start_x, start_y coordinates and position.
-    """
-    try:
-        df_crop_location = df.loc[df["crop_index"] == crop_index]
-    except KeyError:
-        raise KeyError(
-            f"crop_index {crop_index} not found in the DataFrame." + "Please check the crop index."
-        )
-
-    df_crop_location.sort_values(
-        by=["frame_number"], inplace=True
-    )  # sort by timepoint so images are in order
-
-    if frame_range is None:  # default to all timepoints
-        frame_range = df_crop_location["frame_number"]
-
-    # for rows in df_crop_location, load the image
-    images = []
-    for frame in frame_range:
-        row = df_crop_location.iloc[frame]
-        img = load_egfp_crop_image(row)
-        images.append(img)
-    return images
