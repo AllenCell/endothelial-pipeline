c8628ed5e137d26ec691ac15e60621f8d77f378e
Erin Angelini <erin.angelini@alleninstitute.org>
2025-07-16 16:48:48 -0700

    fix fit pca calls

--- src/endo_pipeline/workflows/diffae_dynamics/build_train_and_test.py
+++ src/endo_pipeline/workflows/diffae_dynamics/build_train_and_test.py
@@ -49,7 +49,7 @@ def main(dynamics_config_name: str = "default", model_name: str = "diffae_04_10"
     )
 
     # fit PCA to reference timepoints of reference datasets
-    pca = fit_pca()
+    pca = fit_pca(model_name=model_name)
 
     ################### Visualize PCA results ###################
     # plot explained variance ratio of PCA components
