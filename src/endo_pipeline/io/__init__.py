f6ea37d558268e85f33e7ac3a1cbb555d7ac4516
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2025-07-29 12:58:23 -0400

    Add generic `load_dataframe` method and `DataframeLocation` object (#499)
    * Add dataframe location dataclass
    
    * Add general load dataframe method
    
    * Add initial implementation for loading dataframes from S3

--- src/endo_pipeline/io/__init__.py
+++ src/endo_pipeline/io/__init__.py
@@ -1,4 +1,10 @@
-from .input import load_dataframe_from_fms, load_local_path_as_dataframe, load_zarr_as_dask_array
+from .input import (
+    load_dataframe,
+    load_dataframe_from_fms,
+    load_dataframe_from_path,
+    load_dataframe_from_s3,
+    load_zarr_as_dask_array,
+)
 from .log_funcs import configure_logging
 from .output import (
     build_fms_annotations,
@@ -13,8 +19,10 @@ __all__ = [
     "configure_logging",
     "get_output_dir",
     "get_output_path",
+    "load_dataframe",
     "load_dataframe_from_fms",
-    "load_local_path_as_dataframe",
+    "load_dataframe_from_path",
+    "load_dataframe_from_s3",
     "load_zarr_as_dask_array",
     "save_plot_to_path",
     "upload_file_to_fms",
