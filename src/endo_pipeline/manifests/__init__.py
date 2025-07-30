e4e68c87f4feb613d9b51f7551de983869d42666
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2025-07-30 10:06:03 -0400

    Add basic manifest io methods

--- src/endo_pipeline/manifests/__init__.py
+++ src/endo_pipeline/manifests/__init__.py
@@ -1,9 +1,25 @@
 from .dataframe_manifest import DataframeLocation, DataframeManifest
+from .dataframe_manifest_io import (
+    get_dataframe_manifest_dir,
+    load_dataframe_manifest,
+    save_dataframe_manifest,
+)
 from .segmentation_manifest import SegmentationLocation, SegmentationManifest
+from .segmentation_manifest_io import (
+    get_segmentation_manifest_dir,
+    load_segmentation_manifest,
+    save_segmentation_manifest,
+)
 
 __all__ = [
     "DataframeLocation",
     "DataframeManifest",
     "SegmentationLocation",
     "SegmentationManifest",
+    "get_dataframe_manifest_dir",
+    "get_segmentation_manifest_dir",
+    "load_dataframe_manifest",
+    "load_segmentation_manifest",
+    "save_dataframe_manifest",
+    "save_segmentation_manifest",
 ]
