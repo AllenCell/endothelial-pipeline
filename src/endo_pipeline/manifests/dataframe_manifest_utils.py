6067e52aff4a6bb9e3baae4a2226663af572a24c
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2025-07-30 18:44:10 -0400

    Add manifest utility methods

---
+++ src/endo_pipeline/manifests/dataframe_manifest_utils.py
@@ -0,0 +1,33 @@
+"""Methods for working with dataframe manifests."""
+
+import logging
+
+from src.endo_pipeline.manifests import DataframeLocation, DataframeManifest
+
+logger = logging.getLogger(__name__)
+
+
+def list_datasets_with_dataframes(manifest: DataframeManifest) -> list[str]:
+    """Get list of dataset names that have valid dataframe locations in the given manifest."""
+
+    return [
+        name
+        for name, location in manifest.locations.items()
+        if location.fmsid is not None or location.s3uri is not None
+    ]
+
+
+def get_dataframe_location_for_dataset(
+    manifest: DataframeManifest, dataset_name: str
+) -> DataframeLocation:
+    """Get the dataframe location for the given dataset from the manifest, if it exists."""
+
+    if dataset_name not in manifest.locations:
+        logger.error(
+            "Dataset [ %s ] does not have a location in dataframe manifest [ %s ]",
+            dataset_name,
+            manifest.name,
+        )
+        raise KeyError("Unable to find dataset %s in dataframe manifest.")
+
+    return manifest.locations[dataset_name]
---
+++ tests/endo_pipeline/manifests/test_dataframe_manifest_utils.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+import pytest
+
+from src.endo_pipeline.manifests.dataframe_manifest import DataframeLocation, DataframeManifest
+from src.endo_pipeline.manifests.dataframe_manifest_utils import (
+    get_dataframe_location_for_dataset,
+    list_datasets_with_dataframes,
+)
+
+
+@pytest.fixture
+def manifest():
+    return DataframeManifest(
+        name="unique_dataframe_manifest_name",
+        workflow="workflow_name",
+        locations={
+            "dataset_one": DataframeLocation(fmsid="dataset_one_fmsid"),
+            "dataset_two": DataframeLocation(fmsid="dataset_two_fmsid", s3uri="s3://dataset_two"),
+            "dataset_three": DataframeLocation(s3uri="s3://dataset_three"),
+        },
+    )
+
+
+def test_list_datasets_with_dataframes_with_valid_locations(manifest):
+    datasets = list_datasets_with_dataframes(manifest)
+
+    assert datasets == ["dataset_one", "dataset_two", "dataset_three"]
+
+
+def test_list_datasets_with_dataframes_with_invalid_location(manifest):
+    manifest.locations["dataset_two"] = DataframeLocation()
+
+    datasets = list_datasets_with_dataframes(manifest)
+
+    assert datasets == ["dataset_one", "dataset_three"]
+
+
+@pytest.mark.parametrize(
+    "dataset_name,expected_fmsid,expected_s3uri",
+    [
+        ("dataset_one", "dataset_one_fmsid", None),
+        ("dataset_two", "dataset_two_fmsid", "s3://dataset_two"),
+        ("dataset_three", None, "s3://dataset_three"),
+    ],
+)
+def test_get_dataframe_location_for_dataset_valid_dataset(
+    manifest, dataset_name, expected_fmsid, expected_s3uri
+):
+    location = get_dataframe_location_for_dataset(manifest, dataset_name)
+
+    if expected_fmsid is None:
+        assert location.fmsid is None
+    else:
+        assert location.fmsid == expected_fmsid
+
+    if expected_s3uri is None:
+        assert location.s3uri is None
+    else:
+        assert location.s3uri == expected_s3uri
+
+
+def test_get_dataframe_location_for_dataset_invalid_dataset(manifest):
+    with pytest.raises(KeyError):
+        get_dataframe_location_for_dataset(manifest, "invalid_dataset")
