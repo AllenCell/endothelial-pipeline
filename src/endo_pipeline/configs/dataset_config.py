1b0aa62e3b518eba9997684471c7aa7245b50ffb
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2025-07-31 11:12:53 -0400

    Remove nuclei_label_free_seg_manifest_fmsid from dataset configs

--- src/endo_pipeline/configs/dataset_config.py
+++ src/endo_pipeline/configs/dataset_config.py
@@ -130,9 +130,6 @@ class DatasetConfig:
     cdh5_seg_path: str | None = None
     """Path to Cdh5 segmentations."""
 
-    nuclei_label_free_seg_manifest_fmsid: str | None = None
-    """FMS ID for nuclei label free segmentation measurement output manifest."""
-
     live_merged_seg_features_manifest_fmsid: str | None = None
     """FMS ID for live dataset merged segmentation features manifest."""
 
