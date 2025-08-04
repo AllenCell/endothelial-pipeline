47a95e179503dadd8655636bd570f3277b9f1dfd
chantelleleveille <109111973+chantelleleveille@users.noreply.github.com>
2025-08-04 11:43:45 -0700

    Visualize model input pre-processing steps  (#514)
    * first pass
    
    * add mean bf summary and cdh5
    
    * update workflow
    
    * organize workflow with library functions
    
    * add line 20 for faster testing
    
    * move and rename workflow
    
    * fix datasetname

---
+++ src/endo_pipeline/library/visualize/model_inputs/image_processing_steps.py
@@ -0,0 +1,70 @@
+# %%
+from typing import Any
+
+import numpy as np
+
+from src.endo_pipeline.library.process.image_processing import (
+    clip_image,
+    max_proj,
+    scale_intensity_range_percentiles,
+    std_dev,
+    z_score_normalize_intensity,
+)
+
+
+def process_brightfield(bf_stack: Any) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Process a brightfield image stack following the same steps as the diffae model DataLoader:
+    type conversion, standard deviation projection, clipping, and normalization.
+
+    Args:
+        bf_stack (Any): Input brightfield image stack as a Dask array.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: A tuple containing:
+            - bf_stack_float32_computed: The bf stack as a computed np.ndarray in float32 format.
+            - standard_dev_proj: The standard deviation projection along the Z-axis.
+            - clipped_im: The image clipped by specified percentiles.
+            - normalized_im: The Z-score normalized image.
+    """
+    # STEP 1: Load the brightfield stack as a Dask array, convert to float32
+    bf_stack_float32 = bf_stack.astype("float32")
+    bf_stack_float32_computed = bf_stack_float32.compute()  # compute for visualization
+
+    # STEP 2: Standard deviation projection along the Z-axis
+    standard_dev_proj = std_dev(bf_stack_float32, axis=0, unbiased=False).astype("float32")
+
+    # STEP 3: Clip image by percentiles
+    clipped_im = clip_image(standard_dev_proj, low_pct=0.1, high_pct=99.9)
+
+    # STEP 4: Z-score normalize
+    normalized_im = z_score_normalize_intensity(clipped_im)
+
+    return bf_stack_float32_computed, standard_dev_proj, clipped_im, normalized_im
+
+
+def process_cdh5(cdh5_stack: Any) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Process a CDH5 image stack following the same steps as the diffae model DataLoader:
+    type conversion, maximum projection, and intensity scaling.
+
+    Args:
+        cdh5_stack (Any): Input CDH5 image stack as a Dask array.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray]: A tuple containing:
+            - cdh5_stack_float32_computed: The CDH5 stack as a computed np.ndarray in float32 format.
+            - max_proj_im: The maximum projection along the Z-axis.
+            - scaled_im: The image scaled to a specified intensity range.
+    """
+    # STEP 1: Load the CDH5 stack as a Dask array, convert to float32
+    cdh5_stack_float32 = cdh5_stack.astype("float32")
+    cdh5_stack_float32_computed = cdh5_stack_float32.compute()  # compute for visualization
+
+    # STEP 2: Maximum projection along the Z-axis
+    max_proj_im = max_proj(cdh5_stack_float32, axis=0).astype("float32")
+
+    # STEP 3: Clip image by percentiles, Map linearly -1 to 1
+    scaled_im = scale_intensity_range_percentiles(max_proj_im, 10, 98, -1.0, 1.0, clip=True)
+
+    return cdh5_stack_float32_computed, max_proj_im, scaled_im
---
+++ src/endo_pipeline/workflows/vis_model_input_processing_nb.py
@@ -0,0 +1,68 @@
+# %%
+from src.endo_pipeline.configs import (
+    get_datasets_in_collection,
+    get_zarr_file_for_position,
+    load_dataset_config,
+)
+from src.endo_pipeline.io import load_zarr_as_dask_array
+from src.endo_pipeline.io.output import get_output_path
+from src.endo_pipeline.library.visualize.model_inputs.image_processing_steps import (
+    process_brightfield,
+    process_cdh5,
+)
+from src.endo_pipeline.library.visualize.model_inputs.plot import visualize_images_with_histograms
+
+# %%
+POSITION = 0
+TIMEPOINT = 0
+
+datasets = get_datasets_in_collection("live_20X_objective_3i_microscope")
+# datasets = ["20241016_20X"] # Uncomment this line to test with a single dataset
+
+# %% Brightfield Visualization
+for dataset in datasets:
+    config = load_dataset_config(dataset)
+
+    save_dir = get_output_path("model_input_visualization", "brightfield")
+    zarr_file = get_zarr_file_for_position(config, POSITION)
+    bf_stack = load_zarr_as_dask_array(
+        zarr_file, channels=["BF"], timepoints=TIMEPOINT, level=1, squeeze=True
+    )
+
+    bf_stack_float32_computed, standard_dev_proj, clipped_im, normalized_im = process_brightfield(
+        bf_stack
+    )
+
+    visualize_images_with_histograms(
+        [
+            ("BF Slice", bf_stack_float32_computed[15]),
+            ("Std Dev Projection", standard_dev_proj),
+            ("Clipped Std Dev Projection", clipped_im),
+            ("Z-score Normalized Image", normalized_im),
+        ],
+        save_dir=save_dir,
+        fname_prefix=f"{dataset}_P{POSITION}_T{TIMEPOINT}_BF",
+    )
+
+# %% CDH5 Visualization
+for dataset in datasets:
+    config = load_dataset_config(dataset)
+
+    save_dir = get_output_path("model_input_visualization", "cdh5")
+    zarr_file = get_zarr_file_for_position(config, POSITION)
+    cdh5_stack = load_zarr_as_dask_array(
+        zarr_file, channels=["EGFP"], timepoints=TIMEPOINT, level=1, squeeze=True
+    )
+    
+    cdh5_stack_float32_computed, max_proj_im, scaled_im = process_cdh5(cdh5_stack)
+
+    visualize_images_with_histograms(
+        [
+            ("CDH5 Slice", cdh5_stack_float32_computed[15]),
+            ("Max Projection", max_proj_im),
+            ("Scaled, Clipped Image", scaled_im),
+        ],
+        save_dir=save_dir,
+        fname_prefix=f"{dataset}_P{POSITION}_T{TIMEPOINT}_CDH5",
+    )
+# %%
