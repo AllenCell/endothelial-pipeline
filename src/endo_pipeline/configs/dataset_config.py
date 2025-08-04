6f37a6b54c07532cab8c87357e0a5d1c36d02ec2
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2025-08-04 10:49:13 -0400

    Migrate CDH5 classic segmentation and nuclei label free segmentation dataset config fields (#511)
    * Remove cdh5_classic_seg_manifest_fmsid from dataset configs
    
    * Add non-null cdh5 classic seg manifest locations to dataframe manifest
    
    * Update all uses of the cdh5 classic seg manifest field
    
    * Remove cdh5_classic_seg_tracking_manifest_fmsid from dataset configs
    
    * Add non-null cdh5 classic seg tracking manifest locations to dataframe manifest
    
    * Update all uses of the cdh5 classic seg tracking manifest field
    
    * Remove nuclei_label_free_seg_manifest_fmsid from dataset configs
    
    * Add non-null nuclei label free seg manifest locations to dataframe manifest
    
    * Update all uses of the nuclei label free seg manifest field

--- src/endo_pipeline/configs/dataset_config.py
+++ src/endo_pipeline/configs/dataset_config.py
@@ -130,15 +130,6 @@ class DatasetConfig:
     cdh5_seg_path: str | None = None
     """Path to Cdh5 segmentations."""
 
-    cdh5_classic_seg_tracking_manifest_fmsid: str | None = None
-    """FMS ID for classic segmentation tracking output manifest."""
-
-    cdh5_classic_seg_manifest_fmsid: str | None = None
-    """FMS ID for classic segmentation measurement output manifest."""
-
-    nuclei_label_free_seg_manifest_fmsid: str | None = None
-    """FMS ID for nuclei label free segmentation measurement output manifest."""
-
     live_merged_seg_features_manifest_fmsid: str | None = None
     """FMS ID for live dataset merged segmentation features manifest."""
 
