c5c29bfde7eefc227b6f550933d9a55fff441b29
Serge Parent <serge.edouard.parent@gmail.com>
2025-08-05 17:35:19 -0700

    - added correlation plots for when timepoints are restricted to approximately steady state - added correlation comparing measured features to each other

--- src/endo_pipeline/workflows/pc_interpretability/feats_diffae_classic_correlations.py
+++ src/endo_pipeline/workflows/pc_interpretability/feats_diffae_classic_correlations.py
@@ -17,6 +17,7 @@ from tqdm import tqdm
 from src.endo_pipeline.configs import load_dataset_collection_config
 from src.endo_pipeline.configs.dataset_io import load_nuclei_prediction
 from src.endo_pipeline.io import get_output_path, save_plot_to_path
+from src.endo_pipeline.library.analyze.diffae_manifest.diffae_manifest_utils import get_valid_subset
 from src.endo_pipeline.library.analyze.integration.track_integration import (  # get_approx_point_from_grid,; get_approx_vec_from_grid,; get_gridcrop_and_cellcentric_trajectories_and_flow_fields,; get_vector_angles_as_grid,; get_vector_dot_products_as_grid,; get_vector_vector_angle_fast,; make_angular_deviation_test,
     get_preprocessed_manifests_and_km_bounds,
 )
@@ -247,8 +248,6 @@ def add_num_nuclei_in_crop_column(
         merged_feats_df,
         nuc_centroid_indices,
         on=["dataset_name", "position", "image_index"],
-        # left_on=["dataset_name", "position", "image_index"],
-        # right_on=["dataset_name", "position", "timeframe"],
         how="left",
     )
     groups = merged_feats_df.groupby(["dataset_name", "position", "image_index"])
@@ -268,6 +267,87 @@ def add_num_nuclei_in_crop_column(
         num_nuclei_in_crop, axis=0, ignore_index=False
     )
     return merged_feats_df
+
+
+def get_correlation_matrix_df(
+    features_df: pd.DataFrame,
+    column_names_for_x_axis: list[str],
+    column_names_for_y_axis: list[str],
+    name_of_x_axis: str,
+    name_of_y_axis: str,
+    df_format: Literal["long", "wide-corrcoeff", "wide-pval"] = "long",
+) -> pd.DataFrame:
+    """
+    Get the Pearson correlations between each column in `column_names_for_x_axis`
+    compared with each column in `column_names_for_y_axis`.
+    This is used to compare the diffae features and the measured features,
+    and then used again to compare the PCs and the measured features.
+    If `df_format` is one of the "wide" options then the outputted dataframe
+    of this function can be passed directly to `seaborn.heatmap` or
+    `seaborn.clustermap` for visualization.
+
+    Parameters
+    ----------
+    features_df : pd.DataFrame
+        The DataFrame containing the features to correlate.
+    column_names_for_x_axis : list[str]
+        The names of the columns to use for the x-axis.
+    column_names_for_y_axis : list[str]
+        The names of the columns to use for the y-axis.
+    name_of_x_axis : str
+        The name of the x-axis.
+    name_of_y_axis : str
+        The name of the y-axis.
+    df_format : Literal["long", "wide"], optional
+        The format of the output DataFrame. If "long", the output DataFrame will have columns:
+        - name_of_y_axis
+        - name_of_x_axis
+        - pearsonr
+        - pval
+        If "wide-corrcoeff", the output DataFrame will have a column for each column in
+        column_names_for_x_axis and the index will be the column names in
+        column_names_for_y_axis, with the values in the DataFrame corresponding to the
+        correlation coefficients from the "long" version of the table.
+        "wide-pval" is similar to "wide-corrcoeff" but the values correspond to the p-values.
+        Defaults to "long".
+    """
+    records = []
+    for col_for_y in column_names_for_y_axis:
+        for col_for_x in column_names_for_x_axis:
+            valid_records = np.isfinite(features_df[[col_for_y, col_for_x]]).all(axis=1)
+            corr, pval = pearsonr(
+                features_df[col_for_y][valid_records],
+                features_df[col_for_x][valid_records],
+            )
+            records.append(
+                {
+                    name_of_y_axis: col_for_y,
+                    name_of_x_axis: col_for_x,
+                    "pearsonr": corr,
+                    "pval": pval,
+                }
+            )
+    correlation_df = pd.DataFrame(records)
+
+    if df_format in ("wide-corrcoeff", "wide-pval"):
+        if df_format == "wide-corrcoeff":
+            value_col = "pearsonr"
+        elif df_format == "wide-pval":
+            value_col = "pval"
+        correlation_df = correlation_df.pivot(
+            index=name_of_y_axis,
+            columns=name_of_x_axis,
+            values=value_col,
+        )
+        correlation_df = correlation_df[column_names_for_x_axis]  # sort the columns
+        correlation_df = correlation_df.reindex(index=column_names_for_y_axis)  # sort the index
+    elif df_format == "long":
+        pass
+    else:
+        raise ValueError(
+            f"Unsupported df_format: {df_format}. Supported formats are 'long', 'wide-corrcoeff', 'wide-pval'."
+        )
+    return correlation_df
 
 
 if __name__ == "__main__":
@@ -290,28 +370,17 @@ if __name__ == "__main__":
 
         out_subdir = get_output_path(Path(__file__).stem, dataset_name, include_timestamp=False)
 
-        # # keep only the columns that are needed for the analysis to reduce memory usage
-        # cols_to_keep = [
-        #     "dataset_name",
-        #     "position",
-        #     "position_as_str",
-        #     "track_id",
-        #     "label",
-        #     "crop_index",
-        #     "mlflow_id",
-        #     "model_name",
-        #     "image_index",
-        #     "frame_number",
-        #     "time_hours",
-        #     "time_minutes",
-        #     "track_duration",
-        # ] + [col for col in merged_feats_df.columns if "feat" in col or "pc" in col]
-        # merged_feats_df = merged_feats_df[cols_to_keep]
+        # get the names of the columns that you are interested in using
+        pc_col_names = []
+        diffae_feature_col_names = []
+        for col_nm in merged_feats_df.columns:
+            if re.match("pc[0-9]", col_nm):
+                pc_col_names.append(col_nm)
+            elif re.match("feat_[0-9]+", col_nm):
+                diffae_feature_col_names.append(col_nm)
+            else:
+                continue
 
-        pc_col_names = [col_nm for col_nm in merged_feats_df.columns if re.match("pc[0-9]", col_nm)]
-        diffae_feature_col_names = [
-            col_nm for col_nm in merged_feats_df.columns if re.match("feat_[0-9]+", col_nm)
-        ]
         measured_col_names = [
             "alignment_deg_rel_to_flow",
             "nematic_order",
@@ -325,114 +394,119 @@ if __name__ == "__main__":
             "number_of_neighbors",
             "nuc_pos_rel_cell_angle_deg",
         ]
-        for col in measured_col_names + pc_col_names + diffae_feature_col_names:
-            if col not in merged_feats_df.columns:
-                print(
-                    f"Column {col} not found in merged_feats_df. Available columns: {merged_feats_df.columns.tolist()}"
-                )
-
-        from src.endo_pipeline.library.analyze.diffae_manifest.manifest_pca import fit_pca
+        assert all(np.isin(measured_col_names, merged_feats_df.columns)), (
+            f"Not all measured_col_names are in merged_feats_df. "
+            f"Missing: {set(measured_col_names) - set(merged_feats_df.columns)}"
+        )
 
         for meas in measured_col_names:
             if not np.isfinite(merged_feats_df[meas]).all():
                 print(f"{meas} contains non-finite values")
 
-        # 1. get the PCA
-        pca = fit_pca()
-
-        # 2. find which of the diffae features make up each PC
-        # pca.components_
-
-        # 3. find which of the diffae features correlate with which measured features
-        def get_correlation_matrix_df(
-            merged_feats_df: pd.DataFrame,
-            diffae_feature_col_names: Sequence[str],
-            measured_col_names: Sequence[str],
-        ) -> pd.DataFrame:
-            """
-            Get the correlations between the diffae features and the measured features.
-            """
-            records = []
-            for feat in diffae_feature_col_names:
-                for meas in measured_col_names:
-                    # np.isfinite(merged_feats_df[feat])
-                    valid_records = np.isfinite(merged_feats_df[feat])
-                    corr, pval = pearsonr(
-                        merged_feats_df[feat][valid_records],
-                        merged_feats_df[meas][valid_records],
-                    )
-                    # print(f"{feat} vs. {meas}: r={corr:.2f}, p={pval:.2e}")
-
-                    records.append(
-                        {"feature": feat, "measurement": meas, "pearsonr": corr, "pval": pval}
-                    )
-
-            return pd.DataFrame(records)
-
-        records = []
-        for feat in feat_col_names:
-            for meas in measured_col_names:
-                # np.isfinite(merged_feats_df[feat])
-                valid_records = np.isfinite(merged_feats_df[meas])
-                corr, pval = pearsonr(
-                    merged_feats_df[feat][valid_records],
-                    merged_feats_df[meas][valid_records],
-                )
-                # print(f"{feat} vs. {meas}: r={corr:.2f}, p={pval:.2e}")
-
-                records.append(
-                    {"feature": feat, "measurement": meas, "pearsonr": corr, "pval": pval}
-                )
-
-        correlation_table_feats = pd.DataFrame(records)
+        # 1. find which of the diffae features correlate with which measured features
+        correlation_df_feats = get_correlation_matrix_df(
+            features_df=merged_feats_df,
+            column_names_for_x_axis=measured_col_names,
+            column_names_for_y_axis=diffae_feature_col_names,
+            name_of_x_axis="measurement",
+            name_of_y_axis="feature",
+            df_format="wide-corrcoeff",
+        )
 
         fig, ax = plt.subplots(figsize=(10, 10))
-        data = correlation_table_feats.pivot(
-            index="feature", columns="measurement", values="pearsonr"
-        )
-        data.sort_index(axis="index", ascending=True, inplace=True)  # sort the PCs
-        data = data[measured_col_names]  # sort the measured features
-        sns.heatmap(data, annot=True, cmap="RdBu", center=0, vmin=-1, vmax=1)
+        sns.heatmap(correlation_df_feats, annot=True, cmap="RdBu", center=0, vmin=-1, vmax=1)
         save_plot_to_path(
             figure=fig,
             output_path=out_subdir,
-            figure_name=f"correlation_feats_vs_measured_feats_{dataset_name}",
+            figure_name=f"{dataset_name}_correlation_feats_vs_measured_feats",
         )
 
-        records = []
-        for pc in pc_col_names:
-            for meas in measured_col_names:
-                # np.isfinite(merged_feats_df[feat])
-                valid_records = np.isfinite(merged_feats_df[meas])
-                corr, pval = pearsonr(
-                    merged_feats_df[pc][valid_records],
-                    merged_feats_df[meas][valid_records],
-                )
-                # print(f"{pc} vs. {meas}: r={corr:.2f}, p={pval:.2e}")
-
-                records.append({"PC": pc, "measurement": meas, "pearsonr": corr, "pval": pval})
-
-        correlation_table_pcs = pd.DataFrame.from_records(records)
+        correlation_df_pcs = get_correlation_matrix_df(
+            features_df=merged_feats_df,
+            column_names_for_x_axis=measured_col_names,
+            column_names_for_y_axis=pc_col_names,
+            name_of_x_axis="measurement",
+            name_of_y_axis="PC",
+            df_format="wide-corrcoeff",
+        )
 
         fig, ax = plt.subplots(figsize=(10, 10))
-        data = correlation_table_pcs.pivot(index="PC", columns="measurement", values="pearsonr")
-        data.sort_index(axis="index", ascending=True, inplace=True)  # sort the PCs
-        data = data[measured_col_names]  # sort the measured features
-        sns.heatmap(data, annot=True, cmap="RdBu", center=0, vmin=-1, vmax=1)
+        sns.heatmap(correlation_df_pcs, annot=True, cmap="RdBu", center=0, vmin=-1, vmax=1)
         save_plot_to_path(
             figure=fig,
             output_path=out_subdir,
-            figure_name=f"correlation_pcs_vs_measured_feats_{dataset_name}",
+            figure_name=f"{dataset_name}_correlation_pcs_vs_measured_feats",
         )
 
         # repeat the above correlations but filter data table
         # to only include the steady state timepoints
-        from src.endo_pipeline.library.analyze.diffae_manifest.diffae_manifest_utils import (
-            get_valid_subset,
-        )
-
         merged_feats_df = get_valid_subset(
             merged_feats_df,
             dataset_name=dataset_name,
             verbose=False,
         )
+
+        correlation_df_feats = get_correlation_matrix_df(
+            features_df=merged_feats_df,
+            column_names_for_x_axis=measured_col_names,
+            column_names_for_y_axis=diffae_feature_col_names,
+            name_of_x_axis="measurement",
+            name_of_y_axis="feature",
+            df_format="wide-corrcoeff",
+        )
+
+        fig, ax = plt.subplots(figsize=(10, 10))
+        sns.heatmap(correlation_df_feats, annot=True, cmap="RdBu", center=0, vmin=-1, vmax=1)
+        save_plot_to_path(
+            figure=fig,
+            output_path=out_subdir,
+            figure_name=f"{dataset_name}_correlation_feats_vs_measured_feats_steady_state",
+        )
+
+        correlation_df_pcs = get_correlation_matrix_df(
+            features_df=merged_feats_df,
+            column_names_for_x_axis=measured_col_names,
+            column_names_for_y_axis=pc_col_names,
+            name_of_x_axis="measurement",
+            name_of_y_axis="PC",
+            df_format="wide-corrcoeff",
+        )
+
+        fig, ax = plt.subplots(figsize=(10, 10))
+        sns.heatmap(correlation_df_pcs, annot=True, cmap="RdBu", center=0, vmin=-1, vmax=1)
+        save_plot_to_path(
+            figure=fig,
+            output_path=out_subdir,
+            figure_name=f"{dataset_name}_correlation_pcs_vs_measured_feats_steady_state",
+        )
+
+        # 2. plot the PC loadings
+        # from src.endo_pipeline.workflows.development.visualize_pca_attributes import (
+        #     plot_component_loadings,
+        # )
+
+        # 3. correlations between diffae features and PCs
+        # (NOTE THAT GETTING THE PC LOADINGS IS THE CORRECT APPROACH)
+        correlation_df = get_correlation_matrix_df(
+            features_df=merged_feats_df,
+            column_names_for_x_axis=diffae_feature_col_names,
+            column_names_for_y_axis=pc_col_names,
+            name_of_x_axis="feature",
+            name_of_y_axis="PC",
+            df_format="wide-corrcoeff",
+        )
+        fig, ax = plt.subplots(figsize=(10, 10))
+        sns.heatmap(correlation_df, annot=True, cmap="RdBu", center=0, vmin=-1, vmax=1)
+
+        # 3. correlate the measured features with each other
+        # to see if any measures tend to co-occur
+        correlation_df = get_correlation_matrix_df(
+            features_df=merged_feats_df,
+            column_names_for_x_axis=measured_col_names,
+            column_names_for_y_axis=measured_col_names,
+            name_of_x_axis="measure1",
+            name_of_y_axis="measure2",
+            df_format="wide-corrcoeff",
+        )
+        fig, ax = plt.subplots(figsize=(10, 10))
+        sns.heatmap(correlation_df, annot=True, cmap="RdBu", center=0, vmin=-1, vmax=1)
