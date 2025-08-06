8c29388e242f0c5b664bfb70e8cf88cd198fe100
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2025-08-06 14:19:04 -0400

    Move nuclear_label_free_seg_path from dataset configs to seg manifest

--- src/endo_pipeline/configs/dataset_config.py
+++ src/endo_pipeline/configs/dataset_config.py
@@ -121,9 +121,6 @@ class DatasetConfig:
     channel_640_index: int | None = None
     """Index of the 640 channel."""
 
-    nuclear_label_free_seg_path: str | None = None
-    """Path to nuclear label free segmentation."""
-
     nuclear_stain_seg_path: str | None = None
     """Path to nuclear stain segmentation."""
 
