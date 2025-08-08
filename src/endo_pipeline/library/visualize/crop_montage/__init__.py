30e619e43f10672aea25ee26cb44ce711bcf937f
Erin Angelini <erin.angelini@alleninstitute.org>
2025-08-08 16:04:24 -0700

    Fix erroneous removal of `crop_montage` library methods (#539)
    * bring back files, make formatting changes
    
    * formatting
    
    * type checking, remove calls to deprecated methods
    
    * debugged issue with processing multiple datasets
    
    * debugging
    
    * fix crop reconstruction
    
    * linting and type checking
    
    * formalize documentation
    
    * finishing touches, tested workflow

---
+++ src/endo_pipeline/library/visualize/crop_montage/__init__.py
@@ -0,0 +1,9 @@
+from .create_montage import generate_contact_sheet
+from .specify_crops import filter_dataframe, load_data_for_montage, sample_dataframe
+
+__all__ = [
+    "filter_dataframe",
+    "generate_contact_sheet",
+    "load_data_for_montage",
+    "sample_dataframe",
+]
