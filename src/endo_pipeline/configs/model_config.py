2d3262891cc52f08a1cd89bba716c2fea34e93cb
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2025-08-08 20:44:16 -0400

    Move model manifests from model config to dataframe manifest

--- src/endo_pipeline/configs/model_config.py
+++ src/endo_pipeline/configs/model_config.py
@@ -4,8 +4,6 @@ from mashumaro.config import BaseConfig
 from mashumaro.types import Discriminator
 from pydantic.dataclasses import dataclass
 
-from src.endo_pipeline.manifests import DataframeLocation
-
 
 @dataclass
 class ModelManifest:
@@ -40,21 +38,8 @@ class CytoDLModelConfig(ModelConfig):
     mlflow_run_id: str
     """MLFlow run id for model."""
 
-    manifest_fmsids: list[ModelManifest] = field(default_factory=list)
-    """
-    List of DataframeLocations for datasets with features obtained from the model.
-
-    (i.e., as obtained from the `apply_diffae_model` workflow)
-    """
-
     training_datasets: list[str] = field(default_factory=list)
     """List of datasets used for training the model."""
-
-    train_dataframe: DataframeLocation | None = None
-    """DataframeLocation of the training dataset used during model training."""
-
-    test_dataframe: DataframeLocation | None = None
-    """DataframeLocation of the validation dataset used during model training."""
 
 
 @dataclass
@@ -62,4 +47,4 @@ class CellposeModelConfig(ModelConfig):
     """Cellpose model configuration for pipeline."""
 
     model_path: str
-    """Path to model trained model"""
+    """Path to trained model"""
