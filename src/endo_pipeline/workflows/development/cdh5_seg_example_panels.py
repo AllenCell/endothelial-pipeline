081b9aadee521b74503bde197f37791197d3a5c4
Serge Parent <serge.edouard.parent@gmail.com>
2025-08-15 11:04:59 -0700

    - initial commit

---
+++ src/endo_pipeline/workflows/development/cdh5_seg_example_panels.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+# Becky: I would say 20250326 (15 dyn) is probably the overall
+# most ideal dataset. The recent no flow dataset (20250728) is
+# also quite good it just has some quirks around the feedings.
+
+datasets = ["20250326_20X"]
+
+
+# panel of raw max project
+
+# panel of hysteresis thresholding
+
+# panel of
