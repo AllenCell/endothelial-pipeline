f83a11c52a2dd428de07dff0b47c463424e18874
Serge Parent <serge.edouard.parent@gmail.com>
2025-08-25 12:41:06 -0700

    - added some comments

--- src/endo_pipeline/workflows/development/apply_model_on_array_test.py
+++ src/endo_pipeline/workflows/development/apply_model_on_array_test.py
@@ -11,14 +11,17 @@ def apply_model_on_array_test(
     dataset_name: str = "20241120_20X", model_name: str = "diffae_04_10"
 ) -> np.ndarray:
 
+    # load an example image
     img = get_zarr_img_for_dataset(dataset_name, 0, resolution_level=1)
     dim_order = "TCZYX"
     img_arr = img.get_image_dask_data(dim_order, T=0)
-    crop_ex = (slice(None), slice(0, 128), slice(0, 128))  # Example crop
-    img_arr_crop_bf = img_arr[(0, slice(1, 2), *crop_ex)].compute()
+    crop_example = (slice(None), slice(0, 128), slice(0, 128))
+    img_arr_crop_bf = img_arr[(0, slice(1, 2), *crop_example)].compute()
 
+    # load the diffae model and modify the config to accept array inputs
     diffae_model = get_model_for_array_inputs(model_name, save_config_locally=True)
 
+    # run th model on the example image crop
     cytodl_output = apply_model_on_array(diffae_model, img_arr_crop_bf)
 
     return cytodl_output
