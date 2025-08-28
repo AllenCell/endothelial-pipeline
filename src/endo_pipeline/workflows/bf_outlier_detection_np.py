974fba0c4d59496beb086c56b14b27768b5dab8d
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-08-28 09:53:04 -0700

    update workflow to calc stats from dataframe

--- src/endo_pipeline/workflows/bf_outlier_detection_np.py
+++ src/endo_pipeline/workflows/bf_outlier_detection_np.py
@@ -1,6 +1,7 @@
 # %%
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 from endo_pipeline.configs import (
     DatasetConfig,
@@ -9,6 +10,7 @@ from endo_pipeline.configs import (
     load_dataset_config,
     save_dataset_config,
 )
+from endo_pipeline.io.output import get_output_path
 from endo_pipeline.library.process.bf_timepoint_outlier import detect_outliers
 
 # %% LOAD DATA
@@ -38,59 +40,49 @@ for position in dataset_config.zarr_positions:
 
 # %% Save the updated dataset configuration with auto-detected annotations
 dataset_config.timepoint_annotations = tp_annotations | auto_tp_annotations
+print(dataset_config)
+# %%
 save_dataset_config(dataset_config)
 
-# %%
-# Initialize overall statistics
-overall_detected = 0
-overall_manual = 0
-overall_missed = 0
 
-for position in dataset_config.zarr_positions:
-    # Combine manual_tp_annotations for the current position
-    manual_scope_error = set(
-        tp_annotations.get(TimepointAnnotation.BF_SCOPE_ERROR, {}).get(position, [])
-    )
-    manual_temp_artifact = set(
-        tp_annotations.get(TimepointAnnotation.BF_TEMP_ARTIFACT, {}).get(position, [])
-    )
-    manual_tps = manual_scope_error | manual_temp_artifact
+# %% Initialize overall statistics
+# Initialize an empty list to store statistics for each position
+stats = []
+for dataset_name in datasets:
+    dataset_config = load_dataset_config(dataset_name)
+    for position in dataset_config.zarr_positions:
+        # Combine manual_tp_annotations for the current position
+        manual_scope_error = set(
+            tp_annotations.get(TimepointAnnotation.BF_SCOPE_ERROR, {}).get(position, [])
+        )
+        manual_temp_artifact = set(
+            tp_annotations.get(TimepointAnnotation.BF_TEMP_ARTIFACT, {}).get(position, [])
+        )
+        manual_tps = manual_scope_error | manual_temp_artifact
 
-    # Combine auto_tp_annotations for the current position
-    auto_tps = set(auto_tp_annotations[TimepointAnnotation.BF_SCOPE_ERROR].get(position, [])) | set(
-        auto_tp_annotations.get(TimepointAnnotation.BF_TEMP_ARTIFACT, {}).get(position, [])
-    )
+        # Combine auto_tp_annotations for the current position
+        auto_scope_error = set(
+            auto_tp_annotations.get(TimepointAnnotation.AUTO_BF_SCOPE_ERROR, {}).get(position, [])
+        )
+        auto_temp_artifact = set(
+            auto_tp_annotations.get(TimepointAnnotation.AUTO_BF_TEMP_ARTIFACT, {}).get(position, [])
+        )
+        auto_tps = auto_scope_error | auto_temp_artifact
 
-    # Check if all manual_tps are accounted for in auto_tps
-    missing_tps = manual_tps - auto_tps
-    if missing_tps:
-        print(f"P{position}: Missing timepoints in auto_tp_annotations: {missing_tps}")
+        # Calculate statistics
+        stats.append(
+            {
+                "dataset_name": dataset_name,
+                "position": position,
+                "n_auto_detected": len(auto_tps),
+                "n_manual_annotated": len(manual_tps),
+                "n_missed": len(manual_tps - auto_tps),
+                "list_of_missed_tps": list(manual_tps - auto_tps),
+            }
+        )
 
-    total_detected, total_manual, missed = len(auto_tps), len(manual_tps), len(missing_tps)
-
-    print(
-        f"P{position}: \
-            Total Detected: {total_detected}, Total Manual: {total_manual}, Missed: {missed}"
-    )
-
-    overall_detected += total_detected
-    overall_manual += total_manual
-    overall_missed += missed
-
-print("\nOverall Statistics:")
-print(f"Total Detected: {overall_detected}")
-print(f"Total Manual: {overall_manual}")
-print(f"Total Missed: {overall_missed}")
-
-# %% 0.004 Stats
-import numpy as np
-
-total_detected = np.sum([70, 70, 13, 15, 140, 57, 94, 41, 78, 94, 70, 41, 84])
-total_manual = np.sum([19, 43, 6, 6, 10, 6, 66, 0, 37, 35, 1, 22, 28])
-total_missed = np.sum([1, 5, 0, 3, 1, 2, 4, 0, 0, 0, 0, 0, 0])
-
-percent_missed = (total_missed / total_manual) * 100
-print(f"Overall: Detected {total_detected}, Manual {total_manual - 1}, Missed {total_missed}")
-print(f"Percent Missed: {percent_missed:.2f}%")
-
+# Create a DataFrame from the statistics
+df = pd.DataFrame(stats)
+save_dir = get_output_path("brightfield_outlier_detection")
+df.to_parquet(save_dir / "bf_outlier_detection_stats.parquet", index=False)
 # %%
