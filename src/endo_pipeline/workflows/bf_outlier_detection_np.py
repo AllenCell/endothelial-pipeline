86b95ec63d304a24cf484f5bc901a0a4d391b2f7
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-08-28 12:44:18 -0700

    add line for testing

--- src/endo_pipeline/workflows/bf_outlier_detection_np.py
+++ src/endo_pipeline/workflows/bf_outlier_detection_np.py
@@ -14,6 +14,7 @@ from endo_pipeline.library.process.bf_timepoint_outlier import detect_outliers
 
 # %% UPDATE ANNOTATIONS IN DATASET CONFIGS
 datasets = get_datasets_in_collection("live_20X_objective_3i_microscope")
+# for dataset_name in [datasets[0]]: # For testing with a single dataset
 for dataset_name in datasets:
     dataset_config = load_dataset_config(dataset_name)
     tp_annotations = (
