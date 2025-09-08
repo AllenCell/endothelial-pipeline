16e5f17053f81afd28a6f224fe89f97ea774441a
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-09-08 12:02:10 -0700

    stack

--- src/endo_pipeline/configs/global_settings/image_data.py
+++ src/endo_pipeline/configs/global_settings/image_data.py
@@ -1,10 +1,10 @@
 """Global settings for image data processing in the endo_pipeline."""
 
-LOWER_Z_OFFSET = 4
+LOWER_Z_STACK_OFFSET = 4
 """How many slices below the "center" Z-plane to include in projections."""
 
-UPPER_Z_OFFSET = 11
+UPPER_Z_STACK_OFFSET = 11
 """How many slices above the "center" Z-plane to include in projections."""
 
-Z_OFFSETS = (LOWER_Z_OFFSET, UPPER_Z_OFFSET)
+Z_STACK_OFFSETS = (LOWER_Z_STACK_OFFSET, UPPER_Z_STACK_OFFSET)
 """Tuple containing the lower and upper Z-offsets for projections."""
