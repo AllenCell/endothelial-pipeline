ca21af81a3ea4f41f626a2843e74789cb1750704
Serge Parent <serge.edouard.parent@gmail.com>
2025-09-10 13:01:05 -0700

    - matched apply_model_on_array_test.py to Matheus' modifications

--- src/endo_pipeline/workflows/development/apply_model_on_array_test.py
+++ src/endo_pipeline/workflows/development/apply_model_on_array_test.py
@@ -1,18 +1,24 @@
 import numpy as np
 
+from endo_pipeline.library.analyze.integration import track_integration
 from endo_pipeline.library.model.apply_model import apply_model_on_array, get_model_for_array_inputs
 from endo_pipeline.library.process.get_images import get_zarr_img_for_dataset
 
 
 def apply_model_on_array_test(
-    dataset_name: str = "20241120_20X", model_name: str = "diffae_04_10"
+    dataset_name: str = "20241120_20X",
+    model_name: str = "diffae_04_10",
+    start_x: int = 0,
+    start_y: int = 0,
+    crop_size_x: int = 128,
+    crop_size_y: int = 128,
 ) -> np.ndarray:
 
     # load an example image
     img = get_zarr_img_for_dataset(dataset_name, 0, resolution_level=1)
     dim_order = "TCZYX"
     img_arr = img.get_image_dask_data(dim_order, T=0)
-    crop_example = (slice(None), slice(0, 128), slice(0, 128))
+    crop_example = (slice(None), slice(start_x, crop_size_x), slice(start_y, crop_size_y))
     img_arr_crop_bf = img_arr[(0, slice(1, 2), *crop_example)].compute()
 
     # load the diffae model and modify the config to accept array inputs
@@ -25,6 +31,22 @@ def apply_model_on_array_test(
 
 
 if __name__ == "__main__":
-    cytodl_output_example = apply_model_on_array_test()
-    for features, affine in cytodl_output_example:
-        print(features)
+
+    ds = "20241120_20X"
+
+    # cytodl_output_example = apply_model_on_array_test(dataset_name=ds)
+    # for features, affine in cytodl_output_example:
+    #     print(features)
+
+    # df = track_integration.get_diffae_feats_liveseg_feats_merged_table(dataset_name=ds)
+
+    df_precomp = track_integration.get_preprocessed_manifests_and_km_bounds(dataset_name=ds)[1]
+
+    for idx, row in df_precomp.sample(n=12).iterrows():
+        features, affine = apply_model_on_array_test(
+            dataset_name=ds,
+            start_x=row.start_x,
+            crop_size_x=row.crop_size_x,
+            start_y=row.start_y,
+            crop_size_y=row.crop_size_y,
+        )
