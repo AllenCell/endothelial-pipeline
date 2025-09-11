a49a47d4e0d6ca8d16fcdfbade2e46473a13ff4f
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-09-11 15:23:46 -0700

    reorg

--- src/endo_pipeline/workflows/z_range_selection_nb.py
+++ src/endo_pipeline/workflows/z_range_selection_nb.py
@@ -12,17 +12,17 @@ from endo_pipeline.settings import LOWER_Z_SLICE_OFFSET, UPPER_Z_SLICE_OFFSET
 datasets = get_datasets_in_collection("timelapse")
 
 # %%
+save_dir = get_output_path("z_range_selection")
+plot_histogram_upper_slices_available(datasets, save_dir)
+
+# %%
 save_dir = get_output_path(
     "z_range_selection", f"images_offsets_{LOWER_Z_SLICE_OFFSET}_{UPPER_Z_SLICE_OFFSET}"
 )
 for dataset in datasets:
     dataset_config = load_dataset_config(dataset)
     for position in dataset_config.zarr_positions:
-        visualize_z_slices_with_offsets(dataset_config, position, 0, save_dir)
-
-# %%
-save_dir = get_output_path("z_range_selection")
-plot_histogram_upper_slices_available(datasets, save_dir)
+        visualize_z_slices_with_offsets(dataset_config, position, 270, save_dir)
 
 # %%
 save_dir = get_output_path(
