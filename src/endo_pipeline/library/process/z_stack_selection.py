59ef1e5fcdb0e77fefb21311ae535f4902493ea5
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-09-11 14:47:39 -0700

    organize workflow

--- src/endo_pipeline/library/process/z_stack_selection.py
+++ src/endo_pipeline/library/process/z_stack_selection.py
@@ -5,11 +5,14 @@ from typing import cast
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from dask.array import Array
+from matplotlib.ticker import MaxNLocator
 
-from endo_pipeline.configs import DatasetConfig, get_zarr_file_for_position
+from endo_pipeline.configs import DatasetConfig, get_zarr_file_for_position, load_dataset_config
 from endo_pipeline.io import load_zarr_as_dask_array, save_plot_to_path
 from endo_pipeline.library.process.image_processing import contrast_stretching
+from endo_pipeline.settings import LOWER_Z_SLICE_OFFSET, UPPER_Z_SLICE_OFFSET
 
 logger = logging.getLogger(__name__)
 
@@ -518,3 +521,168 @@ def plot_bottom_top_slices(
 
     fname = f"{dataset}_P{position}_T{timepoint}_{label}_bottom_top_slices"
     save_plot_to_path(fig, save_dir, fname)
+
+
+def plot_vlines(
+    axis: plt.Axes,
+    center: int,
+    lower_offset: int,
+    upper_offset: int,
+    y_min: float,
+    y_max: float,
+) -> None:
+    """Plot vertical lines of global center and offsets."""
+    axis.vlines(
+        center,
+        ymin=y_min,
+        ymax=y_max,
+        colors="red",
+        linestyles="solid",
+        label=f"Global Center Slice {center}",
+    )
+    axis.vlines(
+        center - lower_offset,
+        ymin=y_min,
+        ymax=y_max,
+        colors="magenta",
+        linestyles="dashed",
+        label=f"Center - {lower_offset} Slices {center - lower_offset}",
+    )
+    axis.vlines(
+        center + upper_offset,
+        ymin=y_min,
+        ymax=y_max,
+        colors="black",
+        linestyles="dashed",
+        label=f"Center + {upper_offset} Slices {center + upper_offset}",
+    )
+
+
+def visualize_z_slices_with_offsets(
+    dataset_config: DatasetConfig, position: int, timepoint: int, save_dir: Path
+) -> None:
+    """Visualize specific z-slices from BF and CDH5 stacks based on center plane and offsets."""
+    zarr_file = get_zarr_file_for_position(dataset_config, position)
+    bf_stack = load_zarr_as_dask_array(
+        zarr_file, channels=["BF"], timepoints=timepoint, level=1, squeeze=True
+    )
+    cdh5_stack = load_zarr_as_dask_array(
+        zarr_file, channels=["EGFP"], timepoints=timepoint, level=1, squeeze=True
+    )
+
+    if dataset_config.center_z_plane is None:
+        # Handle the case where the value is None
+        print(f"The center slice is None for dataset {dataset_config.name}")
+        return
+    else:
+        center_slice = dataset_config.center_z_plane[position]
+
+    top_slice = 24
+    available_slices_above = top_slice - center_slice
+
+    if UPPER_Z_SLICE_OFFSET > available_slices_above:
+        print(f"Not enough slices above center for dataset {dataset_config.name}, skipping...")
+        return
+
+    # Brightfield (bf) variables
+    bf_center = bf_stack[center_slice, :, :].compute()
+    bf_top = bf_stack[top_slice, :, :].compute()
+    bf_lower_offset = bf_stack[center_slice - LOWER_Z_SLICE_OFFSET, :, :].compute()
+    bf_upper_offset = bf_stack[center_slice + UPPER_Z_SLICE_OFFSET, :, :].compute()
+
+    # CDH5 (cdh5) variables
+    cdh5_center = cdh5_stack[center_slice, :, :].compute()
+    cdh5_top = cdh5_stack[top_slice, :, :].compute()
+    cdh5_lower_offset = cdh5_stack[center_slice - LOWER_Z_SLICE_OFFSET, :, :].compute()
+    cdh5_upper_offset = cdh5_stack[center_slice + UPPER_Z_SLICE_OFFSET, :, :].compute()
+
+    # Brightfield (bf) min and max calculations
+    min_bf = np.percentile(bf_center, 0.2)
+    max_bf = np.percentile(bf_center, 99.8)
+
+    # CDH5 (cdh5) min and max calculations
+    min_cdh5 = np.percentile(cdh5_center, 0.2)
+    max_cdh5 = np.percentile(cdh5_center, 99.8)
+
+    # Contrast stretching
+    bf_center = contrast_stretching(bf_center, custom_range=(min_bf, max_bf))
+    bf_top = contrast_stretching(bf_top, custom_range=(min_bf, max_bf))
+    bf_lower_offset = contrast_stretching(bf_lower_offset, custom_range=(min_bf, max_bf))
+    bf_upper_offset = contrast_stretching(bf_upper_offset, custom_range=(min_bf, max_bf))
+
+    cdh5_center = contrast_stretching(cdh5_center, custom_range=(min_cdh5, max_cdh5))
+    cdh5_top = contrast_stretching(cdh5_top, custom_range=(min_cdh5, max_cdh5))
+    cdh5_lower_offset = contrast_stretching(cdh5_lower_offset, custom_range=(min_cdh5, max_cdh5))
+    cdh5_upper_offset = contrast_stretching(cdh5_upper_offset, custom_range=(min_cdh5, max_cdh5))
+
+    # Define the data and titles for each subplot
+    bf_slices = [bf_lower_offset, bf_center, bf_upper_offset, bf_top]
+    cdh5_slices = [cdh5_lower_offset, cdh5_center, cdh5_upper_offset, cdh5_top]
+    titles = [
+        f"Lower Offset Slice - {center_slice - LOWER_Z_SLICE_OFFSET}",
+        f"Center Slice - {center_slice}",
+        f"Upper Offset Slice - {center_slice + UPPER_Z_SLICE_OFFSET}",
+        "Top Slice - 24",
+    ]
+
+    fig, axes = plt.subplots(2, 4, figsize=(22, 12))  # 2 rows, 4 columns
+    for i in range(4):
+        # Brightfield (BF) slices
+        axes[0, i].imshow(bf_slices[i], cmap="gray")
+        axes[0, i].set_title(f"BF {titles[i]}")
+        axes[0, i].axis("off")
+
+        # CDH5 slices
+        axes[1, i].imshow(cdh5_slices[i], cmap="gray")
+        axes[1, i].set_title(f"CDH5 {titles[i]}")
+        axes[1, i].axis("off")
+
+    plt.suptitle(f"{dataset_config.name} Position {position} Timepoint {timepoint}\n")
+    plt.tight_layout()
+    plt.show()
+
+    save_plot_to_path(fig, save_dir, f"{dataset_config.name}_pos{position}_tp{timepoint}_im_slices")
+    plt.close()
+
+
+def plot_histogram_upper_slices_available(datasets: list[str], save_dir: Path) -> None:
+    """Plot histogram of available slices above center slice across datasets."""
+    data = []
+    for dataset in datasets:
+        dataset_config = load_dataset_config(dataset)
+        for position in dataset_config.zarr_positions:
+            center_slice = dataset_config.center_z_plane[position]
+            top_slice = 24
+            available_slices_above = top_slice - center_slice
+            data.append(
+                {
+                    "dataset": dataset_config.name,
+                    "position": position,
+                    "available_slices_above": available_slices_above,
+                }
+            )
+
+    df = pd.DataFrame(data)
+
+    fig = plt.figure(figsize=(6, 6))
+    plt.hist(df["available_slices_above"], bins=range(8, 20, 1), align="left", edgecolor="black")
+    plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.xlabel("Available Slices Above Center Slice")
+    plt.ylabel("Number of Positions")
+
+    df_11 = df[df["available_slices_above"] == 11]
+    limiting_datasets = df_11.dataset.unique()
+    text = "Datasets in 11:\n" + "\n".join(limiting_datasets)
+    plt.text(
+        0.95,
+        0.95,
+        text,
+        transform=plt.gca().transAxes,
+        fontsize=10,
+        verticalalignment="top",
+        horizontalalignment="right",
+        bbox=dict(facecolor="white", alpha=0.8),
+    )
+
+    plt.show()
+    save_plot_to_path(fig, save_dir, "available_slices_above_center_histogram")
--- src/endo_pipeline/workflows/z_range_selection_nb.py
+++ src/endo_pipeline/workflows/z_range_selection_nb.py
@@ -1,57 +1,21 @@
 # %%
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from matplotlib import colormaps
-from matplotlib.ticker import MaxNLocator
 
 from endo_pipeline.configs import get_zarr_file_for_position, load_dataset_config
 from endo_pipeline.io import load_zarr_as_dask_array
 from endo_pipeline.io.output import get_output_path, save_plot_to_path
-from endo_pipeline.library.process.image_processing import contrast_stretching
+from endo_pipeline.library.process.z_stack_selection import (
+    plot_histogram_upper_slices_available,
+    plot_vlines,
+    visualize_z_slices_with_offsets,
+)
 from endo_pipeline.settings import LOWER_Z_SLICE_OFFSET, UPPER_Z_SLICE_OFFSET
 
 # %%
-TIMEPOINT = 90
+TIMEPOINT = 0
 
-
-# %%
-def plot_vlines(
-    axis: plt.Axes,
-    center: int,
-    lower_offset: int,
-    upper_offset: int,
-    y_min: float,
-    y_max: float,
-) -> None:
-    """Plot vertical lines of global center and offsets."""
-    axis.vlines(
-        center,
-        ymin=y_min,
-        ymax=y_max,
-        colors="red",
-        linestyles="solid",
-        label=f"Global Center Slice {center}",
-    )
-    axis.vlines(
-        center - lower_offset,
-        ymin=y_min,
-        ymax=y_max,
-        colors="magenta",
-        linestyles="dashed",
-        label=f"Center - {lower_offset} Slices {center - lower_offset}",
-    )
-    axis.vlines(
-        center + upper_offset,
-        ymin=y_min,
-        ymax=y_max,
-        colors="black",
-        linestyles="dashed",
-        label=f"Center + {upper_offset} Slices {center + upper_offset}",
-    )
-
-
-# %%
 datasets = [
     # "20241120_20X", # To be excluded
     # "20241217_20X", # To be excluded
@@ -71,146 +35,23 @@ datasets = [
     "20250806_20X",
     "20250813_20X",
     "20250818_20X",
+    "20250825_20X",
+    "20250827_20X",
 ]
 
 # %%
-# for dataset in [datasets[0]]:
+save_dir = get_output_path(
+    "z_range_selection", f"offsets_{LOWER_Z_SLICE_OFFSET}_{UPPER_Z_SLICE_OFFSET}", "images"
+)
 for dataset in datasets:
     dataset_config = load_dataset_config(dataset)
     for position in dataset_config.zarr_positions:
-        zarr_file = get_zarr_file_for_position(dataset_config, position)
-
-        bf_stack = load_zarr_as_dask_array(
-            zarr_file, channels=["BF"], timepoints=TIMEPOINT, level=1, squeeze=True
-        )
-        cdh5_stack = load_zarr_as_dask_array(
-            zarr_file, channels=["EGFP"], timepoints=TIMEPOINT, level=1, squeeze=True
-        )
-
-        if dataset_config.center_z_plane is None:
-            # Handle the case where the value is None
-            print(f"The center slice is None for dataset {dataset_config.name}")
-            continue
-        else:
-            center_slice = dataset_config.center_z_plane[position]
-
-        top_slice = 24
-        available_slices_above = top_slice - center_slice
-
-        if UPPER_Z_SLICE_OFFSET > available_slices_above:
-            print(f"Not enough slices above center for dataset {dataset_config.name}, skipping...")
-            continue
-
-        # Brightfield (bf) variables
-        bf_center = bf_stack[center_slice, :, :].compute()
-        bf_top = bf_stack[top_slice, :, :].compute()
-        bf_lower_offset = bf_stack[center_slice - LOWER_Z_SLICE_OFFSET, :, :].compute()
-        bf_upper_offset = bf_stack[center_slice + UPPER_Z_SLICE_OFFSET, :, :].compute()
-
-        # CDH5 (cdh5) variables
-        cdh5_center = cdh5_stack[center_slice, :, :].compute()
-        cdh5_top = cdh5_stack[top_slice, :, :].compute()
-        cdh5_lower_offset = cdh5_stack[center_slice - LOWER_Z_SLICE_OFFSET, :, :].compute()
-        cdh5_upper_offset = cdh5_stack[center_slice + UPPER_Z_SLICE_OFFSET, :, :].compute()
-
-        # Brightfield (bf) min and max calculations
-        min_bf = np.percentile(bf_center, 0.2)
-        max_bf = np.percentile(bf_center, 99.8)
-
-        # CDH5 (cdh5) min and max calculations
-        min_cdh5 = np.percentile(cdh5_center, 0.2)
-        max_cdh5 = np.percentile(cdh5_center, 99.8)
-
-        # Contrast stretching
-        bf_center = contrast_stretching(bf_center, custom_range=(min_bf, max_bf))
-        bf_top = contrast_stretching(bf_top, custom_range=(min_bf, max_bf))
-        bf_lower_offset = contrast_stretching(bf_lower_offset, custom_range=(min_bf, max_bf))
-        bf_upper_offset = contrast_stretching(bf_upper_offset, custom_range=(min_bf, max_bf))
-
-        cdh5_center = contrast_stretching(cdh5_center, custom_range=(min_cdh5, max_cdh5))
-        cdh5_top = contrast_stretching(cdh5_top, custom_range=(min_cdh5, max_cdh5))
-        cdh5_lower_offset = contrast_stretching(
-            cdh5_lower_offset, custom_range=(min_cdh5, max_cdh5)
-        )
-        cdh5_upper_offset = contrast_stretching(
-            cdh5_upper_offset, custom_range=(min_cdh5, max_cdh5)
-        )
-
-        # Define the data and titles for each subplot
-        bf_slices = [bf_lower_offset, bf_center, bf_upper_offset, bf_top]
-        cdh5_slices = [cdh5_lower_offset, cdh5_center, cdh5_upper_offset, cdh5_top]
-        titles = [
-            f"Lower Offset Slice - {center_slice - LOWER_Z_SLICE_OFFSET}",
-            f"Center Slice - {center_slice}",
-            f"Upper Offset Slice - {center_slice + UPPER_Z_SLICE_OFFSET}",
-            "Top Slice - 24",
-        ]
-
-        fig, axes = plt.subplots(2, 4, figsize=(22, 12))  # 2 rows, 4 columns
-        for i in range(4):
-            # Brightfield (BF) slices
-            axes[0, i].imshow(bf_slices[i], cmap="gray")
-            axes[0, i].set_title(f"BF {titles[i]}")
-            axes[0, i].axis("off")
-
-            # CDH5 slices
-            axes[1, i].imshow(cdh5_slices[i], cmap="gray")
-            axes[1, i].set_title(f"CDH5 {titles[i]}")
-            axes[1, i].axis("off")
-
-        plt.suptitle(f"{dataset_config.name} Position {position} Timepoint {TIMEPOINT}\n")
-        plt.tight_layout()
-        plt.show()
-        save_dir = get_output_path(
-            "z_range_selection", f"offsets_{LOWER_Z_SLICE_OFFSET}_{UPPER_Z_SLICE_OFFSET}", "images"
-        )
-        save_plot_to_path(
-            fig, save_dir, f"{dataset_config.name}_pos{position}_tp{TIMEPOINT}_im_slices"
-        )
-        plt.close()
+        visualize_z_slices_with_offsets(dataset_config, position, TIMEPOINT, save_dir)
+        break
 
 # %%
-data = []
-
-for dataset in datasets:
-    dataset_config = load_dataset_config(dataset)
-    for position in dataset_config.zarr_positions:
-        center_slice = dataset_config.center_z_plane[position]
-        top_slice = 24
-        available_slices_above = top_slice - center_slice
-        data.append(
-            {
-                "dataset": dataset_config.name,
-                "position": position,
-                "available_slices_above": available_slices_above,
-            }
-        )
-
-df = pd.DataFrame(data)
-
-fig = plt.figure(figsize=(6, 6))
-plt.hist(df["available_slices_above"], bins=range(8, 20, 1), align="left", edgecolor="black")
-plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
-plt.xlabel("Available Slices Above Center Slice")
-plt.ylabel("Number of Positions")
-
-df_11 = df[df["available_slices_above"] == 11]
-limiting_datasets = df_11.dataset.unique()
-text = "Datasets in 11:\n" + "\n".join(limiting_datasets)
-plt.text(
-    0.95,
-    0.95,
-    text,
-    transform=plt.gca().transAxes,
-    fontsize=10,
-    verticalalignment="top",
-    horizontalalignment="right",
-    bbox=dict(facecolor="white", alpha=0.8),
-)
-
-plt.show()
 save_dir = get_output_path("z_range_selection")
-save_plot_to_path(fig, save_dir, "available_slices_above_center_histogram")
+plot_histogram_upper_slices_available(datasets, save_dir)
 
 # %%
 colormap = colormaps["tab20"]
@@ -268,11 +109,13 @@ for POSITION in range(6):
         axes[0].plot(normalized_x, bf_std_normalized, color=colors[i])
         axes[0].set_xlabel("Normalized Z Slice (Center = 0)")
         axes[0].set_ylabel("Normalized BF Standard Deviation")
+        axes[0].set_ylim(0.99, 1.35)
 
         # Plot CDH5 Total Intensity
         axes[1].plot(normalized_x, cdh5_hist_normalized, label=dataset_config.name, color=colors[i])
         axes[1].set_xlabel("Normalized Z Slice (Center = 0)")
         axes[1].set_ylabel("Normalized CDH5 Total Intensity")
+        axes[1].set_ylim(0.84, 1.1)
 
     plot_vlines(
         axes[0],
@@ -303,7 +146,7 @@ for POSITION in range(6):
     save_plot_to_path(fig, save_dir, f"pos{POSITION}_tp{TIMEPOINT}_normalized_profiles")
 
 # %%
-for dataset in datasets:
+for dataset in ["20250813_20X"]:
     fig, axes = plt.subplots(1, 2, figsize=(12, 5))
     dataset_config = load_dataset_config(dataset)
     for POSITION in dataset_config.zarr_positions:
@@ -314,6 +157,7 @@ for dataset in datasets:
         )
         zarr_file = get_zarr_file_for_position(dataset_config, POSITION)
 
+        # for TIMEPOINT in [0, 90, 180, 270]:
         bf_stack = load_zarr_as_dask_array(
             zarr_file, channels=["BF"], timepoints=TIMEPOINT, level=1, squeeze=True
         )
@@ -343,6 +187,7 @@ for dataset in datasets:
         axes[0].plot(normalized_x, bf_std_normalized)
         axes[0].set_xlabel("Normalized Z Slice (Center = 0)")
         axes[0].set_ylabel("Normalized BF Standard Deviation")
+        axes[0].set_ylim(0.99, 1.35)
 
         # Plot CDH5 Total Intensity
         axes[1].plot(
@@ -350,6 +195,7 @@ for dataset in datasets:
         )
         axes[1].set_xlabel("Normalized Z Slice (Center = 0)")
         axes[1].set_ylabel("Normalized CDH5 Total Intensity")
+        axes[1].set_ylim(0.84, 1.1)
 
     plot_vlines(
         axes[0],
