6b75af0a80af147b8f7729883e38abc40743ffdf
Serge Parent <serge.edouard.parent@gmail.com>
2025-09-24 15:35:07 -0700

    - added manifest for cdh5 seg validation images and saving panels for them

--- src/endo_pipeline/workflows/development/cdh5_seg_example_panels.py
+++ src/endo_pipeline/workflows/development/cdh5_seg_example_panels.py
@@ -2,11 +2,15 @@ from pathlib import Path
 from typing import Literal
 
 import numpy as np
+from bioio import BioImage
 from matplotlib import pyplot as plt
+
+from endo_pipeline.library.process.general_image_preprocessing import get_default_dim_order
 
 ## NOTE TO SELF: MOVE THIS CODE TO A LIBRARY FILE
 DPI_IMAGING = 300
 DPI_PLOTS = 1000
+DIMENSION_ORDER = get_default_dim_order()
 
 PANEL_SIZE = (3, 3)
 CROP_YX = (slice(None), slice(None))
@@ -25,99 +29,139 @@ def save_image_as_panel(
         plt.close(fig)
 
 
-## NOTE TO SELF: END OF LIBRARY CODE
-
-
-def main() -> None:
-    import numpy as np
-    from matplotlib import pyplot as plt
+def make_imaging_panels() -> None:
     from skimage.color import label2rgb
     from skimage.exposure import rescale_intensity
+    from skimage.morphology import binary_dilation
 
     from endo_pipeline.configs import get_zarr_file_for_position, load_dataset_config
     from endo_pipeline.io import get_output_path, load_image, load_zarr_as_dask_array
-    from endo_pipeline.library.process.general_image_preprocessing import get_default_dim_order
     from endo_pipeline.manifests import get_image_location_for_dataset, load_image_manifest
 
     # Becky: I would say 20250326 (15 dyn) is probably the overall
     # most ideal dataset. The recent no flow dataset (20250728) is
     # also quite good it just has some quirks around the feedings.
-
-    dataset_name = "20250326_20X"
+    # [x] panel of raw nuclei brightfield
+    # [x] panel of nuclei brightfield std
+    # [x] panel of labelfree nuclei prediction
+    # [x] panel of raw max project
+    # [x] panel of hysteresis thresholding
+    # [x] panel of initial cdh5 segmentations
+    # [x] panel of merged cdh5 segmentations
+    # [x] panel of labelfree nuclei-refined cdh5 segmentations
+    # dataset_name = "20250326_20X"
+    dataset_name = "20250728_20X"
     position = 0
-    timeframe = 276
+    timeframe = 288
 
     out_dir = get_output_path(__file__)
 
-    # panel of raw nuclei brightfield
-    # [x] panel of nuclei brightfield std
-    # panel of labelfree nuclei prediction
-    # panel of raw max project
-    # panel of hysteresis thresholding
-    # panel of initial cdh5 segmentations
-    # panel of merged cdh5 segmentations
-    # panel of labelfree nuclei-refined cdh5 segmentations
+    # Load the validation image (which has some intermediate steps saved)
+    val_manifest = load_image_manifest("cdh5_seg_validations")
+    val_location = get_image_location_for_dataset(val_manifest, dataset_name, position, timeframe)
+    val_image = BioImage(val_location.path)  # type:ignore[arg-type]
+    channel_names = val_image.channel_names
+    val_dask = val_image.get_image_dask_data(DIMENSION_ORDER)
 
-    dim_order = get_default_dim_order()
+    panel_dict = {}
+    for i, chan in enumerate(channel_names):
+        panel = np.take(val_dask, indices=[i], axis=DIMENSION_ORDER.index("C"))
+        panel = panel.compute().squeeze()  # type:ignore[attr-defined]
+        panel_dict[chan] = panel
 
+    # Rename some keys for clarity
+    # "nuclei_predictions" is combo of segmentation skeletons and nuclei predictions; used as seeds
+    panel_dict["seeds"] = panel_dict.pop("nuclei_predictions")
+    # "raw" is a max intensity projection (MIP) of the cdh5 channel
+    panel_dict["cdh5_mip"] = panel_dict.pop("raw")
+    # "processed" is the preprocessed cdh5 MIP channel
+    panel_dict["cdh5_processed"] = panel_dict.pop("processed")
+
+    # Load the nuclei predictions image (this one is nuclei predictions only)
     nuc_manifest = load_image_manifest("nuclear_labelfree_seg")
     nuc_location = get_image_location_for_dataset(nuc_manifest, dataset_name, position, timeframe)
-    nuc_pred = load_image(nuc_location)
-
-    cdh5_manifest = load_image_manifest("cdh5_classic_seg")
-    cdh5_location = get_image_location_for_dataset(cdh5_manifest, dataset_name, position, timeframe)
-    cdh5_seg = load_image(cdh5_location)
+    nuc_pred = np.asarray(load_image(nuc_location))
 
     dataset_config = load_dataset_config(dataset_name)
     bf_center_Z = dataset_config.center_z_plane[position]  # type:ignore[index]
     zarr_file = get_zarr_file_for_position(dataset_config, position)
-    raw_cdh5 = load_zarr_as_dask_array(zarr_file, channels=["EGFP"], timepoints=timeframe, level=0)
     raw_bf = load_zarr_as_dask_array(zarr_file, channels=["BF"], timepoints=timeframe, level=0)
 
-    cdh5_mip = raw_cdh5.max(axis=dim_order.index("Z"), keepdims=True).compute()
-    bf_std = raw_bf.std(axis=dim_order.index("Z"), keepdims=True).compute()
+    # Get the focal plane of the brightfield image
     bf_center = np.take(
-        raw_bf, indices=[bf_center_Z], axis=dim_order.index("Z")
+        raw_bf, indices=[bf_center_Z], axis=DIMENSION_ORDER.index("Z")
     ).compute()  # type:ignore[attr-defined]
-    bf_center_clipped = np.clip(
-        bf_center, a_min=np.percentile(bf_center, 0.01), a_max=np.percentile(bf_center, 99.9)
-    )
-    cdh5_mip_clipped = np.clip(
-        cdh5_mip, a_min=np.percentile(cdh5_mip, 0.01), a_max=np.percentile(cdh5_mip, 99.9)
-    )
-    bf_std_clipped = np.clip(
-        bf_std, a_min=np.percentile(bf_std, 0.01), a_max=np.percentile(bf_std, 99.9)
-    )
+    # bf_center_clipped = np.clip(
+    #     bf_center, a_min=np.percentile(bf_center, 0.01), a_max=np.percentile(bf_center, 99.9)
+    # )
+    # bf_center_normd = rescale_intensity(bf_center_clipped, in_range="image", out_range=(0, 1))
 
-    bf_center_normd = rescale_intensity(bf_center_clipped, in_range="image", out_range=(0, 1))
-    cdh5_mip_normd = rescale_intensity(cdh5_mip_clipped, in_range="image", out_range=(0, 1))
-    bf_std_normd = rescale_intensity(bf_std_clipped, in_range="image", out_range=(0, 1))
+    # Get the standard deviation projection of the brightfield image
+    bf_std = raw_bf.std(axis=DIMENSION_ORDER.index("Z"), keepdims=True).compute()
+    # bf_std_clipped = np.clip(
+    #     bf_std, a_min=np.percentile(bf_std, 0.01), a_max=np.percentile(bf_std, 99.9)
+    # )
+    # bf_std_normd = rescale_intensity(bf_std_clipped, in_range="image", out_range=(0, 1))
 
-    panel_dict = {
-        "bf_center": bf_center_normd,
-        "cdh5_mip": cdh5_mip_normd,
-        "bf_std": bf_std_normd,
-        "nuc_pred": nuc_pred,
-        "cdh5_seg": cdh5_seg,
-    }
+    # Add brightfield and nuclei prediction panels to the dict
+    panel_dict.update({"bf_center": bf_center, "bf_std": bf_std, "nuc_pred": nuc_pred})
+
+    # Clip and normalize channels with microscopy images
+    imaging_panels = ("bf_center", "bf_std", "cdh5_mip", "cdh5_processed")
+    for panel_name in imaging_panels:
+        panel = panel_dict[panel_name]
+        panel_clipped = np.clip(
+            panel, a_min=np.percentile(panel, 0.01), a_max=np.percentile(panel, 99.9)
+        )
+        panel_normd = rescale_intensity(panel_clipped, in_range="image", out_range=(0, 1))
+        panel_dict[panel_name] = panel_normd
+
     # Take crops and reduce dimensionality to 2D
-    panel_dict = {key: panel[CROP_YX].squeeze() for key, panel in panel_dict.items()}
+    panel_dict = {panel_name: panel[CROP_YX].squeeze() for panel_name, panel in panel_dict.items()}
     # Add overlay panels to the dict
+    seed_and_border_mask = (
+        binary_dilation(panel_dict["seeds"]).astype(int) * 1
+        + binary_dilation(panel_dict["cdh5_segmentations_split_by_nuclei_borders"]).astype(int) * 2
+    )
     panel_dict.update(
         {
             "nuc_pred_overlay": label2rgb(
-                panel_dict["nuc_pred"], image=panel_dict["bf_std"], bg_label=0
+                label=panel_dict["nuc_pred"], image=panel_dict["bf_std"], bg_label=0
             ),
             "cdh5_seg_overlay": label2rgb(
-                panel_dict["cdh5_seg"], image=panel_dict["cdh5_mip"], bg_label=0
+                label=panel_dict["cdh5_segmentations_split_by_nuclei"],
+                image=panel_dict["cdh5_mip"],
+                bg_label=0,
+            ),
+            "seeds_overlay": label2rgb(
+                label=seed_and_border_mask,
+                image=panel_dict["cdh5_mip"],
+                bg_label=0,
+                colors=("cyan", "magenta"),
+                alpha=0.5,
             ),
         }
     )
 
-    for panel in panel_dict:
+    for panel_name in panel_dict:
         save_image_as_panel(
-            image=panel_dict[panel],
+            image=panel_dict[panel_name],
             figsize=PANEL_SIZE,
-            out_path=out_dir / f"{panel}.png",
+            out_path=out_dir / f"{panel_name}.png",
             show=True,
         )
+
+
+def make_classic_feature_panels() -> None:
+    pass
+
+
+## NOTE TO SELF: END OF LIBRARY CODE
+
+
+def main() -> None:
+    make_imaging_panels()
+    # make_classic_feature_panels()
+
+
+main()
