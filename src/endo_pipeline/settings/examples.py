608636a5be1d7c6a7a1141aa9409e77a5d662b10
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-10-03 12:10:17 -0700

    remove comment

--- src/endo_pipeline/settings/examples.py
+++ src/endo_pipeline/settings/examples.py
@@ -2,5 +2,5 @@ EXAMPLE_DATASET = {
     "SUP_FIG_Z_SLICE": "20250428_20X",
     "SUP_FIG_IMG_PROC": "20250110_paired20X",
     "SUPP_FIG_SINGLE_TP_OUTLIER": "20250319_20X",
-}  # Single timepoint live cell zarr
+}
 """Dictionary of example datasets for specific supplemental figures."""
