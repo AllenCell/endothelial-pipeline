6be54017c6b552f2c4e179be6946bc915407f52e
Erin Angelini <erin.angelini@alleninstitute.org>
2025-10-03 13:23:47 -0700

    rm yaml from settings names

--- src/endo_pipeline/settings/diffae_configs.py
+++ src/endo_pipeline/settings/diffae_configs.py
@@ -1,16 +1,16 @@
 """Global settings for template DiffAE model configurations."""
 
-EVAL_CONFIG = "diffae_eval.yaml"
+EVAL_CONFIG = "diffae_eval"
 """Name of the template configuration file for evaluating a DiffAE model."""
 
-TRAIN_CONFIG = "diffae_train.yaml"
+TRAIN_CONFIG = "diffae_train"
 """Name of the template configuration file for training a DiffAE model."""
 
-FINETUNE_TRAIN_CONFIG = "diffae_finetune_train.yaml"
+FINETUNE_TRAIN_CONFIG = "diffae_finetune_train"
 """Name of the template configuration file for finetuning a DiffAE model."""
 
-FINETUNE_EVAL_CONFIG = "diffae_finetune_eval.yaml"
+FINETUNE_EVAL_CONFIG = "diffae_finetune_eval"
 """Name of the template configuration file for evaluating a finetuned DiffAE model."""
 
-LEGACY_CONFIG = "diffae_04_10_eval.yaml"
+LEGACY_CONFIG = "diffae_04_10_eval"
 """Name of the configuration file for evaluating the legacy DiffAE model diffae_04_10."""
