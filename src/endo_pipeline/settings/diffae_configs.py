daf2bca4c1d1fc8ed2f0e0d1e0a01e30cfa3653d
Erin <erin.angelini@alleninstitute.org>
2025-10-06 11:37:12 -0700

    flip order of TRAIN/EVAL and FINETUNE in global constant names for sorting purposes

--- src/endo_pipeline/settings/diffae_configs.py
+++ src/endo_pipeline/settings/diffae_configs.py
@@ -6,10 +6,10 @@ DIFFAE_MODEL_EVAL_CONFIG = "diffae_eval"
 DIFFAE_MODEL_TRAIN_CONFIG = "diffae_train"
 """Name of the template configuration file for training a DiffAE model."""
 
-DIFFAE_MODEL_FINETUNE_TRAIN_CONFIG = "diffae_finetune_train"
+DIFFAE_MODEL_TRAIN_FINETUNE_CONFIG = "diffae_finetune_train"
 """Name of the template configuration file for finetuning a DiffAE model."""
 
-DIFFAE_MODEL_FINETUNE_EVAL_CONFIG = "diffae_finetune_eval"
+DIFFAE_MODEL_EVAL_FINETUNE_CONFIG = "diffae_finetune_eval"
 """Name of the template configuration file for evaluating a finetuned DiffAE model."""
 
 DIFFAE_MODEL_LEGACY_CONFIG = "diffae_04_10_eval"
