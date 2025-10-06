b67c5b11b98ecda9a011421b961041e632e777fd
Erin <erin.angelini@alleninstitute.org>
2025-10-06 11:34:47 -0700

    add DIFFAE_MODEL_ to global constant names

--- src/endo_pipeline/settings/diffae_configs.py
+++ src/endo_pipeline/settings/diffae_configs.py
@@ -1,16 +1,16 @@
-"""Global settings for template DiffAE model configurations."""
+"""Global settings for template DiffAE model configuration files."""
 
-EVAL_CONFIG = "diffae_eval"
+DIFFAE_MODEL_EVAL_CONFIG = "diffae_eval"
 """Name of the template configuration file for evaluating a DiffAE model."""
 
-TRAIN_CONFIG = "diffae_train"
+DIFFAE_MODEL_TRAIN_CONFIG = "diffae_train"
 """Name of the template configuration file for training a DiffAE model."""
 
-FINETUNE_TRAIN_CONFIG = "diffae_finetune_train"
+DIFFAE_MODEL_FINETUNE_TRAIN_CONFIG = "diffae_finetune_train"
 """Name of the template configuration file for finetuning a DiffAE model."""
 
-FINETUNE_EVAL_CONFIG = "diffae_finetune_eval"
+DIFFAE_MODEL_FINETUNE_EVAL_CONFIG = "diffae_finetune_eval"
 """Name of the template configuration file for evaluating a finetuned DiffAE model."""
 
-LEGACY_CONFIG = "diffae_04_10_eval"
+DIFFAE_MODEL_LEGACY_CONFIG = "diffae_04_10_eval"
 """Name of the configuration file for evaluating the legacy DiffAE model diffae_04_10."""
