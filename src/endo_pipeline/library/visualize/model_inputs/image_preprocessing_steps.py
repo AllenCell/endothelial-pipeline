d9a8ea29caa3bd230f9318b3ccb40ca299b416b2
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-10-07 10:52:14 -0700

    update methods and documentation

--- src/endo_pipeline/library/visualize/model_inputs/image_preprocessing_steps.py
+++ src/endo_pipeline/library/visualize/model_inputs/image_preprocessing_steps.py
@@ -5,7 +5,9 @@ from typing import Any
 
 import numpy as np
 import torch
+from dask.array import Array
 from matplotlib import pyplot as plt
+from omegaconf import OmegaConf
 
 from endo_pipeline.io.output import save_plot_to_path, save_thumbnail_to_path
 from endo_pipeline.settings.image_data import PIXEL_SIZE_3i_20x
@@ -13,29 +15,47 @@ from endo_pipeline.settings.image_data import PIXEL_SIZE_3i_20x
 logger = logging.getLogger(__name__)
 
 
-def plot_and_save_histogram(
-    value_np: np.ndarray, transform: Any, key: str, save_dir: Path, i: int
-) -> None:
+def save_stack_slices_as_thumbnails(img: Array, save_dir: Path) -> None:
+    """
+    Save each slice of a multi-channel image stack as individual thumbnail images.
 
-    fig, ax = plt.subplots(figsize=(6, 6))
-    ax.hist(value_np.ravel(), bins=50, color="grey", alpha=0.7)
-    ax.set_title(f"{transform.__class__.__name__} ({key})", fontsize=14)
-    ax.set_xlabel("Intensity")
-    ax.set_ylabel("Frequency")
-    # ax.grid(True)
-    plt.show()
-    save_plot_to_path(
-        fig,
-        save_dir,
-        f"{key}_{i}_{transform.__class__.__name__}_histogram",
-        dpi=300,
-        file_format=".pdf",
-        transparent=True,
-    )
-    plt.close(fig)
+    This function iterates through all channels and slices of a Dask array representing
+    an image stack, computes each slice, and saves it as a thumbnail image to the specified directory.
+
+    Args:
+        img (dask.array.Array): A Dask array representing the image stack.
+            The array is expected to have the shape (channels, slices, height, width).
+        save_dir (Path): The directory where the thumbnail images will be saved.
+    """
+    for channel in range(img.shape[0]):
+        for slice_idx in range(img.shape[1]):
+            slice_img = img[channel, slice_idx, :, :].compute()
+            save_thumbnail_to_path(
+                slice_img,
+                f"original_channel{channel}_slice{slice_idx}",
+                save_dir,
+                figsize=(6, 6),
+                scalebar_size_um=50,
+                pixel_size=PIXEL_SIZE_3i_20x,
+            )
 
 
 def initialize_transform(transform_cfg):
+    """
+    Dynamically initialize a transform object from a configuration dictionary.
+
+    This function takes a configuration dictionary that specifies the target class
+    (using the `_target_` key) and its parameters. It dynamically imports the module,
+    retrieves the class, and initializes an instance of the class with the provided parameters.
+
+    Args:
+        transform_cfg (dict): A dictionary containing the configuration for the transform.
+            - `_target_` (str): The full path to the class (e.g., "module.submodule.ClassName").
+            - Other keys: Parameters to initialize the class.
+
+    Returns:
+        Any: An instance of the dynamically initialized class.
+    """
     # Extract the module and class name from the _target_ field
     target_path = transform_cfg["_target_"]
     module_name, class_name = target_path.rsplit(".", 1)
@@ -49,10 +69,76 @@ def initialize_transform(transform_cfg):
     return target_class(**params)
 
 
-def run_and_visualize_transforms(transforms, sample, save_dir, target_key):
+def prepare_train_transforms(
+    model_config,
+):
     """
-    Apply each transform in sequence to the sample, but only visualize
-    the specified target_key if the transform actually modifies it.
+    Prepare and initialize the training transform pipeline with an optional squeeze operation.
+
+    Args:
+        model_config: The model configuration containing the training dataloader transform settings.
+
+    Returns:
+        list: A list of initialized transforms for the training pipeline.
+    """
+    # Access the training transform configuration
+    train_transform_cfg = model_config.data.train_dataloaders.dataset.transform
+
+    # Define the squeeze transform
+    temp_squeeze = {"_target_": "monai.transforms.SqueezeDimd", "keys": "raw", "dim": 0}
+
+    # Insert the squeeze transform into the pipeline
+    train_transform_cfg.transforms.insert(1, OmegaConf.create(temp_squeeze))
+
+    # Initialize all transforms in the pipeline
+    return [initialize_transform(t) for t in train_transform_cfg.transforms]
+
+
+def plot_and_save_histogram(
+    value_np: np.ndarray, transform: Any, key: str, save_dir: Path, i: int
+) -> None:
+    """
+    Plot and save a histogram of the values in a NumPy array for a given image transform.
+
+    Args:
+        value_np (np.ndarray): The NumPy array containing the values to plot.
+        transform (Any): The transform object whose class name is used for labeling the plot.
+        key (str): channel key of the image being processed (e.g., 'raw_bf').
+        save_dir (Path): The directory where the histogram plot will be saved.
+        i (int): An index representing the order of the transform in the pipeline.
+    """
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.hist(value_np.ravel(), bins=50, color="grey", alpha=0.7)
+    ax.set_title(f"{transform.__class__.__name__} ({key})", fontsize=14)
+    ax.set_xlabel("Intensity")
+    ax.set_ylabel("Frequency")
+    plt.show()
+    save_plot_to_path(
+        fig,
+        save_dir,
+        f"{key}_{i}_{transform.__class__.__name__}_histogram",
+        dpi=300,
+        file_format=".pdf",
+        transparent=True,
+    )
+    plt.close(fig)
+
+
+def run_and_visualize_transforms(
+    transforms: list[Any], sample: dict[str, Any], save_dir: Path, target_key: str
+) -> None:
+    """
+    Apply a sequence of transforms to a sample and visualize the results.
+
+    This function iterates through a list of transforms, applies each transform
+    to the input sample, and visualizes the specified `target_key` only if the
+    transform modifies it. The visualizations are saved to the specified directory.
+
+    Args:
+        transforms (List[Any]): A list of transform objects to apply to the sample.
+        sample (Dict[str, Any]): A dictionary representing the input data
+        save_dir (Path): The directory where visualizations will be saved.
+        target_key (str): The key in the sample dictionary to visualize (e.g., 'raw_bf').
     """
     for i, transform in enumerate(transforms):
         logger.info(f"\nApplying Transform {i+1}: {transform.__class__.__name__}")
--- src/endo_pipeline/workflows/figures/supp_fig_model_input_preprocessing_nb.py
+++ src/endo_pipeline/workflows/figures/supp_fig_model_input_preprocessing_nb.py
@@ -1,22 +1,17 @@
 # %%
 import logging
 
-from omegaconf import OmegaConf
-
 from endo_pipeline.configs import get_zarr_file_for_position, load_dataset_config
 from endo_pipeline.io.input import load_model_config_from_path, load_zarr_as_dask_array
-from endo_pipeline.io.output import get_output_path, save_thumbnail_to_path
+from endo_pipeline.io.output import get_output_path
 from endo_pipeline.library.visualize.model_inputs.image_preprocessing_steps import (
-    initialize_transform,
+    prepare_train_transforms,
     run_and_visualize_transforms,
+    save_stack_slices_as_thumbnails,
 )
 from endo_pipeline.settings import RELATIVE_PATH_TO_TRAIN_CONFIG
 from endo_pipeline.settings.examples import EXAMPLE_DATASET
-from endo_pipeline.settings.image_data import (
-    LOWER_Z_SLICE_OFFSET,
-    UPPER_Z_SLICE_OFFSET,
-    PIXEL_SIZE_3i_20x,
-)
+from endo_pipeline.settings.image_data import LOWER_Z_SLICE_OFFSET, UPPER_Z_SLICE_OFFSET
 
 logger = logging.getLogger(__name__)
 
@@ -34,41 +29,22 @@ zarr_path = get_zarr_file_for_position(dataset_config, POSITION)
 img = load_zarr_as_dask_array(zarr_path, level=1, squeeze=True)
 
 # %% Panel A - Thumbnail of each slice in Z-stack for each channel
-for channel in range(img.shape[0]):
-    for slice_idx in range(img.shape[1]):
-        slice_img = img[channel, slice_idx, :, :].compute()
-        save_thumbnail_to_path(
-            slice_img,
-            f"original_channel{channel}_slice{slice_idx}",
-            save_dir,
-            figsize=(6, 6),
-            scalebar_size_um=50,
-            pixel_size=PIXEL_SIZE_3i_20x,
-        )
+save_stack_slices_as_thumbnails(img, save_dir)
+
 # %% Panel B - Z slices used in preprocessing steps
 in_focus_slice = dataset_config.center_z_plane[POSITION]
 logging.info(f"Dataset {DATASET}, position {POSITION}, in-focus z slice: {in_focus_slice}")
 logging.info("Lower z slice:", in_focus_slice - LOWER_Z_SLICE_OFFSET)
 logging.info("Upper z slice:", in_focus_slice + UPPER_Z_SLICE_OFFSET)
 
-# %% Load model config
-model_config = load_model_config_from_path(RELATIVE_PATH_TO_TRAIN_CONFIG)
-
-# Access the training transform configuration
-train_transform_cfg = model_config.data.train_dataloaders.dataset.transform
-temp_squeeze = {"_target_": "monai.transforms.SqueezeDimd", "keys": "raw", "dim": 0}
-# Insert squeeze into the list of transforms so that we can visualize the 2D image
-train_transform_cfg.transforms.insert(1, OmegaConf.create(temp_squeeze))
-
+# %% Load model config and initialize transforms
 sample = {"original_path": str(zarr_path)}
+model_config = load_model_config_from_path(RELATIVE_PATH_TO_TRAIN_CONFIG)
+transforms = prepare_train_transforms(model_config)
 
-# Initialize all transforms in the pipeline
-transforms = [initialize_transform(t) for t in train_transform_cfg.transforms]
-
-
-# Step through each transformation and visualize the output
-# %%
+# Step through each transformation and visualize the processing steps for each channel
+# %% Panel C - BF
 run_and_visualize_transforms(transforms, sample, save_dir, target_key="raw_bf")
-# %%
+# %% Panel D - CDH5
 run_and_visualize_transforms(transforms, sample, save_dir, target_key="raw_cdh5")
 # %%
