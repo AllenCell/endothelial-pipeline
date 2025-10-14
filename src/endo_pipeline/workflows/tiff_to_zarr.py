0eabca424c82d2a0e976f250f72f70b9ad6cbf58
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-10-14 14:13:38 -0700

    tiff to zarr conversion

---
+++ src/endo_pipeline/workflows/tiff_to_zarr.py
@@ -0,0 +1,64 @@
+# %%
+import logging
+from pathlib import Path
+
+import pandas as pd
+
+from endo_pipeline.configs import get_zarr_file_for_position, load_dataset_config
+from endo_pipeline.io import get_output_path
+from endo_pipeline.manifests import (
+    get_image_location_for_dataset,
+    list_datasets_with_images,
+    load_image_manifest,
+)
+
+log = logging.getLogger(__name__)
+
+
+# %%
+IMAGE_MANIFEST = "nuclear_labelfree_seg"
+ZARR_SEG_PATH = (
+    f"//allen/aics/endothelial/morphological_features/segmentations/{IMAGE_MANIFEST}_zarr/"
+)
+
+out_dir = get_output_path("tiff_to_zarr")
+
+# %%
+image_manifest = load_image_manifest(IMAGE_MANIFEST)
+datasets = list_datasets_with_images(image_manifest)
+
+data = []
+
+for dataset_name in datasets:
+    dataset_config = load_dataset_config(dataset_name)
+    for position in dataset_config.zarr_positions:
+
+        location = get_image_location_for_dataset(
+            image_manifest, dataset_name, position=position, timepoint=0
+        )
+        if location is None:
+            log.warning(
+                "No location found for dataset [ %s ] position [ %s ]", dataset_name, position
+            )
+
+        else:
+            seg_dir = location.path.parent
+            original_zarr = get_zarr_file_for_position(dataset_config, position)
+            save_path = Path(ZARR_SEG_PATH) / original_zarr.name
+
+            data.append(
+                {
+                    "dataset_name": dataset_name,
+                    "original_zarr": original_zarr,
+                    "tiff_seg_dir": str(seg_dir),
+                    "save_zarr_path": str(save_path),
+                }
+            )
+
+df = pd.DataFrame(data)
+
+# %%
+
+df.to_csv(out_dir / f"tiff_to_zarr_conversion_{IMAGE_MANIFEST}.csv", index=False)
+
+# %%
