2274fdd92b9ccdb0573d8a57098539949eabe565
Erin <erin.angelini@alleninstitute.org>
2025-10-17 10:43:18 -0700

    move contents of dataframe_preprocessing to diffae_dataframe_utils

--- src/endo_pipeline/library/analyze/diffae_dataframe/__init__.py
+++ src/endo_pipeline/library/analyze/diffae_dataframe/__init__.py
@@ -1,16 +1,14 @@
 from ..diffae_dataframe_utils import (
-    get_dataset_descriptions,
-    get_traj_and_diff,
-    get_valid_subset,
-    split_dataset_by_flow,
-)
-from .dataframe_preprocessing import (
     add_crop_index,
     add_description_column,
     df_to_array,
     filter_dataframe_by_annotations,
     get_dataframe_for_dynamics_workflows,
+    get_dataset_descriptions,
+    get_traj_and_diff,
+    get_valid_subset,
     project_features_to_pcs,
+    split_dataset_by_flow,
 )
 from .diffae_features_pca import fit_pca, get_pca_loadings, get_pca_loadings_as_df
 
