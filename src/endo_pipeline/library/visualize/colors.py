ce763b2aceecff6b5d359fc1698dfe8400411cb3
Saurabh Mogre <saurabh.mogre@alleninstitute.org>
2025-10-23 13:43:22 -0700

    Update type hint for `angle_deg_to_color`

--- src/endo_pipeline/library/visualize/colors.py
+++ src/endo_pipeline/library/visualize/colors.py
@@ -1,6 +1,6 @@
 import numpy as np
-from seaborn import color_palette
+from matplotlib.colors import ListedColormap
 
 
-def angle_deg_to_color(a, cmap: color_palette):
+def angle_deg_to_color(a, cmap: ListedColormap):
     return cmap(np.abs(a) / 180.0)
