5ca6721359469367c70968364b90d1c37bb0307d
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-10-22 17:33:58 -0700

    update workflow

--- src/endo_pipeline/workflows/figures/supp_fig_img_preprocessing_nb.py
+++ src/endo_pipeline/workflows/figures/supp_fig_img_preprocessing_nb.py
@@ -4,14 +4,20 @@ import logging
 from endo_pipeline.configs import get_zarr_file_for_position, load_dataset_config, load_model_config
 from endo_pipeline.io.input import load_image_from_path
 from endo_pipeline.io.output import get_output_path
+from endo_pipeline.library.visualize.figure_utils import plot_image_thumbnail
 from endo_pipeline.library.visualize.model_inputs.image_preprocessing_steps import (
-    prepare_train_transforms,
-    run_and_visualize_transforms,
+    create_data_dict_loaded_image,
+    get_image_transforms,
     save_stack_slices_as_thumbnails,
+    visualize_fov_transform_steps,
 )
 from endo_pipeline.settings.diffae_configs import DIFFAE_MODEL_TRAIN_CONFIG
 from endo_pipeline.settings.examples import EXAMPLE_DATASET
-from endo_pipeline.settings.image_data import LOWER_Z_SLICE_OFFSET, UPPER_Z_SLICE_OFFSET
+from endo_pipeline.settings.image_data import (
+    LOWER_Z_SLICE_OFFSET,
+    UPPER_Z_SLICE_OFFSET,
+    PIXEL_SIZE_3i_20x,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -23,11 +29,12 @@ TAGS = ["supfig", "preprocessing", "diffae"]
 FIGURE_ID = "SUPP_FIG_IMG_PROC"
 DATASET = EXAMPLE_DATASET[FIGURE_ID]
 POSITION = 0
+TIMEPOINT = 0
 save_dir = get_output_path("model_input_preprocessing_viz", f"{DATASET}_P{POSITION}")
 
 dataset_config = load_dataset_config(DATASET)
 zarr_path = get_zarr_file_for_position(dataset_config, POSITION)
-img = load_image_from_path(zarr_path, level=1, squeeze=True)
+img = load_image_from_path(zarr_path, level=1, timepoints=TIMEPOINT, squeeze=True, compute=True)
 
 # %% Panel A - Thumbnail of each slice in Z-stack for each channel
 save_stack_slices_as_thumbnails(img, save_dir)
@@ -39,13 +46,35 @@ logging.info("Lower z slice: %s", in_focus_slice - LOWER_Z_SLICE_OFFSET)
 logging.info("Upper z slice: %s", in_focus_slice + UPPER_Z_SLICE_OFFSET)
 
 # %% Load model config and initialize transforms
-sample = {"original_path": str(zarr_path)}
 model_config = load_model_config(DIFFAE_MODEL_TRAIN_CONFIG)
-transforms = prepare_train_transforms(model_config)
+transforms = get_image_transforms(model_config)
+data = create_data_dict_loaded_image(img)
 
 # Step through each transformation and visualize the processing steps for each channel
 # %% Panel C - BF
-run_and_visualize_transforms(transforms, sample, save_dir, target_key="raw_bf")
+transformed_bf = visualize_fov_transform_steps(transforms, data, save_dir, target_key="raw_bf")
 # %% Panel D - CDH5
-run_and_visualize_transforms(transforms, sample, save_dir, target_key="raw_cdh5")
+transformed_cdh5 = visualize_fov_transform_steps(transforms, data, save_dir, target_key="raw_cdh5")
+
+
+# %% Visualize cropped images
+def crop_image(img, start, crop_size):
+    end = start + crop_size
+    return img[:, start:end, start:end]
+
+
+for image, name in [(transformed_bf, "crop_bf"), (transformed_cdh5, "crop_cdh5")]:
+    cropped_image = crop_image(image, 100, 128)
+    plot_image_thumbnail(
+        cropped_image.squeeze(),
+        name,
+        save_dir,
+        figsize=(6, 6),
+        scalebar_size_um=10,
+        pixel_size=PIXEL_SIZE_3i_20x,
+        bar_thickness=4,
+        bar_padding=5,
+    )
+
+
 # %%
