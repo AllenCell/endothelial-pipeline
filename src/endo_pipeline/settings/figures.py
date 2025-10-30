867af46f42af66227591f0fcdfe21e35ca138ca3
Erin Angelini <erin.angelini@alleninstitute.org>
2025-10-30 10:45:50 -0700

    Create model QC workflow (#900)
    * Regression test for failing import
    
    * Fix extract_t import
    
    * move and rename draft_model_qc_workflow
    
    * add in barebones workflow code
    
    * add initial plotting scripts
    
    * bugfix with call to shape
    
    * squeeze is optional, default False, for get_latent_vector_from_crop as input to model expects unsqueezed
    
    * Feature/contact sheet function (#907)
    
    * - added generic function for preparing contact sheets from lists of images
    
    * - expanded kwargs that can be passed to `make_contact_sheet` and also added 1 more negative control where latent features are extracted from a scrambled version of the bf image
    
    * - fiddled with make_contact_sheet "McCabe complexity" to keep it < 20
    
    * - added a line to actually call `main`
    
    * - addressed PR comment
    
    * - increased figure size a bit
    - changed a panel title
    
    * rename workflow to model_qc from model_qc_workflow
    
    * Update src/endo_pipeline/workflows/development/model_qc.py
    
    Co-authored-by: chantelleleveille <109111973+chantelleleveille@users.noreply.github.com>
    
    * image loading pattern update
    
    * Update src/endo_pipeline/workflows/development/model_qc.py
    
    Co-authored-by: chantelleleveille <109111973+chantelleleveille@users.noreply.github.com>
    
    * workflow default settings
    
    ---------
    
    Co-authored-by: Philip Garrison <philip.garrison@alleninstitute.org>
    Co-authored-by: Serge E. Parent <42447651+SergeEParent@users.noreply.github.com>
    Co-authored-by: chantelleleveille <109111973+chantelleleveille@users.noreply.github.com>

--- src/endo_pipeline/settings/figures.py
+++ src/endo_pipeline/settings/figures.py
@@ -6,3 +6,8 @@ FONT_FAMILY = "Arial"
 
 PDF_FONT_TYPE = 42
 """Default font type for PDF figures to ensure text is editable in Adobe Illustrator"""
+
+FONTSIZE_SMALL = 8
+FONTSIZE_MEDIUM = 9
+FONTSIZE_LARGE = 10  # max size indicated by author guidelines for figure plots
+FONTSIZE_XLARGE = 12  # this fontsize falls outside the recommended size in author guidelines
