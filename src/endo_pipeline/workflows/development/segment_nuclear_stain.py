40f17d34646854f53543fb76d78d31b1953439f3
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-11-03 11:14:38 -0800

    update image manifest with seg path

--- src/endo_pipeline/workflows/development/segment_nuclear_stain.py
+++ src/endo_pipeline/workflows/development/segment_nuclear_stain.py
@@ -15,7 +15,7 @@ def main(
     Segment nuclear stain channel using Cellpose for immunofluorescence datasets.
 
     To save segmentation masks to program folder use:
-    --output_dir "//allen/aics/endothelial/morphological_features/segmentations/nuclear_stain_seg/"
+    --output_dir //allen/aics/endothelial/morphological_features/segmentations/nuclear_stain_seg/multiscale_zarr/
 
     Args:
         dataset (str): Dataset name.
@@ -34,6 +34,7 @@ def main(
         segment_nuclei,
         visualize_results,
     )
+    from endo_pipeline.manifests import ImageLocation, load_image_manifest, save_image_manifest
 
     logger = logging.getLogger(__name__)
 
@@ -46,15 +47,15 @@ def main(
 
     for dataset_name in datasets:
         logging.info(f"Processing dataset: {dataset_name}")
+        dataset_config = load_dataset_config(dataset_name)
 
-        datset_config = load_dataset_config(dataset_name)
-
-        # Step 1: Get maximum intensity projections
-        positions = datset_config.zarr_positions
+        positions = dataset_config.zarr_positions
         if DEMO_MODE:
             positions = positions[:1]
+
+        # Step 1: Get maximum intensity projections
         max_int_projections, xy_pixel_size_um = get_max_int_projections(
-            datset_config, nuc_stain, positions
+            dataset_config, nuc_stain, positions
         )
 
         # Step 2: Perform nuclear segmentation
@@ -64,14 +65,26 @@ def main(
         if visualize:
             visualize_results(max_int_projections, masks, dataset_name)
 
-        # Step 4: Save segmentation masks
+        # # Step 4: Save segmentation masks
         if DEMO_MODE or output_dir is None:
             output_path = get_output_path("nuclear_stain_segmentation")
         else:
             output_path = Path(output_dir)
             logger.info(f"Outputs saved to {output_path}")
 
-        save_segmentation_masks(masks, datset_config, output_path, xy_pixel_size_um, positions)
+        save_segmentation_masks(masks, dataset_config, output_path, xy_pixel_size_um, positions)
+
+        # Step 5: Update image manifest
+        if not DEMO_MODE:
+            img_manifest = load_image_manifest("nuclear_stain_seg")
+
+            date = dataset_config.name[:8]
+            fmsid = dataset_config.fmsid
+            suffix = "P{{position}}.ome.zarr"
+            new_path = f"{output_path}/{date}_{fmsid}/{date}_{fmsid}_{suffix}"
+            img_manifest.locations[dataset_config.name] = ImageLocation(path=Path(new_path))
+
+            save_image_manifest(img_manifest)
 
 
 if __name__ == "__main__":
