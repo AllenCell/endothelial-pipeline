95cab041b25cc950364043f12abd8690d7c999be
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2025-11-04 15:55:38 -0800

    use new method

--- src/endo_pipeline/workflows/development/segment_nuclear_stain.py
+++ src/endo_pipeline/workflows/development/segment_nuclear_stain.py
@@ -34,7 +34,7 @@ def main(
         segment_nuclei,
         visualize_results,
     )
-    from endo_pipeline.manifests import ImageLocation, load_image_manifest, save_image_manifest
+    from endo_pipeline.manifests import add_image_location_to_manifest
 
     logger = logging.getLogger(__name__)
 
@@ -79,15 +79,7 @@ def main(
             continue
 
         # Step 5: Update image manifest
-        img_manifest = load_image_manifest("nuclear_stain_seg")
-
-        date = dataset_config.name[:8]
-        fmsid = dataset_config.fmsid
-        suffix = "P{{position}}.ome.zarr"
-        new_path = f"{output_path}/{date}_{fmsid}/{date}_{fmsid}_{suffix}"
-        img_manifest.locations[dataset_config.name] = ImageLocation(path=Path(new_path))
-
-        save_image_manifest(img_manifest)
+        add_image_location_to_manifest(dataset_config, "nuclear_stain_seg", output_path)
 
 
 if __name__ == "__main__":
