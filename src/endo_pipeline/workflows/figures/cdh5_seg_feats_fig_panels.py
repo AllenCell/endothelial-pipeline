7a6e0c379272992b99c4de21628ddc64c455ee86
Serge Parent <serge.edouard.parent@gmail.com>
2025-11-05 11:43:23 -0800

    - updated workflow docstring for correctness

--- src/endo_pipeline/workflows/figures/cdh5_seg_feats_fig_panels.py
+++ src/endo_pipeline/workflows/figures/cdh5_seg_feats_fig_panels.py
@@ -3,9 +3,9 @@ Note
 Becky says 20250326 (15 dyn) is probably the overall most ideal dataset for
 creating panels depicting the segmentation workflow. The no flow dataset from
 20250728 is also quite good but has some quirks around the feedings.
-I have used the 20250728 dataset for the current panels since it is no flow.
-It is worth noting that the plots use the PCA reference datasets which does NOT
-include 20250728.
+I have used the 20250818_20X dataset for the imaging panels current panels since
+it is no flow and part of the PCA reference datasets.
+Features are plotted for each of the PCA reference datasets.
 """
 
 from endo_pipeline.io import get_output_path
