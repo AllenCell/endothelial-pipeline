d3da879762979683e7297c6879d051d719d102bf
Philip Garrison <philip.garrison@alleninstitute.org>
2025-11-20 14:41:18 -0800

    #881 Put quotes around optionally-imported types

--- src/endo_pipeline/configs/demo_mode_defaults.py
+++ src/endo_pipeline/configs/demo_mode_defaults.py
@@ -4,7 +4,7 @@ if typing.TYPE_CHECKING:
     from endo_pipeline.cli import Datasets
 
 
-def use_default_collection(datasets: Datasets | None, collection_name: str) -> Datasets:
+def use_default_collection(datasets: "Datasets | None", collection_name: str) -> "Datasets":
     from endo_pipeline import DEMO_MODE
     from endo_pipeline.configs import get_datasets_in_collection
 
