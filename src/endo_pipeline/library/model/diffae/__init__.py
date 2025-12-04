426d82291210a67719ea083305e3f73c0ce5cfe2
Fatwir Sheikh Mohammed <81345858+fatwir@users.noreply.github.com>
2025-12-04 13:19:52 -0800

    Model Updates (#995)
    * updated manifests
    
    * model_changes
    
    * model_architecture_changes
    
    * pre_commit
    
    * pre_commit
    
    * pre_commit
    
    * pre_commit_mypy
    
    * pre_commit
    
    * ligtning_toml
    
    * removed_manifests
    
    * pre-commit
    
    * pre_commit
    
    * changes
    
    * typo
    
    * precommit
    
    * typo
    
    * diffae_edit_pre_commit
    
    * pre_commit
    
    * Update src/endo_pipeline/library/model/diffae/mlflow.py
    
    Co-authored-by: Philip Garrison <pgarrison@users.noreply.github.com>
    
    * Delete src/endo_pipeline/manifests/models/diffae_resolution_2_patch_64x64_exclude_cell_piling.yaml
    
    * format/log
    
    * mypy
    
    * Delete src/endo_pipeline/manifests/dataframes/diffae_training_dataframe_resolution_2_exclude_cell_piling.yaml
    
    Removing manifest that is not going to be used!
    
    * Delete src/endo_pipeline/manifests/dataframes/diffae_training_dataframe_resolution_1_exclude_cell_piling.yaml
    
    Deleting manifest!
    
    * minor_bug
    
    * fix
    
    ---------
    
    Co-authored-by: Philip Garrison <pgarrison@users.noreply.github.com>

--- src/endo_pipeline/library/model/diffae/__init__.py
+++ src/endo_pipeline/library/model/diffae/__init__.py
@@ -1,19 +1,24 @@
 from .diffae_finetune import DiffAEFinetune
-from .diffusion_autoencoder import DiffusionAutoEncoder
+from .diffusion_autoencoder import DiffusionAutoEncoder, detach
 from .generate_image import (
     generate_from_coords,
     generate_from_coords_and_noised_image,
     generate_from_coords_batch,
 )
 from .latent_walk import DiffAELatentWalkRank0
+from .log_grad import GradientLoggingCallback
+from .mlflow import MLFlowLogger
 from .transforms import MinStdCropd, RotateRanged
 
 __all__ = [
     "DiffAEFinetune",
     "DiffAELatentWalkRank0",
     "DiffusionAutoEncoder",
+    "GradientLoggingCallback",
+    "MLFlowLogger",
     "MinStdCropd",
     "RotateRanged",
+    "detach",
     "generate_from_coords",
     "generate_from_coords_and_noised_image",
     "generate_from_coords_batch",
