2001c5f8422f032e04fb24ba376bcb296cdcc005
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2025-12-05 18:47:17 +0000

    Add figure maker for z slice selection

--- src/endo_pipeline/workflows/figures/supp_fig_z_slice_selection_nb.py
+++ src/endo_pipeline/workflows/figures/supp_fig_z_slice_selection_nb.py
@@ -1,6 +1,6 @@
 # %%
 import numpy as np
-
+import matplotlib.pyplot as plt
 from endo_pipeline.configs import get_datasets_in_collection, load_dataset_config
 from endo_pipeline.io import get_output_path, load_image
 from endo_pipeline.library.process.z_stack_selection import (
@@ -13,10 +13,15 @@ from endo_pipeline.library.process.z_stack_selection import (
 from endo_pipeline.manifests import get_zarr_location_for_position
 from endo_pipeline.settings.examples import EXAMPLE_DATASET
 from endo_pipeline.settings.image_data import LOWER_Z_SLICE_OFFSET, UPPER_Z_SLICE_OFFSET
+from endo_pipeline.settings.figures import MAX_FIGURE_WIDTH
+from endo_pipeline.library.visualize.figures import FigurePanel, build_figure_from_panels
 
 # %%
 DESCRIPTION = "Visualize the selection of Z slices for image preprocessing."
 TAGS = ["supfig", "preprocessing"]
+
+# %%
+plt.style.use("endo_pipeline.figure")
 
 # %% Load dataset
 FIGURE_ID = "SUPP_FIG_Z_SLICE"
@@ -35,7 +40,6 @@ stdevs = [plane.std().compute() for plane in bf_stack]
 focal_plane_tp = max(0, np.argmin(stdevs))
 plot_standard_devs_per_slice(stdevs, int(focal_plane_tp), dataset, position, frame, save_dir)
 
-
 # %% Panel B - In focus Z slice selection per position over time
 focal_planes = calculate_center_planes_all_tp_for_pos(dataset_config, position)
 focal_plane_pos, std_dev = plot_global_center_plane(
@@ -45,7 +49,6 @@ focal_plane_pos, std_dev = plot_global_center_plane(
 # %% Panel C - Distribution of upper slices available across datasets
 datasets = get_datasets_in_collection("timelapse")
 plot_histogram_upper_slices_available(datasets, save_dir)
-
 
 # %% Panel D - Example images of selected Z slices
 visualize_slice_selection(
@@ -59,3 +62,43 @@ visualize_slice_selection(
     LOWER_Z_SLICE_OFFSET,
     UPPER_Z_SLICE_OFFSET,
 )
+
+# %% Figure
+output_path = save_dir / "supp_fig_z_slice_selection.svg"
+panels = [
+    FigurePanel(
+        letter="A",
+        path=save_dir / f"standard_devs_{dataset}_P{position}_{frame}.svg",
+        x_position=0,
+        y_position=0,
+        x_offset=0,
+        y_offset=0,
+    ),
+    FigurePanel(
+        letter="B",
+        path=save_dir / f"global_center_plane_{dataset}_P{position}.svg",
+        x_position=2.5,
+        y_position=0,
+        x_offset=0,
+        y_offset=0,
+    ),
+    FigurePanel(
+        letter="C",
+        path=save_dir / f"n_slices_above_in_focus_z_histogram.svg",
+        x_position=5,
+        y_position=0,
+        x_offset=0,
+        y_offset=0.14,
+    ),
+    FigurePanel(
+        letter="D",
+        path=save_dir / f"plane_selection_vis_{dataset}_P{position}_{frame}_offset{LOWER_Z_SLICE_OFFSET}_{UPPER_Z_SLICE_OFFSET}_scalebar50um.svg",
+        x_position=0,
+        y_position=2.4,
+        x_offset=0.1,
+        y_offset=0,
+    ),
+]
+build_figure_from_panels(panels, output_path, width=MAX_FIGURE_WIDTH, height=7.2)
+
+# %%
