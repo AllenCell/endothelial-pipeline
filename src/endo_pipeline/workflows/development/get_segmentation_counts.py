7b4e3f5576afad772a3d6fc80260d4032eca5835
Serge Parent <serge.edouard.parent@gmail.com>
2025-12-15 12:17:36 -0800

    - also removed GFP scope errors

--- src/endo_pipeline/workflows/development/get_segmentation_counts.py
+++ src/endo_pipeline/workflows/development/get_segmentation_counts.py
@@ -5,8 +5,13 @@ def main():
     import numpy as np
     import pandas as pd
 
-    from endo_pipeline.configs import get_datasets_in_collection, load_dataset_config
+    from endo_pipeline.configs import (
+        TimepointAnnotation,
+        get_datasets_in_collection,
+        load_dataset_config,
+    )
     from endo_pipeline.io import get_output_path, load_dataframe
+    from endo_pipeline.library.analyze.diffae_dataframe_utils import filter_dataframe_by_annotations
     from endo_pipeline.library.process.general_image_preprocessing import sequence_to_scalar
     from endo_pipeline.manifests import get_dataframe_location_for_dataset, load_dataframe_manifest
     from endo_pipeline.settings import DEFAULT_SEG_FEATURE_MANIFEST_NAME, ColumnName
@@ -28,6 +33,11 @@ def main():
     )
 
     for dataset_name in dataset_name_list:
+        # load the dataset config file to get some identifying information about the dataset
+        dataset_config = load_dataset_config(dataset_name)
+        shear_stress = [flow.shear_stress for flow in dataset_config.flow_conditions]
+        cell_line = sequence_to_scalar(dataset_config.cell_lines)
+
         # load in segmentation features dataframe for the dataset if it was put through the classical
         # feature workflows, otherwise record "NaN" for the segmentation counts
         if dataset_name not in dataset_name_list_segmented:
@@ -57,6 +67,18 @@ def main():
                 .num_unique_tracks_before_filtering_at_T.apply(sequence_to_scalar)
                 .sum()
             )
+
+            # filter out rows based on automatic and manual timepoint annotations
+            live_seg_feats_df["dataset"] = live_seg_feats_df["dataset_name"]
+            live_seg_feats_df["frame_number"] = live_seg_feats_df["image_index"]
+            annotations_to_filter_out = [
+                TimepointAnnotation.AUTO_GFP_SCOPE_ERROR,
+                TimepointAnnotation.GFP_SCOPE_ERROR,
+            ]
+            live_seg_feats_df = filter_dataframe_by_annotations(
+                live_seg_feats_df, dataset_config, timepoint_annotations=annotations_to_filter_out
+            )
+
             # dropna is here to remove NaNs which will raise an error when trying to
             # extract a single number from the table at that timepoint and position
             # using the `sequence_to_scalar` function
@@ -75,11 +97,6 @@ def main():
 
             # delete the segmentation features dataframe to keep memory usage down
             del live_seg_feats_df
-
-        # load the dataset config file to get some other identifying information about the dataset
-        dataset_config = load_dataset_config(dataset_name)
-        shear_stress = [flow.shear_stress for flow in dataset_config.flow_conditions]
-        cell_line = sequence_to_scalar(dataset_config.cell_lines)
 
         # add identifying information and segmentation counts to the seg_counts dictionary
         seg_counts["dataset_name"].append(dataset_name)
