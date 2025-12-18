7a5dfba1d7c54eb99eaa79c8e371f714d4847492
Erin <erin.angelini@alleninstitute.org>
2025-12-18 13:31:56 -0800

    rm reconstruct-crops-csv, grab reconstruct-from-dataframe from open branch

--- src/endo_pipeline/workflows/production/reconstruct_from_dataframe.py
+++ src/endo_pipeline/workflows/production/reconstruct_from_dataframe.py
@@ -8,35 +8,41 @@ TAGS = ["diffae_image_generation", "diffae_features"]
 
 
 def main(
-    csv_path: str,
+    path: str,
     model_manifest_name: str = DEFAULT_MODEL_MANIFEST_NAME,
     run_name: str | None = DEFAULT_MODEL_RUN_NAME,
     num_pcs: int = NUM_PCS_TO_ANALYZE,
+    pc_column_names: list[str] | None = None,
 ) -> None:
     """
     Reconstruct crops from PC space coordinates stored in a given CSV file.
 
     The reconstructed crops are saved as PNG files in a local directory.
 
-    **CSV file format**:
+    **Dataframe file format**:
 
-    The CSV file should contain rows of latent space coordinates, with each row representing a
-    point in the PCA-transformed space. The number of columns should match the number of principal
-    components used during PCA fitting and transformation, which is specified by the `num_pcs` parameter.
+    The dataframe file (.csv, .parquet, etc.) should contain rows of latent space coordinates,
+    with each row representing a point in the PCA-transformed space.
 
-    The default number of principal components is set via ``NUM_PCS_TO_ANALYZE`` in
-    ``endo_pipeline.settings.diffae_feature_dataframes``.
+    The number of columns should match the number of principal components used during PCA fitting and
+    transformation, which is specified by the `num_pcs` parameter. The default number of principal
+    components is set via ``NUM_PCS_TO_ANALYZE`` in ``endo_pipeline.settings.diffae_feature_dataframes``.
+
+    The column names for the principal components can either be specified via the ``pc_column_names``
+    parameter or will default to the standard naming convention defined in ``DIFFAE_PC_COLUMN_NAMES``.
 
     Parameters
     ----------
-    csv_path
-        Path to a CSV file containing latent space coordinates along trajectories.
+    path
+        Path to a dataframe file containing PC space coordinates.
     model_manifest_name
         Name of the model manifest containing the specific run to load features from.
     run_name
         Run name corresponding to features to load and the model to use for image reconstruction.
     num_pcs
         Number of principal components used in the PCA transformation.
+    pc_column_names
+        List of column names in the dataframe corresponding to the principal components.
     """
     import logging
     from pathlib import Path
@@ -45,22 +51,29 @@ def main(
     import numpy as np
 
     from endo_pipeline import NUM_GPUS
-    from endo_pipeline.io import get_output_path, load_model, save_plot_to_path
-    from endo_pipeline.library.analyze.diffae_dataframe_utils import fit_pca
+    from endo_pipeline.io import (
+        get_output_path,
+        load_dataframe_from_path,
+        load_model,
+        save_plot_to_path,
+    )
+    from endo_pipeline.library.analyze.diffae_dataframe_utils import (
+        check_required_columns_in_dataframe,
+        fit_pca,
+    )
     from endo_pipeline.library.model import generate_from_coords_batch
     from endo_pipeline.manifests import (
         get_feature_dataframe_manifest_name,
         get_most_recent_run_name,
         load_model_manifest,
     )
+    from endo_pipeline.settings.diffae_feature_dataframes import DIFFAE_PC_COLUMN_NAMES
 
     logger = logging.getLogger(__name__)
 
     # convert csv_path to Path object
-    csv_path_obj = Path(csv_path)
-    if not csv_path_obj.exists():
-        logger.error("CSV file [ %s ] does not exist.", csv_path)
-        raise FileNotFoundError(f"CSV file [ {csv_path} ] does not exist.")
+    dataframe_path_obj = Path(path).resolve()
+    dataframe = load_dataframe_from_path(dataframe_path_obj)
 
     # load model manifest, get run name, and load model
     model_manifest = load_model_manifest(model_manifest_name)
@@ -72,34 +85,38 @@ def main(
     )
 
     # Get fit (3D) PCA object from manifest
-    pca = fit_pca(dataframe_manifest_name=dataframe_manifest_name, num_pcs=3)
+    pca = fit_pca(dataframe_manifest_name=dataframe_manifest_name, num_pcs=num_pcs)
 
     # Directory to save reconstructed crops
-    csv_file_name = csv_path_obj.stem
+    dataframe_file_name = dataframe_path_obj.stem
     crop_savedir = get_output_path(
-        "reconstructed_crops", model_manifest_name, run_name_, csv_file_name
+        "reconstructed_crops", model_manifest_name, run_name_, dataframe_file_name
     )
 
-    # load coordinates from csv
-    pc_coords = np.loadtxt(csv_path_obj, delimiter=",")
+    # get coordinates as array from dataframe, using specified pc column names or default names
+    pc_column_names_ = (
+        DIFFAE_PC_COLUMN_NAMES[:num_pcs] if pc_column_names is None else pc_column_names
+    )
+    check_required_columns_in_dataframe(dataframe, pc_column_names_)  # validate required columns
+    pc_coords = dataframe[pc_column_names_].values  # extract coordinate values
 
     # make sure that coords is a 2D array with shape (num_points, num_dimensions)
     pc_coords = np.atleast_2d(pc_coords)
     num_points, num_dims = pc_coords.shape
     logger.debug(
-        "Loaded [ %d ] points with [ %d ] dimensions from CSV file [ %s ].",
+        "Loaded [ %d ] points with [ %d ] dimensions from dataframe file [ %s ].",
         num_points,
         num_dims,
-        csv_path,
+        dataframe_file_name,
     )
     if num_dims != num_pcs:
         logger.error(
-            "Expected coordinates of [ %d ] dimensions from CSV, but got [ %d ] dimensions.",
+            "Expected coordinates of [ %d ] dimensions from loaded dataframe, but got [ %d ] dimensions.",
             num_pcs,
             num_dims,
         )
         raise ValueError(
-            f"Expected coordinates of [ {num_pcs} ] dimensions from CSV, but got [ {num_dims} ] dimensions."
+            f"Expected coordinates of [ {num_pcs} ] dimensions from loaded dataframe, but got [ {num_dims} ] dimensions."
         )
 
     # transform interpolated points to full latent space
