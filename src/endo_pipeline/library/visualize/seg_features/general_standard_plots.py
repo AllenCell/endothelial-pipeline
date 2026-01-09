6c1600372804bfb99499a361055b9da4a49250a6
Serge E. Parent <42447651+SergeEParent@users.noreply.github.com>
2026-01-09 09:06:49 -0800

    Fig4A grid DiffAE scatterplot (#1134)
    * - fixed bug in pc column naming scheme
    - added file for testing different measurements plotted against other measurements
    
    * - work in progress
    
    * - updated the flow field calls and usage to be consistent with Erin's recent updates
    - also made some minor modifications to flow_field_viz.py to make some functions more flexible / work with my workflow a bit more smoothly so I can re-use code from other people
    
    * - introduced multiprocessing when plotting
    - filtered out not steady state and cell piling data from the grid diffae dataframe
    
    * - trying to get multiproc working
    
    * - multiprocessing
    
    * - multiprocessing
    
    * - mp
    
    * - mp
    
    * - cleaned up code commented-out code
    
    * - deleted some comments
    
    * update bounds to be per dataset
    
    * no longer use extrapolation for vector field
    
    * log norm colormap
    
    * - minor cosmetic changes to integrated track plots
    
    * - removed a file that was incorrectly added to this branch
    - added togglable indicator for track start and end points
    
    * - bugfix
    
    * - bugfix in start + stop timepoint column specifications
    
    * - adjusted an edgewidth in plot
    
    * - removed face color from start and end point scatterpoints
    
    * - end scatterpoint face color issue
    
    * - created plot of grid-based PC1 vs PC2 for Fig 4
    
    * - moved workflow file so that it could be called with workflow_cli
    
    * - bit of spring cleaning
    
    * - added saving plots as pdfs too
    - made plots match the figure from the draft more closely
    - refactored code to reduce logic duplication
    
    * Update src/endo_pipeline/workflows/figures/fig4_grid_diffae_scatter.py
    
    Co-authored-by: Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
    
    * - moved the plotting function from workflow to a library file
    - updated naming algorithm for save_colorbar
    
    ---------
    
    Co-authored-by: Erin <erin.angelini@alleninstitute.org>
    Co-authored-by: Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>

--- src/endo_pipeline/library/visualize/seg_features/general_standard_plots.py
+++ src/endo_pipeline/library/visualize/seg_features/general_standard_plots.py
@@ -12,7 +12,11 @@ AX_HEIGHT = AX_WIDTH * 2 / 3
 
 
 def save_colorbar(
-    outdir: Path, colormap_name: str = "viridis", figsize: tuple[int, int] = (1, 5)
+    outdir: Path,
+    colormap_name: str = "viridis",
+    filename: str | None = None,
+    figsize: tuple[int, int] = (1, 5),
+    filetype: str = ".png",
 ) -> None:
     """Plots and saves the colorbar specified by "colormap_name".
     A list of all available colormaps can be found with:
@@ -26,7 +30,9 @@ def save_colorbar(
     fig, ax = plt.subplots(figsize=figsize)
     ax.set_axis_off()
     ColorbarBase(ax, cmap=colormap_name)
-    fig.savefig(outdir / f"{colormap_name}", bbox_inches="tight", pad_inches=0)
+    filename = f"{filename}_" if filename else ""
+
+    fig.savefig(outdir / f"{filename}{colormap_name}{filetype}", bbox_inches="tight", pad_inches=0)
 
 
 def lineplot_of_feats(
