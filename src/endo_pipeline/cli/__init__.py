dc2c167952278fc7896ec1fe5ae1551ca0a4e9fd
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-01-09 10:04:42 -0500

    Replace TrackIds with UniqueIntList

--- src/endo_pipeline/cli/__init__.py
+++ src/endo_pipeline/cli/__init__.py
@@ -1,13 +1,11 @@
 from .datasets import Datasets
 from .list_types import FloatList, IntList, StrList, UniqueIntList, UniqueStrList
-from .track_ids import TrackIds
 
 __all__ = [
     "Datasets",
     "FloatList",
     "IntList",
     "StrList",
-    "TrackIds",
     "UniqueIntList",
     "UniqueStrList",
 ]
