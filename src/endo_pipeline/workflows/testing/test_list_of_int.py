bb03236b8f4b6ce9fbf0b9cdfe12d0ad2dca38f6
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-01-12 13:58:39 -0500

    Remove testing workflows before merge

--- src/endo_pipeline/workflows/testing/test_list_of_int.py
+++
@@ -1,5 +0,0 @@
-from endo_pipeline.cli import UniqueIntList
-
-
-def main(ids: UniqueIntList) -> None:
-    print(ids)
--- src/endo_pipeline/workflows/testing/test_list_of_str.py
+++
@@ -1,5 +0,0 @@
-from endo_pipeline.cli import UniqueStrList
-
-
-def main(names: UniqueStrList) -> None:
-    print(names)
