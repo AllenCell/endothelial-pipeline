60d56b13bc0bf36215d3a29f78969e6b20ca0262
Erin Angelini <erin.angelini@alleninstitute.org>
2026-01-21 14:09:31 -0800

    add histogram plot to workflow

--- src/endo_pipeline/settings/polar_coords.py
+++ src/endo_pipeline/settings/polar_coords.py
@@ -7,19 +7,20 @@ from endo_pipeline.settings.diffae_feature_dataframes import ColumnName
 KERNEL_BANDWIDTH_POLAR: float = 0.175
 """Kernel bandwidth for 1D polar coordinate density and flow field estimation."""
 
-BIN_WIDTH_POLAR: float = 0.05
-"""Bin width for 1D polar coordinate density and flow field estimation."""
+SPLIT_BY_FLOW: bool = True
+"""Whether to split data by flow conditions for polar coordinate analysis."""
+
+BIN_WIDTHS_POLAR: tuple[float, float] = (0.05, 0.05)
+"""Bin widths for polar coordinate density and flow field estimation in the order [angle, radius]."""
 
 DEFAULT_DATASET_COLLECTION_POLAR_VIS: str = "diffae_model_training"
 """Default dataset collection for polar coordinate visualization workflow."""
 
 POLAR_COLUMN_NAMES = [ColumnName.POLAR_ANGLE, ColumnName.POLAR_RADIUS]
+"""Column names for polar coordinates in the DiffAE feature dataframe, in the order [angle, radius]."""
 
-BIN_LIMITS_ANGLE: tuple[float, float] = (-pi, pi)
-"""Bin limits for the angular coordinate in polar coordinate analysis."""
-
-BIN_LIMITS_RADIUS: tuple[float, float] = (0, 3.5)
-"""Bin limits for the radial coordinate in polar coordinate analysis."""
+BIN_LIMITS_POLAR = [(-pi, pi), (0.0, 3.5)]
+"""Bin limits for polar coordinate analysis, in the order [angle, radius]."""
 
 TICK_STEP_NUM: int = 7
 """Number of axes ticks for coordinate axis in histogram plots."""
--- src/endo_pipeline/workflows/development/polar_coords_vis.py
+++ src/endo_pipeline/workflows/development/polar_coords_vis.py
@@ -1,5 +1,5 @@
 from endo_pipeline.cli import Datasets
-from endo_pipeline.settings.polar_coords import KERNEL_BANDWIDTH_POLAR
+from endo_pipeline.settings.polar_coords import KERNEL_BANDWIDTH_POLAR, SPLIT_BY_FLOW
 from endo_pipeline.settings.workflow_defaults import (
     DEFAULT_MODEL_MANIFEST_NAME,
     DEFAULT_MODEL_RUN_NAME,
@@ -11,6 +11,7 @@ def main(
     model_manifest_name: str = DEFAULT_MODEL_MANIFEST_NAME,
     run_name: str = DEFAULT_MODEL_RUN_NAME,
     bw: float = KERNEL_BANDWIDTH_POLAR,
+    split_by_flow: bool = SPLIT_BY_FLOW,
 ) -> None:
     """
     Analyze and visualize DiffAE feature dynamics in polar coordinates.
@@ -40,26 +41,38 @@ def main(
         The name of the model run to use.
     bw
         The kernel bandwidth for polar coordinate density and flow field estimation.
+    split_by_flow
+        Whether to split data by flow conditions for polar coordinate analysis.
     """
 
     import logging
 
+    import numpy as np
+
     from endo_pipeline.configs import get_datasets_in_collection, load_dataset_config
-    from endo_pipeline.io import get_output_path
+    from endo_pipeline.io import get_output_path, save_plot_to_path
     from endo_pipeline.library.analyze.diffae_dataframe_utils import (
         fit_pca,
         get_dataframe_for_dynamics_workflows,
         split_dataset_by_flow,
     )
-    from endo_pipeline.library.visualize.diffae_features.feature_viz import get_label_for_column
+    from endo_pipeline.library.analyze.numerics.binning import get_bins
+    from endo_pipeline.library.visualize.diffae_features.feature_viz import (
+        get_label_for_column,
+        plot_component_histograms_over_time,
+    )
     from endo_pipeline.manifests import (
         get_feature_dataframe_manifest_name,
         load_dataframe_manifest,
         load_model_manifest,
     )
+    from endo_pipeline.settings.diffae_feature_dataframes import ColumnName
     from endo_pipeline.settings.polar_coords import (
+        BIN_LIMITS_POLAR,
+        BIN_WIDTHS_POLAR,
         DEFAULT_DATASET_COLLECTION_POLAR_VIS,
         POLAR_COLUMN_NAMES,
+        TICK_STEP_NUM,
     )
 
     logger = logging.getLogger(__name__)
@@ -88,7 +101,13 @@ def main(
     else:
         dataset_names = [name for name in datasets if name in valid_dataset_options]
 
-        # loop over datasets in collection
+    # compute bins for polar coordinates
+    bins, _ = get_bins(
+        bin_widths=BIN_WIDTHS_POLAR,
+        bin_limits=BIN_LIMITS_POLAR,
+    )
+
+    # loop over datasets in collection
     # plot summary plots
     # compute drift and diffusion coefficients in polar coordinates
     for dataset_name in dataset_names:
@@ -108,3 +127,36 @@ def main(
             df,
             dataset_config,
         )
+
+        for df_, shear_stress in zip(df_by_flow, shear_stress_list, strict=True):
+            dataset_name_flow = f"{dataset_name}_shear_{int(shear_stress)}"
+            fig_title = f"{dataset_name} ({shear_stress} dym/cm$^2$)"
+
+            hist_arrays = []
+
+            for i, column_name in enumerate(POLAR_COLUMN_NAMES):
+                # plot histogram heatmap over time
+                num_bins = len(bins[i]) - 1
+                frame_min = df_[ColumnName.TIMEPOINT].min()
+                frame_max = df_[ColumnName.TIMEPOINT].max()
+                num_frames = frame_max - frame_min + 1
+                hist_array = np.zeros((num_bins, num_frames))
+
+                for t, df_frame in df_.groupby(ColumnName.TIMEPOINT):
+                    timepoint_idx = int(t - frame_min)
+                    values = df_frame[column_name].values
+                    hist = np.histogram(values, bins=bins[i], density=True)[0]
+                    hist_array[:, timepoint_idx] = hist
+
+                hist_arrays.append(hist_array)
+
+            fig, ax = plot_component_histograms_over_time(
+                hist_arrays,
+                bins,
+                frame_range=(frame_min, frame_max),
+                feature_names=variable_names,
+                time_tick_step=50,
+                bin_tick_num=TICK_STEP_NUM,
+            )
+            fig.suptitle(fig_title)
+            save_plot_to_path(fig, fig_savedir, f"{dataset_name_flow}_polar_histogram_heatmap")
