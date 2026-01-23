15e564886f86a627f2ab5fbcaca09a1d61d64230
Erin Angelini <erin.angelini@alleninstitute.org>
2026-01-23 14:47:14 -0800

    Initialize workflow for visualizing polar coordinate data (#1160)
    * initialize workflow and workflow constants in settings
    
    * update plot_principal_component_histogram method to be more flexible with frame and bin ranges
    
    * rename plot_principal_component_histogram
    
    * update get_label_for_column method to work with polar coordinate column names
    
    * update label method docstring
    
    * add histogram plot to workflow
    
    * rm unused inputs
    
    * fix type error with frame_range input to plot_component_histograms
    
    * add option to run for tracked crops
    
    * bugfix with getting column labels
    
    * add plotting of per position averages over time
    
    * add in if __name__ == main
    
    * Dynamically shift range of polar angle for datasets where angle distribution is centered near `pi` and `-pi` (#1161)
    
    * add option to shift the polar angle range for the per position mean computations
    
    * caught bug with using non-shifted dataframe
    
    * after debugging, add line back in to shift the range back for plotting
    
    * improve visualization for big jumps in theta when shifted back
    
    * set global axes limits
    
    * make global bounds an optional toggle for per position averages, set plot suptitle
    
    * update default label for histogram plots
    
    * get explicit feature labels in specify_crops
    
    * pass in PC labels explicitly for real-walk

---
+++ src/endo_pipeline/settings/polar_coords.py
@@ -0,0 +1,27 @@
+"""Default settings for polar coordinate analysis and visualization."""
+
+from numpy import pi
+
+from endo_pipeline.settings.diffae_feature_dataframes import ColumnName
+
+BIN_WIDTHS_POLAR: tuple[float, float] = (0.05, 0.05)
+"""Bin widths for polar coordinate density and flow field estimation in the order [angle, radius]."""
+
+DEFAULT_DATASET_COLLECTION_POLAR_VIS: str = "diffae_model_training"
+"""Default dataset collection for polar coordinate visualization workflow."""
+
+POLAR_COLUMN_NAMES = [ColumnName.POLAR_ANGLE, ColumnName.POLAR_RADIUS]
+"""Column names for polar coordinates in the DiffAE feature dataframe, in the order [angle, radius]."""
+
+BIN_LIMITS_POLAR = [(-pi, pi), (0.0, 3.5)]
+"""Bin limits for polar coordinate analysis, in the order [angle, radius]."""
+
+TICK_STEP_NUM: int = 7
+"""Number of axes ticks for coordinate axis in histogram plots."""
+
+BEHAVES_LIKE_MIN_SHEAR_STRESS: tuple[str, ...] = (
+    "20250428_20X",
+    "20250604_20X",
+    "20250716_20X",
+)
+"""Datasets with distribution of polar angle coordinate qualitatively similar to MIN shear stress datasets."""
