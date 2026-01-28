300f291c931336aeb773f302c4e3ebc1cbc70a78
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2026-01-28 15:47:03 -0500

    Add workflow to run DiffAE eval (#1169)
    * Add workflow for diffae eval
    
    * Save configs with key values instead of enum name)
    
    * Add model eval dataframe manifests for select datasets
    
    * Add more model eval dataframes for select datasets
    
    * Update CONTRIBUTING with mode eval workflow
    
    * Add model evaluation options and identifier defaults

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -221,8 +221,8 @@ There are two main identifiers for models. These identifiers may be passed to th
 
 | Identifier | Description | CLI | Default |
 | - | - | - | - |
-| **model manifest name** | general name for a set of models | `--model-manifest-name MODEL_MANIFEST_NAME` | `diffae_RESOLUTION_PATCH_PILING` |
-| **run name** | specific name of a model training run | `--run-name RUN_NAME` | `diffae_YYYYMMDD_MMHHSS` |
+| **model manifest name** | general name for a set of models | `--model-manifest-name MODEL_MANIFEST_NAME` | `diffae_baseline_exclude_cell_piling` |
+| **run name** | specific name of a model training run | `--run-name RUN_NAME` | `20251110_latent_512` |
 
 _Note that **model manifest name** is analogous to MLflow "experiments" and **run name** is analogous to MLflow "runs" (these mappings are reflected in the model configs)._
 
@@ -232,9 +232,18 @@ There are currently three main model training options. These options may be pass
 
 | Option | Description | CLI | Formatting |
 | - | - | - | - |
-| **image crop size** | length of the 2D image crop in pixels used for training | `--crop-size CROP_SIZE` | `patch_<CROP_SIZE>x<CROP_SIZE>` |
+| **image crop size** | length of the 2D image crop in pixels used for training | `--crop-size CROP_SIZE` | `patch_CROP_SIZExCROP_SIZE` |
 | **cell piling timepoints** | if timepoints annotated as cell piling are included in the training | `--include-cell-piling` or `--exclude-cell-piling` | `include_cell_piling` or `exclude_cell_piling` |
 | **conditioning image type** | name of image type to use for semantic conditioning (`cdh5` or `bf`) | `--condition-on CONDITION_IMG_NAME` | `condition_on_CONDITION_IMG_NAME`
+
+### Model evaluation options
+
+There is currently one main model evaluation option. This options may be passed to the appropriate workflows via the CLI. Where indicated, these options will also be used to format default manifest names.
+
+| Option | Description | CLI | Options |
+| - | - | - | - |
+| **crop pattern** | select grid-based or track-based crops | `--crop-pattern CROP_PATTERN` | `grid` or `tracked` |
+
 
 ### Model training workflows
 
@@ -284,3 +293,10 @@ There are currently three main model training options. These options may be pass
 - input (evaluation options) = **crop pattern**
 - :white_circle: output = resolved model config for each dataset at `results/models/MODEL_MANIFEST_NAME/RUN_NAME/configs/eval_CROP_DATASET.yaml`
 - :purple_circle: output = updated model manifest with pending evaluation run(s) at `src/endo_pipeline/manifests/models/diffae_(MODEL_MANIFEST_NAME)_(RUN_NAME)_(CROP_PATTERN)` (recommendation is to open a draft PR with this change until the next step is complete)
+
+#### 3. Evaluate the model
+
+- workflow = `eval-diffae`
+- input (identifier options) = **model manifest name**, **run_name**
+- input (evaluation options) = **crop pattern**
+- :purple_circle: output = updated model manifest with MLflow run id at `src/endo_pipeline/manifests/models/diffae_(MODEL_MANIFEST_NAME)_(RUN_NAME)_(CROP_PATTERN)`
---
+++ src/endo_pipeline/workflows/production/eval_diffae.py
@@ -0,0 +1,147 @@
+from endo_pipeline.cli import CropPattern
+from endo_pipeline.settings.workflow_defaults import (
+    DEFAULT_MODEL_MANIFEST_NAME,
+    DEFAULT_MODEL_RUN_NAME,
+)
+
+
+def main(
+    crop_pattern: CropPattern,
+    model_manifest_name: str = DEFAULT_MODEL_MANIFEST_NAME,
+    run_name: str = DEFAULT_MODEL_RUN_NAME,
+    upload_to_fms: bool = True,
+) -> None:
+    """
+    Evaluate a DiffAE model using the provided configuration.
+
+    #diffae #model-evaluation
+
+    This workflow runs model evaluations based on configs produced by the
+    `build-diffae-eval-config` workflow to calculate latent features. The
+    workflow will only run the evaluation for datasets that do not already have
+    features calculated (based on the corresponding dataframe manifest).
+
+    ## Workflow demo
+
+    If demo mode is enabled, this workflow will use the config with the suffix
+    ``_demo`` and only evaluate a single dataset.
+
+    Parameters
+    ----------
+    crop_pattern
+        Crop pattern used for model evaluation.
+    model_manifest_name
+        Name for the model manifest to use for evaluation.
+    run_name
+        Name for the model run to use for evaluation.
+    upload_to_fms
+        True to upload model evaluation results to FMS, False otherwise.
+    """
+
+    import logging
+    from pathlib import Path
+
+    from cyto_dl.api import CytoDLModel
+
+    from endo_pipeline.cli import DEMO_MODE
+    from endo_pipeline.configs import load_dataset_config
+    from endo_pipeline.io import build_fms_annotations, get_output_path, upload_file_to_fms
+    from endo_pipeline.library.model.eval_model import (
+        update_prediction_from_crops_with_metadata,
+        update_prediction_from_tracks_with_metadata,
+    )
+    from endo_pipeline.manifests import (
+        DataframeLocation,
+        get_feature_dataframe_manifest_name,
+        load_dataframe_manifest,
+        load_model_manifest,
+        save_dataframe_manifest,
+    )
+
+    logger = logging.getLogger(__name__)
+
+    # Get available evaluation runs from given model manifest.
+    model_manifest = load_model_manifest(model_manifest_name)
+    name_suffix = "_demo" if DEMO_MODE else ""
+    feature_manifest_name = get_feature_dataframe_manifest_name(
+        model_manifest, run_name, crop_pattern
+    )
+    feature_manifest = load_dataframe_manifest(f"{feature_manifest_name}{name_suffix}")
+    datasets = [key for key, loc in feature_manifest.locations.items() if loc.fmsid is None]
+
+    if not datasets:
+        logger.error("No pending evaluation runs in model manifest [ %s ]", model_manifest_name)
+        raise ValueError(
+            "No pending evaluation run configs available. "
+            "Use build-diffae-eval-config to create new evaluation run configs."
+        )
+
+    # When running workflow in demo mode, only evaluate the first dataset.
+    if DEMO_MODE:
+        logger.warning("DEMO MODE - Only evaluating first dataset")
+        datasets = datasets[:1]
+
+    # Get config path based on model manifest and run name.
+    name_suffix = "_demo" if DEMO_MODE else ""
+    config_path = get_output_path(
+        "models",
+        model_manifest_name,
+        run_name,
+        "configs",
+        include_timestamp=False,
+        create_directories=False,
+    )
+
+    logger.info("Model manifest name: [ %s ]", model_manifest_name)
+    logger.info("Run name: [ %s ]", run_name)
+
+    for dataset in datasets:
+        # Build evaluation config path.
+        config_file = config_path / f"eval_{crop_pattern}_{dataset}{name_suffix}.yaml"
+
+        # Initialize the model with evaluation config.
+        cytodl_model = CytoDLModel()
+        cytodl_model.load_config_from_file(config_file.as_posix())
+        logger.info("Evaluation config loaded from [ %s ]", config_file)
+
+        # Run prediction.
+        cytodl_model.predict()
+
+        # Get output information from model config.
+        output_dir = Path(cytodl_model.cfg.paths.output_dir)
+        filename_suffix = cytodl_model.cfg.callbacks.prediction_saver.save_suffix
+        output_path = output_dir / f"predict_{filename_suffix}.parquet"
+
+        # Add metadata to prediction output.
+        if crop_pattern == "grid":
+            crop_size = cytodl_model.cfg.model.spatial_inferer.splitter.patch_size
+            update_prediction_from_crops_with_metadata(
+                dataset_name=dataset,
+                model_manifest_name=model_manifest_name,
+                run_name=run_name,
+                crop_size=crop_size,
+                prediction_path=output_path,
+            )
+        elif crop_pattern == "tracked":
+            update_prediction_from_tracks_with_metadata(
+                dataset_name=dataset,
+                model_manifest_name=model_manifest_name,
+                run_name=run_name,
+                prediction_path=output_path,
+            )
+
+        # Upload feature dataframe to FMS.
+        if upload_to_fms:
+            dataset_config = load_dataset_config(dataset)
+            annotations = build_fms_annotations(
+                dataset_config, model_manifest=model_manifest, run_name=run_name
+            )
+            fmsid = upload_file_to_fms(output_path, annotations=annotations, file_type="parquet")
+            feature_manifest.locations[dataset] = DataframeLocation(fmsid=fmsid)
+            save_dataframe_manifest(feature_manifest)
+
+
+if __name__ == "__main__":
+    from endo_pipeline.cli import workflow_cli
+
+    workflow_cli(main)
