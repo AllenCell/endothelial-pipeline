1835b8ccc4e174fca9e32ed32151e4e97267b390
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-02-04 18:23:33 -0500

    Remove old eval workflows and dependent methods

--- src/endo_pipeline/library/model/__init__.py
+++ src/endo_pipeline/library/model/__init__.py
@@ -6,15 +6,11 @@ from .diffae import (
     generate_from_coords_batch,
 )
 from .eval_model import (
-    evaluate_model_on_grid_of_crops_from_one_dataset,
-    evaluate_model_on_tracked_crops_from_one_dataset,
     generate_overrides_for_model_eval,
-    generate_overrides_for_track_based_crops,
     load_model_for_inference,
     preprocess_tracking_manifest_for_model_eval,
     update_prediction_from_crops_with_metadata,
     update_prediction_from_tracks_with_metadata,
-    upload_prediction_dataframe_to_fms,
 )
 from .image_loading import (
     BioIOImageLoaderd,
@@ -39,12 +35,9 @@ __all__ = [
     "RotateRanged",
     "build_and_save_dataframe_manifest_for_training",
     "build_zarr_image_loading_dataframe",
-    "evaluate_model_on_grid_of_crops_from_one_dataset",
-    "evaluate_model_on_tracked_crops_from_one_dataset",
     "generate_from_coords",
     "generate_from_coords_batch",
     "generate_overrides_for_model_eval",
-    "generate_overrides_for_track_based_crops",
     "get_dataset_names_used_for_training",
     "get_z_slice_bounds_per_position",
     "initialize_diffae_model",
@@ -53,5 +46,4 @@ __all__ = [
     "preprocess_tracking_manifest_for_model_eval",
     "update_prediction_from_crops_with_metadata",
     "update_prediction_from_tracks_with_metadata",
-    "upload_prediction_dataframe_to_fms",
 ]
