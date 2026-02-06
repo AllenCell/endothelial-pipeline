acce29aea5a3458b8c8fc65aee0bb95060b7be81
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-02-06 17:58:43 -0500

    Add demo workflow with all tags for review

---
+++ src/endo_pipeline/workflows/testing/demo_workflow.py
@@ -0,0 +1,14 @@
+def main() -> None:
+    """
+    Short description of the workflow.
+
+    #internal #fms #vast
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    from endo_pipeline.cli import workflow_cli
+
+    workflow_cli(main)
