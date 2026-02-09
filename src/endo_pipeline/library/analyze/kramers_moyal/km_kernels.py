9f5d75408d12c58bc4ea520c362775c9276fe9f9
Erin Angelini <erin.angelini@alleninstitute.org>
2026-02-09 15:50:03 -0800

    Simplify contents of `km_kernel` module (#1209)
    * rm __init__ file, update imports to import directly from kramers_moyal submodule
    
    * move methods from _km_computation into kramers_moyal, update import in tests/endo_pipeline, rm _km_computation
    
    * rename kramers_moyal to km_computation
    
    * rename _km_kernels to km_kernels
    
    * change relative import to full import in km_computation
    
    * rename module from kramersmoyal to kramers_moyal, update imports
    
    * rename _get_km_coeff to get_kramers_moyal_coeff
    
    * rm unnecessary wrapper method get_kramers_moyal, calls are now directly to get_kramers_moyal_coeffs
    
    * merge _km_worker and _km_wrapper into one simplified function
    
    * use consistent arg names for get_kramers_moyal_coeffs and _km_wrapper
    
    * move _string_to_kernel into km_kernels, update imports, and refactor method
    
    * move unit ball volume outside wrapper
    
    * move getting dims and euclidean norm to be outside the kernel decorator
    
    * clarify documentation of wrapper function
    
    * rm unused function silvermans_rule
    
    * rename function for clarity
    
    * set up flag for including per-dimension normalization, add temporary workflow to compare outputs with and without the additional normalization
    
    * polish temp workflow
    
    * rm 'normalisation' within kernels, and with it, the dims input
    
    * rm unused kernels: uniform, triangular, quartic
    
    * use dict for AVAILABLE_KERNEL_FUNCTIONS constant
    
    * rm spaces from docstring
    
    * warning messages for input reshaping / modifying cases
    
    * rename input 'kernel' to 'kernel_name'
    
    * replace instances of 'kernel_params' arg with two args: 'kernel_name' and 'kernel_bw'
    
    * Simplify method structure and calls in the `kramers_moyal` module (#1205)
    
    * rename _get_km_coeff to get_kramers_moyal_coeff
    
    * rm unnecessary wrapper method get_kramers_moyal, calls are now directly to get_kramers_moyal_coeffs
    
    * merge _km_worker and _km_wrapper into one simplified function
    
    * use consistent arg names for get_kramers_moyal_coeffs and _km_wrapper
    
    * move _string_to_kernel into km_kernels, update imports, and refactor method
    
    * rm spaces from docstring
    
    * warning messages for input reshaping / modifying cases
    
    * rename input 'kernel' to 'kernel_name'
    
    * replace instances of 'kernel_params' arg with two args: 'kernel_name' and 'kernel_bw'
    
    * turn comments into docstrings

--- src/endo_pipeline/library/analyze/kramers_moyal/km_kernels.py
+++ src/endo_pipeline/library/analyze/kramers_moyal/km_kernels.py
@@ -1,135 +1,95 @@
-import inspect
+import logging
 from collections.abc import Callable
 from functools import wraps
-from typing import Any
 
 import numpy as np
-from scipy.special import factorial2, gamma
-from scipy.stats import norm
+from scipy.special import gamma
 
-AVAILABLE_KERNEL_FUNCTIONS = ["epanechnikov", "gaussian", "uniform", "triangular", "quartic"]
+logger = logging.getLogger(__name__)
 
 
-def string_to_kernel(kernel_name: str) -> Callable:
+def _volume_unit_ball(dims: int) -> float:
+    """Calculate the volume of a unit ball in a given number of dimensions."""
+    # volume of a unit ball in dimension dims
+    return np.pi ** (dims / 2.0) / gamma(dims / 2.0 + 1.0)
+
+
+def _get_input_dims_and_distances(x: np.ndarray) -> tuple[int, np.ndarray]:
+    """Get the number of dimensions and the Euclidean norm of the input array."""
+    if len(x.shape) == 1:
+        x = x.reshape(-1, 1)
+
+    # x is an array of shape (n_points, n_dims), where each row corresponds
+    # to the difference between a pair of points along each dimension
+    dims = x.shape[-1]
+
+    # Euclidean norm of the array of vector differences
+    euc_norm = np.sqrt((x * x).sum(axis=-1))
+
+    return dims, euc_norm
+
+
+def scaled_kernel(kernel_func: Callable) -> Callable:
     """
-    Convert a kernel name to the corresponding callable (scaled) kernel function.
-    """
-    # get dictionary of all callable kernel functions in this module
-    import sys
+    Transform a pre-defined kernel function into a scaled kernel function
+    that can be used for kernel density estimation.
 
-    kernel_dict = {
-        name: func
-        for name, func in inspect.getmembers(sys.modules[__name__], inspect.isfunction)
-        if name in AVAILABLE_KERNEL_FUNCTIONS
-    }
-    if kernel_name in kernel_dict:
-        return kernel_dict[kernel_name]
-    else:
-        raise ValueError(
-            f"Kernel '{kernel}' not recognized. " f" Available kernels: {list(kernel_dict.keys())}"
-        )
+    **Original kernel function**
 
+    The original kernel function ``kernel_func`` should take in an array of distances and the
+    number of dimensions, and return the kernel values. Specifically, the array of distances
+    is an m x n array, where m is the number of pairs of points and n is the number of dimensions.
+    Then row i of the array corresponds to the difference between the i-th pair of points along each dimension.
 
-def kernel(kernel_func: Callable) -> Callable:
-    """
-    Transform a kernel function into a scaled kernel function with a given bandwidth.
+    **Kernel evaluation and scaling**
 
-    The output kernel function takes in a distance array and a bandwidth,
-    and returns the scaled kernel values.
+    Using this decorator, the resulting scaled kernel function will take in an said
+    array of distances and a bandwidth, compute the norm of the distances (i.e., turn x-y to ||x-y||),
+    and return the scaled kernel values. The scaling is done by dividing the distances by the bandwidth,
+    and then normalizing by the bandwidth raised to the power of the number of dimensions.
+
+    The value is also divided by the volume of the unit ball in that number of dimensions, so that
+    resulting kernel function can be used for kernel density estimation in any number of dimensions.
     """
 
     @wraps(kernel_func)  # just for naming
     def decorated(x: np.ndarray, bw: float) -> np.ndarray:
-        def _volume_unit_ball(dims: int) -> float:
-            # volume of a unit ball in dimension dims
-            return np.pi ** (dims / 2.0) / gamma(dims / 2.0 + 1.0)
-
-        if len(x.shape) == 1:
-            x = x.reshape(-1, 1)
-
-        dims = x.shape[-1]
-
-        # Euclidean norm
-        dist = np.sqrt((x * x).sum(axis=-1))
-
-        return kernel_func(dist / bw, dims) / (bw**dims) / _volume_unit_ball(dims)
+        dims, dist = _get_input_dims_and_distances(x)
+        return kernel_func(dist / bw) / (bw**dims) / _volume_unit_ball(dims)
 
     return decorated
 
 
-@kernel
-def epanechnikov(x: np.ndarray, dims: int) -> np.ndarray:
-    """Define the Epanechnikov kernel in dimensions dims."""
-    normalisation = 2.0 / (dims + 2.0)
+@scaled_kernel
+def epanechnikov(x: np.ndarray) -> np.ndarray:
+    """Define the Epanechnikov kernel."""
     x2 = x**2
     mask = x2 < 1.0
     kernel = np.zeros_like(x)
-    kernel[mask] = (1.0 - x2[mask]) / normalisation
+    kernel[mask] = 1.0 - x2[mask]
     return kernel
 
 
-@kernel
-def gaussian(x: np.ndarray, dims: int) -> np.ndarray:
-    """Define the Gaussian kernel in dimensions dims."""
-
-    def _gaussian_integral(n: int) -> float:
-        if n == 0:  # the integral of the 1D Gaussian is sqrt(2*pi)
-            return np.sqrt(np.pi * 2)
-        elif n % 2 == 0:
-            return np.sqrt(np.pi * 2) * factorial2(n - 1) / 2
-        else:
-            return np.sqrt(np.pi * 2) * factorial2(n - 1) * norm.pdf(0)
-
-    normalisation = dims * _gaussian_integral(dims - 1)
-    kernel = np.exp(-(x**2) / 2.0) / normalisation
+@scaled_kernel
+def gaussian(x: np.ndarray) -> np.ndarray:
+    """Define the Gaussian kernel."""
+    kernel = np.exp(-(x**2) / 2.0) / np.sqrt(2 * np.pi)
     return kernel
 
 
-@kernel
-def uniform(x: np.ndarray, dims: int) -> np.ndarray:
-    """Define the uniform, or rectangular, kernel in dimensions dims."""
-    mask = x < 1.0
-    kernel = np.zeros_like(x)
-    kernel[mask] = 1.0
-    return kernel
+AVAILABLE_KERNEL_FUNCTIONS = {"epanechnikov": epanechnikov, "gaussian": gaussian}
 
 
-@kernel
-def triangular(x: np.ndarray, dims: int) -> np.ndarray:
-    """Define the triangular kernel in dimensions dims."""
-    normalisation = 1.0 / 2.0
-    mask = x < 1.0
-    kernel = np.zeros_like(x)
-    kernel[mask] = (1.0 - np.abs(x[mask])) / normalisation
-    return kernel
-
-
-@kernel
-def quartic(x: np.ndarray, dims: int) -> np.ndarray:
-    """Define the quartic, or biweight, kernel in dimensions dims."""
-    normalisation = 2.0 / (dims + 2.0)
-    x2 = x**2
-    mask = x2 < 1.0
-    kernel = np.zeros_like(x)
-    kernel[mask] = ((1.0 - x2[mask]) ** 2) / normalisation
-    return kernel
-
-
-def silvermans_rule(
-    timeseries: np.ndarray, multi_traj: bool = False
-) -> np.ndarray[Any, np.dtype[np.floating[Any]]]:
-    """Apply Silverman's rule of thumb for bandwidth selection."""
-    if multi_traj:  # take average of std of each trajectory along each dimension
-        n = 0
-        dim = timeseries[0].shape[1]
-        sigma = np.zeros(dim)
-        for traj in timeseries:
-            n += len(traj)
-            sigma = sigma + traj.std(axis=0)
-        sigma = sigma / len(timeseries)
-    else:  # take std of all data points along each dimension
-        n = timeseries.shape[0]
-        sigma = timeseries.std(axis=0)
-
-    sigma = sigma.max()  # take max std across dimensions
-    return ((4.0 * sigma**5) / (3 * n)) ** (1 / 5)
+def string_to_kernel(kernel: str) -> Callable:
+    """
+    Convert a kernel name to the corresponding callable (scaled) kernel function.
+    """
+    # check if kernel is in the available implemented kernels,
+    # and return the corresponding function
+    if kernel in AVAILABLE_KERNEL_FUNCTIONS.keys():
+        return AVAILABLE_KERNEL_FUNCTIONS[kernel]
+    else:
+        raise ValueError(
+            f"Kernel '{kernel}' not recognized. "
+            f" Available kernels: {list(AVAILABLE_KERNEL_FUNCTIONS.keys())}"
+        )
