43b7b262f3796ece124e1c3fbc2bfc573b8b7750
Serge Parent <serge.edouard.parent@gmail.com>
2026-02-09 16:31:28 -0800

    - refactored code into workflow and library files - introduced multiprocessing if checking existing segmentations against diffae dataframes

---
+++ src/endo_pipeline/library/process/lib_grid_seg.py
@@ -0,0 +1,168 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from bioio_base.types import PhysicalPixelSizes
+from skimage.measure import regionprops
+from tqdm import tqdm
+
+from endo_pipeline.io import load_image_from_path
+from endo_pipeline.library.analyze.diffae_dataframe_utils import (
+    get_dataframe_for_dynamics_workflows,
+)
+from endo_pipeline.library.process.general_image_preprocessing import save_image_output
+from endo_pipeline.manifests import (
+    get_feature_dataframe_manifest_name,
+    load_dataframe_manifest,
+    load_model_manifest,
+)
+from endo_pipeline.settings.diffae_feature_dataframes import ColumnName
+from endo_pipeline.settings.image_data import (
+    IMG_SHAPE_RESOLUTION_1_3i_X,
+    IMG_SHAPE_RESOLUTION_1_3i_Y,
+    PIXEL_SIZE_3i_20x,
+)
+from endo_pipeline.settings.workflow_defaults import (
+    DEFAULT_MODEL_MANIFEST_NAME,
+    DEFAULT_MODEL_RUN_NAME,
+)
+
+
+def load_grid_diffae_df(
+    dataset_name: str,
+    model_manifest_name: str = DEFAULT_MODEL_MANIFEST_NAME,
+    model_run_name: str = DEFAULT_MODEL_RUN_NAME,
+) -> pd.DataFrame:
+    model_manifest = load_model_manifest(model_manifest_name)
+
+    dataframe_manifest_name = get_feature_dataframe_manifest_name(
+        model_manifest, model_run_name, crop_pattern="grid"
+    )
+    dataframe_manifest = load_dataframe_manifest(dataframe_manifest_name)
+
+    # load the grid-based diffae features dataframe to get the crop locations and
+    # crop labels for a given dataset
+    grid_df = get_dataframe_for_dynamics_workflows(
+        dataset_name, dataframe_manifest, filter_dataframe=False
+    )
+
+    # we don't need the latent feature columns for this workflow
+    feat_cols = [col for col in grid_df.columns if ColumnName.LATENT_FEATURE_PREFIX in col]
+    grid_df = grid_df.drop(columns=feat_cols)
+    return grid_df
+
+
+def make_grid_seg_filename(position: int, timepoint: int) -> str:
+    return f"{position}_T{timepoint}_grid_segmentation.ome.tiff"
+
+
+def make_crop_index_to_slice_mapping(grid_df: pd.DataFrame) -> dict[int, tuple[slice, slice]]:
+    """Returns a dictionary with the following structure from a grid-based DiffAE dataframe:
+    {crop_index: (slice(start_y, end_y), slice(start_x, end_x))}
+
+    This will be used to assign crop_index labels to the correct locations in the
+    grid segmentation images.
+    """
+    crop_index_slices = dict(
+        zip(
+            grid_df.crop_index.values,
+            zip(
+                map(slice, grid_df.start_y.values, grid_df.end_y.values),
+                map(slice, grid_df.start_x.values, grid_df.end_x.values),
+                strict=True,
+            ),
+            strict=True,
+        )
+    )
+    return crop_index_slices
+
+
+def create_grid_segmentation_images(grid_df: pd.DataFrame, out_dir: Path) -> None:
+    """Creates and saves grid segmentation images to the specified output directory
+    for each position and timepoint in the grid-based DiffAE dataframe.
+
+    Note:
+    The segmentation labels in the image will be equal to 1 + the crop_index from the grid_df.
+    This is because 0 must be reserved for the background.
+    """
+    # when creating the segmentation image assign the crop_index from grid_df
+    # to be the "segmentation" label. We will use the crop index as the
+    # segmentation ID.
+    dataset_name = grid_df.dataset.unique().item()
+
+    crop_index_slices = make_crop_index_to_slice_mapping(grid_df)
+
+    # check that the crops will fit in an initialized image
+    grid_seg = np.zeros((IMG_SHAPE_RESOLUTION_1_3i_Y, IMG_SHAPE_RESOLUTION_1_3i_X), dtype=np.uint16)
+
+    if (
+        grid_df.end_x.max() > IMG_SHAPE_RESOLUTION_1_3i_X
+        or grid_df.end_y.max() > IMG_SHAPE_RESOLUTION_1_3i_Y
+    ):
+        raise ValueError(
+            f"Grid crop locations exceed expected image shape of\
+            {(IMG_SHAPE_RESOLUTION_1_3i_Y, IMG_SHAPE_RESOLUTION_1_3i_X)}"
+        )
+
+    # we can probably do the multiprocessing at the position level
+    for pos, df in grid_df.groupby("position"):
+        out_subdir = out_dir / pos
+        out_subdir.mkdir(exist_ok=True)
+
+        # each position has a unique set of crop index labels
+        # intialize an empty image to hold the segmentation labels
+        grid_seg = np.zeros(
+            (IMG_SHAPE_RESOLUTION_1_3i_Y, IMG_SHAPE_RESOLUTION_1_3i_X), dtype=np.uint16
+        )
+
+        for crop_i in df.crop_index.unique():
+            grid_seg[crop_index_slices[crop_i]] = (
+                crop_i + 1
+            )  # add 1 so that background is 0 and first crop is label 1
+
+        # save the grid segmentation image for this position and timepoint
+        for tp in tqdm(
+            range(df.duration.unique().item()),
+            desc=f"Saving grid segmentation for {dataset_name} {pos}",
+        ):
+            fname = make_grid_seg_filename(pos, tp)
+
+            resolution_level = df.resolution_level.unique().item()
+            px_res_xy = PIXEL_SIZE_3i_20x * 2**resolution_level
+            px_res = PhysicalPixelSizes(Z=None, Y=px_res_xy, X=px_res_xy)
+
+            metadata = {
+                "image_name": f"{dataset_name}_{tp}",
+                "channel_colors": [(255, 255, 255)],
+                "channel_names": ["grid_segmentation"],
+                "physical_pixel_sizes": px_res,
+                "dim_order": "YX",
+            }
+            save_image_output(
+                out_path=out_subdir / fname,
+                images=[grid_seg],
+                images_metadata=metadata,
+                dtype=np.uint16,
+            )
+
+
+def check_crop_indices_against_existing_segmentations(df: pd.DataFrame, out_dir: Path) -> None:
+
+    pos = df.position.unique().item()
+    tp = df.frame_number.unique().item()
+    # for (pos, tp), df in grid_df.groupby(["position", "frame_number"]):
+    fp = out_dir / pos / make_grid_seg_filename(pos, tp)
+
+    segmentation = load_image_from_path(fp)
+
+    segprops = regionprops(label_image=segmentation.squeeze())
+    for prop in segprops:
+        crop_index_from_seg = prop.label - 1
+        bbox_cols = ["start_y", "start_x", "end_y", "end_x"]
+
+        crop_loc_matched = df[df.crop_index == crop_index_from_seg][bbox_cols] == prop.bbox
+        if crop_loc_matched is False:
+            raise ValueError(
+                f"Crop index {crop_index_from_seg} in segmentation does not\
+                    match bbox in grid_df for position {pos} and timepoint {tp}"
+            )
--- src/endo_pipeline/workflows/development/grid_seg.py
+++ src/endo_pipeline/workflows/development/grid_seg.py
@@ -1,115 +1,43 @@
-import numpy as np
-from bioio_base.types import PhysicalPixelSizes
+from concurrent.futures import ProcessPoolExecutor
 
+from tqdm import tqdm
+
+from endo_pipeline.configs import get_datasets_in_collection
 from endo_pipeline.io import get_output_path
-from endo_pipeline.library.analyze.diffae_dataframe_utils import (
-    get_dataframe_for_dynamics_workflows,
-)
-from endo_pipeline.library.process.general_image_preprocessing import save_image_output
-from endo_pipeline.manifests import (
-    get_feature_dataframe_manifest_name,
-    load_dataframe_manifest,
-    load_model_manifest,
-)
-from endo_pipeline.settings.diffae_feature_dataframes import ColumnName
-from endo_pipeline.settings.image_data import (
-    IMG_SHAPE_RESOLUTION_1_3i_X,
-    IMG_SHAPE_RESOLUTION_1_3i_Y,
-    PIXEL_SIZE_3i_20x,
-)
-from endo_pipeline.settings.workflow_defaults import (
-    DEFAULT_MODEL_MANIFEST_NAME,
-    DEFAULT_MODEL_RUN_NAME,
+from endo_pipeline.library.process.lib_grid_seg import (
+    check_crop_indices_against_existing_segmentations,
+    create_grid_segmentation_images,
+    load_grid_diffae_df,
 )
 
-dataset_name = "20250618_20X"
 
-model_manifest = load_model_manifest(DEFAULT_MODEL_MANIFEST_NAME)
-run_name = DEFAULT_MODEL_RUN_NAME
+def main(n_cores=4):
+    datasets = get_datasets_in_collection("diffae_model_training")
 
-dataframe_manifest_name = get_feature_dataframe_manifest_name(
-    model_manifest, run_name, crop_pattern="grid"
-)
-dataframe_manifest = load_dataframe_manifest(dataframe_manifest_name)
+    for i, dataset_name in enumerate(datasets):
+        out_dir = get_output_path(__file__)
+        out_dir.mkdir(parents=True, exist_ok=True)
 
-# load the grid-based diffae features dataframe to get the crop locations and
-# crop labels for a given dataset
-grid_df = get_dataframe_for_dynamics_workflows(dataset_name, dataframe_manifest)
+        grid_df = load_grid_diffae_df(dataset_name)
 
-# we don't need the latent feature columns for this workflow
-feat_cols = [col for col in grid_df.columns if ColumnName.LATENT_FEATURE_PREFIX in col]
-grid_df = grid_df.drop(columns=feat_cols)
+        if i == 0:
+            create_grid_segmentation_images(grid_df, out_dir)
 
-# when creating the segmentation image assign the crop_index from grid_df
-# to be the "segmentation" label. We will use the crop index as the
-# segmentation ID.
+        else:
+            print(f"Checking {dataset_name} grid segmentation against existing segmentations...")
+            nm, df = zip(*grid_df.groupby(["position", "frame_number"]), strict=False)
+            num_seg_files = len(nm)
+            with ProcessPoolExecutor(max_workers=n_cores) as worker_pool:
+                list(
+                    tqdm(
+                        worker_pool.map(
+                            check_crop_indices_against_existing_segmentations,
+                            df,
+                            [out_dir] * num_seg_files,
+                        ),
+                        desc=f"Checking grid segmentations for {dataset_name}",
+                        total=num_seg_files,
+                    )
+                )
 
-crop_index_slices = dict(
-    zip(
-        grid_df.crop_index.values,
-        zip(
-            map(slice, grid_df.start_y.values, grid_df.end_y.values),
-            map(slice, grid_df.start_x.values, grid_df.end_x.values),
-            strict=True,
-        ),
-        strict=True,
-    )
-)
-
-# check that the crops will fit in an initialized image
-grid_seg = np.zeros((IMG_SHAPE_RESOLUTION_1_3i_Y, IMG_SHAPE_RESOLUTION_1_3i_X), dtype=np.uint16)
-
-if (
-    grid_df.end_x.max() > IMG_SHAPE_RESOLUTION_1_3i_X
-    or grid_df.end_y.max() > IMG_SHAPE_RESOLUTION_1_3i_Y
-):
-    raise ValueError(
-        f"Grid crop locations exceed expected image shape of\
-        {(IMG_SHAPE_RESOLUTION_1_3i_Y, IMG_SHAPE_RESOLUTION_1_3i_X)}"
-    )
-
-# for crop_i in crop_index_slices:
-#     grid_seg[crop_index_slices[crop_i]] = (
-#         crop_i + 1
-#     )  # add 1 so that background is 0 and first crop is label 1
-
-out_dir = get_output_path(__file__)
-out_dir.mkdir(parents=True, exist_ok=True)
-for tp in sorted(grid_df.frame_number.unique()):
-    grid_df.sort_values(by="frame_number")
-
-
-# we can probably do the multiprocessing at the position level
-for nm, df in grid_df.groupby("position"):
-    # each position has a unique set of crop index labels
-    # intialize an empty image to hold the segmentation labels
-    grid_seg = np.zeros((IMG_SHAPE_RESOLUTION_1_3i_Y, IMG_SHAPE_RESOLUTION_1_3i_X), dtype=np.uint16)
-
-    for crop_i in df.crop_index.unique():
-        grid_seg[crop_index_slices[crop_i]] = (
-            crop_i + 1
-        )  # add 1 so that background is 0 and first crop is label 1
-
-    # save the grid segmentation image for this position and timepoint
-    for tp in range(df.duration.unique().item()):
-        fname = f"{dataset_name}_{tp}_grid_segmentation.ome.tiff"
-
-        resolution_level = df.resolution_level.unique().item()
-        px_res_xy = PIXEL_SIZE_3i_20x * 2**resolution_level
-        px_res = PhysicalPixelSizes(Z=None, Y=px_res_xy, X=px_res_xy)
-
-        metadata = {
-            "image_name": f"{dataset_name}_{tp}",
-            "channel_colors": [(255, 255, 255)],
-            "channel_names": "grid_segmentation",
-            "physical_pixel_sizes": px_res,
-            "dim_order": "YX",
-        }
-        save_image_output(
-            out_path=out_dir / fname, images=[grid_seg], images_metadata=metadata, dtype=np.uint16
-        )
-    break
-
-
-for nm, df in grid_df.groupby(["crop_index", "start_y", "end_y", "start_x", "end_x"]):
-    break
+    print("\N{PARTY POPPER} Done.")
