624722975177bf02cc0d990a4bc7314877db6b30
Serge Parent <serge.edouard.parent@gmail.com>
2026-02-13 17:17:05 -0800

    - SUPER dirty first pass

---
+++ src/endo_pipeline/library/analyze/intensity_features.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from skimage.segmentation import find_boundaries
+
+from endo_pipeline.configs import load_dataset_config
+from endo_pipeline.io import load_image
+from endo_pipeline.manifests import (
+    get_image_location_for_dataset,
+    get_zarr_location_for_position,
+    load_image_manifest,
+)
+from endo_pipeline.settings.image_data import DIMENSION_ORDER
+
+
+def calculate_edge_intensity_distribution_for_segmentations(
+    dataset_name, position, timepoint, df_at_tp, dim_order=DIMENSION_ORDER
+):
+
+    out_dir = Path(df_at_tp["output_dir"].unique().item())
+    dataset_config = load_dataset_config(dataset_name)
+    seg_manifest = load_image_manifest("cdh5_classic_seg")
+
+    image_loc = get_zarr_location_for_position(dataset_config, position)
+    raw_arr = load_image(image_loc, channels=["EGFP"], timepoints=timepoint, level=0)
+    raw_arr = raw_arr.max(axis=dim_order.index("Z")).squeeze().compute()
+    # voxel_size = load_image(image_loc, read=False).physical_pixel_sizes
+
+    seg_location = get_image_location_for_dataset(seg_manifest, dataset_config, position, timepoint)
+    seg_arr = load_image(seg_location, squeeze=True, compute=True)
+    # seg_filepath = seg_location.path.as_posix() if seg_location.path is not None else ""
+
+    # initialize dataframe
+    df_edge_intens = pd.DataFrame(columns=df_at_tp.columns)
+    df_edge_intens = df_edge_intens.drop("is_included", axis=1)
+
+    for label, seg_record in df_at_tp.groupby("label"):
+        seg_bound = find_boundaries(seg_arr == label)
+        seg_bound_locs = np.where(seg_bound)
+        seg_centroid = (
+            seg_record["centroid_Y"].values.item(),
+            seg_record["centroid_X"].values.item(),
+        )
+
+        # get the angle from each pixel in seg_bound to seg_centroid and also
+        # the fluorescence intensity at each of those pixels
+        angles = np.arctan2(
+            seg_bound_locs[0] - seg_centroid[0],
+            seg_bound_locs[1] - seg_centroid[1],
+        )
+        intensities = raw_arr[seg_bound_locs]
+
+        seg_record["angle"] = [angles.tolist()]
+        seg_record["intensity"] = [intensities.tolist()]
+
+        if df_edge_intens.empty:
+            df_edge_intens = seg_record.copy(deep=True)
+        else:
+            df_edge_intens = pd.concat([df_edge_intens, seg_record], ignore_index=True)
+    out_subdir = out_dir / dataset_name / f"P{position}"
+    out_subdir.mkdir(exist_ok=True)
+    df_edge_intens.to_parquet(
+        out_subdir / f"{dataset_name}_P{position}_T{timepoint}_edge_intensities.parquet"
+    )
+
+
+def calculate_edge_intensity_distribution_for_segmentations_mp(args):
+    (dataset_name, position, timepoint), df_at_tp = args
+    dataset_name = str(dataset_name)
+    position = int(position)
+    timepoint = int(timepoint)
+    return calculate_edge_intensity_distribution_for_segmentations(
+        dataset_name, position, timepoint, df_at_tp
+    )
--- src/endo_pipeline/workflows/development/cdh5_puncta_quant.py
+++ src/endo_pipeline/workflows/development/cdh5_puncta_quant.py
@@ -1,22 +1,3 @@
-import numpy as np
-from matplotlib import pyplot as plt
-from skimage.color import label2rgb
-from skimage.exposure import rescale_intensity
-from skimage.segmentation import find_boundaries
-
-from endo_pipeline.configs import load_dataset_config
-from endo_pipeline.io import load_image
-from endo_pipeline.library.analyze.integration.track_integration import (
-    load_pc_diffae_liveseg_feats_merged_table,
-)
-from endo_pipeline.library.process.general_image_preprocessing import build_analysis_queue
-from endo_pipeline.manifests import (
-    get_image_location_for_dataset,
-    get_zarr_location_for_position,
-    load_image_manifest,
-)
-from endo_pipeline.settings.image_data import DIMENSION_ORDER
-
 # examples of endo cells with puncta
 # dataset_name = "20250402_20X"
 # timepoint = 166
@@ -33,99 +14,211 @@ from endo_pipeline.settings.image_data import DIMENSION_ORDER
 # 2964 (193)
 # 3064 (47)
 
-analysis_queue = build_analysis_queue(
-    dataset_name_list=["20250402_20X"],
-    save_output=False,
-    out_dir=None,
-    overwrite=False,
-    verbose=True,
-    image_validation_frequency=48,
-    is_test=True,
-    t_start=166,
-    t_final=167,
-)
 
-args = analysis_queue[0]
-dataset_name = args["dataset_name"]
-position = args["position"]
-tp = args["T"]
+# examples of endo cell under high flow
+# dataset_name = "20250611_20X"
+# timepoint = 343
+# position = 0
+# 6342 (169)
 
-dataset_config = load_dataset_config(dataset_name)
-image_loc = get_zarr_location_for_position(dataset_config, position)
-raw_arr = load_image(image_loc, channels=["EGFP"], timepoints=tp, level=0)
-raw_arr = raw_arr.max(axis=DIMENSION_ORDER.index("Z")).squeeze().compute()
-voxel_size = load_image(image_loc, read=False).physical_pixel_sizes
 
-seg_manifest = load_image_manifest("cdh5_classic_seg")
-seg_location = get_image_location_for_dataset(seg_manifest, dataset_config, position, tp)
-seg_arr = load_image(seg_location, squeeze=True, compute=True)
-seg_filepath = seg_location.path.as_posix() if seg_location.path is not None else ""
+def main(n_cores=1):
+    from concurrent.futures import ProcessPoolExecutor
 
-seg_of_interest = 203
-# check that the segmentation of interest is in fact what I thought it would be
-overlay = label2rgb(
-    seg_arr == seg_of_interest,
-    rescale_intensity(np.clip(raw_arr, a_min=20, a_max=150), out_range=(0, 1)),
-    bg_label=0,
-    alpha=0.3,
-)
-plt.imshow(overlay)
+    from tqdm import tqdm
 
-plt.imshow(seg_arr == seg_of_interest)
-plt.imshow(raw_arr, vmax=140)
-# looks good.
+    from endo_pipeline.configs import get_subset_of_timepoint_annotations, load_dataset_config
+    from endo_pipeline.configs.dataset_io import concatenate_and_save_feature_tables
+    from endo_pipeline.io import get_output_path
+    from endo_pipeline.library.analyze.diffae_dataframe_utils import filter_dataframe_by_annotations
+    from endo_pipeline.library.analyze.integration.track_integration import (
+        load_pc_diffae_liveseg_feats_merged_table,
+    )
+    from endo_pipeline.library.analyze.intensity_features import (
+        calculate_edge_intensity_distribution_for_segmentations_mp,
+    )
 
-df = load_pc_diffae_liveseg_feats_merged_table(dataset_name)
-[x for x in df.columns if "fluor" in x]
+    low_flow_dataset_name = "20250402_20X"
+    high_flow_dataset_name = "20250611_20X"
+    dataset_name_list = [low_flow_dataset_name, high_flow_dataset_name]
 
-dataset_info_cols = [
-    "dataset",
-    "position",
-    "image_index",
-]
-crop_cols = [
-    "start_x_cdh5_seg",
-    "start_y_cdh5_seg",
-    "end_x_cdh5_seg",
-    "end_y_cdh5_seg",
-]
-seg_info_cols = [
-    "label",
-    "track_id",
-    "centroid_X",
-    "centroid_Y",
-]
-df_subset = df[dataset_info_cols + crop_cols + seg_info_cols].compute()
-record = df_subset.query(
-    "label == @seg_of_interest and position == @position and image_index == @tp"
-)
+    for dataset_name in dataset_name_list:
+        out_dir = get_output_path(__file__)
 
-x_slice = slice(record.start_x_cdh5_seg.values.item(), record.end_x_cdh5_seg.values.item())
-y_slice = slice(record.start_y_cdh5_seg.values.item(), record.end_y_cdh5_seg.values.item())
+        # analysis_queue = build_analysis_queue(
+        #     dataset_name_list=[dataset_name],
+        #     save_output=False,
+        #     out_dir=out_dir,
+        # )
+        # seg_of_interest = 203
 
-plt.imshow(overlay[y_slice, x_slice])
+        # analysis_queue = build_analysis_queue(
+        #     dataset_name_list=["20250611_20X"],
+        #     save_output=False,
+        #     out_dir=None,
+        #     overwrite=False,
+        #     verbose=True,
+        #     image_validation_frequency=48,
+        #     is_test=True,
+        #     t_start=343,
+        #     t_final=344,
+        # )
+        # seg_of_interest = 169
 
-overlay2 = label2rgb(
-    find_boundaries(seg_arr == seg_of_interest),
-    rescale_intensity(np.clip(raw_arr, a_min=20, a_max=150), out_range=(0, 1)),
-    bg_label=0,
-    alpha=0.3,
-)
-plt.imshow(overlay2[y_slice, x_slice])
+        # args = analysis_queue[0]
+        # dataset_name = args["dataset_name"]
+        # position = args["position"]
+        # tp = args["T"]
 
-seg_bound = find_boundaries(seg_arr == seg_of_interest)
-seg_bound_locs = np.where(seg_bound)
-crop = tuple(slice(locs_dim.min(), locs_dim.max() + 1) for locs_dim in seg_bound_locs)
-plt.imshow(seg_bound[crop])
+        # dataset_name = "20250402_20X"
 
-seg_centroid = tuple(int(locs_dim.mean()) for locs_dim in seg_bound_locs)
+        # # check that the segmentation of interest is in fact what I thought it would be
+        # overlay = label2rgb(
+        #     seg_arr == seg_of_interest,
+        #     rescale_intensity(np.clip(raw_arr, a_min=20, a_max=150), out_range=(0, 1)),
+        #     bg_label=0,
+        #     alpha=0.3,
+        # )
+        # plt.imshow(overlay)
 
-# get the angle from each pixel in seg_bound to seg_centroid and also
-# the fluorescence intensity at each of those pixels
-angles = np.arctan2(
-    seg_bound_locs[0] - seg_centroid[0],
-    seg_bound_locs[1] - seg_centroid[1],
-)
-intensities = raw_arr[seg_bound_locs]
+        # plt.imshow(seg_arr == seg_of_interest)
+        # plt.imshow(raw_arr, vmax=140)
+        # # looks good.
 
-plt.scatter(angles, intensities)
+        df = load_pc_diffae_liveseg_feats_merged_table(dataset_name)
+        dataset_config = load_dataset_config(dataset_name)
+
+        dataset_info_cols = [
+            "dataset",
+            "position",
+            "image_index",
+        ]
+        crop_cols = [
+            "start_x_cdh5_seg",
+            "start_y_cdh5_seg",
+            "end_x_cdh5_seg",
+            "end_y_cdh5_seg",
+        ]
+        seg_info_cols = [
+            "label",
+            "track_id",
+            "centroid_X",
+            "centroid_Y",
+        ]
+        filter_cols = ["is_included"]
+        df_subset = df[dataset_info_cols + crop_cols + seg_info_cols + filter_cols].compute()
+        # record = df_subset.query(
+        #     "label == @seg_of_interest and position == @position and image_index == @tp"
+        # )
+
+        df_subset = df_subset[df_subset.is_included]
+        df_subset["frame_number"] = df_subset["image_index"]
+        df_subset["dataset"] = dataset_name
+
+        annotations_to_ignore: list = []
+        timepoint_annotations = get_subset_of_timepoint_annotations(
+            annotations_to_ignore=annotations_to_ignore
+        )
+        df_subset = filter_dataframe_by_annotations(
+            df_subset,
+            dataset_config=dataset_config,
+            timepoint_annotations=timepoint_annotations,
+        )
+        df_subset["output_dir"] = out_dir.as_posix()
+
+        df_subset = df_subset.query("position == 0")
+
+        test_timepoints = sorted(df_subset.image_index.unique())[:5]
+        df_subset = df_subset[df_subset["image_index"] == test_timepoints]
+        # valid_timepoints = get_all_unannotated_timepoints(dataset_config)
+        # get_unannotated_timepoints_for_position(dataset_config, position)
+
+        # for grp in df_subset.groupby("position"):
+        #     break
+        # df_subset.groupby("position").apply(lambda x: x.image_index.isin(valid_timepoints[position]))
+
+        grps = df_subset.groupby(["dataset", "position", "image_index"])
+
+        worker_pool = ProcessPoolExecutor(max_workers=n_cores)
+
+        list(
+            tqdm(
+                worker_pool.map(
+                    calculate_edge_intensity_distribution_for_segmentations_mp,
+                    grps,
+                ),
+                desc=f"Calculating edge intensity distributions for {dataset_name}",
+                total=len(grps),
+            )
+        )
+
+        # for dataset_name in tqdm(
+        #     dataset_name_list, desc="Replacing individual tables with combined table..."
+        # ):
+        concatenate_and_save_feature_tables(
+            out_dir=out_dir,
+            dataset_name=dataset_name,
+            out_file_suffix="edge_intensities",
+            input_filename_contains="edge_intensities",
+            file_extension=".parquet",
+            remove_initial_files_and_folders=True,
+        )
+
+    # def initial_test():
+    #     x_slice = slice(record.start_x_cdh5_seg.values.item(), record.end_x_cdh5_seg.values.item())
+    #     y_slice = slice(record.start_y_cdh5_seg.values.item(), record.end_y_cdh5_seg.values.item())
+
+    #     # plt.imshow(overlay[y_slice, x_slice])
+    #     # plt.show()
+    #     # plt.clf()
+
+    #     overlay2 = label2rgb(
+    #         find_boundaries(seg_arr == seg_of_interest),
+    #         rescale_intensity(np.clip(raw_arr, a_min=20, a_max=150), out_range=(0, 1)),
+    #         bg_label=0,
+    #         alpha=0.3,
+    #     )
+    #     plt.imshow(overlay2[y_slice, x_slice])
+    #     plt.show()
+    #     plt.clf()
+
+    #     seg_bound = find_boundaries(seg_arr == seg_of_interest)
+    #     seg_bound_locs = np.where(seg_bound)
+    #     crop = tuple(slice(locs_dim.min(), locs_dim.max() + 1) for locs_dim in seg_bound_locs)
+    #     plt.imshow(seg_bound[crop])
+    #     plt.show()
+    #     plt.clf()
+
+    #     seg_centroid = tuple(int(locs_dim.mean()) for locs_dim in seg_bound_locs)
+
+    #     # get the angle from each pixel in seg_bound to seg_centroid and also
+    #     # the fluorescence intensity at each of those pixels
+    #     angles = np.arctan2(
+    #         seg_bound_locs[0] - seg_centroid[0],
+    #         seg_bound_locs[1] - seg_centroid[1],
+    #     )
+    #     intensities = raw_arr[seg_bound_locs]
+
+    #     plt.scatter(angles, intensities, color="k")
+    #     plt.show()
+    #     plt.clf()
+
+    #     import seaborn as sns
+
+    #     # Are the distributions of intensities different between high and low AT ALL?
+    #     sns.histplot(intensities)
+
+    #     intensities.std()
+
+    #     # take peak of histogram (use mean instead?) to get baseline of fluorescence intensity
+    #     # compare with 95th percentile of intensities(??) to determine if there is a "puncta"
+    #     # of high fluorescence at the cell boundary(??)
+
+    #     # make sure to filter to steady state only
+    #     # pick just 1 position to start with for low and high each
+
+
+if __name__ == "__main__":
+    from endo_pipeline.cli import workflow_cli
+
+    workflow_cli(main)
