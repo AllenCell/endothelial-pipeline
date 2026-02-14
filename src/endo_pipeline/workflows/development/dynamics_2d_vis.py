96aeafc3744efd3f276984d6caecda558c80b565
Erin Angelini <erin.angelini@alleninstitute.org>
2026-02-13 16:24:06 -0800

    accidentally explicity passed in 'tracked', should be crop_pattern

--- src/endo_pipeline/workflows/development/dynamics_2d_vis.py
+++ src/endo_pipeline/workflows/development/dynamics_2d_vis.py
@@ -155,7 +155,7 @@ def main(
             pca=pca,
             include_cell_piling=False,
             include_not_steady_state=False,
-            crop_pattern="tracked",
+            crop_pattern=crop_pattern,
             compute_polar=True,
             rescale_theta=RESCALE_THETA,
         )
