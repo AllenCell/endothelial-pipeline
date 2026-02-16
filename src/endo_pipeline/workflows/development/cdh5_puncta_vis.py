b4c162365389e92f87c3276742eb1b9cafc8f801
Serge Parent <serge.edouard.parent@gmail.com>
2026-02-16 11:30:23 -0800

    - finished first-pass edge fluorescence intensity quant vis workflow

--- src/endo_pipeline/workflows/development/cdh5_puncta_vis.py
+++ src/endo_pipeline/workflows/development/cdh5_puncta_vis.py
@@ -8,58 +8,81 @@ def main():
     import pandas as pd
     from matplotlib import pyplot as plt
 
+    from endo_pipeline.io import get_output_path, save_plot_to_path
+    from endo_pipeline.library.analyze.live_data_manifest.lib_make_seg_feats_manifest import (
+        calculate_derived_data_dynamics_dependent,
+    )
+
+    outdir = get_output_path(__file__)
+
     low_flow_fp = Path(
-        "//allen/aics/users/serge.parent/cellsmap/results/2026-02-14/cdh5_puncta_quant/20250402_20X_edge_intensities.parquet"
+        "//allen/aics/users/serge.parent/cellsmap/results/2026-02-16/cdh5_puncta_quant/20250402_20X_edge_intensities.parquet"
     )
     high_flow_fp = Path(
-        "//allen/aics/users/serge.parent/cellsmap/results/2026-02-14/cdh5_puncta_quant/20250611_20X_edge_intensities.parquet"
+        "//allen/aics/users/serge.parent/cellsmap/results/2026-02-16/cdh5_puncta_quant/20250611_20X_edge_intensities.parquet"
     )
 
     low_flow_df = pd.read_parquet(low_flow_fp)
     high_flow_df = pd.read_parquet(high_flow_fp)
 
-    df = pd.concat([low_flow_df, high_flow_df], ignore_index=True)
-    df.drop(
-        columns=[
-            "start_x_cdh5_seg",
-            "start_y_cdh5_seg",
-            "end_x_cdh5_seg",
-            "end_y_cdh5_seg",
-            "centroid_X",
-            "centroid_Y",
-            "frame_number",
-        ],
-        inplace=True,
-    )
-    df = df.explode(["intensity", "angle"]).reset_index(drop=True)
+    for df in [low_flow_df, high_flow_df]:
+        df = calculate_derived_data_dynamics_dependent(df)
+        df["intens_angle_rel_migration"] = df.apply(
+            lambda x: x["angle"] - x["centroid_velocity_angle"], axis=1
+        )
 
-    intens_sorted = np.sort(df.intensity.values)
-    intens_lim = np.percentile(intens_sorted, 99.9)
+    df = pd.concat([low_flow_df["intensity"], high_flow_df["intensity"]], ignore_index=True)
+    intens_all = df.explode("intensity")
+    intens_lim = np.percentile(intens_all.values, 99.9)
 
-    # fig, ax = plt.subplots()
-    # ax.semilogy()
-    # sns.histplot(data=df, x="intensity", hue="dataset", binwidth=1)
-    # ax.set_xlim(0, intens_lim)
-
-    # fig, ax = plt.subplots()
-    # # ax.semilogy()
-    # for df_sub in [low_flow_df, high_flow_df]:
-    #     intens_sorted = np.sort(np.concatenate(df_sub.intensity.values))
-    #     sns.kdeplot(intens_sorted)
-    # ax.set_xlim(0, intens_lim)
-
-    # ax.semilogy()
     for df_sub in [low_flow_df, high_flow_df]:
+        dataset_name = df_sub.dataset.unique().item()
+        if df_sub is low_flow_df:
+            flow_cond = "low_flow"
+        elif df_sub is high_flow_df:
+            flow_cond = "high_flow"
+        else:
+            flow_cond = "unknown_flow"
+
+        cmap = "inferno"
         fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
         intens = np.concatenate(df_sub.intensity.values)
         ang = np.concatenate(df_sub.angle.values)
-        # ax.scatter(x=ang, y=intens, s=1, alpha=0.1)
         ax.hist2d(
             x=ang,
             y=intens,
             bins=[72, 100],
             range=[[-np.pi, np.pi], [0, intens_lim]],
-            # norm=plt.matplotlib.colors.LogNorm(),
+            cmap=cmap,
         )
-        plt.show()
-        # break
+        for tick_label in ax.get_yticklabels():
+            tick_label.set_color("lightgrey")
+        fig.suptitle(f"{dataset_name} {flow_cond.replace('_', ' ').title()}")
+        plt.tight_layout()
+        fname = f"cdh5_edge_intens_quant_angle_rel_flow_{flow_cond}.png"
+        save_plot_to_path(fig, outdir, fname)
+        plt.close(fig)
+
+        fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
+        intens = np.concatenate(df_sub.intensity.values)
+        ang = np.concatenate(df_sub.intens_angle_rel_migration.values)
+        ax.hist2d(
+            x=ang,
+            y=intens,
+            bins=[72, 100],
+            range=[[-np.pi, np.pi], [0, intens_lim]],
+            cmap=cmap,
+        )
+        for tick_label in ax.get_yticklabels():
+            tick_label.set_color("lightgrey")
+        fig.suptitle(f"{dataset_name} {flow_cond.replace('_', ' ').title()}")
+        plt.tight_layout()
+        fname = f"cdh5_edge_intens_quant_angle_rel_migration_{flow_cond}.png"
+        save_plot_to_path(fig, outdir, fname)
+        plt.close(fig)
+
+
+if __name__ == "__main__":
+    from endo_pipeline.cli import workflow_cli
+
+    workflow_cli(main)
