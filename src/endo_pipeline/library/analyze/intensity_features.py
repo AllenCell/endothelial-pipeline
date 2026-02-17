4bd587f5f9c60c5308913a1346fd14c64ca5de4a
Serge Parent <serge.edouard.parent@gmail.com>
2026-02-16 17:48:57 -0800

    - added more plots to visualization output

--- src/endo_pipeline/library/analyze/intensity_features.py
+++ src/endo_pipeline/library/analyze/intensity_features.py
@@ -2,6 +2,10 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+from scipy import signal
+from skimage.color import label2rgb
+from skimage.exposure import rescale_intensity
 from skimage.segmentation import find_boundaries
 
 from endo_pipeline.configs import load_dataset_config
@@ -13,6 +17,62 @@ from endo_pipeline.manifests import (
 )
 from endo_pipeline.settings.image_data import DIMENSION_ORDER
 
+EXAMPLES = {
+    "high_flow": [
+        {
+            "dataset_name": "20250611_20X",
+            "position": 0,
+            "timepoint": 275,
+            "track_id": 5175,
+            "label": 135,
+        }
+    ],
+    "low_flow": [
+        # {
+        #     "dataset_name": "20250402_20X",
+        #     "position": 0,
+        #     "timepoint": 166,
+        #     "track_id": 775,
+        #     "label": 141,
+        # },
+        # {
+        #     "dataset_name": "20250402_20X",
+        #     "position": 0,
+        #     "timepoint": 166,
+        #     "track_id": 4076,
+        #     "label": 343,
+        # },
+        {
+            "dataset_name": "20250402_20X",
+            "position": 0,
+            "timepoint": 166,
+            "track_id": 3647,
+            "label": 203,
+        },
+        {
+            "dataset_name": "20250402_20X",
+            "position": 0,
+            "timepoint": 166,
+            "track_id": 1781,
+            "label": 213,
+        },
+        {
+            "dataset_name": "20250402_20X",
+            "position": 0,
+            "timepoint": 166,
+            "track_id": 2964,
+            "label": 193,
+        },
+        {
+            "dataset_name": "20250402_20X",
+            "position": 0,
+            "timepoint": 166,
+            "track_id": 3064,
+            "label": 47,
+        },
+    ],
+}
+
 
 def calculate_edge_intensity_distribution_for_segmentations(
     dataset_name, position, timepoint, df_at_tp, dim_order=DIMENSION_ORDER
@@ -20,16 +80,14 @@ def calculate_edge_intensity_distribution_for_segmentations(
 
     out_dir = Path(df_at_tp["output_dir"].unique().item())
     dataset_config = load_dataset_config(dataset_name)
-    seg_manifest = load_image_manifest("cdh5_classic_seg")
 
     image_loc = get_zarr_location_for_position(dataset_config, position)
     raw_arr = load_image(image_loc, channels=["EGFP"], timepoints=timepoint, level=0)
     raw_arr = raw_arr.max(axis=dim_order.index("Z")).squeeze().compute()
-    # voxel_size = load_image(image_loc, read=False).physical_pixel_sizes
 
+    seg_manifest = load_image_manifest("cdh5_classic_seg")
     seg_location = get_image_location_for_dataset(seg_manifest, dataset_config, position, timepoint)
     seg_arr = load_image(seg_location, squeeze=True, compute=True)
-    # seg_filepath = seg_location.path.as_posix() if seg_location.path is not None else ""
 
     # initialize dataframe
     df_edge_intens = pd.DataFrame(columns=df_at_tp.columns)
@@ -73,3 +131,146 @@ def calculate_edge_intensity_distribution_for_segmentations_mp(args):
     return calculate_edge_intensity_distribution_for_segmentations(
         dataset_name, position, timepoint, df_at_tp
     )
+
+
+def show_intensity_measure_example(
+    df: pd.DataFrame,
+    dataset_name: str,
+    position: int,
+    timepoint: int,
+    seg_label: int,
+    dim_order: str = DIMENSION_ORDER,
+) -> tuple[plt.Figure, tuple[plt.Axes, plt.Axes]]:
+    dataset_config = load_dataset_config(dataset_name)
+
+    image_loc = get_zarr_location_for_position(dataset_config, position)
+    raw_arr = load_image(image_loc, channels=["EGFP"], timepoints=timepoint, level=0)
+    raw_arr = raw_arr.max(axis=dim_order.index("Z")).squeeze().compute()
+
+    seg_manifest = load_image_manifest("cdh5_classic_seg")
+    seg_location = get_image_location_for_dataset(seg_manifest, dataset_config, position, timepoint)
+    seg_arr = load_image(seg_location, squeeze=True, compute=True)
+
+    record = df[
+        (df.dataset == dataset_name)
+        & (df.position == position)
+        & (df.image_index == timepoint)
+        & (df.label == seg_label)
+    ]
+    # x_slice = slice(record.start_x_cdh5_seg.values.item(), record.end_x_cdh5_seg.values.item())
+    # y_slice = slice(record.start_y_cdh5_seg.values.item(), record.end_y_cdh5_seg.values.item())
+    y_slice, x_slice = [slice(d.min() - 10, d.max() + 11) for d in np.where(seg_arr == seg_label)]
+
+    overlay = label2rgb(
+        find_boundaries(seg_arr == seg_label),
+        rescale_intensity(np.clip(raw_arr, a_min=20, a_max=150), out_range=(0, 1)),
+        bg_label=0,
+        alpha=0.3,
+    )
+    # fig, ax = plt.subplots()
+    # ax.imshow(overlay[y_slice, x_slice])
+    # plt.show()
+    # plt.clf()
+
+    seg_bound = find_boundaries(seg_arr == seg_label)
+    seg_bound_locs = np.where(seg_bound)
+    # crop = tuple(slice(locs_dim.min(), locs_dim.max() + 1) for locs_dim in seg_bound_locs)
+    # plt.imshow(seg_bound[crop])
+    # plt.show()
+    # plt.clf()
+
+    seg_centroid = (
+        record["centroid_Y"].values.item(),
+        record["centroid_X"].values.item(),
+    )
+
+    # get the angle from each pixel in seg_bound to seg_centroid and also
+    # the fluorescence intensity at each of those pixels
+    angles = np.arctan2(
+        seg_bound_locs[0] - seg_centroid[0],
+        seg_bound_locs[1] - seg_centroid[1],
+    )
+    intensities = raw_arr[seg_bound_locs]
+    # intensities_smoothed = (
+    #     pd.Series(intensities).rolling(window=11, min_periods=1, center=True).mean().values
+    # )
+    edge_data, peak_angles, peak_intensities, _ = get_peaks_of_edge_intensities(angles, intensities)
+
+    fig = plt.figure(figsize=(9, 3))
+    gs = fig.add_gridspec(nrows=1, ncols=3)  # , width_ratios=[1, 1], wspace=0.05)
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[0, 1:3])
+    ax1.imshow(overlay[y_slice, x_slice])
+    ax1.set_frame_on(False)
+    ax1.axis("off")
+    ax2.scatter(
+        record["angle"].values[0], record["intensity"].values[0], marker="o", color="tab:red", s=5
+    )
+    ax2.scatter(edge_data.angle, edge_data.intensity, marker=".", color="tab:blue", s=3)
+    ax2.plot(edge_data.angle, edge_data.intensity_smoothed, color="k", lw=1, ls="--")
+    ax2.set_xlabel("Angle (rad)")
+    ax2.set_xticks(np.linspace(-np.pi, np.pi, 5, endpoint=True))
+    ax2.set_xticklabels(["-π", "-π/2", "0", "π/2", "π"])
+    for x, y in zip(peak_angles.values, peak_intensities.values, strict=True):
+        ax2.annotate(
+            text=" ",
+            xy=(x, y),
+            xytext=(x + 0.3, y + 20),
+            xycoords="data",
+            arrowprops={
+                "arrowstyle": "-|>",
+                "linestyle": "-",
+                "color": "tab:orange",
+                "lw": 1,
+            },
+        )
+
+    return fig, (ax1, ax2)
+
+
+def get_peaks_of_edge_intensities(
+    angles: np.ndarray,
+    intensities: np.ndarray,
+    peak_prominence: int = 50,
+    peak_to_peak_distance_minimum: int = 100,
+) -> tuple[pd.DataFrame, np.ndarray, np.ndarray, dict]:
+    edge_data = pd.DataFrame({"angle": angles, "intensity": intensities}).sort_values("angle")
+    # edge_data["intensity_smoothed"] = ndimage.gaussian_filter1d(edge_data["intensity"], sigma=3)
+    # edge_data["intensity_smoothed"] = (
+    #     edge_data["intensity"].rolling(window=21, center=True, win_type="gaussian").mean(std=3)
+    # )
+    edge_data["intensity_smoothed"] = (
+        edge_data["intensity"].rolling(window=5, center=True).quantile(0.75)
+    )
+    peak_locs, peak_details = signal.find_peaks(
+        edge_data.intensity_smoothed,
+        distance=peak_to_peak_distance_minimum,
+        prominence=peak_prominence,
+    )
+    peak_angles = edge_data.angle.iloc[peak_locs]
+    peak_intensities = edge_data.intensity_smoothed.iloc[peak_locs]
+    return edge_data, peak_angles, peak_intensities, peak_details
+
+
+def count_peaks(
+    angles: np.ndarray,
+    intensities: np.ndarray,
+    peak_prominence_min: int = 50,
+    peak_to_peak_distance_minimum: int = 100,
+) -> int:
+    _, peak_angles, _, _ = get_peaks_of_edge_intensities(
+        angles, intensities, peak_prominence_min, peak_to_peak_distance_minimum
+    )
+    return len(peak_angles)
+
+
+def get_peak_prominence_vals(
+    angles: np.ndarray,
+    intensities: np.ndarray,
+    peak_prominence_min: int = 50,
+    peak_to_peak_distance_minimum: int = 100,
+) -> list:
+    _, _, _, peak_details = get_peaks_of_edge_intensities(
+        angles, intensities, peak_prominence_min, peak_to_peak_distance_minimum
+    )
+    return peak_details["prominences"].tolist()
--- src/endo_pipeline/workflows/development/cdh5_puncta_vis.py
+++ src/endo_pipeline/workflows/development/cdh5_puncta_vis.py
@@ -2,13 +2,21 @@
 
 
 def main():
+    from functools import reduce
     from pathlib import Path
 
     import numpy as np
     import pandas as pd
+    import seaborn as sns
     from matplotlib import pyplot as plt
 
     from endo_pipeline.io import get_output_path, save_plot_to_path
+    from endo_pipeline.library.analyze.intensity_features import (
+        EXAMPLES,
+        count_peaks,
+        get_peak_prominence_vals,
+        show_intensity_measure_example,
+    )
     from endo_pipeline.library.analyze.live_data_manifest.lib_make_seg_feats_manifest import (
         calculate_derived_data_dynamics_dependent,
     )
@@ -30,6 +38,11 @@ def main():
         df["intens_angle_rel_migration"] = df.apply(
             lambda x: x["angle"] - x["centroid_velocity_angle"], axis=1
         )
+        df["num_peaks"] = df.apply(lambda x: count_peaks(x["angle"], x["intensity"]), axis=1)
+        df["peak_prominences"] = df.apply(
+            lambda x: get_peak_prominence_vals(x["angle"], x["intensity"]),
+            axis=1,
+        )
 
     df = pd.concat([low_flow_df["intensity"], high_flow_df["intensity"]], ignore_index=True)
     intens_all = df.explode("intensity")
@@ -37,10 +50,21 @@ def main():
 
     for df_sub in [low_flow_df, high_flow_df]:
         dataset_name = df_sub.dataset.unique().item()
+        position = df_sub.position.unique().item()
+
+        examples_to_plot = []
         if df_sub is low_flow_df:
             flow_cond = "low_flow"
+            for ex in EXAMPLES["low_flow"]:
+                if ex["dataset_name"] == dataset_name and ex["position"] == position:
+                    examples_to_plot.append(ex)
+                    break  # only plot the first example for this proof-of-concept
         elif df_sub is high_flow_df:
             flow_cond = "high_flow"
+            for ex in EXAMPLES["high_flow"]:
+                if ex["dataset_name"] == dataset_name and ex["position"] == position:
+                    examples_to_plot.append(ex)
+                    break  # only plot the first example for this proof-of-concept
         else:
             flow_cond = "unknown_flow"
 
@@ -57,7 +81,7 @@ def main():
         )
         for tick_label in ax.get_yticklabels():
             tick_label.set_color("lightgrey")
-        fig.suptitle(f"{dataset_name} {flow_cond.replace('_', ' ').title()}")
+        fig.suptitle(f"{dataset_name} P{position} {flow_cond.replace('_', ' ').title()}")
         plt.tight_layout()
         fname = f"cdh5_edge_intens_quant_angle_rel_flow_{flow_cond}.png"
         save_plot_to_path(fig, outdir, fname)
@@ -75,11 +99,139 @@ def main():
         )
         for tick_label in ax.get_yticklabels():
             tick_label.set_color("lightgrey")
-        fig.suptitle(f"{dataset_name} {flow_cond.replace('_', ' ').title()}")
+        fig.suptitle(f"{dataset_name} P{position} {flow_cond.replace('_', ' ').title()}")
         plt.tight_layout()
         fname = f"cdh5_edge_intens_quant_angle_rel_migration_{flow_cond}.png"
         save_plot_to_path(fig, outdir, fname)
         plt.close(fig)
+
+        for ex in examples_to_plot:
+            timepoint = ex["timepoint"]
+            seg_label = ex["label"]
+            fig, axs = show_intensity_measure_example(
+                df=df_sub,
+                dataset_name=dataset_name,
+                position=position,
+                timepoint=timepoint,
+                seg_label=seg_label,
+            )
+            fname = (
+                f"cdh5_edge_intens_quant_example_{dataset_name}_T{timepoint}_label{seg_label}.png"
+            )
+            save_plot_to_path(fig, outdir, fname)
+            plt.close(fig)
+
+    fig, ax = plt.subplots()
+    sns.histplot(
+        low_flow_df.num_peaks,
+        binwidth=1,
+        color="tab:blue",
+        label=low_flow_df.dataset.unique().item(),
+        discrete=True,
+        ax=ax,
+    )
+    sns.histplot(
+        high_flow_df.num_peaks,
+        binwidth=1,
+        color="tab:orange",
+        label=high_flow_df.dataset.unique().item(),
+        discrete=True,
+        ax=ax,
+    )
+    ax.set_xlabel("Number of peaks in edge intensity distribution")
+    ax.legend()
+    plt.tight_layout()
+    fname = "cdh5_edge_intens_quant_num_peaks.png"
+    save_plot_to_path(fig, outdir, fname)
+    plt.close(fig)
+
+    fig, ax = plt.subplots()
+    sns.histplot(
+        low_flow_df.num_peaks,
+        binwidth=1,
+        color="tab:blue",
+        label=low_flow_df.dataset.unique().item(),
+        element="step",
+        fill=False,
+        discrete=True,
+        cumulative=True,
+        ax=ax,
+    )
+    sns.histplot(
+        high_flow_df.num_peaks,
+        binwidth=1,
+        color="tab:orange",
+        label=high_flow_df.dataset.unique().item(),
+        element="step",
+        fill=False,
+        discrete=True,
+        cumulative=True,
+        ax=ax,
+    )
+    ax.set_ylabel("Cumulative count")
+    ax.set_xlabel("Number of peaks in edge intensity distribution")
+    ax.legend()
+    plt.tight_layout()
+    fname = "cdh5_edge_intens_quant_num_peaks_cumulative.png"
+    save_plot_to_path(fig, outdir, fname)
+    plt.close(fig)
+
+    fig, ax = plt.subplots()
+    sns.histplot(
+        reduce(lambda a, b: a + b, low_flow_df.peak_prominences.values.tolist(), []),
+        binwidth=1,
+        color="tab:blue",
+        label=low_flow_df.dataset.unique().item(),
+        discrete=True,
+        ax=ax,
+    )
+    sns.histplot(
+        reduce(lambda a, b: a + b, high_flow_df.peak_prominences.values.tolist(), []),
+        binwidth=1,
+        color="tab:orange",
+        label=high_flow_df.dataset.unique().item(),
+        discrete=True,
+        ax=ax,
+    )
+    ax.set_xlim(0, intens_lim)
+    ax.set_ylabel("Cumulative count")
+    ax.set_xlabel("Peak prominence value")
+    ax.legend()
+    plt.tight_layout()
+    fname = "cdh5_edge_intens_quant_peak_prom.png"
+    save_plot_to_path(fig, outdir, fname)
+    plt.close(fig)
+
+    fig, ax = plt.subplots()
+    sns.histplot(
+        reduce(lambda a, b: a + b, low_flow_df.peak_prominences.values.tolist(), []),
+        binwidth=1,
+        color="tab:blue",
+        label=low_flow_df.dataset.unique().item(),
+        element="step",
+        fill=False,
+        discrete=True,
+        cumulative=True,
+        ax=ax,
+    )
+    sns.histplot(
+        reduce(lambda a, b: a + b, high_flow_df.peak_prominences.values.tolist(), []),
+        binwidth=1,
+        color="tab:orange",
+        label=high_flow_df.dataset.unique().item(),
+        element="step",
+        fill=False,
+        discrete=True,
+        cumulative=True,
+        ax=ax,
+    )
+    ax.set_ylabel("Cumulative count")
+    ax.set_xlabel("Peak prominence value")
+    ax.legend()
+    plt.tight_layout()
+    fname = "cdh5_edge_intens_quant_peak_prom_cumulative.png"
+    save_plot_to_path(fig, outdir, fname)
+    plt.close(fig)
 
 
 if __name__ == "__main__":
