d3395af2a1c8dc30915346bf454973cde68f2ddb
Erin Angelini <erin.angelini@alleninstitute.org>
2026-02-19 10:35:05 -0800

    Create workflow to generate 2D "heatmap"/contour plots of the (2D, pairwise) drift coefficients for `r`, `theta`, and `rho` (#1217)
    * initialize workflow
    
    * use new kernel specification
    
    * create helper method for plotting contours of drift function
    
    * rm plots of quasi phase portrait
    
    * name subdir by crop pattern type
    
    * rename settings module polar_coords to dynamics_workflow
    
    * restructure settings modules; polar_coords becomes dynamics_workflows, use dict mapping between feature column names and tuples to consistently set bin & plot limits; update imports in other files and workflows to use correct settings module structure
    
    * simplify workflow structure by placing pairwise plots in a loop
    
    * update comments for clarity; set num PCs to fit via global constant
    
    * specify kernel function names and bandwidths in settings, initialize inside loop
    
    * missed erroneuous enumerate() call
    
    * update docstring
    
    * add method for plotting 2D vector field with nullclines, add docstrings for both plotting methods
    
    * add quiver plot to workflow
    
    * plot formatting updates for nullclines
    
    * update nullcline formatting
    
    * Delay dataframe loading in `get_dataframe_for_dynamics_workflows` to speed up processing of tracked crop features (#1219)
    
    * add optional columns_to_keep input for get_dataframe_for_dynamics_workflows
    
    * set default metadata to keep on initial loading via global constant
    
    * add track_id as ColumnName attribute
    
    * add ColumnName.TRACK_ID to columns to keep, use strenum in add_crop_index
    
    * catch bug with filter_dataframe_by_annotations call inside get_dataframe_... method
    
    * use delay=True, compute only after filtering down to columns_to_keep_
    
    * drop latent feature columns in the pc projection case to save memory
    
    * update docstring with info about column filtering
    
    * don't need to copy, can just compute
    
    * big bugfix: have to always pass in grid-based crop dataframe manifest name to fit_pca
    
    * need to pass crop pattern into get_dataframe_... explicitly
    
    * accidentally explicity passed in 'tracked', should be crop_pattern
    
    * small update with figure axes labels
    
    * merge conflict update; resimplify add_crop_index

--- src/endo_pipeline/library/analyze/diffae_dataframe_utils.py
+++ src/endo_pipeline/library/analyze/diffae_dataframe_utils.py
@@ -29,7 +29,11 @@ from endo_pipeline.settings.diffae_feature_dataframes import (
     DIFFAE_PC_COLUMN_NAME_GROUPS,
     ColumnName,
 )
-from endo_pipeline.settings.polar_coords import RESCALE_THETA, THETA_RESCALED_PERIOD
+from endo_pipeline.settings.dynamics_workflows import (
+    METADATA_COLUMNS_TO_KEEP,
+    PERIOD_THETA_RESCALED,
+    RESCALE_THETA,
+)
 from endo_pipeline.settings.workflow_defaults import (
     DEFAULT_MODEL_MANIFEST_NAME,
     DEFAULT_MODEL_RUN_NAME,
@@ -614,6 +618,7 @@ def project_features_to_pcs(
 def get_dataframe_for_dynamics_workflows(
     dataset_name: str,
     manifest: DataframeManifest,
+    columns_to_keep: list[str] | None = None,
     pca: PCA | None = None,
     filter_dataframe: bool = True,
     include_cell_piling: bool = True,
@@ -627,22 +632,39 @@ def get_dataframe_for_dynamics_workflows(
     analysis in the stochastic dynamics workflow. Adds crop index column to
     DataFrame, and projects feature data onto PC axes.
 
+    **Column selection and filtering**
+
+    The input DataFrame is filtered to keep only necessary columns to save
+    memory. At a minimum, the metadata columns defined in
+    METADATA_COLUMNS_TO_KEEP and the latent feature columns needed for PCA
+    projection are kept. Additional columns can be specified to keep via the
+    input ``columns_to_keep``.
+
+    In the case that the input ``pca`` is not None, the feature data is
+    projected onto the PC axes defined by the PCA model, and the original latent
+    feature columns are dropped to save memory.
+
     Parameters
     ----------
     dataset_name
         Name of dataset
     manifest
         Dataframe manifest for loading model features.
+    columns_to_keep
+        List of additional column names to keep in the output DataFrame.
     pca
         PCA model to fit to feature data. If None, do not project feature data.
     filter_dataframe
-        Whether to filter out annotated timepoints and positions from the dataframe.
+        Whether to filter out annotated timepoints and positions from the
+        dataframe.
     include_cell_piling
         True keep timepoints annotated as "cell_piling", False to remove them.
     include_not_steady_state
-        True to keep timepoints annotated as "not_steady_state", False to remove them.
+        True to keep timepoints annotated as "not_steady_state", False to remove
+        them.
     crop_pattern
-        Crop pattern used to generate the feature dataframe. Either 'grid' or 'tracked'.
+        Crop pattern used to generate the feature dataframe. Either 'grid' or
+        'tracked'.
     compute_polar
         Whether to compute polar coordinates (r, theta) from the first two PCs.
     rescale_theta
@@ -655,10 +677,22 @@ def get_dataframe_for_dynamics_workflows(
     """
 
     location = get_dataframe_location_for_dataset(manifest, dataset_name)
-    df = load_dataframe(location)
+    df = load_dataframe(location, delay=True)
     feat_cols = get_latent_feature_column_names_from_dataframe(df)
 
-    df_with_crop = add_crop_index(df, crop_pattern)
+    # start with default metadatac columns to keep
+    columns_to_keep_ = list(METADATA_COLUMNS_TO_KEEP)
+    if columns_to_keep is not None:
+        columns_to_keep_.extend(columns_to_keep)  # add any additional specified columns to keep
+    columns_to_keep_.extend(feat_cols)  # also keep feature columns for PCA projection
+    if crop_pattern == "tracked":
+        columns_to_keep_.extend(
+            [ColumnName.TRACK_ID]
+        )  # also keep track ID column for tracked crops
+    columns_to_keep_ = list(set(columns_to_keep_))  # remove duplicates, if any
+
+    # keep only necessary columns to save memory
+    df_ = df[columns_to_keep_].compute()
 
     # filter out annotated timepoints, including or excluding
     # "cell piling" and "not steady state" annotations as specified
@@ -671,13 +705,15 @@ def get_dataframe_for_dynamics_workflows(
         timepoint_annotations = get_subset_of_timepoint_annotations(
             annotations_to_ignore=annotations_to_ignore
         )
-        df_with_crop = filter_dataframe_by_annotations(
-            df_with_crop,
+        df_filtered = filter_dataframe_by_annotations(
+            df_,
             load_dataset_config(dataset_name),
             timepoint_annotations=timepoint_annotations,
         )
     else:
-        pass
+        df_filtered = df_
+
+    df_with_crop = add_crop_index(df_filtered, crop_pattern)
 
     # add dataset duration description column
     dataset_config = load_dataset_config(dataset_name)
@@ -689,13 +725,17 @@ def get_dataframe_for_dynamics_workflows(
 
     else:
         # project feature data onto PC axes
-        return project_features_to_pcs(
+        df_with_pcs = project_features_to_pcs(
             df_with_crop,
             pca,
             feat_cols=feat_cols,
             compute_polar=compute_polar,
             rescale_theta=rescale_theta,
         )
+        df_drop_original_feats = df_with_pcs.drop(
+            columns=feat_cols
+        )  # drop original feature columns to save memory
+        return df_drop_original_feats
 
 
 def get_dataset_descriptions(
@@ -816,8 +856,8 @@ def add_crop_index(
         logger.error("Crop pattern must be 'tracked' or 'grid', got [ %s ]", crop_pattern)
         raise ValueError("Input crop_pattern must be 'grid' or 'tracked'")
 
-    if crop_pattern == "tracked" and "track_id" in df.columns:
-        required_columns = [ColumnName.POSITION, "track_id"]
+    if crop_pattern == "tracked" and ColumnName.TRACK_ID in df.columns:
+        required_columns = [ColumnName.POSITION, ColumnName.TRACK_ID]
     elif crop_pattern == "grid":
         required_columns = [ColumnName.POSITION, ColumnName.START_X, ColumnName.START_Y]
 
@@ -923,7 +963,7 @@ def split_dataset_by_flow(
 
 
 def get_traj_and_diff(
-    df: pd.DataFrame, column_names: list, polar_angle_period: float = THETA_RESCALED_PERIOD
+    df: pd.DataFrame, column_names: list, polar_angle_period: float = PERIOD_THETA_RESCALED
 ) -> tuple[list[np.ndarray], list[np.ndarray]]:
     """
     Get trajectories and single-timepoint displacement vectors for each crop in feature space.
