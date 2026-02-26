1f1558eba1dc896fce8bb1ddf625efd307a62c65
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2026-02-26 22:22:16 +0000

    wip

--- src/endo_pipeline/workflows/development/compare_lda_nb.py
+++ src/endo_pipeline/workflows/development/compare_lda_nb.py
@@ -3,10 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from endo_pipeline.configs import get_subset_of_timepoint_annotations, load_dataset_config
 from endo_pipeline.io import load_dataframe
 from endo_pipeline.library.analyze.diffae_dataframe_utils import (
+    filter_dataframe_by_annotations,
     fit_pca,
     get_dataframe_for_dynamics_workflows,
+)
+from endo_pipeline.library.analyze.live_data_manifest.lib_make_seg_feats_manifest import (
+    calculate_derived_data_dynamics_dependent,
 )
 from endo_pipeline.library.analyze.migration_pc.lda_analysis import apply_lda_projection
 from endo_pipeline.manifests import (
@@ -15,6 +20,7 @@ from endo_pipeline.manifests import (
     load_dataframe_manifest,
     load_model_manifest,
 )
+from endo_pipeline.settings import SEGMENTATION_FEATURE_COLUMNS
 from endo_pipeline.settings.workflow_defaults import (
     DEFAULT_MODEL_MANIFEST_NAME,
     DEFAULT_MODEL_RUN_NAME,
@@ -38,12 +44,8 @@ dataframe_manifest = load_dataframe_manifest(dataframe_manifest_name)
 pca = fit_pca(num_pcs=80)
 
 # %% Load optical flow features
-dataframe_manifest_optical_flow = load_dataframe_manifest("optical_flow")
 dataframe_manifest_optical_flow_new = load_dataframe_manifest("optical_flow_new")
 
-
-# %% Combine and calculate features
-df_proj_full_list = []
 df_proj_full_list_new = []
 
 for dataset_name in datasets:
@@ -62,24 +64,12 @@ for dataset_name in datasets:
     )
 
     # Get optical flow features
-    optical_flow_location = get_dataframe_location_for_dataset(
-        dataframe_manifest_optical_flow, dataset_name
-    )
-    df_optical_flow = load_dataframe(optical_flow_location)
-
     optical_flow_location_new = get_dataframe_location_for_dataset(
         dataframe_manifest_optical_flow_new, dataset_name
     )
     df_optical_flow_new = load_dataframe(optical_flow_location_new)
 
     # merge the two dataframes on the dataset, position, frame_number, start_x, start_y columns
-    df_proj_full_old = df_proj_full.merge(
-        df_optical_flow,
-        on=["dataset", "position", "frame_number", "start_x", "start_y"],
-        how="inner",
-        suffixes=("", "_optical_flow"),
-    )
-
     df_proj_full_new = df_proj_full.merge(
         df_optical_flow_new,
         on=["dataset", "position", "frame_number", "start_x", "start_y"],
@@ -87,11 +77,8 @@ for dataset_name in datasets:
         suffixes=("", "_optical_flow_new"),
     )
 
-    df_proj_full_list.append(df_proj_full_old)
     df_proj_full_list_new.append(df_proj_full_new)
 
-# %% concatenate
-df = pd.concat(df_proj_full_list, ignore_index=True)
 # %%
 df_new = pd.concat(df_proj_full_list_new, ignore_index=True)
 
@@ -140,6 +127,10 @@ def plot_lda_vs_optical_flow(
         for col, feature in enumerate(features):
             ax = axes[row, col]
             feat_data = df_np[feature]
+
+            # calculate correlation coefficient
+            corr_coef = np.corrcoef(feat_data, of_data)[0, 1]
+
             for i, dataset in enumerate(datasets_used):
                 mask = df["dataset"] == dataset
                 x_full = feat_data[mask]
@@ -161,11 +152,7 @@ def plot_lda_vs_optical_flow(
                 ax.set_title(feature)
             if col == 0:
                 ax.set_ylabel(of_feature)
-            if len(x_full) > 1:
-                corr_coef = np.corrcoef(x_full, y_full)[0, 1]
-                ax.annotate(
-                    f"r={corr_coef:.2f}", xy=(0.05, 0.9), xycoords="axes fraction", fontsize=10
-                )
+            ax.annotate(f"r={corr_coef:.2f}", xy=(0.05, 0.9), xycoords="axes fraction", fontsize=10)
             ax.grid(True)
 
     # Add legend to the first axis with handles
@@ -182,13 +169,13 @@ def plot_lda_vs_optical_flow(
 
 # %%
 features = ["LDA", "LDA_SP_2", "LDA_SP_3", "LDA_SP_4", "pc_1", "pc_2", "pc_3"]
-datasets_used = ["20250618_20X", "20250611_20X"]
-
-# for dt in range(1, 11):
-dt = 1
+datasets_used = ["20250611_20X", "20250618_20X", "20250813_20X", "20250319_20X"]
+# %%
+# for dt in range(2, 11):
+dt = 5
 optical_flow_features = [
     f"optical_flow_mean_speed_dt{dt}",
-    f"optical_flow_median_speed_dt{dt}",
+    # f"optical_flow_mean_unit_vector_dt{dt}",
     f"optical_flow_std_speed_dt{dt}",
     f"optical_flow_mean_angle_dt{dt}",
     f"optical_flow_angle_std_dt{dt}",
@@ -199,50 +186,129 @@ optical_flow_features = [
 ]
 
 df_sub_new = df_new[df_new["dataset"].isin(datasets_used)]
-plot_lda_vs_optical_flow(df_sub_new, features, optical_flow_features, color_by_dataset=True)
-# %%
-optical_flow_features = [
-    "optical_flow_mean_speed",
-    "optical_flow_median_speed",
-    "optical_flow_std_speed",
-    "optical_flow_mean_angle",
-    "optical_flow_angle_std",
-    "optical_flow_mean_u",
-    "optical_flow_mean_v",
-    "optical_flow_std_u",
-    "optical_flow_std_v",
-]
-df_sub = df[df["dataset"].isin(datasets_used)]
-plot_lda_vs_optical_flow(df_sub, features, optical_flow_features, color_by_dataset=True)
 
+print("Plotting for dt =", dt)
+plot_lda_vs_optical_flow(df_sub_new, features, optical_flow_features, color_by_dataset=False)
 
 # %% Load classic features
+dataframe_manifest_classic = load_dataframe_manifest("test_live_merged_seg_features")
+dataframe_cell_centric_diffae = load_dataframe_manifest("pc_diffae_tracked_seg_features")
 
-# dataframe_manifest_classic = load_dataframe_manifest("test_live_merged_seg_features")
-# dataframe_cell_centric_diffae = load_dataframe_manifest("pc_diffae_tracked_seg_features")
+# %%
+df_list = []
+for dataset_name in datasets:
+    dataframe_location_classic = get_dataframe_location_for_dataset(
+        dataframe_manifest_classic, dataset_name
+    )
+    df_classic_delayed = load_dataframe(dataframe_location_classic, delay=True)
 
-# # for dataset_name in datasets:
-# dataset_name = datasets[0]
-# dataframe_location_classic = get_dataframe_location_for_dataset(dataframe_manifest_classic, dataset_name)
-# df_classic_delayed = load_dataframe(dataframe_location_classic, delay=True)
+    dataframe_location_cell_centered = get_dataframe_location_for_dataset(
+        dataframe_cell_centric_diffae, dataset_name
+    )
+    df_cell_centered = load_dataframe(dataframe_location_cell_centered)
 
-# dataframe_location_cell_centered = get_dataframe_location_for_dataset(dataframe_cell_centric_diffae, dataset_name)
-# df_cell_centered = load_dataframe(dataframe_location_cell_centered)
+    cols_to_compute = list(
+        set(
+            SEGMENTATION_FEATURE_COLUMNS["dynamics_calculation_prereq"]
+            + SEGMENTATION_FEATURE_COLUMNS["filters"]
+        )
+    )
+    df_classic = df_classic_delayed[cols_to_compute].compute()
+    df_classic = df_classic[df_classic.is_included]
+    df_classic = calculate_derived_data_dynamics_dependent(
+        df_classic, compute_per_crop_metrics=True
+    )
 
-# cols_to_compute = list(
-#         set(
-#             SEGMENTATION_FEATURE_COLUMNS["dynamics_calculation_prereq"]
-#             + SEGMENTATION_FEATURE_COLUMNS["filters"]
-#         )
-#     )
-# df_classic = df_classic_delayed[cols_to_compute].compute()
-# df_classic = df_classic[df_classic.is_included]
-# df_classic = calculate_derived_data_dynamics_dependent(df_classic, compute_per_crop_metrics=True)
+    df_merged = pd.merge(
+        df_classic,
+        df_cell_centered,
+        left_on=["dataset_name", "position", "T", "label", "track_id"],
+        right_on=["dataset_name", "position", "frame_number", "label", "track_id"],
+        how="inner",
+    )
 
-# df_merged = pd.merge(
-#     df_classic,
-#     df_cell_centered,
-#     left_on=["dataset_name", "position", "frame_number", "label", "track_id"],
-#     right_on=["dataset_name", "position", "image_index", "label", "track_id"],
-#     how="inner"
+    timepoint_annotations = get_subset_of_timepoint_annotations(annotations_to_ignore=[])
+    df_filtered = filter_dataframe_by_annotations(
+        df_merged,
+        load_dataset_config(dataset_name),
+        timepoint_annotations=timepoint_annotations,
+    )
+
+    df_proj_full = apply_lda_projection(
+        df_filtered,
+        features_in_lda_rank=df_lda["features"],
+        lda_weights=df_lda["weights"],
+        lda_intercept=df_lda["intercept"][0],
+        sparse_axes=[2.0, 3.0, 4.0],
+    )
+
+    df_list.append(df_proj_full)
+
+df_all_classic = pd.concat(df_list, ignore_index=True)
+# %%
+# print shape of df_merge and df_filtered
+print("Shape of merged dataframe:", df_merged.shape)
+print("Shape of filtered dataframe:", df_filtered.shape)
+
+# %%
+features = ["LDA", "LDA_SP_2", "LDA_SP_3", "LDA_SP_4", "pc_1", "pc_2", "pc_3"]
+classic_features = ["vec_mean_angle_in_crop", "vec_mean_mag_in_crop"]
+
+datasets_used = ["20250611_20X", "20250618_20X"]
+
+plot_lda_vs_optical_flow(
+    df_all_classic[df_all_classic["dataset_name"].isin(datasets_used)],
+    features,
+    classic_features,
+    color_by_dataset=False,
+    figsize=(24, 2.5 * 2),
+)
+
+# %%
+plot_lda_vs_optical_flow(
+    df_all_classic,
+    features,
+    classic_features,
+    color_by_dataset=False,
+    figsize=(24, 2.5 * 2),
+)
+# %%
+# df_mig_of = pd.merge(
+#     df_mig,
+#     df_new,
+#     left_on=["dataset", "position", "frame_number", "start_x", "start_y"],
+#     right_on=["dataset", "position", "frame_number", "start_x", "start_y"],
+#     how="inner",
 # )
+
+# # %%
+
+# for dt in range(1, 11):
+#     optical_flow_features = [
+#         f"optical_flow_mean_speed_dt{dt}",
+#         f"optical_flow_mean_unit_vector_dt{dt}",
+#         f"optical_flow_std_speed_dt{dt}",
+#         f"optical_flow_mean_angle_dt{dt}",
+#         f"optical_flow_angle_std_dt{dt}",
+#         f"optical_flow_mean_u_dt{dt}",
+#         f"optical_flow_mean_v_dt{dt}",
+#         f"optical_flow_std_u_dt{dt}",
+#         f"optical_flow_std_v_dt{dt}",
+#     ]
+
+#     for feat in optical_flow_features:
+#         true_vals = df_mig_of[df_mig_of["coherent_migration"] == True][feat]
+#         false_vals = df_mig_of[df_mig_of["coherent_migration"] == False][feat]
+
+#         plt.figure(figsize=(8, 5))
+#         plt.hist(true_vals, bins=30, alpha=0.6, label="True", color="blue", density=True)
+#         plt.hist(false_vals, bins=30, alpha=0.6, label="False", color="orange", density=True)
+
+#         plt.xlabel(feat)
+#         plt.ylabel("Density")
+#         plt.title("Distribution by Coherent Migration")
+#         plt.legend()
+#         plt.show()
+#     break
+
+# %%
