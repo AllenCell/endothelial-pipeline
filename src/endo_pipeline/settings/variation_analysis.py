2e2bfc5907bfa61fe19ebec364ea464785f13482
Erin <erin.angelini@alleninstitute.org>
2026-03-03 09:40:59 -0800

    settings module for cov bin limits

---
+++ src/endo_pipeline/settings/variation_analysis.py
@@ -0,0 +1,10 @@
+"""Settings for coefficient of variation analysis."""
+
+from endo_pipeline.settings.diffae_feature_dataframes import ColumnName
+
+BIN_LIMITS_COV_VS_TIME: dict[str, tuple[float, float]] = {
+    ColumnName.POLAR_ANGLE.value: (0.0, 1.5),
+    ColumnName.POLAR_RADIUS.value: (0.0, 1.5),
+    ColumnName.PC3_FLIPPED.value: (0.0, 1.5),
+}
+"""Bin limits for coefficient of variation vs time plots in visualization."""
