1f114bdb5e55cbc7e330beb800b66bed12502124
Serge Parent <serge.edouard.parent@gmail.com>
2026-03-14 20:27:21 -0700

    - missed a couple of columns to be added to the live_seg_features tables ("shear_stress" and "time_hours_since_flow_start")

--- src/endo_pipeline/library/analyze/live_data_manifest/lib_make_seg_feats_manifest.py
+++ src/endo_pipeline/library/analyze/live_data_manifest/lib_make_seg_feats_manifest.py
@@ -428,15 +428,24 @@ def calculate_derived_data_dynamics_independent(big_table: pd.DataFrame) -> pd.D
     shear_stress_regime = "_to_".join([shear.value for shear in data_config.shear_stress_regime])
     big_table[ColNmSeg.SHEAR_STRESS_REGIME] = shear_stress_regime
 
+    shear_stresses = [condition.shear_stress for condition in data_config.flow_conditions]
+    big_table[ColNmSeg.SHEAR_STRESS] = shear_stresses
+
     # dimensionalize the time column
     logger.info("Adding time intervals per timepoint...")
-    big_table[ColNmSeg.TIME_RESOLUTION_MINUTES] = data_config.time_interval_in_minutes
+    if data_config.time_interval_in_minutes is not None:
+        dt_in_mins = data_config.time_interval_in_minutes
+    else:
+        dt_in_mins = np.nan
+    big_table[ColNmSeg.TIME_RESOLUTION_MINUTES] = dt_in_mins
 
     logger.info("Calculating time in minutes and hours...")
-    big_table[ColNmSeg.TIME_MINS] = (
-        big_table[ColNmSeg.TIMEPOINT] * big_table[ColNmSeg.TIME_RESOLUTION_MINUTES]
-    )
+    big_table[ColNmSeg.TIME_MINS] = big_table[ColNmSeg.TIMEPOINT] * dt_in_mins
     big_table[ColNmSeg.TIME_HRS] = big_table[ColNmSeg.TIME_MINS] / 60
+
+    # add time elapsed since flow onset (in hours)
+    flow_start_time_hrs = data_config.flow_conditions[0].shear_stress * dt_in_mins / 60.0
+    big_table[ColNmSeg.TIME_HRS_SINCE_FLOW] = big_table[ColNmSeg.TIME_HRS] - flow_start_time_hrs
 
     # add a column for the number of unique tracks
     # per dataset per position per timepoint
