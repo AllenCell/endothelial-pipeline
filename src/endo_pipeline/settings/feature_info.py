b276c4c2e4a85b823d398d9bf6320f1860338b1c
Serge Parent <serge.edouard.parent@gmail.com>
2026-03-15 18:12:16 -0700

    - updated feature_info with StrEnum columns

--- src/endo_pipeline/settings/feature_info.py
+++ src/endo_pipeline/settings/feature_info.py
@@ -1,31 +1,32 @@
 from endo_pipeline.settings import DIFFAE_PC_COLUMN_NAMES, NUM_PCS_TO_ANALYZE
 from endo_pipeline.settings.diffae_feature_dataframes import MAX_PCS_TO_COMPUTE, ColumnName
+from endo_pipeline.settings.segmentation_feature_dataframes import ColumnNameSeg as ColNmSeg
 
 LABEL_MAP = {
-    "alignment_deg_rel_to_flow": "Alignment Relative to Flow (degrees)",
-    "alignment_rel_to_flow": "Alignment Relative to Flow (rad)",
-    "cell_orientation": "Cell Orientation (rad)",
-    "area (um**2)": "Cell Area (µm²)",
-    "aspect_ratio": "Aspect Ratio",
-    "perimeter (um)": "Cell Perimeter (µm)",
-    "cell_eccentricity": "Cell Eccentricity",
-    "cell_fluorescence_max (a.u.)": "Cell Fluorescence Max (a.u.)",
-    "cell_fluorescence_mean (a.u.)": "Cell Fluorescence Mean (a.u.)",
-    "cell_fluorescence_median (a.u.)": "Cell Fluorescence Median (a.u.)",
-    "cell_solidity": "Cell Solidity",
-    "time_hours": "Time (hours)",
-    "time_minutes": "Time (minutes)",
-    ColumnName.TRACK_ID: "Track ID",
-    "smoothed_area_normd_diff": "Smoothed Area Difference (Normalized)",
-    "track_duration": "Track Duration",
-    "num_valid_tp_per_track": "Number of Valid Timepoints",
-    "number_of_neighbors": "Number of Neighbors",
+    ColNmSeg.ALIGNMENT_DEG: "Alignment Relative to Flow (degrees)",
+    ColNmSeg.ALIGNMENT: "Alignment Relative to Flow (rad)",
+    ColNmSeg.ORIENTATION: "Cell Orientation (rad)",
+    ColNmSeg.AREA_UM_SQ: "Cell Area (µm²)",
+    ColNmSeg.ASPECT_RATIO: "Aspect Ratio",
+    ColNmSeg.PERIMETER_UM: "Cell Perimeter (µm)",
+    ColNmSeg.ECCENTRICITY: "Cell Eccentricity",
+    ColNmSeg.CELL_FLUOR_MAX: "Cell Fluorescence Max (a.u.)",
+    ColNmSeg.CELL_FLUOR_MEAN: "Cell Fluorescence Mean (a.u.)",
+    ColNmSeg.CELL_FLUOR_MEDIAN: "Cell Fluorescence Median (a.u.)",
+    ColNmSeg.SOLIDITY: "Cell Solidity",
+    ColNmSeg.TIME_HRS: "Time (hours)",
+    ColNmSeg.TIME_MINS: "Time (minutes)",
+    ColNmSeg.TRACK_ID: "Track ID",
+    ColNmSeg.SMOOTHED_AREA_NORMD_DIFF: "Smoothed Area Difference (Normalized)",
+    ColNmSeg.TRACK_LENGTH: "Track Duration",
+    ColNmSeg.NUM_VALID_TIMEPOINTS_IN_TRACK: "Number of Valid Timepoints",
+    ColNmSeg.NUM_NEIGHBORS: "Number of Neighbors",
     # filters
-    "is_edge_segmentation": "Filter: Touches Edge of Field of View",
-    "is_less_than_max_smoothed_area_normd_change": "Filter: Smoothed Area Change Below Threshold",
-    "is_greater_than_min_track_duration": "Filter: Exceeds Min Track Duration",
-    "has_more_than_min_num_valid_points_per_track": "Filter: Num Valid Points Exceeds Threshold",
-    "is_included": "Filter: Passed All Filters",
+    ColNmSeg.IS_EDGE_SEGMENTATION: "Filter: Touches Edge of Field of View",
+    ColNmSeg.IS_LESS_THAN_MAX_SMOOTHED_AREA_NORMD_CHANGE: "Filter: Smoothed Area Change Below Threshold",
+    ColNmSeg.IS_GREATER_THAN_MIN_TRACK_DURATION: "Filter: Exceeds Min Track Duration",
+    ColNmSeg.HAS_MORE_THAN_MIN_NUM_VALID_POINTS_PER_TRACK: "Filter: Num Valid Points Exceeds Threshold",
+    ColNmSeg.IS_INCLUDED: "Filter: Passed All Filters",
     # Cell-centric DiffAE features and PCs
     **{
         f"{pc_col}": f"{pc_col.replace('pc_', 'PC ')}"
@@ -33,11 +34,12 @@ LABEL_MAP = {
     },
     ColumnName.POLAR_ANGLE.value: "PC Polar Angle",
     ColumnName.POLAR_RADIUS.value: "PC Polar Radius",
+    ColumnName.PC3_FLIPPED.value: "PC Rho",
 }
 
 LABEL_MAP_GRID = {
-    "time_hours": "Time (hours)",
-    "time_minutes": "Time (minutes)",
+    ColNmSeg.TIME_HRS: "Time (hours)",
+    ColNmSeg.TIME_MINS: "Time (minutes)",
     # various PC values
     **{
         f"{pc_col}": f"{pc_col.replace('pc_', 'PC ')}"
@@ -47,15 +49,15 @@ LABEL_MAP_GRID = {
     ColumnName.POLAR_RADIUS.value: "PC Polar Radius",
     ColumnName.PC3_FLIPPED.value: "PC Rho",
     # filters
-    "auto_bf_scope_error": "Filter: Auto-detected Brightfield Microscope Error",
-    "auto_bf_temp_artifact": "Filter: Auto-detected Temporary Artifact",
-    "auto_gfp_scope_error": "Filter: Auto-detected GFP Channel Microscope Error",
-    "bf_scope_error": "Filter: Manually Annotated Brightfield Microscope Error",
-    "bf_temp_artifact": "Filter: Manually Annotated Temporary Artifact",
-    "gfp_scope_error": "Filter: Manually Annotated GFP Channel Microscope Error",
-    "cell_piling": "Filter: Manually Annotated Significant Cell Piling",
-    "not_steady_state": "Filter: Cells Not At Steady State",
-    "unfed": "Filter: Unfed (More Than 3 Hours Since Fresh Media Introduced)",
-    "xy_shift": "Filter: Significant Change in XY position of FOV",
-    "z_shift": "Filter: Significant Change in Z position of FOV",
+    ColNmSeg.AUTO_BF_SCOPE_ERROR: "Filter: Auto-detected Brightfield Microscope Error",
+    ColNmSeg.AUTO_BF_TEMP_ARTIFACT: "Filter: Auto-detected Temporary Artifact",
+    ColNmSeg.AUTO_GFP_SCOPE_ERROR: "Filter: Auto-detected GFP Channel Microscope Error",
+    ColNmSeg.BF_SCOPE_ERROR: "Filter: Manually Annotated Brightfield Microscope Error",
+    ColNmSeg.BF_TEMP_ARTIFACT: "Filter: Manually Annotated Temporary Artifact",
+    ColNmSeg.GFP_SCOPE_ERROR: "Filter: Manually Annotated GFP Channel Microscope Error",
+    ColNmSeg.CELL_PILING: "Filter: Manually Annotated Significant Cell Piling",
+    ColNmSeg.NOT_STEADY_STATE: "Filter: Cells Not At Steady State",
+    ColNmSeg.UNFED: "Filter: Unfed (More Than 3 Hours Since Fresh Media Introduced)",
+    ColNmSeg.XY_SHIFT: "Filter: Significant Change in XY position of FOV",
+    ColNmSeg.Z_SHIFT: "Filter: Significant Change in Z position of FOV",
 }
