3b80e16981d930aee70219547648a58a1988ed0a
Serge Parent <serge.edouard.parent@gmail.com>
2026-03-19 17:59:38 -0700

    - removed filtering before model eval

--- src/endo_pipeline/library/model/eval_model.py
+++ src/endo_pipeline/library/model/eval_model.py
@@ -269,15 +269,11 @@ def preprocess_tracking_manifest_for_model_eval(
     ]
 
     columns_for_filtering = [
-        Column.SegDataFilters.IS_INCLUDED,
         Column.SegDataFilters.IS_VALID_BBOX,
     ]
 
     # compute the required and filtering columns
     df = df[columns_to_keep + columns_for_filtering].compute()
-
-    # keep only rows that were not filtered out
-    df = df[df[Column.SegDataFilters.IS_INCLUDED]]
 
     # filter the dataframe in-place to remove clipped bounding boxes
     df = df[df[Column.SegDataFilters.IS_VALID_BBOX]]
