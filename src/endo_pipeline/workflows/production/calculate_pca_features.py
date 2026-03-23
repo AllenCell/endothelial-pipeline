565763e1da8a4c9fc2ed229a261f5cde87bbcd62
Erin Angelini <erin.angelini@alleninstitute.org>
2026-03-23 21:31:50 +0000

    calculate-pca-features

--- src/endo_pipeline/workflows/production/calculate_pca_features.py
+++ src/endo_pipeline/workflows/production/calculate_pca_features.py
@@ -86,7 +86,7 @@ def main(
     run_name = DEFAULT_MODEL_RUN_NAME
     model_manifest = load_model_manifest(model_manifest_name)
     feature_dataframe_manifest_name = get_feature_dataframe_manifest_name(
-        model_manifest, run_name, crop_pattern
+        model_manifest, run_name, crop_pattern, feature_type="latent", is_filtered=False
     )
     feature_manifest = load_dataframe_manifest(feature_dataframe_manifest_name)
 
