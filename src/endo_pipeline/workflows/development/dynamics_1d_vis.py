fe0211f8bb3e718a33fc0515a9ab8831bb4c9bfc
Erin Angelini <erin.angelini@alleninstitute.org>
2026-03-24 17:39:45 +0000

    add command line inputs

--- src/endo_pipeline/workflows/development/dynamics_1d_vis.py
+++ src/endo_pipeline/workflows/development/dynamics_1d_vis.py
@@ -1,4 +1,9 @@
-def main() -> None:
+from endo_pipeline.cli import CropPattern, Datasets
+
+
+def main(
+    crop_pattern: CropPattern = "grid", datasets: Datasets | None = None, column: str | None = None
+) -> None:
     import logging
 
     import matplotlib.pyplot as plt
@@ -27,6 +32,7 @@ def main() -> None:
         BIN_LIMITS_DYNAMICS,
         BIN_LIMITS_THETA_RESCALED,
         BIN_WIDTHS_DYNAMICS,
+        DEFAULT_DATASET_DYNAMICS_VIS,
         KERNEL_BANDWIDTHS_DYNAMICS,
         KERNEL_NAMES_DYNAMICS,
         NUM_PCS_TO_FIT_FOR_DYNAMICS,
@@ -40,17 +46,26 @@ def main() -> None:
 
     logger = logging.getLogger(__name__)
 
+    # always use defaults for model manifest and run name
     model_manifest_name = DEFAULT_MODEL_MANIFEST_NAME
     run_name = DEFAULT_MODEL_RUN_NAME
-    crop_pattern = "grid"
 
-    dataset_name = "20250409_20X"
+    # unpack command line inputs, using defaults if not provided
+    dataset_names = datasets or [DEFAULT_DATASET_DYNAMICS_VIS]
+    column_name = column or ColumnName.DiffAEData.POLAR_ANGLE
 
-    # get labels for provided set of feature columns
-    column_name = ColumnName.DiffAEData.POLAR_ANGLE
+    # get dataframe manifest for features for given crop pattern
+    model_manifest = load_model_manifest(model_manifest_name)
+    dataframe_manifest_name = get_feature_dataframe_manifest_name(
+        model_manifest, run_name, crop_pattern=crop_pattern
+    )
+    dataframe_manifest = load_dataframe_manifest(dataframe_manifest_name)
+
+    # get plot labels for provided feature column name
     variable_label = get_label_for_column(column_name).replace("polar ", "")
 
-    # unpack default bin widths and limits for each column, adjusting limits if rescaling theta
+    # unpack default bin widths and limits for each column, adjusting limits if
+    # rescaling theta
     global_bin_limits_dict = BIN_LIMITS_DYNAMICS.copy()
     if RESCALE_THETA:
         global_bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE] = BIN_LIMITS_THETA_RESCALED
@@ -58,13 +73,6 @@ def main() -> None:
         global_bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][1]
         - global_bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][0]
     )
-
-    # get dataframe manifest for grid-based crop features
-    model_manifest = load_model_manifest(model_manifest_name)
-    dataframe_manifest_name = get_feature_dataframe_manifest_name(
-        model_manifest, run_name, crop_pattern=crop_pattern
-    )
-    dataframe_manifest = load_dataframe_manifest(dataframe_manifest_name)
 
     # fit PCA - ALWAYS on grid-based crop features
     dataframe_manifest_name_for_pca = get_feature_dataframe_manifest_name(
@@ -74,74 +82,81 @@ def main() -> None:
         dataframe_manifest_name=dataframe_manifest_name_for_pca, num_pcs=NUM_PCS_TO_FIT_FOR_DYNAMICS
     )
 
-    # loop over datasets in collection, compute 2D drift coefficients for each
-    # pairwise combination of polar coordinates, and plot contours of drift coefficients
-    fig_savedir = get_output_path(__file__, crop_pattern, dataset_name)
-    logger.debug("Saving summary plots to [ %s ]", fig_savedir)
-    dataset_config = load_dataset_config(dataset_name)
-
-    df = get_dataframe_for_dynamics_workflows(
-        dataset_name,
-        dataframe_manifest,
-        pca=pca,
-        include_cell_piling=False,
-        include_not_steady_state=False,
-        crop_pattern=crop_pattern,
-        compute_polar=True,
-        rescale_theta=RESCALE_THETA,
-    )
-
-    df_by_flow, shear_stress_list = split_dataset_by_flow(
-        df,
-        dataset_config,
-    )
-
-    # compute on a per-shear stress condition basis
-    for df_, shear_stress in zip(df_by_flow, shear_stress_list, strict=True):
-        dataset_name_flow = f"{dataset_name}_shear_{int(shear_stress)}"
-        fig_title = f"{dataset_name} ({shear_stress} dym/cm$^2$)"
-
-        # get bins and centers for each variable based on bin widths and limits
-        if column_name == ColumnName.DiffAEData.POLAR_ANGLE:
-            bins, centers = get_bins(
-                bin_widths=(BIN_WIDTHS_DYNAMICS[column_name],),
-                bin_limits=[global_bin_limits_dict[column_name]],
+    # loop over datasets in collection, compute 1D drift for given variable, and
+    # plot results, skipping datasets not found in manifest
+    for dataset_name in dataset_names:
+        if dataset_name not in dataframe_manifest.locations:
+            logger.warning(
+                f"Dataset {dataset_name} not found in manifest {dataframe_manifest_name}. Skipping."
             )
-        else:
-            bins, centers = get_bins(
-                bin_widths=(BIN_WIDTHS_DYNAMICS[column_name],),
-                data=df_[column_name].to_numpy(),
-                lower_percentile=BIN_LIMIT_PERCENTILE_CUTOFF,
-                upper_percentile=100 - BIN_LIMIT_PERCENTILE_CUTOFF,
+            continue
+        fig_savedir = get_output_path(__file__, crop_pattern, dataset_name)
+        dataset_config = load_dataset_config(dataset_name)
+
+        df = get_dataframe_for_dynamics_workflows(
+            dataset_name,
+            dataframe_manifest,
+            pca=pca,
+            include_cell_piling=False,
+            include_not_steady_state=False,
+            crop_pattern=crop_pattern,
+            compute_polar=True,
+            rescale_theta=RESCALE_THETA,
+        )
+
+        df_by_flow, shear_stress_list = split_dataset_by_flow(
+            df,
+            dataset_config,
+        )
+
+        # compute on a per-shear stress condition basis
+        for df_, shear_stress in zip(df_by_flow, shear_stress_list, strict=True):
+            dataset_name_flow = f"{dataset_name}_shear_{int(shear_stress)}"
+            fig_title = f"{dataset_name} ({shear_stress} dym/cm$^2$)"
+
+            # get bins and centers for each variable based on bin widths and limits
+            if column_name == ColumnName.DiffAEData.POLAR_ANGLE:
+                bins, centers = get_bins(
+                    bin_widths=(BIN_WIDTHS_DYNAMICS[column_name],),
+                    bin_limits=[global_bin_limits_dict[column_name]],
+                )
+            else:
+                bins, centers = get_bins(
+                    bin_widths=(BIN_WIDTHS_DYNAMICS[column_name],),
+                    data=df_[column_name].to_numpy(),
+                    lower_percentile=BIN_LIMIT_PERCENTILE_CUTOFF,
+                    upper_percentile=100 - BIN_LIMIT_PERCENTILE_CUTOFF,
+                )
+
+            # get trajectories and differences for the given variable, adjusting
+            # polar angle differences for periodicity if needed
+            trajectories, differences = get_traj_and_diff(
+                df_, column_names=[column_name], polar_angle_period=polar_angle_period
             )
 
-        # get trajectories and differences for each variable, adjusting
-        # polar angle differences for periodicity if needed
-        trajectories, differences = get_traj_and_diff(
-            df_, column_names=[column_name], polar_angle_period=polar_angle_period
-        )
+            kernel = KramersMoyalKernel(
+                name=KERNEL_NAMES_DYNAMICS[column_name],
+                bandwidth=KERNEL_BANDWIDTHS_DYNAMICS[column_name],
+                period=(
+                    polar_angle_period if column_name == ColumnName.DiffAEData.POLAR_ANGLE else None
+                ),
+            )
 
-        kernel = KramersMoyalKernel(
-            name=KERNEL_NAMES_DYNAMICS[column_name],
-            bandwidth=KERNEL_BANDWIDTHS_DYNAMICS[column_name],
-            period=polar_angle_period if column_name == ColumnName.DiffAEData.POLAR_ANGLE else None,
-        )
+            drift, _ = get_kramers_moyal_coeffs(
+                trajectories=trajectories,
+                displacements=differences,
+                bins=bins,
+                dt=TIME_STEP_IN_MINUTES / 60,  # convert to unit hours
+                kernel=kernel,
+            )
 
-        drift, _ = get_kramers_moyal_coeffs(
-            trajectories=trajectories,
-            displacements=differences,
-            bins=bins,
-            dt=TIME_STEP_IN_MINUTES / 60,  # convert to unit hours
-            kernel=kernel,
-        )
-
-        fig, ax = plt.subplots()
-        ax.plot(centers[-1], drift, "k-", linewidth=2)
-        ax.plot(centers[-1], np.zeros_like(centers[-1]), "b--", linewidth=1, alpha=0.7)
-        ax.set_xlabel(variable_label)
-        ax.set_ylabel(f"Drift in {variable_label}")
-        ax.set_title(fig_title)
-        save_plot_to_path(fig, fig_savedir, f"{dataset_name_flow}_drift_{column_name}.png")
+            fig, ax = plt.subplots()
+            ax.plot(centers[-1], drift, "k-", linewidth=2)
+            ax.plot(centers[-1], np.zeros_like(centers[-1]), "b--", linewidth=1, alpha=0.7)
+            ax.set_xlabel(variable_label)
+            ax.set_ylabel(f"Drift in {variable_label}")
+            ax.set_title(fig_title)
+            save_plot_to_path(fig, fig_savedir, f"{dataset_name_flow}_drift_{column_name}.png")
 
 
 if __name__ == "__main__":
