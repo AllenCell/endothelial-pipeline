c8955fd5edd7e65f31aa06e74d5a6d7ddbeac9fd
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-03-24 21:02:16 -0400

    Remove finetune model methods, configs, and workflow

--- src/endo_pipeline/library/model/diffae/__init__.py
+++ src/endo_pipeline/library/model/diffae/__init__.py
@@ -1,4 +1,3 @@
-from .diffae_finetune import DiffAEFinetune
 from .diffusion_autoencoder import DiffusionAutoEncoder, detach
 from .generate_image import (
     generate_from_coords,
@@ -11,7 +10,6 @@ from .mlflow import MLFlowLogger
 from .transforms import MinStdCropd, RotateRanged
 
 __all__ = [
-    "DiffAEFinetune",
     "DiffAELatentWalkRank0",
     "DiffusionAutoEncoder",
     "GradientLoggingCallback",
