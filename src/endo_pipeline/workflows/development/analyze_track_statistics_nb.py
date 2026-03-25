6cb0ff185d9b2dd4b0b59bed8e5490ae5d519135
Erin Angelini <erin.angelini@alleninstitute.org>
2026-03-25 20:16:05 +0000

    turn into main() style workflow

--- src/endo_pipeline/workflows/development/analyze_track_statistics_nb.py
+++ src/endo_pipeline/workflows/development/analyze_track_statistics_nb.py
@@ -1,183 +1,185 @@
-# %%
-import logging
+from endo_pipeline.cli import CropPattern, Datasets
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from scipy.stats import circmean, circvar
 
-from endo_pipeline.cli.apps import WorkflowOptions, apply_workflow_options
-from endo_pipeline.configs import (
-    TimepointAnnotation,
-    get_datasets_in_collection,
-    load_dataset_config,
-)
-from endo_pipeline.io import get_output_path, load_dataframe, save_plot_to_path
-from endo_pipeline.library.analyze.diffae_dataframe_utils import (
-    filter_dataframe_by_annotations,
-    filter_dataframe_by_track_length,
-)
-from endo_pipeline.library.visualize.diffae_features.feature_viz import get_dataset_color
-from endo_pipeline.manifests import load_dataframe_manifest
-from endo_pipeline.settings.column_names import ColumnName
-from endo_pipeline.settings.dynamics_workflows import (
-    BIN_LIMITS_DYNAMICS,
-    BIN_LIMITS_THETA_RESCALED,
-    DYNAMICS_COLUMN_NAMES,
-    METADATA_COLUMNS_TO_KEEP,
-    RESCALE_THETA,
-    TRACK_METADATA_COLUMNS_TO_KEEP,
-)
-from endo_pipeline.settings.workflow_defaults import (
-    DEFAULT_MODEL_MANIFEST_NAME,
-    DEFAULT_MODEL_RUN_NAME,
-)
+def main(crop_pattern: CropPattern, datasets: Datasets) -> None:
+    import logging
 
-apply_workflow_options(WorkflowOptions(verbose=True))
-logger = logging.getLogger(__name__)
-# %%
-# set workflow defaults
-model_manifest_name = DEFAULT_MODEL_MANIFEST_NAME
-run_name = DEFAULT_MODEL_RUN_NAME
-column_names: list[ColumnName.DiffAEData] = list(DYNAMICS_COLUMN_NAMES)
-drift_column_names: list[str] = [f"{name}_drift" for name in column_names]
-crop_pattern = "grid"  # default to grid-based crops for flow field estimation and analysis
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
+    import seaborn as sns
+    from scipy.stats import circmean, circvar
 
-# Load dataframe manifest for the features to be used in flow field
-# estimation and analysis.
-if crop_pattern == "grid":
-    base_name = f"{model_manifest_name}_{run_name}_{crop_pattern}"
-    feature_dataframe_manifest_name = f"{base_name}_pca_filtered"
-else:
-    feature_dataframe_manifest_name = "pc_diffae_tracked_seg_features"
-feature_dataframe_manifest = load_dataframe_manifest(feature_dataframe_manifest_name)
-
-# Default list of datasets if not provided. Filter by datasets available in
-# the manifest.
-dataset_names = get_datasets_in_collection("timelapse")
-
-# unpack default bin widths and limits for each column, adjusting limits if rescaling theta
-bin_limits_dict = BIN_LIMITS_DYNAMICS.copy()
-if RESCALE_THETA:
-    bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE] = BIN_LIMITS_THETA_RESCALED
-polar_angle_period = (
-    bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][1]
-    - bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][0]
-)
-
-# %%
-for dataset_name in dataset_names:
-    if dataset_name not in feature_dataframe_manifest.locations:
-        logger.warning(
-            "No feature dataframe found in manifest [ %s ] for dataset [ %s ]. Skipping this dataset.",
-            feature_dataframe_manifest_name,
-            dataset_name,
-        )
-        continue
-
-    dataset_config = load_dataset_config(dataset_name)
-    if len(dataset_config.shear_stress_regime) > 1:
-        logger.warning(
-            "Dataset [ %s ] has more than one shear stress condition: [ %s ]. "
-            "Skipping for 3D flow field analysis.",
-            dataset_name,
-            dataset_config.shear_stress_regime,
-        )
-        continue
-
-    hist_color = get_dataset_color(dataset_name)
-    shear_stress = dataset_config.flow_conditions[0].shear_stress
-    dataset_name_flow = f"{dataset_name}_shear_{int(shear_stress)}"
-    plot_label = f"{dataset_name}, ({shear_stress} dyn/cm$^2$)"
-    fig_savedir = get_output_path(__file__, crop_pattern, dataset_name)
-
-    # load dataframe and perform additional filtering (remove
-    # non-steady-state timepoints based on annotations), computing
-    # only the columns needed for flow field estimation and analysis to save memory.
-    df = load_dataframe(feature_dataframe_manifest.locations[dataset_name], delay=True)
-    # start with default metadata columns to keep
-    columns_to_compute = [*METADATA_COLUMNS_TO_KEEP, *column_names]
-    if crop_pattern == "tracked":
-        # also keep track ID and track length columns for tracked crops
-        columns_to_compute = [*columns_to_compute, *TRACK_METADATA_COLUMNS_TO_KEEP]
-    df_: pd.DataFrame = df[columns_to_compute].compute()
-    df_steady_state = filter_dataframe_by_annotations(
-        df_,
-        load_dataset_config(dataset_name),
-        timepoint_annotations=[TimepointAnnotation.NOT_STEADY_STATE],
+    from endo_pipeline.configs import (
+        TimepointAnnotation,
+        get_datasets_in_collection,
+        load_dataset_config,
+    )
+    from endo_pipeline.io import get_output_path, load_dataframe, save_plot_to_path
+    from endo_pipeline.library.analyze.diffae_dataframe_utils import (
+        filter_dataframe_by_annotations,
+        filter_dataframe_by_track_length,
+    )
+    from endo_pipeline.library.visualize.diffae_features.feature_viz import get_dataset_color
+    from endo_pipeline.manifests import load_dataframe_manifest
+    from endo_pipeline.settings.column_names import ColumnName
+    from endo_pipeline.settings.dynamics_workflows import (
+        BIN_LIMITS_DYNAMICS,
+        BIN_LIMITS_THETA_RESCALED,
+        DEFAULT_DATASETS_DYNAMICS_VIS,
+        DYNAMICS_COLUMN_NAMES,
+        METADATA_COLUMNS_TO_KEEP,
+        RESCALE_THETA,
+        TRACK_METADATA_COLUMNS_TO_KEEP,
+    )
+    from endo_pipeline.settings.workflow_defaults import (
+        DEFAULT_MODEL_MANIFEST_NAME,
+        DEFAULT_MODEL_RUN_NAME,
     )
 
-    if crop_pattern == "tracked":
-        # also filter out tracks that are too short for reliable flow field estimation and analysis
-        df_steady_state = filter_dataframe_by_track_length(
-            df_steady_state, ColumnName.TRACK_LENGTH, min_length=100
+    logger = logging.getLogger(__name__)
+
+    # set workflow defaults
+    model_manifest_name = DEFAULT_MODEL_MANIFEST_NAME
+    run_name = DEFAULT_MODEL_RUN_NAME
+    column_names: list[ColumnName.DiffAEData] = list(DYNAMICS_COLUMN_NAMES)
+
+    # Load dataframe manifest for the features to be used in flow field
+    # estimation and analysis.
+    if crop_pattern == "grid":
+        base_name = f"{model_manifest_name}_{run_name}_{crop_pattern}"
+        feature_dataframe_manifest_name = f"{base_name}_pca_filtered"
+    else:
+        feature_dataframe_manifest_name = "pc_diffae_tracked_seg_features"
+    feature_dataframe_manifest = load_dataframe_manifest(feature_dataframe_manifest_name)
+
+    # Default list of datasets if not provided. Filter by datasets available in
+    # the manifest.
+    dataset_names = datasets or get_datasets_in_collection(DEFAULT_DATASETS_DYNAMICS_VIS)
+
+    # unpack default bin widths and limits for each column, adjusting limits if rescaling theta
+    bin_limits_dict = BIN_LIMITS_DYNAMICS.copy()
+    if RESCALE_THETA:
+        bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE] = BIN_LIMITS_THETA_RESCALED
+
+    for dataset_name in dataset_names:
+        if dataset_name not in feature_dataframe_manifest.locations:
+            logger.warning(
+                "No feature dataframe found in manifest [ %s ] for dataset [ %s ]. Skipping this dataset.",
+                feature_dataframe_manifest_name,
+                dataset_name,
+            )
+            continue
+
+        dataset_config = load_dataset_config(dataset_name)
+        if len(dataset_config.shear_stress_regime) > 1:
+            logger.warning(
+                "Dataset [ %s ] has more than one shear stress condition: [ %s ]. "
+                "Skipping for 3D flow field analysis.",
+                dataset_name,
+                dataset_config.shear_stress_regime,
+            )
+            continue
+
+        hist_color = get_dataset_color(dataset_name)
+        shear_stress = dataset_config.flow_conditions[0].shear_stress
+        dataset_name_flow = f"{dataset_name}_shear_{int(shear_stress)}"
+        plot_label = f"{dataset_name}, ({shear_stress} dyn/cm$^2$)"
+        fig_savedir = get_output_path(__file__, crop_pattern, dataset_name)
+
+        # load dataframe and perform additional filtering (remove
+        # non-steady-state timepoints based on annotations), computing
+        # only the columns needed for flow field estimation and analysis to save memory.
+        df = load_dataframe(feature_dataframe_manifest.locations[dataset_name], delay=True)
+        # start with default metadata columns to keep
+        columns_to_compute = [*METADATA_COLUMNS_TO_KEEP, *column_names]
+        if crop_pattern == "tracked":
+            # also keep track ID and track length columns for tracked crops
+            columns_to_compute = [*columns_to_compute, *TRACK_METADATA_COLUMNS_TO_KEEP]
+        df_: pd.DataFrame = df[columns_to_compute].compute()
+        df_steady_state = filter_dataframe_by_annotations(
+            df_,
+            load_dataset_config(dataset_name),
+            timepoint_annotations=[TimepointAnnotation.NOT_STEADY_STATE],
         )
 
-    num_traj = df_steady_state[ColumnName.CROP_INDEX].nunique()
-    print(f"Dataset [ {dataset_name} ] has [ {num_traj} ] trajectories after filtering.")
+        if crop_pattern == "tracked":
+            # also filter out tracks that are too short for reliable flow field estimation and analysis
+            df_steady_state = filter_dataframe_by_track_length(
+                df_steady_state, ColumnName.TRACK_LENGTH, min_length=100
+            )
 
-    column_avg_df = pd.DataFrame(columns=[ColumnName.CROP_INDEX, *column_names])
-    column_variance_df = pd.DataFrame(columns=[ColumnName.CROP_INDEX, *column_names])
+        num_traj = df_steady_state[ColumnName.CROP_INDEX].nunique()
+        print(f"Dataset [ {dataset_name} ] has [ {num_traj} ] trajectories after filtering.")
 
-    for traj_index, df_traj in df_steady_state.groupby(ColumnName.CROP_INDEX):
-        # sort by timepoint to ensure that trajectory is in correct order before
-        # computing differences
-        df_traj = df_traj.sort_values(by=ColumnName.TIMEPOINT)
+        column_avg_df = pd.DataFrame(columns=[ColumnName.CROP_INDEX, *column_names])
+        column_variance_df = pd.DataFrame(columns=[ColumnName.CROP_INDEX, *column_names])
+
+        for traj_index, df_traj in df_steady_state.groupby(ColumnName.CROP_INDEX):
+            # sort by timepoint to ensure that trajectory is in correct order before
+            # computing differences
+            df_traj = df_traj.sort_values(by=ColumnName.TIMEPOINT)
+            for column_name in column_names:
+                if column_name == ColumnName.DiffAEData.POLAR_ANGLE:
+                    # take circular mean for polar angle to account for periodicity
+                    column_avg_df.loc[traj_index, column_name] = circmean(
+                        df_traj[column_name],
+                        high=bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][1],
+                        low=bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][0],
+                    )
+                    column_variance_df.loc[traj_index, column_name] = circvar(
+                        df_traj[column_name],
+                        high=bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][1],
+                        low=bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][0],
+                    )
+                else:
+                    column_avg_df.loc[traj_index, column_name] = np.nanmean(df_traj[column_name])
+                    column_variance_df.loc[traj_index, column_name] = np.nanvar(
+                        df_traj[column_name]
+                    )
+
+        # plot histograms of the column averages and variances across trajectories
+        # for each column
         for column_name in column_names:
-            if column_name == ColumnName.DiffAEData.POLAR_ANGLE:
-                # take circular mean for polar angle to account for periodicity
-                column_avg_df.loc[traj_index, column_name] = circmean(
-                    df_traj[column_name],
-                    high=bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][1],
-                    low=bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][0],
-                )
-                column_variance_df.loc[traj_index, column_name] = circvar(
-                    df_traj[column_name],
-                    high=bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][1],
-                    low=bin_limits_dict[ColumnName.DiffAEData.POLAR_ANGLE][0],
-                )
-            else:
-                column_avg_df.loc[traj_index, column_name] = np.nanmean(df_traj[column_name])
-                column_variance_df.loc[traj_index, column_name] = np.nanvar(df_traj[column_name])
+            fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+            sns.histplot(
+                column_avg_df[column_name],
+                kde=True,
+                stat="density",
+                color=hist_color,
+                binwidth=0.05,
+                ax=ax[0],
+            )
+            ax[0].set_title(f"Histogram of average {column_name} across trajectories")
+            ax[0].set_xlabel(f"$\\langle${column_name}$\\rangle$")
+            ax[0].set_xlim(bin_limits_dict[column_name])
+            ax[0].set_ylabel(f"P($\\langle${column_name}$\\rangle$)")
 
-    # plot histograms of the column averages and variances across trajectories
-    # for each column
-    for column_name in column_names:
-        fig, ax = plt.subplots(1, 2, figsize=(12, 5))
-        sns.histplot(
-            column_avg_df[column_name],
-            kde=True,
-            stat="density",
-            color=hist_color,
-            binwidth=0.05,
-            ax=ax[0],
-        )
-        ax[0].set_title(f"Histogram of average {column_name} across trajectories")
-        ax[0].set_xlabel(f"$\\langle${column_name}$\\rangle$")
-        ax[0].set_xlim(bin_limits_dict[column_name])
-        ax[0].set_ylabel(f"P($\\langle${column_name}$\\rangle$)")
+            sns.histplot(
+                column_variance_df[column_name],
+                kde=True,
+                stat="density",
+                color=hist_color,
+                binwidth=0.05,
+                ax=ax[1],
+            )
+            ax[1].set_title(f"Histogram of variance of {column_name} across trajectories")
+            ax[1].set_xlabel(
+                f"$\\langle$({column_name} - $\\langle${column_name}$\\rangle$)$^2$$\\rangle$"
+            )
+            ax[1].set_xlim((-0.01, 0.9))
+            ax[1].set_ylabel(
+                f"P($\\langle$({column_name} - $\\langle${column_name}$\\rangle$)$^2$$\\rangle$)"
+            )
 
-        sns.histplot(
-            column_variance_df[column_name],
-            kde=True,
-            stat="density",
-            color=hist_color,
-            binwidth=0.05,
-            ax=ax[1],
-        )
-        ax[1].set_title(f"Histogram of variance of {column_name} across trajectories")
-        ax[1].set_xlabel(
-            f"$\\langle$({column_name} - $\\langle${column_name}$\\rangle$)$^2$$\\rangle$"
-        )
-        ax[1].set_xlim((-0.01, 0.9))
-        ax[1].set_ylabel(
-            f"P($\\langle$({column_name} - $\\langle${column_name}$\\rangle$)$^2$$\\rangle$)"
-        )
+            plt.suptitle(plot_label)
+            plt.tight_layout()
+            save_plot_to_path(
+                fig, fig_savedir, f"{dataset_name_flow}_{column_name}_statistics_histograms"
+            )
 
-        plt.suptitle(plot_label)
-        plt.tight_layout()
-        save_plot_to_path(
-            fig, fig_savedir, f"{dataset_name_flow}_{column_name}_statistics_histograms"
-        )
-# %%
+
+if __name__ == "__main__":
+    from endo_pipeline.cli import workflow_cli
+
+    workflow_cli(main)
