9942d27ad52a0bb6443639e84769dda2b3562af9
Chantelle Leveille <chantelle.leveille@alleninstitute.org>
2026-03-27 02:12:38 +0000

    update bff csv

--- src/endo_pipeline/workflows/internal/create_bff_csv.py
+++ src/endo_pipeline/workflows/internal/create_bff_csv.py
@@ -9,8 +9,17 @@ def main():
     from endo_pipeline.configs import get_datasets_in_collection, load_dataset_config
     from endo_pipeline.io import make_name_unique
     from endo_pipeline.io.output import get_output_path
-    from endo_pipeline.manifests import get_zarr_location_for_position
-    from endo_pipeline.settings.data_release import BFF_FILE_PATH_COL, S3_INTERNAL_DIRECTORY
+    from endo_pipeline.manifests import (
+        get_image_location_for_dataset,
+        get_zarr_location_for_position,
+        load_image_manifest,
+    )
+    from endo_pipeline.settings.data_release import (
+        BFF_FILE_PATH_COL,
+        DEST_CDH5_SEG_DIR,
+        DEST_NUC_SEG_DIR,
+        S3_INTERNAL_DIRECTORY,
+    )
     from endo_pipeline.settings.image_data import (
         NUM_ZSLICES,
         IMG_SHAPE_RESOLUTION_0_3i_X,
@@ -99,6 +108,8 @@ def main():
                     BFF_FILE_PATH_COL: str(s3_zarr_path),
                     "File Name": zarr_name,
                     "Date": dataset_config.date,
+                    "File ID": dataset_config.fmsid,
+                    "Plate Barcode": dataset_config.barcode,
                     "Biological Entity": "WTC-11 hiPSC derived endothelial cells",
                     "Organism": "human",
                     "Cell Line": cell_line,
@@ -126,8 +137,9 @@ def main():
                     "Shear Stress 2 Frame Start": ss_2_start,
                     "Shear Stress 2 Frame Stop": ss_2_stop,
                     "Shear Stress 2 (dynes/cm²)": round(ss_2) if not np.isnan(ss_2) else np.NaN,
+                    "Image Type": "raw",
                     "Image Format": "ome.zarr",
-                    "Raw Image Shape (TCZYX)": (
+                    "Image Shape (TCZYX)": (
                         dataset_config.duration,
                         sum(
                             v is not None
@@ -142,6 +154,59 @@ def main():
                     "Pixel Size Z (microns)": Z_STEP_SIZE_ACTUAL_3i_20x,
                 }
             )
+
+            for manifest_name, destination_dir in [
+                ("nuclear_labelfree_seg_zarr", DEST_NUC_SEG_DIR),
+                ("cdh5_classic_seg_zarr", DEST_CDH5_SEG_DIR),
+            ]:
+                img_manifest = load_image_manifest(manifest_name)
+                img_location = get_image_location_for_dataset(
+                    img_manifest, dataset_config, position
+                )
+                zarr_name = img_location.path.name
+                s3_zarr_path = s3_directory + destination_dir + zarr_name
+                rows.append(
+                    {
+                        BFF_FILE_PATH_COL: str(s3_zarr_path),
+                        "File Name": zarr_name,
+                        "Date": dataset_config.date,
+                        "File ID": dataset_config.fmsid,
+                        "Plate Barcode": dataset_config.barcode,
+                        "Segmentation Structure": (
+                            "Nuclei"
+                            if manifest_name == "nuclear_labelfree_seg_zarr"
+                            else "VE-cadherin"
+                        ),
+                        "Study Title": "placeholder",
+                        "Publication": "doi: placeholder",
+                        "Is Timelapse": dataset_config.is_timelapse,
+                        "Timelapse Duration (frames)": dataset_config.duration,
+                        "Time Interval (min)": dataset_config.time_interval_in_minutes,
+                        "Shear Stress Regime": " to ".join(
+                            r.value for r in dataset_config.shear_stress_regime
+                        ),
+                        "Shear Stress 1 (dynes/cm²)": round(ss_1),
+                        "Shear Stress 1 Duration Prior to Imaging (min)": abs(ss_1_start)
+                        * dataset_config.time_interval_in_minutes,
+                        "Shear Stress 1 Frame Start": 0,
+                        "Shear Stress 1 Frame Stop": ss_1_stop,
+                        "Shear Stress 2 Frame Start": ss_2_start,
+                        "Shear Stress 2 Frame Stop": ss_2_stop,
+                        "Shear Stress 2 (dynes/cm²)": round(ss_2) if not np.isnan(ss_2) else np.NaN,
+                        "Image Type": "segmentation",
+                        "Image Format": "ome.zarr",
+                        "Image Shape (TCZYX)": (
+                            dataset_config.duration,
+                            1,  # segmentation has one channel
+                            1,  # segmentation is on a 2d projection
+                            IMG_SHAPE_RESOLUTION_0_3i_Y,
+                            IMG_SHAPE_RESOLUTION_0_3i_X,
+                        ),
+                        "Pixel Size X (microns)": PIXEL_SIZE_3i_20x,
+                        "Pixel Size Y (microns)": PIXEL_SIZE_3i_20x,
+                    }
+                )
+
     df = pd.DataFrame(rows)
     file_path = make_name_unique(save_dir / "endo_release_data.csv")
     df.to_csv(file_path, index=False)
