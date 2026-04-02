5142afa7008babc0deff2e70ec69be5c7d6491ac
Erin Angelini <erin.angelini@alleninstitute.org>
2026-04-02 04:43:13 +0000

    to fix circular import, move get_dataframe_for_dynamics_workflows into pca module

--- src/endo_pipeline/library/analyze/diffae_dataframe_utils.py
+++ src/endo_pipeline/library/analyze/diffae_dataframe_utils.py
@@ -1,10 +1,9 @@
 import logging
 import re
-from typing import Literal, cast, overload
+from typing import cast, overload
 
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import PCA
 
 from endo_pipeline.configs import (
     DatasetConfig,
@@ -12,25 +11,11 @@ from endo_pipeline.configs import (
     TimepointAnnotation,
     get_all_unannotated_timepoints,
     get_frame_after_flow_change,
-    get_subset_of_timepoint_annotations,
     get_unannotated_positions,
     load_dataset_config,
 )
-from endo_pipeline.io import load_dataframe
-from endo_pipeline.library.analyze.pca import project_features_to_pcs
-from endo_pipeline.manifests import (
-    DataframeManifest,
-    get_dataframe_location_for_dataset,
-    load_dataframe_manifest,
-)
 from endo_pipeline.settings.column_names import ColumnName as Column
-from endo_pipeline.settings.diffae_feature_dataframes import DIFFAE_FEATURE_COLUMN_NAMES
-from endo_pipeline.settings.dynamics_workflows import (
-    METADATA_COLUMNS_TO_KEEP,
-    PERIOD_THETA_RESCALED,
-    RESCALE_THETA,
-)
-from endo_pipeline.settings.workflow_defaults import DEFAULT_SEG_FEATURE_MANIFEST_NAME
+from endo_pipeline.settings.dynamics_workflows import PERIOD_THETA_RESCALED, RESCALE_THETA
 
 logger = logging.getLogger(__name__)
 
@@ -361,203 +346,6 @@ def filter_dataframe_by_annotations(
     return dataframe_filtered
 
 
-def get_dataframe_for_dynamics_workflows(
-    dataset_name: str,
-    manifest: DataframeManifest,
-    columns_to_keep: list[str] | None = None,
-    pca: PCA | None = None,
-    filter_by_annotations: bool = True,
-    include_cell_piling: bool = True,
-    include_not_steady_state: bool = True,
-    crop_pattern: Literal["grid", "tracked"] = "grid",
-    compute_polar: bool = True,
-    rescale_theta: bool = True,
-    flip_pc3_sign: bool = True,
-    minimum_track_length: int | None = None,
-    segmentation_feature_manifest_name: str = DEFAULT_SEG_FEATURE_MANIFEST_NAME,
-) -> pd.DataFrame:
-    """
-    Load DiffAE dataframe data projected onto given PC axes for downstream
-    analysis in the stochastic dynamics workflow. Adds crop index column to
-    DataFrame, and projects feature data onto PC axes.
-
-    **Column selection and memory optimization**
-
-    The input DataFrame is filtered to keep only necessary columns when
-    initially loaded to save memory. At a minimum, the metadata columns defined
-    in METADATA_COLUMNS_TO_KEEP and the latent feature columns needed for PCA
-    projection are kept. Additional columns can be specified to keep via the
-    input ``columns_to_keep``.
-
-    In the case that the input ``pca`` is not None, the feature data is
-    projected onto the PC axes defined by the PCA model, and the original latent
-    feature columns are dropped to save memory.
-
-    **Dataframe filtering**
-
-    The input ``filter_by_annotations`` flag determines whether to filter out
-    annotated timepoints and positions from the dataframe. If filtering is
-    applied, the input flags ``include_cell_piling`` and
-    ``include_not_steady_state`` determine whether to include or exclude
-    timepoints annotated as "cell piling" and "not steady state", respectively.
-
-    If ``minimum_track_length`` is specified and the crop pattern is 'tracked',
-    tracks below the minimum track length will be filtered out.
-
-    Parameters
-    ----------
-    dataset_name
-        Name of dataset
-    manifest
-        Dataframe manifest for loading model features.
-    columns_to_keep
-        List of additional column names to keep in the output DataFrame.
-    pca
-        PCA model to fit to feature data. If None, do not project feature data.
-    filter_by_annotations
-        Whether to filter out annotated timepoints and positions from the
-        dataframe.
-    include_cell_piling
-        True keep timepoints annotated as "cell_piling", False to remove them.
-    include_not_steady_state
-        True to keep timepoints annotated as "not_steady_state", False to remove
-        them.
-    crop_pattern
-        Crop pattern used to generate the feature dataframe. Either 'grid' or
-        'tracked'.
-    compute_polar
-        Whether to compute polar coordinates (r, theta) from the first two PCs.
-    rescale_theta
-        Whether to rescale the polar angle theta to be in the range [0, pi].
-    flip_pc3_sign
-        True to add an additional column with the sign of PC3 flipped for
-        consistency, False otherwise.
-    minimum_track_length
-        If crop_pattern is 'tracked', minimum track length (in number of
-        timepoints) of tracks to keep in the dataframe. If None, do not filter
-        by track length.
-
-    Returns
-    -------
-    :
-        DataFrame with specified feature columns (PC projected or not),
-        specified metadata columns, and crop index column for downstream
-        analysis in various "dynamics workflows".
-    """
-
-    location = get_dataframe_location_for_dataset(manifest, dataset_name)
-    df = load_dataframe(location, delay=True)
-    feat_cols = DIFFAE_FEATURE_COLUMN_NAMES
-
-    # start with default metadata columns to keep
-    # temporarily drop the "crop_index" column while workflows that use this
-    # method are being refactored
-    columns_to_keep_ = [
-        column for column in METADATA_COLUMNS_TO_KEEP[crop_pattern] if column != "crop_index"
-    ]
-    if columns_to_keep is not None:
-        columns_to_keep_.extend(columns_to_keep)  # add any additional specified columns to keep
-    columns_to_keep_.extend(feat_cols)  # also keep feature columns for PCA projection
-    columns_to_keep_ = list(set(columns_to_keep_))  # remove duplicates, if any
-
-    # keep only necessary columns to save memory
-    df_ = df[columns_to_keep_].compute()
-
-    # the crop indices have to be added before any filtering
-    # so that they are consistently assigned across datasets
-    # for the grid crop pattern, which is critical for the
-    # grid-based TFE workflow to run correctly
-    df_ = add_crop_index(df_, crop_pattern)
-
-    # filter out annotated timepoints, including or excluding
-    # "cell piling" and "not steady state" annotations as specified
-    if filter_by_annotations:
-        annotations_to_ignore = []
-        if include_cell_piling:
-            annotations_to_ignore.append(TimepointAnnotation.CELL_PILING)
-        if include_not_steady_state:
-            annotations_to_ignore.append(TimepointAnnotation.NOT_STEADY_STATE)
-        timepoint_annotations = get_subset_of_timepoint_annotations(
-            annotations_to_ignore=annotations_to_ignore
-        )
-        df_filtered = filter_dataframe_by_annotations(
-            df_,
-            load_dataset_config(dataset_name),
-            timepoint_annotations=timepoint_annotations,
-        )
-    else:
-        df_filtered = df_
-
-    if crop_pattern == "tracked":
-        # some additional filtering with the "is_included" column from the
-        # segmentation features dataframe is needed to remove some of the
-        # incorrect segmentations that are present in the tracked crops data
-        seg_feat_manifest = load_dataframe_manifest(segmentation_feature_manifest_name)
-        seg_feat_loc = get_dataframe_location_for_dataset(seg_feat_manifest, dataset_name)
-        df_segmentations_delayed = load_dataframe(seg_feat_loc, delay=True)
-        cols_to_compute = [
-            Column.DATASET,
-            Column.POSITION,
-            Column.TIMEPOINT,
-            Column.TRACK_ID,
-            Column.TRACK_LENGTH,
-            Column.SegDataFilters.IS_INCLUDED,
-        ]
-        df_segmentations = df_segmentations_delayed[cols_to_compute].compute()
-        # NOTE the 2 lines below are temporary until we update how we store position
-        # and change the column names to be consistent across dataframes
-        df_segmentations[Column.POSITION] = df_segmentations[Column.POSITION].transform(
-            lambda pos: f"P{pos}"
-        )
-        original_df_length = len(df_filtered)
-        df_filtered = df_filtered.merge(
-            df_segmentations,
-            on=[
-                Column.DATASET,
-                Column.POSITION,
-                Column.TIMEPOINT,
-                Column.TRACK_ID,
-            ],
-            how="left",
-            validate="one_to_one",
-        )
-        if original_df_length != len(df_filtered):
-            raise ValueError(
-                f"Length of df_diffae_dynamics changed after merging with segmentation features dataframe. "
-                f"Original length: {original_df_length}, new length: {len(df_filtered)}. "
-                f"Check the merge keys and the dataframes to ensure that the merge is correct."
-            )
-        df_filtered = df_filtered[df_filtered[Column.SegDataFilters.IS_INCLUDED]]
-
-    if minimum_track_length is not None:
-        df_filtered = filter_dataframe_by_track_length(
-            df_filtered, Column.TRACK_LENGTH, minimum_track_length
-        )
-
-    # add dataset duration description column
-    dataset_config = load_dataset_config(dataset_name)
-    df_filtered[Column.DURATION] = dataset_config.duration
-
-    if pca is None:
-        # do not project feature data onto PCA axes
-        return df_filtered
-
-    else:
-        # project feature data onto PC axes
-        df_with_pcs = project_features_to_pcs(
-            df_filtered,
-            pca,
-            feat_cols=feat_cols,
-            compute_polar=compute_polar,
-            rescale_theta=rescale_theta,
-            flip_pc3_sign=flip_pc3_sign,
-        )
-        df_drop_original_feats = df_with_pcs.drop(
-            columns=feat_cols
-        )  # drop original feature columns to save memory
-        return df_drop_original_feats
-
-
 def get_dataset_descriptions(
     list_of_datasets: list[str],
     include_duration: bool = True,
@@ -650,42 +438,6 @@ def add_description_column(
 
     # add description column to DataFrame
     df["description"] = description[dataset_name]  # add description to DataFrame
-
-    return df
-
-
-def add_crop_index(
-    df: pd.DataFrame,
-    crop_pattern: Literal["grid", "tracked"] = "grid",
-) -> pd.DataFrame:
-    """
-    Add crop index column to DataFrame df. (Crops are currently identified by
-        their starting position in x and y.).
-
-    Inputs:
-    - df: pd.DataFrame, DataFrame of feature data with metadata
-        columns for start_x, start_y, and FOV_ID
-        - IMPORTANT: DataFrame must be restricted to one dataset only,
-            as identified by the dataset_name column
-
-    Outputs:
-    - df: pd.DataFrame, DataFrame of feature data for one
-        dataset with added crop index column
-    """
-    if crop_pattern not in ["grid", "tracked"]:
-        logger.error("Crop pattern must be 'tracked' or 'grid', got [ %s ]", crop_pattern)
-        raise ValueError("Input crop_pattern must be 'grid' or 'tracked'")
-
-    if crop_pattern == "tracked" and Column.TRACK_ID in df.columns:
-        required_columns = [Column.POSITION, Column.TRACK_ID]
-    elif crop_pattern == "grid":
-        required_columns = [Column.POSITION, Column.DiffAEData.START_X, Column.DiffAEData.START_Y]
-
-    check_required_columns_in_dataframe(df, required_columns)
-
-    # group by the required columns and assign a unique integer (the crop_index)
-    # to each group based on the index of that group
-    df[Column.CROP_INDEX] = df.groupby(required_columns, as_index=False).ngroup().astype(int)
 
     return df
 
