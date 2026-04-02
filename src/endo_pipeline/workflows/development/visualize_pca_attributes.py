730575f07c83c2abf3b488a00a02d6c4af75290f
Erin Angelini <erin.angelini@alleninstitute.org>
2026-04-02 01:26:23 +0000

    rm plot scatter call and simplify cli inputs accordingly

--- src/endo_pipeline/workflows/development/visualize_pca_attributes.py
+++ src/endo_pipeline/workflows/development/visualize_pca_attributes.py
@@ -1,14 +1,8 @@
-from endo_pipeline.cli import CropPattern, Datasets
-
-
 def main(
-    datasets: Datasets | None = None,
-    crop_pattern: CropPattern = "grid",
     num_pcs: int | None = None,
     num_features: int | None = None,
     include_loadings_legend: bool = False,
     annotate: bool = True,
-    color_by_time: bool = False,
 ) -> None:
     """
     Visualize key attributes of a fit PCA model.
@@ -25,10 +19,6 @@ def main(
 
     Parameters
     ----------
-    datasets
-        Datasets and / or dataset collection(s) to visualize in the PCA scatter plot.
-    crop_pattern
-        Crop pattern for the features to visualize.
     num_pcs
         Number of principal components to analyze and visualize.
     num_features
@@ -40,9 +30,8 @@ def main(
     color_by_time
         If true, color points in the PCA scatter plot by timepoint. If false, color by dataset.
     """
-    import logging
 
-    from endo_pipeline.configs import get_datasets_in_collection, get_latent_dim_from_config
+    from endo_pipeline.configs import get_latent_dim_from_config
     from endo_pipeline.io import get_config_dict_from_mlflow, get_output_path, save_plot_to_path
     from endo_pipeline.library.analyze.diffae_dataframe_utils import (
         fit_pca,
@@ -55,11 +44,7 @@ def main(
     from endo_pipeline.library.visualize.multi_feature_correlation_viz import (
         plot_and_save_clustermap,
     )
-    from endo_pipeline.manifests import (
-        get_model_location_for_run,
-        load_dataframe_manifest,
-        load_model_manifest,
-    )
+    from endo_pipeline.manifests import get_model_location_for_run, load_model_manifest
     from endo_pipeline.settings.diffae_feature_dataframes import (
         NUM_LATENT_FEATURES,
         NUM_PCS_TO_ANALYZE,
@@ -67,24 +52,12 @@ def main(
     from endo_pipeline.settings.workflow_defaults import (
         DEFAULT_MODEL_MANIFEST_NAME,
         DEFAULT_MODEL_RUN_NAME,
-        DEFAULT_PCA_DATASET_COLLECTION_NAME,
     )
-
-    # set up logger
-    logger = logging.getLogger(__name__)
 
     # load model manifest for default model manifest name and run name
     model_manifest_name = DEFAULT_MODEL_MANIFEST_NAME
     run_name = DEFAULT_MODEL_RUN_NAME
     model_manifest = load_model_manifest(model_manifest_name)
-
-    # get dataframe manifest for crop-based features
-    base_name = f"{model_manifest_name}_{run_name}_{crop_pattern}"
-    feature_dataframe_manifest_name = f"{base_name}_pca_filtered"
-    feature_dataframe_manifest = load_dataframe_manifest(feature_dataframe_manifest_name)
-
-    # Use provided datasets or default if none provided.
-    dataset_names = datasets or get_datasets_in_collection(DEFAULT_PCA_DATASET_COLLECTION_NAME)
 
     # get latent dimension from model config
     model_location = get_model_location_for_run(model_manifest, run_name)
@@ -97,10 +70,7 @@ def main(
     pc_col_names = get_pc_column_names(num_pc_dim)
 
     # set up output directory for figures
-    fig_savedir = get_output_path(
-        __file__,
-        crop_pattern,
-    )
+    fig_savedir = get_output_path(__file__)
 
     # fit PCA model with the specified number of components using the method
     # defaults
@@ -135,16 +105,6 @@ def main(
     )
     save_plot_to_path(fig, fig_savedir, "pc_loadings_unscaled_magnitude")
 
-    # scatter plot of pca reference datasets
-    fig, _ = feature_viz.plot_pc_scatter(
-        dataset_names,
-        feature_dataframe_manifest,
-        scatter_size=1,
-        alpha=0.2,
-        color_by_time=color_by_time,
-        save_dir=fig_savedir,
-    )
-
     # heatmap and clustermap of PC loadings
     pca_loadings_df = get_pca_loadings_as_df(pca, df_format="wide")
     fig_heatmap = feature_viz.pc_loading_heatmap_workflow(
@@ -166,8 +126,6 @@ def main(
         filename="pca_loadings_clustermap",
         data_type="samples",
     )
-
-    logger.info("PCA visualization complete. Figures saved to [ %s ]", fig_savedir)
 
 
 if __name__ == "__main__":
