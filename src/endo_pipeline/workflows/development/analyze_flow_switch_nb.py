983cb69bc9f40a69a1ef80b17c4da753705fa186
Erin Angelini <erin.angelini@alleninstitute.org>
2026-04-07 22:44:44 +0000

    maintain after dataframe util refactors

--- src/endo_pipeline/workflows/development/analyze_flow_switch_nb.py
+++ src/endo_pipeline/workflows/development/analyze_flow_switch_nb.py
@@ -3,17 +3,15 @@ import logging
 
 from endo_pipeline.cli.apps import WorkflowOptions, apply_workflow_options
 from endo_pipeline.configs import (
-    PositionAnnotation,
-    TimepointAnnotation,
     get_datasets_in_collection,
     load_dataset_collection_config,
     load_dataset_config,
 )
 from endo_pipeline.io import load_dataframe
-from endo_pipeline.library.analyze.diffae_dataframe_utils import (
-    filter_dataframe_by_annotations,
+from endo_pipeline.library.analyze.dataframe_filtering import (
+    filter_dataframe_by_flow_condition,
     filter_dataframe_by_track_length,
-    split_dataset_by_flow,
+    filter_dataframe_to_steady_state,
 )
 from endo_pipeline.library.visualize.diffae_features.feature_viz import get_label_for_column
 from endo_pipeline.manifests import load_dataframe_manifest
@@ -21,7 +19,6 @@ from endo_pipeline.settings.column_names import ColumnName
 from endo_pipeline.settings.dynamics_workflows import (
     DYNAMICS_COLUMN_NAMES,
     METADATA_COLUMNS_TO_KEEP,
-    TRACK_METADATA_COLUMNS_TO_KEEP,
 )
 from endo_pipeline.settings.workflow_defaults import (
     DEFAULT_MODEL_MANIFEST_NAME,
@@ -45,18 +42,12 @@ dataset_collection_config = load_dataset_collection_config(dataset_collection_na
 
 # Load dataframe manifest for the features to be used in flow field
 # estimation and analysis.
-if crop_pattern == "grid":
-    base_name = f"{model_manifest_name}_{run_name}_{crop_pattern}"
-    feature_dataframe_manifest_name = f"{base_name}_pca_filtered"
-else:
-    # NOTE: current tracked feature dataframe has no filtering applied, so needs additional
-    # timepoint + position + "is_included" track filtering applied before analysis
-    feature_dataframe_manifest_name = "pc_diffae_tracked_seg_features"
+base_name = f"{model_manifest_name}_{run_name}_{crop_pattern}"
+feature_dataframe_manifest_name = f"{base_name}_pca_filtered"
 feature_dataframe_manifest = load_dataframe_manifest(feature_dataframe_manifest_name)
 
 valid_dataset_options = list(feature_dataframe_manifest.locations.keys())
 dataset_names = get_datasets_in_collection(dataset_collection_name, subset=valid_dataset_options)
-
 
 # %%
 # loop over datasets in collection and do analysis
@@ -65,37 +56,23 @@ for dataset_name in dataset_names:
     # load dataframe and perform additional filtering (remove
     # non-steady-state timepoints based on annotations), computing
     # only the columns needed for flow field estimation and analysis to save memory.
-    df = load_dataframe(feature_dataframe_manifest.locations[dataset_name], delay=True)
+    df_ = load_dataframe(feature_dataframe_manifest.locations[dataset_name], delay=True)
     # start with default metadata columns to keep
-    columns_to_compute = [*METADATA_COLUMNS_TO_KEEP, *column_names]
-    if crop_pattern == "tracked":
-        # also keep track ID and track length columns for tracked crops
-        columns_to_compute = [*columns_to_compute, *TRACK_METADATA_COLUMNS_TO_KEEP]
-    df_ = df[columns_to_compute].compute()
-    df_steady_state = filter_dataframe_by_annotations(
-        df_,
-        load_dataset_config(dataset_name),
-        timepoint_annotations=[TimepointAnnotation.NOT_STEADY_STATE],
-    )
+    columns_to_compute = [*METADATA_COLUMNS_TO_KEEP[crop_pattern], *column_names]
+    df = df_[columns_to_compute].compute()
+    df_steady_state = filter_dataframe_to_steady_state(df, dataset_config)
 
     if crop_pattern == "tracked":
-        # additional filtering currently necessary for loading pattern with
-        # tracked crops; this will be updated once the dataframe tracking
-        # structure is standardized
-        df_steady_state = df_steady_state[df_steady_state[ColumnName.SegDataFilters.IS_INCLUDED]]
-        df_steady_state = filter_dataframe_by_annotations(
-            df_steady_state,
-            dataset_config,
-            timepoint_annotations=list(TimepointAnnotation),
-            position_annotations=list(PositionAnnotation),
-        )
-        # also filter out tracks that are too short for reliable flow field estimation and analysis
+        # can add additional track length filtering
         df_steady_state = filter_dataframe_by_track_length(
             df_steady_state, ColumnName.TRACK_LENGTH, minimum_track_length=100
         )
-    # split dataset by flow condition (e.g., 6 and 20 dyn/cm2 for the 6 to 20 dyn/cm2 dataset)
-    dfs_by_flow, shear_stress_list = split_dataset_by_flow(df, dataset_config)
-    for df_flow, shear_stress in zip(dfs_by_flow, shear_stress_list, strict=True):
+    # split dataset by flow condition
+    for flow_condition in dataset_config.flow_conditions:
+        shear_stress = flow_condition.shear_stress
+        df_flow = filter_dataframe_by_flow_condition(
+            df_steady_state, dataset_config, flow_condition
+        )
         logger.info(f"Processing dataset {dataset_name} with shear stress {shear_stress}")
         logger.info(f"Number of samples: {len(df_flow)}")
         # do analysis here
