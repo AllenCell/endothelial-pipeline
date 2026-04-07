c820fcc7b6053b32c08e3b8ddea1f51f90cd75fc
Erin Angelini <erin.angelini@alleninstitute.org>
2026-04-07 22:08:30 +0000

    turn from notebook style into main with datasets as cli input

--- src/endo_pipeline/workflows/production/validate_dataset_configs_nb.py
+++ src/endo_pipeline/workflows/production/validate_dataset_configs_nb.py
@@ -1,72 +1,80 @@
-# %% [markdown]
-# # Validate dataset configs
+from endo_pipeline.cli import Datasets
 
-# %% [markdown]
-"""
-Validate all existing datasets by checking config schemas and loading files.
 
-For each dataset config in the `configs/datasets` directory, confirm:
+def main(datasets: Datasets | None = None) -> None:
+    """Validate dataset(s) by checking config schemas and loading files.
 
-- All dataset configs follow the schema defined by `DatasetConfig`
-- All original data paths exist and can be opened
-- All zarr data paths exist and can be opened
-- All shear stress regimes are valid based on the flow conditions
-"""
+    #test_ready #CPU_only
 
-# %%
-DESCRIPTION = "Validate all existing datasets by checking config schemas and loading files."
-TAGS = ["test_ready", "CPU_only"]
+    For each specified dataset, confirm:
 
-# %%
-import logging
-from pathlib import Path
+    - The dataset config exists and can be loaded
+    - All dataset configs follow the schema defined by `DatasetConfig`
+    - All original data paths exist and can be opened
+    - All zarr data paths exist and can be opened
+    - All shear stress regimes are valid based on the flow conditions
 
-from bioio import BioImage
+    If `datasets` is not provided, all datasets will be validated.
 
-from endo_pipeline.cli import DEMO_MODE
-from endo_pipeline.configs import (
-    get_available_dataset_names,
-    load_dataset_config,
-    validate_dataset_config,
-)
-from endo_pipeline.manifests import get_zarr_location_for_position
+    Parameters
+    ----------
+    datasets
+        The dataset(s) to validate. If None, all datasets will be validated.
 
-# %%
-logger = logging.getLogger(__name__)
+    """
+    import logging
+    from pathlib import Path
 
-# %%
-names = get_available_dataset_names()
-if DEMO_MODE:
-    # Each dataset takes 2-9 seconds to validate
-    names = names[:2]
+    from bioio import BioImage
 
-for dataset_name in names:
-    logger.info(f"Running validation for dataset [ {dataset_name} ]")
+    from endo_pipeline.cli import DEMO_MODE
+    from endo_pipeline.configs import (
+        get_available_dataset_names,
+        load_dataset_config,
+        validate_dataset_config,
+    )
+    from endo_pipeline.manifests import get_zarr_location_for_position
 
-    # Validate dataset config schema.
-    validate_dataset_config(dataset_name)
+    logger = logging.getLogger(__name__)
 
-    # Load dataset config.
-    dataset_config = load_dataset_config(dataset_name)
+    dataset_names = datasets or get_available_dataset_names()
+    if DEMO_MODE:
+        # Each dataset takes 2-9 seconds to validate
+        dataset_names = dataset_names[:2]
 
-    # Check if file at original path exists and can be opened.
-    try:
-        BioImage(Path(dataset_config.original_path))
-    except FileNotFoundError:
-        logger.error(
-            "Failed to open original for dataset [ %s ] at [ %s ]",
-            dataset_config.name,
-            dataset_config.original_path,
-        )
+    for dataset_name in dataset_names:
+        logger.info(f"Running validation for dataset [ {dataset_name} ]")
 
-    # For each position, check if the local zarr exists and can be opened.
-    for position in dataset_config.zarr_positions:
-        zarr_file = get_zarr_location_for_position(dataset_config, position).path
+        # Validate dataset config schema.
+        validate_dataset_config(dataset_name)
 
+        # Load dataset config.
+        dataset_config = load_dataset_config(dataset_name)
+
+        # Check if file at original path exists and can be opened.
         try:
-            BioImage(zarr_file)
-        except:
+            BioImage(Path(dataset_config.original_path))
+        except FileNotFoundError:
             logger.error(
-                "Failed to load zarr for dataset [ %s ] at [ %s ]", dataset_name, zarr_file
+                "Failed to open original for dataset [ %s ] at [ %s ]",
+                dataset_config.name,
+                dataset_config.original_path,
             )
-            raise
+
+        # For each position, check if the local zarr exists and can be opened.
+        for position in dataset_config.zarr_positions:
+            zarr_file = get_zarr_location_for_position(dataset_config, position).path
+
+            try:
+                BioImage(zarr_file)
+            except:
+                logger.error(
+                    "Failed to load zarr for dataset [ %s ] at [ %s ]", dataset_name, zarr_file
+                )
+                raise
+
+
+if __name__ == "__main__":
+    from endo_pipeline.cli import workflow_cli
+
+    workflow_cli(main)
