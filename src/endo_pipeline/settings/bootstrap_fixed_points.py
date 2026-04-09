60519aff79067c859ad4385362a63aec55d6d464
Erin Angelini <erin.angelini@alleninstitute.org>
2026-04-09 23:34:59 +0000

    increase match radius, CI values in correct scale

--- src/endo_pipeline/settings/bootstrap_fixed_points.py
+++ src/endo_pipeline/settings/bootstrap_fixed_points.py
@@ -7,14 +7,14 @@ in 3D flow field analysis."""
 BATCH_SIZE_SCALING_FACTOR: float = 4
 """Factor to used determine the number of batches for parallel processing in bootstrapping."""
 
-BOOTSTRAP_MATCH_RADIUS: float = 0.1
+BOOTSTRAP_MATCH_RADIUS: float = 0.3
 """Radius threshold for matching bootstrapped fixed points to baseline fixed
 points in 3D flow field analysis."""
 
-FP_CI_LOWER_PERCENTILE: float = 0.05
+FP_CI_LOWER_PERCENTILE: float = 5
 """Lower percentile for fixed point confidence interval estimation in 3D flow
 field analysis."""
 
-FP_CI_UPPER_PERCENTILE: float = 0.95
+FP_CI_UPPER_PERCENTILE: float = 95
 """Upper percentile for fixed point confidence interval estimation in 3D flow
 field analysis."""
