11c7e160d841d43c65e7341d56a99f03122937e9
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-04-10 17:11:40 -0400

    Move extract_t method to library

--- src/endo_pipeline/configs/dataset_io.py
+++ src/endo_pipeline/configs/dataset_io.py
@@ -1,5 +1,4 @@
 import logging
-import re
 from pathlib import Path
 from typing import Any
 
@@ -121,51 +120,3 @@ def load_config(config_type: str = "data") -> dict[Any, Any]:
     with open(config_file) as file:
         config_data = yaml.safe_load(file)
     return config_data
-
-
-def extract_t(
-    fp_as_string: str | Path,
-    int_only: bool = True,
-    use_last_match: bool = True,
-    default_if_not_found: int | str = "",
-) -> str | int:
-    """
-    Extract the timepoint value from a string or Path.name.
-    Searches for the pattern "T[0-9]+" to find the timepoint.
-    If use_last_match is True then the last match will be used,
-    otherwise the first one will be used.
-
-    Parameters
-    ----------
-    fp_as_string: str or Path
-        A string or Path.name to get the timepoint from.
-    int_only: bool
-        Whether to return just the timepoint as an integer or
-        an entire string (i.e. 10 vs 'T010')
-        Default is True.
-    use_last_match: bool
-        Whether to use the last match (in the event that multiple possible
-        timepoint values were found in the string).
-        If False then the first match will be used.
-        E.g. image_name_T1_etc_T57.tif can return either T1 or T57, but
-        will return 57 by default. Ideally the timepoint in fp_as_string
-        would be unambiguous.
-        Default is True.
-
-    Returns
-    -------
-    t: int or str
-        The timepoint represented as an integer if int_only is True, otherwise
-        the timepoint represented as a string including the T before.
-    """
-
-    if isinstance(fp_as_string, Path):
-        fp_as_string = str(fp_as_string)
-
-    index = -1 if use_last_match else 0
-    t = re.findall("T[0-9]+", fp_as_string)
-    t_value = int(t[index].split("T")[-1]) if t else default_if_not_found
-    if not t:
-        logger.debug("""No 'T[0-9]+' found in filename. Using T == default_if_not_found.""")
-
-    return t_value if int_only else f"T{t_value}"
--- src/endo_pipeline/library/analyze/shape_features.py
+++ src/endo_pipeline/library/analyze/shape_features.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from collections.abc import Callable
 from os import scandir
 from pathlib import Path
@@ -8,8 +9,6 @@ import numpy as np
 import pandas as pd
 from dask.array import Array
 from skimage import draw, filters, graph, measure, morphology, segmentation
-
-from endo_pipeline.configs.dataset_io import extract_t
 
 logger = logging.getLogger(__name__)
 
@@ -1940,6 +1939,54 @@ def get_and_save_nuclei_features_arg_unpacker(args: dict) -> None:
         channel_names=channel_names,
         save_output=save_output,
     )
+
+
+def extract_t(
+    fp_as_string: str | Path,
+    int_only: bool = True,
+    use_last_match: bool = True,
+    default_if_not_found: int | str = "",
+) -> str | int:
+    """
+    Extract the timepoint value from a string or Path.name.
+    Searches for the pattern "T[0-9]+" to find the timepoint.
+    If use_last_match is True then the last match will be used,
+    otherwise the first one will be used.
+
+    Parameters
+    ----------
+    fp_as_string: str or Path
+        A string or Path.name to get the timepoint from.
+    int_only: bool
+        Whether to return just the timepoint as an integer or
+        an entire string (i.e. 10 vs 'T010')
+        Default is True.
+    use_last_match: bool
+        Whether to use the last match (in the event that multiple possible
+        timepoint values were found in the string).
+        If False then the first match will be used.
+        E.g. image_name_T1_etc_T57.tif can return either T1 or T57, but
+        will return 57 by default. Ideally the timepoint in fp_as_string
+        would be unambiguous.
+        Default is True.
+
+    Returns
+    -------
+    t: int or str
+        The timepoint represented as an integer if int_only is True, otherwise
+        the timepoint represented as a string including the T before.
+    """
+
+    if isinstance(fp_as_string, Path):
+        fp_as_string = str(fp_as_string)
+
+    index = -1 if use_last_match else 0
+    t = re.findall("T[0-9]+", fp_as_string)
+    t_value = int(t[index].split("T")[-1]) if t else default_if_not_found
+    if not t:
+        logger.debug("""No 'T[0-9]+' found in filename. Using T == default_if_not_found.""")
+
+    return t_value if int_only else f"T{t_value}"
 
 
 def concatenate_and_save_feature_tables(
