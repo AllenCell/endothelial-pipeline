5e6cd2c515ab23849a3670dca23c192adee12c1d
Erin Angelini <erin.angelini@alleninstitute.org>
2026-04-14 23:13:50 +0000

    str enum for unicode characters

---
+++ src/endo_pipeline/settings/unicode.py
@@ -0,0 +1,13 @@
+"""Global settings for unicode characters used in the project."""
+
+from enum import StrEnum
+
+
+class UnicodeCharacters(StrEnum):
+    """Unicode characters used in the project."""
+
+    THETA = "\u03b8"
+    """Unicode for lowercase theta."""
+
+    RHO = "\u03c1"
+    """Unicode for lowercase rho."""
