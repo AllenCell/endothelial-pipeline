f3d8a55c78c72457d6b7a5d9af6c4c749f1eb842
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2026-04-14 11:27:30 -0400

    Deprecate `dataset_io` module (#1430)
    * Deprecate extract_p method
    
    * Move concatenate_and_save_feature_tables method to library
    
    * Move extract_t method to library
    
    * Deprecate dataset_io module
    
    * Add import-not-found mypy ignore for archived workflows

--- src/endo_pipeline/library/analyze/shape_features.py
+++ src/endo_pipeline/library/analyze/shape_features.py
@@ -1,5 +1,7 @@
 import logging
+import re
 from collections.abc import Callable
+from os import scandir
 from pathlib import Path
 from typing import Any, Literal
 
@@ -1937,3 +1939,140 @@ def get_and_save_nuclei_features_arg_unpacker(args: dict) -> None:
         channel_names=channel_names,
         save_output=save_output,
     )
+
+
+def extract_t(
+    fp_as_string: str | Path,
+    int_only: bool = True,
+    use_last_match: bool = True,
+    default_if_not_found: int | str = "",
+) -> str | int:
+    """
+    Extract the timepoint value from a string or Path.name.
+    Searches for the pattern "T[0-9]+" to find the timepoint.
+    If use_last_match is True then the last match will be used,
+    otherwise the first one will be used.
+
+    Parameters
+    ----------
+    fp_as_string: str or Path
+        A string or Path.name to get the timepoint from.
+    int_only: bool
+        Whether to return just the timepoint as an integer or
+        an entire string (i.e. 10 vs 'T010')
+        Default is True.
+    use_last_match: bool
+        Whether to use the last match (in the event that multiple possible
+        timepoint values were found in the string).
+        If False then the first match will be used.
+        E.g. image_name_T1_etc_T57.tif can return either T1 or T57, but
+        will return 57 by default. Ideally the timepoint in fp_as_string
+        would be unambiguous.
+        Default is True.
+
+    Returns
+    -------
+    t: int or str
+        The timepoint represented as an integer if int_only is True, otherwise
+        the timepoint represented as a string including the T before.
+    """
+
+    if isinstance(fp_as_string, Path):
+        fp_as_string = str(fp_as_string)
+
+    index = -1 if use_last_match else 0
+    t = re.findall("T[0-9]+", fp_as_string)
+    t_value = int(t[index].split("T")[-1]) if t else default_if_not_found
+    if not t:
+        logger.debug("""No 'T[0-9]+' found in filename. Using T == default_if_not_found.""")
+
+    return t_value if int_only else f"T{t_value}"
+
+
+def concatenate_and_save_feature_tables(
+    out_dir: Path,
+    dataset_name: str,
+    out_file_suffix: str = "",
+    input_filename_contains: str = "",
+    file_extension: str = ".csv",
+    sort_by_T: bool = True,
+    check_saved_dataframe: bool = True,
+    remove_initial_files_and_folders: bool = False,
+) -> None:
+    """
+    Concatenate the nuclei feature tables for all positions and
+    timepoints for a given dataset in an out_dir and then saves
+    the concatenated table to the output directory.
+    The expected file structure in out_dir is:
+    out_dir/dataset_name/position/*filename_contains*.file_extension.
+    """
+    out_subdir = out_dir / dataset_name
+
+    file_extension = f".{file_extension}" if not file_extension.startswith(".") else file_extension
+    if input_filename_contains and not input_filename_contains.endswith("*"):
+        input_filename_contains = f"{input_filename_contains}*"
+    feats_filepaths = list(out_subdir.glob(f"**/*{input_filename_contains}{file_extension}"))
+    if sort_by_T:
+        feats_filepaths = sorted(feats_filepaths, key=lambda fp: extract_t(fp.stem))
+
+    if file_extension == ".tsv":
+        sep = "\t"
+        table_reader = lambda fp: pd.read_csv(fp, sep=sep)
+        table_writer = lambda df, fp: df.to_csv(fp, sep=sep, index=False)
+    elif file_extension == ".csv":
+        sep = ","
+        table_reader = lambda fp: pd.read_csv(fp, sep=sep)
+        table_writer = lambda df, fp: df.to_csv(fp, sep=sep, index=False)
+    elif file_extension == ".parquet":
+        table_reader = lambda fp: pd.read_parquet(fp)
+        table_writer = lambda df, fp: df.to_parquet(fp, index=False)
+    else:
+        raise ValueError(
+            f"Invalid file extension {file_extension}. Must be .csv, .tsv., or .parquet."
+        )
+    feats_dfs = [table_reader(fp) for fp in feats_filepaths]
+
+    # define the output path for the concatenated dataframe
+    if out_file_suffix:
+        out_file_suffix = (
+            f"_{out_file_suffix}" if not out_file_suffix.startswith("_") else f"{out_file_suffix}"
+        )
+    concatenated_df_out_path = out_dir / f"{dataset_name}{out_file_suffix}{file_extension}"
+
+    if feats_dfs:
+        concatenated_df = pd.concat(feats_dfs, ignore_index=True)
+        table_writer(concatenated_df, concatenated_df_out_path)
+    else:
+        logger.debug(f"No feature tables found for {dataset_name}.")
+
+    if check_saved_dataframe:
+        # check that the concatenated dataframe at least has the same shape
+        # and column names as a proxy for checking if it was saved correctly
+        saved_df = table_reader(concatenated_df_out_path)
+        same_shape = saved_df.shape == concatenated_df.shape
+        same_column_names = all(saved_df.columns == concatenated_df.columns)
+        if not (same_shape and same_column_names):
+            raise ValueError(
+                f"Saved dataframe {concatenated_df_out_path} \
+                    does not match the concatenated dataframe."
+            )
+        logger.info(f"Concatenated dataframe saved to {concatenated_df_out_path}.")
+
+    if remove_initial_files_and_folders:
+        # remove files that match input_filename_contains
+        for fp in feats_filepaths:
+            fp.unlink()
+    dirs_to_remove = list(out_subdir.glob("**/"))
+    # remove the empty directory now that old tables are deleted
+    # (note this must be done in reverse order because a folder with
+    # subfolders does not count as empty and therfore raises an error)
+    for dir_path in dirs_to_remove[::-1]:
+        # NOTE that rmdir only removes empty directories
+        # and will raise an error if it is not empty. If
+        # a directory is not empty then we will skip it
+        if not any(list(scandir(dir_path))):
+            dir_path.rmdir()
+            logger.debug(f"Removed empty directory {dir_path}.")
+        else:
+            logger.debug(f"Directory {dir_path} is not empty, skipping removal.")
+            continue
