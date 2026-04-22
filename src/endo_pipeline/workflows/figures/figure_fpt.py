a72464ad9fa20f18d4b26425fe78643a996837d1
Serge Parent <serge.edouard.parent@gmail.com>
2026-04-22 09:49:39 -0700

    - wrapped figure_fpt in `main` func

--- src/endo_pipeline/workflows/figures/figure_fpt.py
+++ src/endo_pipeline/workflows/figures/figure_fpt.py
@@ -1,53 +1,58 @@
-import matplotlib.pyplot as plt
+def main():
+    import matplotlib.pyplot as plt
 
-from endo_pipeline.io import get_output_path
-from endo_pipeline.library.visualize.figures import FigurePanel, build_figure_from_panels
-from endo_pipeline.settings.figures import MAX_FIGURE_WIDTH
-from endo_pipeline.settings.summary_plot import SUMMARY_PLOT_DATASETS
+    from endo_pipeline.io import get_output_path
+    from endo_pipeline.library.visualize.figures import FigurePanel, build_figure_from_panels
+    from endo_pipeline.settings.figures import MAX_FIGURE_WIDTH
 
-plt.style.use("endo_pipeline.figure")
+    plt.style.use("endo_pipeline.figure")
 
-save_dir = get_output_path("figure_4")
+    save_dir = get_output_path("figure_4")
+
+    panels = [
+        FigurePanel(
+            letter="A",
+            path=save_dir / "20250618_20X_FPT_fp_0_stable_mean_correlation.svg",
+            x_position=0,
+            y_position=0,
+            x_offset=0,  # 0.2,
+            y_offset=0,  # 0.08,
+        ),
+        FigurePanel(
+            letter="B",
+            path=save_dir / "20250611_20X_FPT_fp_3_stable_mean_correlation.svg",
+            x_position=0,
+            y_position=2.5,
+            x_offset=0,  # 0,
+            y_offset=0,  # 0.2,
+        ),
+        FigurePanel(
+            letter="C",
+            path=save_dir / "20250813_20X_FPT_fp_0_stable_mean_correlation.svg",
+            x_position=0,
+            y_position=2.5,
+            x_offset=0,  # 0,
+            y_offset=0,  # 0.2,
+        ),
+        FigurePanel(
+            letter="D",
+            path=save_dir / "FPT_correlation_summary.svg",
+            x_position=0,
+            y_position=2.5,
+            x_offset=0,  # 0,
+            y_offset=0,  # 0.2,
+        ),
+    ]
+
+    build_figure_from_panels(
+        figure_panels=panels,
+        output_path=save_dir / "figure_4.svg",
+        width=MAX_FIGURE_WIDTH,
+        height=4.75,
+    )
 
 
-dataset_summary_list = SUMMARY_PLOT_DATASETS["intermediate"]
+if __name__ == "__main__":
+    from endo_pipeline.cli import workflow_cli
 
-
-panels = [
-    FigurePanel(
-        letter="A",
-        path=save_dir / "20250618_20X_FPT_fp_0_stable_mean_correlation.svg",
-        x_position=0,
-        y_position=0,
-        x_offset=0,  # 0.2,
-        y_offset=0,  # 0.08,
-    ),
-    FigurePanel(
-        letter="B",
-        path=save_dir / "20250611_20X_FPT_fp_3_stable_mean_correlation.svg",
-        x_position=0,
-        y_position=2.5,
-        x_offset=0,  # 0,
-        y_offset=0,  # 0.2,
-    ),
-    FigurePanel(
-        letter="C",
-        path=save_dir / "20250813_20X_FPT_fp_0_stable_mean_correlation.svg",
-        x_position=0,
-        y_position=2.5,
-        x_offset=0,  # 0,
-        y_offset=0,  # 0.2,
-    ),
-    FigurePanel(
-        letter="D",
-        path=save_dir / "FPT_correlation_summary.svg",
-        x_position=0,
-        y_position=2.5,
-        x_offset=0,  # 0,
-        y_offset=0,  # 0.2,
-    ),
-]
-
-build_figure_from_panels(
-    figure_panels=panels, output_path=save_dir / "figure_4.svg", width=MAX_FIGURE_WIDTH, height=4.75
-)
+    workflow_cli(main)
