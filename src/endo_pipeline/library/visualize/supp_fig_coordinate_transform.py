9b02fe4a92d39405f02dba98195d3cb9b5d9ed8d
Erin Angelini <erin.angelini@alleninstitute.org>
2026-04-23 23:48:31 +0000

    top 3 pc latent walk helper method

---
+++ src/endo_pipeline/library/visualize/supp_fig_coordinate_transform.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from endo_pipeline.cli import NUM_GPUS
+from endo_pipeline.configs import get_datasets_in_collection
+from endo_pipeline.io import load_dataframe, load_model
+from endo_pipeline.library.analyze.pca import fit_pca
+from endo_pipeline.library.model.diffae import DiffusionAutoEncoder
+from endo_pipeline.library.model.diffae.generate_image import generate_latent_walk_images
+from endo_pipeline.library.model.latent_walk_utils import get_latent_walk
+from endo_pipeline.library.visualize.latent_walk import plot_latent_walk_as_grid
+from endo_pipeline.manifests import (
+    get_dataframe_location_for_dataset,
+    load_dataframe_manifest,
+    load_model_manifest,
+)
+from endo_pipeline.settings.diffae_feature_dataframes import DIFFAE_PC_COLUMN_NAMES
+from endo_pipeline.settings.figures import MAX_FIGURE_WIDTH
+from endo_pipeline.settings.workflow_defaults import (
+    DEFAULT_MODEL_MANIFEST_NAME,
+    DEFAULT_MODEL_RUN_NAME,
+    DEFAULT_PCA_DATASET_COLLECTION_NAME,
+    RANDOM_SEED,
+)
+
+
+def perform_latent_walk_along_top_pcs(
+    save_path: Path, filename: str, figsize: tuple[float, float] = (MAX_FIGURE_WIDTH, 2.8)
+) -> np.ndarray:
+    """
+    Perform a latent walk along the top principal 3 components of the data.
+    """
+    model_manifest_name = DEFAULT_MODEL_MANIFEST_NAME
+    run_name = DEFAULT_MODEL_RUN_NAME
+    model_manifest = load_model_manifest(model_manifest_name)
+    model = load_model(model_manifest.locations[run_name], instantiate=True)
+    if not isinstance(model, DiffusionAutoEncoder):
+        raise ValueError(
+            f"Model loaded from {model_manifest_name} with run name {run_name} is not a DiffusionAutoEncoder."
+        )
+
+    # set up output directory
+
+    # load model configuration and reference dataset manifests
+    dataframe_manifest_name = f"{model_manifest.name}_{run_name}_grid_pca_filtered"
+    dataframe_manifest = load_dataframe_manifest(dataframe_manifest_name)
+    dataset_names = get_datasets_in_collection(DEFAULT_PCA_DATASET_COLLECTION_NAME)
+
+    num_pcs = 3
+    walk_column_names = DIFFAE_PC_COLUMN_NAMES[:num_pcs]
+    pca = fit_pca(num_pcs=num_pcs)
+    dataframe_all_datasets = pd.concat(
+        [
+            load_dataframe(get_dataframe_location_for_dataset(dataframe_manifest, dataset_name))
+            for dataset_name in dataset_names
+        ]
+    )
+    data_for_walk = dataframe_all_datasets[walk_column_names]
+
+    # get coordinate values for latent walk and the ranges of the walk for each
+    # dimension
+    walk, ranges = get_latent_walk(
+        data_for_walk,
+        walk_column_names,
+        sigma=3,
+        n_steps=7,
+    )
+
+    walk_latent = pca.inverse_transform(walk[walk_column_names].to_numpy())
+
+    # generate images from the latent walk
+    walk_img_grid = generate_latent_walk_images(
+        model, walk_latent, ranges, num_gpus=NUM_GPUS, RANDOM_SEED=RANDOM_SEED
+    )
+
+    plot_latent_walk_as_grid(
+        walk_img_grid,
+        ranges,
+        walk_column_names,
+        save_path,
+        filename,
+        label_sigmas=True,
+        figsize=figsize,
+        file_format=".svg",
+    )
+
+    return walk_img_grid
--- src/endo_pipeline/workflows/figures/supp_fig_coordinate_transform_nb.py
+++ src/endo_pipeline/workflows/figures/supp_fig_coordinate_transform_nb.py
@@ -1,1 +1,17 @@
 # %%
+import matplotlib.pyplot as plt
+
+from endo_pipeline.io import get_output_path
+from endo_pipeline.library.visualize.supp_fig_coordinate_transform import (
+    perform_latent_walk_along_top_pcs,
+)
+
+# %%
+plt.style.use("endo_pipeline.figure")
+output_path = get_output_path("supp_fig_coords")
+
+# load model manifest, get run name, and load model
+latent_walk_filename = "latent_walk_top_3_pcs"
+
+walk_img_grid = perform_latent_walk_along_top_pcs(output_path, latent_walk_filename)
+latent_walk_path = output_path / f"{latent_walk_filename}_scale_bar_10um.svg"
