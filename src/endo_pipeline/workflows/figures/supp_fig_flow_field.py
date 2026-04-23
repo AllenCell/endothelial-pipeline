f1e4c1f1f42ea47b7c51461caf55e6c38287597c
Erin Angelini <erin.angelini@alleninstitute.org>
2026-04-23 18:42:53 +0000

    global settings

--- src/endo_pipeline/workflows/figures/supp_fig_flow_field.py
+++ src/endo_pipeline/workflows/figures/supp_fig_flow_field.py
@@ -10,7 +10,9 @@ from endo_pipeline.library.visualize.flow_field_schematic import (
 )
 from endo_pipeline.settings.column_names import ColumnName as Column
 from endo_pipeline.settings.dynamics_workflows import KERNEL_BANDWIDTHS_DYNAMICS
+from endo_pipeline.settings.examples import EXAMPLE_DATASET
 from endo_pipeline.settings.figures import MAX_FIGURE_WIDTH
+from endo_pipeline.settings.flow_field_figure import SUPP_FIG_TARGET_POINT, SUPP_FIG_ZOOM_FACTOR
 
 # %%
 plt.style.use("endo_pipeline.figure")
@@ -22,12 +24,11 @@ image_panel_path = make_real_image_panel(output_path)
 
 # %% Use the dataset defined in the flow field construction examples for low
 # shear stress
-dataset_name = "20250409_20X"
+dataset_name = EXAMPLE_DATASET["FIGURE_2_LOW_FLOW_DATASET"]
 column_names = [Column.DiffAEData.POLAR_RADIUS, Column.DiffAEData.PC3_FLIPPED]
-target_point = (1.0, -0.1)
-zoom_factor = 3.5
-r_half = zoom_factor * KERNEL_BANDWIDTHS_DYNAMICS[column_names[0]]
-rho_half = zoom_factor * KERNEL_BANDWIDTHS_DYNAMICS[column_names[1]]
+target_point = SUPP_FIG_TARGET_POINT
+r_half = SUPP_FIG_ZOOM_FACTOR * KERNEL_BANDWIDTHS_DYNAMICS[column_names[0]]
+rho_half = SUPP_FIG_ZOOM_FACTOR * KERNEL_BANDWIDTHS_DYNAMICS[column_names[1]]
 xlim = (target_point[0] - r_half, target_point[0] + r_half)
 ylim = (target_point[1] - rho_half, target_point[1] + rho_half)
 # Filter to steady-state timepoints and a single flow condition
