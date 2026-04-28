d24aef26ae07ef04548195ca7983c839ca0b1d11
Erin Angelini <erin.angelini@alleninstitute.org>
2026-04-28 02:24:28 +0000

    vis 1 and 2d

--- src/endo_pipeline/workflows/production/visualize_2d_flow_field.py
+++ src/endo_pipeline/workflows/production/visualize_2d_flow_field.py
@@ -87,11 +87,10 @@ def main(
     from endo_pipeline.settings.flow_field_dataframes import (
         DATAFRAME_MANIFEST_PREFIX_DRIFT,
         DATAFRAME_MANIFEST_PREFIX_FIXED_POINTS,
-        STABILITY_COLOR_DICT,
-        STABILITY_MARKER_DICT,
         StabilityLabel,
         StabilityLegendHandle,
     )
+    from endo_pipeline.settings.plot_defaults import FIXED_POINT_PLOT_STYLE
     from endo_pipeline.settings.workflow_defaults import (
         DEFAULT_MODEL_MANIFEST_NAME,
         DEFAULT_MODEL_RUN_NAME,
@@ -342,8 +341,8 @@ def main(
                 ax.plot(
                     stable_fixed_points[column_names[0]],
                     stable_fixed_points[column_names[1]],
-                    STABILITY_MARKER_DICT[StabilityLabel.STABLE],
-                    color=STABILITY_COLOR_DICT[StabilityLabel.STABLE],
+                    FIXED_POINT_PLOT_STYLE[StabilityLabel.STABLE]["marker"],
+                    color=FIXED_POINT_PLOT_STYLE[StabilityLabel.STABLE]["color"],
                     markeredgecolor="k",
                     markeredgewidth=0.5,
                     markersize=5,
