14d0d52dc14e35b744ee3db55815ab6734e16a6f
Erin Angelini <erin.angelini@alleninstitute.org>
2026-04-28 17:34:48 +0000

    figure 3 bio examples

--- src/endo_pipeline/library/visualize/data_example_figures.py
+++ src/endo_pipeline/library/visualize/data_example_figures.py
@@ -371,7 +371,7 @@ def create_panel_intermediate_examples(
         fig_kwargs={"figsize": figure_size, "layout": "constrained"},
     )
 
-    for ax in fig.axes:
+    for i, ax in enumerate(fig.axes):
         ax.xaxis.labelpad = 3
         ax.yaxis.labelpad = 3
 
@@ -382,7 +382,7 @@ def create_panel_intermediate_examples(
             location="lower right",
             bar_thickness=25,
             padding=25,
-            include_label=True,
+            include_label=True if i == 0 else False,
             label_xy=(0.96, 0.08),
             label_fontsize=FONTSIZE_SMALL,
         )
