802f1df0666278b141e15d56d7a7c343499ef681
Erin Angelini <erin.angelini@alleninstitute.org>
2026-05-01 22:20:13 +0000

    kwargs settings type hints

--- src/endo_pipeline/settings/flow_field_figure.py
+++ src/endo_pipeline/settings/flow_field_figure.py
@@ -3,15 +3,17 @@ Global settings for flow field paper figure (results from dynamics analysis
 at low and high shear stress).
 """
 
+from typing import Any
+
 from endo_pipeline.settings.column_names import ColumnName as Column
 
-GRIDSPEC_KWARGS = {"wspace": 0.1, "hspace": 0.1}
+GRIDSPEC_KWARGS: dict[str, Any] = {"wspace": 0.1, "hspace": 0.1}
 """Gridspec kwargs used for figure panels."""
 
-XLABEL_KWARGS = {"labelpad": 2}
+XLABEL_KWARGS: dict[str, Any] = {"labelpad": 2}
 """X-axis label kwargs used for figure panels."""
 
-YLABEL_KWARGS = {"labelpad": -2}
+YLABEL_KWARGS: dict[str, Any] = {"labelpad": -2}
 """Y-axis label kwargs used for figure panels."""
 
 AXES_LIMITS_2D: dict[Column.DiffAEData, tuple[float, float]] = {
