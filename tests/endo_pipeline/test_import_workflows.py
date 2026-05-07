4120968cb77878759d51d9ed2fc381a5f12d6cc6
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-05-07 00:34:03 -0400

    Update workflow import test to work with new CLI

--- tests/endo_pipeline/test_import_workflows.py
+++ tests/endo_pipeline/test_import_workflows.py
@@ -1,8 +1,12 @@
-import endo_pipeline.__main__ as main
+import pytest
+
+from endo_pipeline.cli import apps
 
 
 def test_import_workflows():
-    # The following line is equivalent to `uv run endopipe`. This will import
+    # The following lines are equivalent to `uv run endopipe`. This will import
     # all the workflow modules registered to the CLI, which can error if some
     # are misconfigured: e.g., incorrect top-level imports.
-    main.build_pipeline_app()
+    with pytest.raises(SystemExit):
+        apps.build_command_groups()
+        apps.pipeline_app([])
