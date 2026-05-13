af5ac0f9db34f5f609c6e65d71cb466b89a04ef5
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-05-13 23:06:08 +0000

    Update and rename label free nuclei seg workflow (docstrings, demo mode, library method)

---
+++ src/endo_pipeline/workflows/production/run_label_free_nuclei_prediction.py
@@ -0,0 +1,97 @@
+from endo_pipeline.cli import Datasets
+
+
+def main(
+    datasets: Datasets | None = None,
+    num_processes: int = 1,
+    overwrite: bool = True,
+) -> None:
+    """
+    Run label-free nuclei prediction.
+
+    #nuclei-prediction #test-ready #gpu
+
+    Label-free nuclei predictions are generated using a CellPose 3 model
+    retrained from the default "nuclei" model. Validation images are saved every
+    48 timepoints (i.e. 4 hrs).
+
+    ## Example usage
+
+    To run the workflow in demo mode:
+
+    ```bash
+    uv run endopipe run-label-free-nuclei-prediction -vd
+    ```
+
+    To run the workflow for a single dataset:
+
+    ```bash
+    uv run endopipe run-label-free-nuclei-prediction --datasets DATASET_NAME
+    ```
+
+    ## Dataset collection
+
+    If datasets are not provided, the workflow will use datasets in the
+    `live_cdh5_seg_based_feat_datasets` dataset collection.
+
+    ## Workflow demo
+
+    Running the workflow in demo mode (`-d` or `--demo-mode`) will segment the
+    first timepoint of the first two positions for the first dataset.
+
+    Parameters
+    ----------
+    datasets
+        List of datasets or dataset collections to segment.
+    num_processes
+        Number of processes to use.
+    overwrite
+        True to overwrite existing images at output path, False otherwise.
+    """
+
+    import logging
+
+    from endo_pipeline.cli import DEMO_MODE
+    from endo_pipeline.cli.demo_mode_defaults import use_default_collection
+    from endo_pipeline.io import get_output_path
+    from endo_pipeline.library.process.general_image_preprocessing import (
+        build_analysis_queue,
+        process_task_queue,
+    )
+    from endo_pipeline.library.process.lib_generate_label_free_nuc_pred import (
+        generate_labelfree_nuclei_predictions,
+    )
+
+    logger = logging.getLogger(__name__)
+
+    out_dir = get_output_path("label_free_nuclei_pred")
+
+    datasets = use_default_collection(datasets, "live_cdh5_seg_based_feat_datasets")
+
+    analysis_queue = build_analysis_queue(
+        datasets,
+        out_dir=out_dir,
+        image_validation_frequency=48,
+        t_start=0,
+        t_final=1 if DEMO_MODE else None,
+        max_positions=2 if DEMO_MODE else None,
+        overwrite=overwrite,
+    )
+
+    logger.debug("Starting nuclei prediction...")
+
+    process_task_queue(
+        generate_labelfree_nuclei_predictions,
+        analysis_queue,
+        num_processes=num_processes,
+        description="Predicting nuclei",
+        chunksize=5,
+    )
+
+    logger.debug("Finished predicting nuclei!")
+
+
+if __name__ == "__main__":
+    from endo_pipeline.cli import workflow_cli
+
+    workflow_cli(main)
