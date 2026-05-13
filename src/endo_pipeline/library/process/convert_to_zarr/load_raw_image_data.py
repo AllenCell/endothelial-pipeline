a4d7fe92962d3627e46db7900bc8a56a227df7b8
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-05-13 12:00:02 -0400

    Update get delayed array for position method docstring and logging

--- src/endo_pipeline/library/process/convert_to_zarr/load_raw_image_data.py
+++ src/endo_pipeline/library/process/convert_to_zarr/load_raw_image_data.py
@@ -1,9 +1,11 @@
-from typing import Any
+import logging
 
 import dask.array as da
 from bioio import BioImage
 
 from endo_pipeline.configs import DatasetConfig
+
+logger = logging.getLogger(__name__)
 
 
 def get_included_scenes(dataset_config: DatasetConfig) -> list | range:
@@ -34,51 +36,56 @@ def get_included_scenes(dataset_config: DatasetConfig) -> list | range:
 
 
 def get_delayed_array_for_position(
-    pos: int,
+    position_index: int,
     dataset_config: DatasetConfig,
     channel_names: list,
-    number_positions: int = 6,
+    num_positions: int = 6,
     scene_index: int = 0,
-    img: Any | None = None,
+    img: BioImage | None = None,
 ) -> da.Array:
     """
-    Load all timepoints for a given position in the dataset as a Dask array.
+    Load selected timepoints for a given position as a Dask array.
+
+    Every nth position (based on total number of positions) in time to create
+    the Dask array.
 
     Parameters
     ----------
-    pos : int
+    position_index
         The position index to process.
-    dataset_name : str
-        The name of the dataset.
-    channel_names : list
-        A list of channel names.
-    number_positions : int, optional
-        The total number of positions in the dataset. Take every nth position in time to create
-        the Dask array.
-    scene_index : int, optional
-        The scene index. You can find the scene names (in their indexed order)
-        using `BioImage(dataset_config.original_path).scenes`. Default is 0.
-    img : BioImage, optional
-        The BioImage object for the dataset. If provided, it will reduce the
-        number of times the image is loaded. If None, it will be loaded for
-        the given dataset_name. Default is None.
+    dataset_config
+        Dataset config for the dataset to be loaded.
+    channel_names
+        List of channel names.
+    num_positions
+        Total number of positions in the dataset.
+    scene_index
+        Scene index.
+    img
+        Loaded image for the dataset. If provided, it will reduce the number of
+        times the image is loaded. If not provided, it will be loaded for the
+        given dataset config.
 
     Returns
     -------
-    dask.array.Array
-        A Dask array containing the processed images for all timepoints at
-        the given position.
+    :
+        A Dask array containing the processed images for all timepoints at the
+        given position.
     """
 
     # Load the dataset as a BioImage object
-    img = img if img else BioImage(dataset_config.original_path)
-    # Set the scene of the image
-    img.set_scene(int(scene_index))
-    # Get the timepoints for the specified position
-    t_final = img.dims.T  #  the total number of timepoints in "img"; if testing set low t_final
-    timepoints = range(pos, t_final, number_positions)
-    # Get the indices of the GFP and brightfield channels
+    img = img if img is not None else BioImage(dataset_config.original_path)
 
+    # Set the scene of the image
+    img.set_scene(scene_index)
+    logger.info("Using scene '%s'", img.scenes[scene_index])
+
+    # Get the timepoints for the specified position using the position index and
+    # the total number of timepoints in the image.
+    t_final = img.dims.T
+    timepoints = range(position_index, t_final, num_positions)
+
+    # Get the indices of the GFP and brightfield channels
     indices = dataset_config.original_channel_indices
     channels = [indices.channel_488, indices.brightfield]
 
@@ -89,16 +96,18 @@ def get_delayed_array_for_position(
     if indices.channel_640 is not None:
         channels.append(indices.channel_640)
 
-    assert len(channels) == len(
-        channel_names
-    ), f"Number of channels ({len(channels)}) does not match number of \
-        channel names ({len(channel_names)})"
+    if len(channels) != len(channel_names):
+        raise ValueError(
+            f"Number of channels '{len(channels)}' does not match "
+            f"the number of channel names '{len(channel_names)}'"
+        )
 
     # Get the delayed arrays for each timepoint at the specified position
-    # with the channels in the following order: (GFP, brightfield)
+    # with the channels in the specified order
     results = [img.get_image_dask_data("CZYX", T=tp, C=channels) for tp in timepoints]
-    # Concatenate the delayed arrays into a single large delayed array
-    # along the time axis
-    scene = da.stack(results, axis=0)  # TCZYX
-    print(f"finished processing {len(timepoints)} timepoints for position {pos}")
+
+    # Concatenate delayed arrays into a single TCZYX delayed array
+    scene = da.stack(results, axis=0)
+    logger.info("Loaded '%d' timepoints for position index '%d'", len(timepoints), position_index)
+
     return scene
