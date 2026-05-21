79d61488953cd872cbe54d930983308c3f84c60d
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-05-21 12:04:00 -0400

    Clean up flow field settings docstrings

--- src/endo_pipeline/settings/flow_field_dataframes.py
+++ src/endo_pipeline/settings/flow_field_dataframes.py
@@ -3,22 +3,20 @@ from enum import StrEnum
 """Global constants and default settings for dataframe creation and processing in 3D flow field analysis."""
 
 DATAFRAME_MANIFEST_PREFIX_VECTOR_FIELD: str = "drift_vector_field"
-"""Prefix for setting and getting dataframe manifest name for drift dataframes
-in 3D flow field analysis."""
+"""Prefix for vector field dataframe manifest name."""
 
 FMS_ANNOTATION_NOTES_VECTOR_FIELD: str = (
     "Drift vectors and corresponding grid points for 3D flow field estimation."
 )
-"""Annotation notes for drift coefficient dataframes uploaded to FMS in 3D flow field analysis."""
+"""Annotation notes for vector field dataframes uploaded to FMS."""
 
 DATAFRAME_MANIFEST_PREFIX_FIXED_POINTS: str = "drift_fixed_points"
-"""Prefix for setting and getting dataframe manifest name for fixed point dataframes
-in 3D flow field analysis."""
+"""Prefix for fixed points dataframe manifest name."""
 
 FMS_ANNOTATION_NOTES_FIXED_POINTS: str = (
     "High-confidence fixed points identified from %dD flow field analysis."
 )
-"""Annotation notes for fixed point dataframes uploaded to FMS in 3D flow field analysis."""
+"""Annotation notes for fixed point dataframes uploaded to FMS."""
 
 DATAFRAME_MANIFEST_PREFIX_BOOTSTRAPPING: str = "bootstrapped_fixed_points"
 """Prefix for setting and getting dataframe manifest name for bootstrapped fixed point dataframes."""
