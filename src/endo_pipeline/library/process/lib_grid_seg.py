e6355c3b7c38744a403d96a194bb8b380141cd04
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2026-05-21 11:00:11 -0400

    Split grid segmentation into production and testing workflows (#1633)
    * Add separate workflow that only creates the segmentations
    
    * Update workflow name in manifest
    
    * Add grid segmentation validation workflow
    
    * Remove original grid segmentation workflow
    
    * Remove variable overload to fix typing
    
    * Add usage and demo notes to docstring

--- src/endo_pipeline/library/process/lib_grid_seg.py
+++ src/endo_pipeline/library/process/lib_grid_seg.py
@@ -3,22 +3,15 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 from bioio_base.types import PhysicalPixelSizes
-from skimage.measure import regionprops
 from tqdm import tqdm
 
-from endo_pipeline.io import load_image
 from endo_pipeline.library.process.general_image_preprocessing import save_image_output
-from endo_pipeline.manifests import ImageLocation
 from endo_pipeline.settings.column_names import ColumnName as Column
 from endo_pipeline.settings.image_data import (
     IMG_SHAPE_RESOLUTION_1_3i_X,
     IMG_SHAPE_RESOLUTION_1_3i_Y,
     PIXEL_SIZE_3i_20x,
 )
-
-
-def make_grid_seg_filename(position: int, timepoint: int) -> str:
-    return f"{position}_T{timepoint}_grid_segmentation.ome.tiff"
 
 
 def make_crop_index_to_slice_mapping(grid_df: pd.DataFrame) -> dict[int, tuple[slice, slice]]:
@@ -57,40 +50,36 @@ def create_grid_segmentation_images(
     img_shape_x: int = IMG_SHAPE_RESOLUTION_1_3i_X,
     pixel_size: float = PIXEL_SIZE_3i_20x,
 ) -> None:
-    """Creates and saves grid segmentation images to the specified output directory
+    """
+    Create and save grid segmentation images to the specified output directory
     for each position and timepoint in the grid-based DiffAE dataframe.
 
-    Note:
-    The segmentation labels in the image will be equal to 1 + the crop_index from the grid_df.
-    This is because 0 must be reserved for the background.
+    When creating the segmentation image, assign the crop_index from grid_df to
+    be the "segmentation" label. We will use the crop index as the segmentation
+    ID. The segmentation labels in the image will be equal to 1 + the crop_index
+    from the grid_df. This is because 0 must be reserved for the background.
     """
-    # when creating the segmentation image assign the crop_index from grid_df
-    # to be the "segmentation" label. We will use the crop index as the
-    # segmentation ID.
-    dataset_name = np.unique(grid_df[Column.DATASET]).item()
 
     crop_index_slices = make_crop_index_to_slice_mapping(grid_df)
 
     # check that the crops will fit in an initialized image
-    grid_seg = np.zeros((img_shape_y, img_shape_x), dtype=np.uint16)
+    img_shape = (img_shape_y, img_shape_x)
+    grid_seg = np.zeros(img_shape, dtype=np.uint16)
 
     if (
         grid_df[Column.DiffAEData.END_X].max() > img_shape_x
         or grid_df[Column.DiffAEData.END_Y].max() > img_shape_y
     ):
-        raise ValueError(
-            f"Grid crop locations exceed expected image shape of\
-            {(img_shape_y, img_shape_x)}"
-        )
+        raise ValueError(f"Grid crop locations exceed expected image shape of '{img_shape}'")
 
     # we can probably do the multiprocessing at the position level
-    for pos, df in grid_df.groupby(Column.POSITION):
-        out_subdir = out_dir / str(pos)
+    for position, df in grid_df.groupby(Column.POSITION):
+        out_subdir = out_dir / f"P{position}"
         out_subdir.mkdir(exist_ok=True)
 
         # each position has a unique set of crop index labels
         # intialize an empty image to hold the segmentation labels
-        grid_seg = np.zeros((img_shape_y, img_shape_x), dtype=np.uint16)
+        grid_seg = np.zeros(img_shape, dtype=np.uint16)
 
         for crop_i in df[Column.CROP_INDEX].unique():
             grid_seg[crop_index_slices[crop_i]] = (
@@ -98,18 +87,18 @@ def create_grid_segmentation_images(
             )  # add 1 so that background is 0 and first crop is label 1
 
         # save the grid segmentation image for this position and timepoint
-        for tp in tqdm(
+        for timepoint in tqdm(
             df[Column.TIMEPOINT].unique(),
-            desc=f"Saving grid segmentation for {dataset_name} {pos}",
+            desc=f"Saving grid segmentation for position {position}",
         ):
-            fname = make_grid_seg_filename(pos, tp)
+            fname = f"P{position}_T{timepoint}_grid_segmentation.ome.tiff"
 
             resolution_level = np.unique(df[Column.DiffAEData.RESOLUTION]).item()
             px_res_xy = pixel_size * 2**resolution_level
             px_res = PhysicalPixelSizes(Z=None, Y=px_res_xy, X=px_res_xy)
 
             metadata = {
-                "image_name": f"{dataset_name}_{tp}",
+                "image_name": f"grid_segmentation_{timepoint}",
                 "channel_colors": [(255, 255, 255)],
                 "channel_names": ["grid_segmentation"],
                 "physical_pixel_sizes": px_res,
@@ -120,30 +109,4 @@ def create_grid_segmentation_images(
                 images=[grid_seg],
                 images_metadata=metadata,
                 dtype=np.uint16,
-            )
-
-
-def check_crop_indices_against_existing_segmentations(df: pd.DataFrame, out_dir: Path) -> None:
-    pos = np.unique(df[Column.POSITION]).item()
-    tp = np.unique(df[Column.TIMEPOINT]).item()
-    fp = out_dir / str(pos) / make_grid_seg_filename(pos, tp)
-
-    location = ImageLocation(path=fp)
-    segmentation = load_image(location)
-
-    segprops = regionprops(label_image=segmentation.squeeze())
-    for prop in segprops:
-        crop_index_from_seg = prop.label - 1
-        bbox_cols = [
-            Column.DiffAEData.START_Y,
-            Column.DiffAEData.START_X,
-            Column.DiffAEData.END_Y,
-            Column.DiffAEData.END_X,
-        ]
-
-        crop_loc_matched = df[df[Column.CROP_INDEX] == crop_index_from_seg][bbox_cols] == prop.bbox
-        if crop_loc_matched is False:
-            raise ValueError(
-                f"Crop index {crop_index_from_seg} in segmentation does not\
-                    match bbox in grid_df for position {pos} and timepoint {tp}"
             )
---
+++ src/endo_pipeline/workflows/production/create_grid_segmentation.py
@@ -0,0 +1,105 @@
+def main():
+    """
+    Create grid-based segmentations based on dataset with longest duration.
+
+    #grid-based
+
+    This workflow creates a "segmentation" image for the grid-based crops, using
+    the unfiltered feature dataframe produced by the `calculate_pca_features`
+    workflow, which assigns a unique "crop index" to each unique crop location.
+    This "crop index" is assigned as the "segmentation label" in the image,
+    where "segmentation label" = 1 + "crop index" (becuase 0 is reserved for
+    background).
+
+    ## Example usage
+
+    To run the workflow in demo mode:
+
+    ```bash
+    uv run endopipe create-grid-segmentation -vd
+    ```
+
+    To run the full workflow:
+
+    ```bash
+    uv run endopipe create-grid-segmentation
+    ```
+
+    ## Workflow demo
+
+    Running the workflow in demo mode (`-d` or `--demo-mode`) will only create
+    segmentations for one position and the first 10 timepoints.
+    """
+
+    import logging
+
+    from endo_pipeline.cli import DEMO_MODE
+    from endo_pipeline.configs import load_all_dataset_configs
+    from endo_pipeline.io import get_output_path, load_dataframe
+    from endo_pipeline.library.process.lib_grid_seg import create_grid_segmentation_images
+    from endo_pipeline.manifests import get_dataframe_location_for_dataset, load_dataframe_manifest
+    from endo_pipeline.settings.column_names import ColumnName as Column
+    from endo_pipeline.settings.workflow_defaults import (
+        GRID_BASED_FEATURES_UNFILTERED_MANIFEST_NAME,
+    )
+
+    logger = logging.getLogger(__name__)
+
+    # Get maximum duration across all available dataset configs. The grid-based
+    # segmentations are reused for multiple datasets since the crop indices are
+    # in the same positions for each movie, therefore we must create the
+    # segmentations based on the longest timelapse duration so segmentations are
+    # present for all timepoints for all other datasets.
+    all_dataset_configs = load_all_dataset_configs()
+    max_timelapse_duration = max(d.duration for d in all_dataset_configs)
+    dataset_config = next(d for d in all_dataset_configs if d.duration == max_timelapse_duration)
+    logger.info(
+        "Dataset '%s' is the first dataset with the longest timelapse duration of '%d' minutes. "
+        "Using this dataset to create the grid segmentations.",
+        dataset_config.name,
+        max_timelapse_duration,
+    )
+
+    max_num_positions: int | None = None
+    max_num_timepoints: int | None = None
+
+    if DEMO_MODE:
+        logger.warning("DEMO_MODE - Limiting to one position and 10 timepoints")
+        max_num_positions = 1
+        max_num_timepoints = 10
+
+    # Load grid-based feature dataframe for selected dataset
+    dataframe_manifest = load_dataframe_manifest(GRID_BASED_FEATURES_UNFILTERED_MANIFEST_NAME)
+    dataframe_location = get_dataframe_location_for_dataset(dataframe_manifest, dataset_config.name)
+    grid_df_ = load_dataframe(dataframe_location, delay=True)
+
+    # Grab only the crop locations and crop labels from the dataframe
+    columns_to_compute = [
+        Column.DATASET,
+        Column.POSITION,
+        Column.TIMEPOINT,
+        Column.CROP_INDEX,
+        Column.DiffAEData.START_Y,
+        Column.DiffAEData.END_Y,
+        Column.DiffAEData.START_X,
+        Column.DiffAEData.END_X,
+        Column.DiffAEData.RESOLUTION,
+    ]
+    grid_df = grid_df_[columns_to_compute].compute()
+
+    if max_num_positions is not None:
+        first_position = sorted(grid_df[Column.POSITION].unique())[0]
+        grid_df = grid_df[grid_df[Column.POSITION] == first_position]
+
+    if max_num_timepoints is not None:
+        timepoints = sorted(grid_df[Column.TIMEPOINT].unique())[:max_num_timepoints]
+        grid_df = grid_df[grid_df[Column.TIMEPOINT].isin(timepoints)]
+
+    out_dir = get_output_path("grid_seg")
+    create_grid_segmentation_images(grid_df, out_dir)
+
+
+if __name__ == "__main__":
+    from endo_pipeline.cli import workflow_cli
+
+    workflow_cli(main)
