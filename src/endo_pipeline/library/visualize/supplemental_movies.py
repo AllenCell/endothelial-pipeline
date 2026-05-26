16ed80b5d7601e9f805487517e46440bb4213673
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-05-26 22:09:46 +0000

    Remove contrast stretching from supp movies

--- src/endo_pipeline/library/visualize/supplemental_movies.py
+++ src/endo_pipeline/library/visualize/supplemental_movies.py
@@ -11,17 +11,15 @@ import numpy as np
 from tqdm import tqdm
 
 from endo_pipeline.configs import DatasetConfig, get_flow_at_frame, load_dataset_config
-from endo_pipeline.io import load_image
-from endo_pipeline.library.process.image_processing import contrast_stretching, stitch_with_overlap
-from endo_pipeline.manifests import get_zarr_location_for_position
-from endo_pipeline.settings import LOG_EPSILON
-from endo_pipeline.settings.figures import MAX_SUPP_MOVIE_HEIGHT, MAX_SUPP_MOVIE_WIDTH
-from endo_pipeline.settings.image_data import PIXEL_SIZE_3i_20x
 from endo_pipeline.library.process.image_processing import (
+    convert_to_uint8,
     load_bf_image,
     load_bf_std_dev_image,
-    load_egfp_image
+    load_egfp_image,
+    stitch_with_overlap,
 )
+from endo_pipeline.settings.figures import MAX_SUPP_MOVIE_HEIGHT, MAX_SUPP_MOVIE_WIDTH
+from endo_pipeline.settings.image_data import PIXEL_SIZE_3i_20x
 
 logger = logging.getLogger(__name__)
 
@@ -209,24 +207,8 @@ def create_timelapse_mp4(
         load_stitched_image, loader=image_loader, config=dataset_config, positions=positions
     )
 
-    # Estimate percentiles using first 10 timepoints if not using the standard
-    # deviation projection. If using the standard deviation projection, which
-    # already include percentile-based clipping, only load the first stitched
-    # image (for calculating sizes) and use min / max stretching instead.
-    method: Literal["min-max", "percentile"]
-    if "std_dev" not in channel_type:
-        stitched_images = load_stitched_image_at_timepoint(timepoints=list(range(10)))
-        percentile_image = da.concatenate(stitched_images).rechunk().ravel()
-        custom_range = da.percentile(percentile_image, [1, 99]).compute()
-        method = "percentile"
-        logger.info("Calculated custom range as [ %.1f - %.1f ] ", custom_range[0], custom_range[1])
-    else:
-        stitched_images = load_stitched_image_at_timepoint(timepoints=0)
-        custom_range = None
-        method = "min-max"
-        logger.info("Skipping custom range for standard deviation projection. Using min-max.")
-
     # Use first timepoint image for frame size calculations
+    stitched_images = load_stitched_image_at_timepoint(timepoints=0)
     first_image = stitched_images[0].squeeze()
     scale_factor, frame_padding = calculate_frame_sizing(first_image)
     pixel_size = PIXEL_SIZE_3i_20x / scale_factor
@@ -250,8 +232,7 @@ def create_timelapse_mp4(
         for tp in tqdm(timepoints, desc=f"{dataset_name}: Creating movie"):
             # Load image and apply contrast stretching and size adjustments
             image = load_stitched_image_at_timepoint(timepoints=tp).squeeze().compute()
-            image = contrast_stretching(image, custom_range=custom_range, method=method)
-            image = resize_and_pad_image(image, scale_factor, frame_padding)
+            image = convert_to_uint8(resize_and_pad_image(image, scale_factor, frame_padding))
 
             # Add scalebar and timestamp directly to image array
             add_scalebar_to_frame(image, scale_bar_um, pixel_size)
