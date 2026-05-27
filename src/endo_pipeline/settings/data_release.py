2a511dde6e5239642461e48faab0c18b46a08e01
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-05-27 21:35:58 +0000

    Remove add and remove s3 dataset workflows

--- src/endo_pipeline/settings/data_release.py
+++ src/endo_pipeline/settings/data_release.py
@@ -1,12 +1,6 @@
 S3_INTERNAL_DIRECTORY: str = "s3://allencell-internal-quilt/endo_stg/"
 """Path prefix for internal S3 storage of release datasets."""
 
-SOURCE_COL: str = "local_zarr_path"
-"""Name of the column for local zarr paths in the upload CSV."""
-
-DEST_COL: str = "s3_zarr_path"
-"""Name of the column for S3 zarr paths in the upload / remove CSV."""
-
 BFF_FILE_PATH_COL: str = "File Path"
 """Name of the required column for file paths in the BFF CSV."""
 
