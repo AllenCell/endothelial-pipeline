844bd1c6217708462dcc45ed8b9722e204044b92
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-06-09 18:22:45 -0400

    Drop class filter and add some custom descriptions

--- src/endo_pipeline/workflows/internal/sync_column_descriptions.py
+++ src/endo_pipeline/workflows/internal/sync_column_descriptions.py
@@ -17,7 +17,7 @@ def main() -> None:
     from endo_pipeline.settings.column_names import ColumnName as Column
     from endo_pipeline.settings.diffae_feature_dataframes import NUM_LATENT_FEATURES
 
-    COLUMN_DESCRIPTIONS = {}
+    column_descriptions = {}
 
     # Method for finding attribute assignment followed by docstring
     def _extract_docstrings_from_body(body) -> None:
@@ -32,7 +32,7 @@ def main() -> None:
             if not a_is_assign or not b_is_string:
                 continue
 
-            COLUMN_DESCRIPTIONS[a.value.value] = b.value.value.strip(".")
+            column_descriptions[a.value.value] = b.value.value.strip(".")
 
     # Parse source into abstract syntax tree
     source = ast.parse(inspect.getsource(Column)).body[0]
@@ -43,19 +43,46 @@ def main() -> None:
     # Extract docstrings from each nested enum class
     for node in source.body:
         if isinstance(node, ast.ClassDef):
-            if node.name != "SegData" and node.name != "DiffAEData":
-                continue
             _extract_docstrings_from_body(node.body)
 
     # Expand prefix features
     for prefix in [Column.DiffAEData.LATENT_FEATURE_PREFIX, Column.DiffAEData.PCA_FEATURE_PREFIX]:
         for i in range(NUM_LATENT_FEATURES + 1):
-            COLUMN_DESCRIPTIONS[f"{prefix}{i}"] = COLUMN_DESCRIPTIONS[prefix].replace("[i]", str(i))
+            column_descriptions[f"{prefix}{i}"] = column_descriptions[prefix].replace("[i]", str(i))
 
     # Add custom column descriptions
-    COLUMN_DESCRIPTIONS["dataset_name"] = COLUMN_DESCRIPTIONS[Column.DATASET]
-    COLUMN_DESCRIPTIONS["area"] = COLUMN_DESCRIPTIONS[Column.SegData.AREA_PX_SQ]
-    COLUMN_DESCRIPTIONS["perimeter"] = COLUMN_DESCRIPTIONS[Column.SegData.PERIMETER_PX]
+    column_descriptions["dataset_name"] = column_descriptions[Column.DATASET]
+    column_descriptions["area"] = column_descriptions[Column.SegData.AREA_PX_SQ]
+    column_descriptions["perimeter"] = column_descriptions[Column.SegData.PERIMETER_PX]
+    column_descriptions["edge_fluorescences (a.u.)"] = column_descriptions[
+        Column.SegData.EDGE_FLUOR
+    ]
+    column_descriptions["node_fluorescences (a.u.)"] = column_descriptions[
+        Column.SegData.NODE_FLUOR
+    ]
+    column_descriptions["cell_fluorescence_max (a.u.)"] = column_descriptions[
+        Column.SegData.CELL_FLUOR_MAX
+    ]
+    column_descriptions["cell_fluorescence_mean (a.u.)"] = column_descriptions[
+        Column.SegData.CELL_FLUOR_MEAN
+    ]
+    column_descriptions["cell_fluorescence_median (a.u.)"] = column_descriptions[
+        Column.SegData.CELL_FLUOR_MEDIAN
+    ]
+    column_descriptions["cell_fluorescence_min (a.u.)"] = column_descriptions[
+        Column.SegData.CELL_FLUOR_MIN
+    ]
+    column_descriptions["cell_fluorescence_pct25 (a.u.)"] = column_descriptions[
+        Column.SegData.CELL_FLUOR_PCT25
+    ]
+    column_descriptions["cell_fluorescence_pct75 (a.u.)"] = column_descriptions[
+        Column.SegData.CELL_FLUOR_PCT75
+    ]
+    column_descriptions["cell_fluorescence_std (a.u.)"] = column_descriptions[
+        Column.SegData.CELL_FLUOR_STD
+    ]
+    column_descriptions["centroid_X"] = column_descriptions[Column.SegData.CENTROID_X]
+    column_descriptions["centroid_Y"] = column_descriptions[Column.SegData.CENTROID_Y]
 
     for manifest_name in get_available_dataframe_manifests():
         manifest = load_dataframe_manifest(manifest_name)
@@ -76,7 +103,7 @@ def main() -> None:
             ):
                 continue
 
-            if column_name in COLUMN_DESCRIPTIONS:
-                manifest.columns[column_name] = COLUMN_DESCRIPTIONS[column_name]
+            if column_name in column_descriptions:
+                manifest.columns[column_name] = column_descriptions[column_name]
 
         save_dataframe_manifest(manifest)
