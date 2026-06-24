a83849d05cda1d4f6a2d034171a8251911a353de
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-06-24 20:45:44 +0000

    Stage diffae_baseline_latent_512_grid_based

--- src/endo_pipeline/settings/manifest_staging.py
+++ src/endo_pipeline/settings/manifest_staging.py
@@ -26,6 +26,7 @@ STAGING_DATAFRAME_MANIFEST_NAMES = [
     "merged_segmentation_features",
     "diffae_training_dataframe",
     "diffae_evaluation_dataframe_cell_centered",
+    "diffae_baseline_latent_512_grid_based",
 ]
 """List of names of dataframe manifests to stage."""
 
