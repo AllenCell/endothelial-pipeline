bcc0af48849fc9724ef0896b8832d62aec56268d
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-06-29 11:18:27 -0400

    Update docstring for run all testable workflows

--- src/endo_pipeline/workflows/testing/run_all_testable_workflows.py
+++ src/endo_pipeline/workflows/testing/run_all_testable_workflows.py
@@ -1,22 +1,34 @@
-"""
-How to register your workflow for testing:
-
-1. Make sure your workflow runs in under 5 minutes in `DEMO_MODE`
-2. Add #test-ready (along with #gpu or #cpu-only) to your docstring
-3. Run all registered workflows with:
-
-.. code-block:: bash
-
-    # runs all testable workflow
-    uv run endopipe -g 1 run-all-testable-workflows
-
-    # run only the CPU workflows
-    uv run endopipe run-all-testable-workflows
-"""
-
-
 def main():
-    """Run all workflows marked as test ready."""
+    """
+    Run all workflows tagged as test ready in demo mode.
+
+    To register a workflow for testing:
+
+    1. Make sure your workflow runs in under 5 minutes in `DEMO_MODE`
+    2. Add #test-ready tag to the docstring
+    3. For workflows that use GPU, also add a #gpu tag to the docstring
+
+    ## Example usage
+
+    To test all testable workflows:
+
+    ```bash
+    uv run endopipe run-all-testable-workflows -g 1
+    ```
+
+    To test only workflows that do not require a GPU:
+
+    ```bash
+    uv run endopipe run-all-testable-workflows
+    ```
+
+    ## CPU vs. GPU
+
+    When this workflow is run without the GPU flag (`-g` or `--num-gpus`), it will
+    not test any workflows that have been tagged with the `#gpu` tag. While these
+    workflows are able to be run without a GPU, it is generally too slow to be
+    practical for testing workflow functionality.
+    """
 
     import asyncio
     import logging
