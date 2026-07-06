c18ce3f0f5b832ff9c78729e8e3d6e017daa2823
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2026-07-06 17:28:03 +0000

    Add run all figure workflows

--- src/endo_pipeline/settings/timeouts.py
+++ src/endo_pipeline/settings/timeouts.py
@@ -13,3 +13,11 @@ Timeout in minutes for when running all validation workflows.
 Workflows that go beyond this will be flagged. Workflows that take twice as long
 will be killed.
 """
+
+WORKFLOW_FIGURE_TIMEOUT = 60
+"""
+Timeout in minutes for when running all figure workflows.
+
+Workflows that go beyond this will be flagged. Workflows that take twice as long
+will be killed.
+"""
