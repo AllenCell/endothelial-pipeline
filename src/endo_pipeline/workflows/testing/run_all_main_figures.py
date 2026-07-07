ec8640a9fc65d80084ee3a325278a0f26c77a752
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2026-07-06 22:11:40 -0400

    Promote all S3 URIs to public bucket (#1800)
    * Update S3 URIs for image manifests
    
    * Update S3 URIs for dataframe manifests
    
    * Update S3 URIs for model manifests
    
    * Add run all figure workflows

---
+++ src/endo_pipeline/workflows/testing/run_all_main_figures.py
@@ -0,0 +1,42 @@
+def main():
+    """
+    Run all main figure workflows.
+
+    To register a main figure workflow, add a #main-figure tag to the docstring.
+
+    ## Example usage
+
+    To run all main figures:
+
+    ```bash
+    uv run endopipe run-all-main-figures
+    ```
+
+    ## CPU vs. GPU
+
+    Some figure panels should be run with an NVIDIA GPU. Run this workflow with
+    the GPU flag (`-g` or `--num-gpus`) to make sure GPUs are visible to the
+    workflows. While all figures are able to be run without a GPU, they will be
+    noticeably slower.
+    """
+
+    import asyncio
+
+    from endo_pipeline.cli.runner import run_all_workflows_with_tag, summarize_workflow_run_results
+    from endo_pipeline.settings.timeouts import WORKFLOW_FIGURE_TIMEOUT
+
+    results = asyncio.run(
+        run_all_workflows_with_tag(
+            workflow_name="run-all-main-figures",
+            select_tag="main-figure",
+            runner_timeout=WORKFLOW_FIGURE_TIMEOUT,
+            force_demo_mode=False,
+        )
+    )
+    summarize_workflow_run_results(results)
+
+
+if __name__ == "__main__":
+    from endo_pipeline.cli import workflow_cli
+
+    workflow_cli(main)
